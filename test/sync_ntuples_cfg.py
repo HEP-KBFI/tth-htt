c0f54c94980c116e3712097cf52bc8ffca014425
Karl Ehatäht <karlehataht@gmail.com>
2016-04-28 19:54:22 +0300

    [sync ntuple]: changed input Ntuple to the latest

--- test/sync_ntuples_cfg.py
+++ test/sync_ntuples_cfg.py
@@ -5,7 +5,7 @@ import os
 process = cms.PSet()
   
 process.fwliteInput = cms.PSet(
-    fileNames = cms.vstring('/home/karl/VHbbNtuples_7_6_x2/CMSSW_7_6_3/src/VHbbAnalysis/Heppy/test/Loop_9/tree.root'),
+    fileNames = cms.vstring('/home/karl/VHbbNtuples_7_6_x2/CMSSW_7_6_3/src/VHbbAnalysis/Heppy/test/Loop_12/tree.root'),
     maxEvents = cms.int32(-1),
     outputEvery = cms.uint32(100000)
 )
