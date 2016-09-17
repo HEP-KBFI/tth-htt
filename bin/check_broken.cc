75aaf539f3e6264dc47ffd78c8cc4db9775ed4af
Karl Ehatäht <karlehataht@gmail.com>
2016-09-18 00:29:20 +0300

    Add a small program that checks for broken files
    Also fix some issues regarding the Makefile of the copy jobs

---
+++ bin/check_broken.cc
@@ -0,0 +1,336 @@
+#include <iostream> // std::cerr, std::cout
+#include <string> // std::string
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE, setenv()
+#include <vector> // std::vector<>
+#include <map> // std::map<,>
+#include <exception> // std::exception
+#include <chrono> // std::chrono::
+#include <iomanip> // std::setprecision()
+#include <fstream> // std::ofstream
+
+#include <boost/filesystem.hpp> // boost::filesystem::
+#include <boost/program_options.hpp> // boost::program_options::
+
+#include <TFile.h> // TFile
+#include <TTree.h> // TTree
+#include <TROOT.h> // gROOT
+#include <TError.h> // kError
+#include <TString.h> // Form()
+
+#define IMPROPER_FILE_C -1
+#define ZOMBIE_FILE_C   -2
+
+#define IMPROPER_FILE_P boost::filesystem::path("improper_files.txt")
+#define ZOMBIE_FILE_P   boost::filesystem::path("broken_files.txt")
+#define ZEROFS_FILE_P   boost::filesystem::path("zerofs_files.txt")
+
+#define LINE std::string(80, '*') + '\n'
+
+/**
+ * @file
+ * Check broken root files in the file system
+ *
+ * You are encouraged to build it with scram, but if you are keen to not use it,
+ * then proceed with the following command in order to build the executable:
+ *
+   g++ -std=c++11 \
+     -I/cvmfs/cms.cern.ch/slc6_amd64_gcc493/external/boost/1.57.0-kpegke/include \
+     -I/cvmfs/cms.cern.ch/slc6_amd64_gcc493/lcg/root/6.02.12-kpegke4/include \
+     check_broken.cc -o check_broken \
+     -L/cvmfs/cms.cern.ch/slc6_amd64_gcc493/external/boost/1.57.0-kpegke/lib \
+     -L/cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_7_6_3/external/slc6_amd64_gcc493/lib \
+     -L/cvmfs/cms.cern.ch/slc6_amd64_gcc493/lcg/root/6.02.12-kpegke4/lib \
+     -lboost_system -lboost_filesystem -lboost_program_options \
+     -llzma -lCore -lRIO -lThread \
+     -Wl,-rpath=/cvmfs/cms.cern.ch/slc6_amd64_gcc493/external/boost/1.57.0-kpegke/lib \
+     -Wl,-rpath=/cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_7_6_3/external/slc6_amd64_gcc493/lib \
+     -Wl,-rpath=/cvmfs/cms.cern.ch/slc6_amd64_gcc493/lcg/root/6.02.12-kpegke4/lib
+
+ * Example output:
+
+  check_broken -h
+  Allowed options:
+    -h [ --help ]               produce help message
+    -p [ --path ] arg           full path to the directory to scan
+    -t [ --tree ] arg (=tree)   expected TTree name
+    -o [ --output ] [=arg(=./)] dump the results to given directory
+    -v [ --verbose ]            log every file and folder
+
+ * Example usage:
+
+   check_broken -p /hdfs/local/karl/FinalNtuples -t tree --output=/home/karl/sandbox -o -v
+ */
+
+namespace std
+{
+  using vstring = std::vector<std::string>;
+}
+
+/**
+ * @brief Helper struct needed to iterate over the directory tree recursively
+ */
+struct recursive_directory_range
+{
+  /**
+   * @brief Simple constructor
+   * @param p Path, the starting point of the file system scan
+   */
+  recursive_directory_range(boost::filesystem::path p)
+    : p_(p)
+  {}
+
+  /**
+   * @brief begin iterator (needed by for-range loops)
+   * @return iterator to the starting point
+   */
+  boost::filesystem::recursive_directory_iterator
+  begin()
+  {
+    return boost::filesystem::recursive_directory_iterator(p_);
+  }
+
+  /**
+   * @brief end iterator (needed by for-range loops)
+   * @return an empty iterator
+   */
+  boost::filesystem::recursive_directory_iterator
+  end()
+  {
+    return boost::filesystem::recursive_directory_iterator();
+  }
+
+  boost::filesystem::path p_;
+};
+
+/**
+ * @brief Checks whether a given root file is broken or not
+ * @param path Full path to an actual root file
+ * @param tree TTree name
+ * @return IMPROPER_FILE if the file doesn't have TTree called 'tree',
+ *         ZOMBIE_FILE   if the file is broken,
+ *         otherwise the number of events in that file
+ */
+long int
+check_broken(const std::string & path,
+             const std::string & tree)
+{
+  TFile f(path.c_str());
+  long int ret_val = ZOMBIE_FILE_C;
+  if(! f.IsZombie())
+  {
+    if(f.GetListOfKeys() -> Contains(tree.c_str()))
+    {
+      TTree * t = static_cast<TTree *>(f.Get("tree"));
+      ret_val = t -> GetEntries();
+    }
+    else
+      return IMPROPER_FILE_C;
+  }
+  f.Close();
+  return ret_val;
+}
+
+int
+main(int argc,
+     char * argv[])
+{
+  setenv("TZ", "/etc/localtime", 1); // needed by std::chrono::
+  gROOT -> ProcessLine(Form("gErrorIgnoreLevel = %d;", kBreak));
+
+//--- parse command line arguments
+  std::string target_str, tree_str, output_dir_str;
+  bool save_zerofs, save_improper, verbose;
+
+  try
+  {
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+      ("help,h",     "produce help message")
+      ("path,p",     boost::program_options::value<std::string>(&target_str) -> required(),
+                     "full path to the directory to scan")
+      ("tree,t",     boost::program_options::value<std::string>(&tree_str) -> default_value("tree"),
+                     "expected TTree name")
+      ("output,o",   boost::program_options::value<std::string>(&output_dir_str) -> default_value("")
+                                                                                -> implicit_value("./"),
+                     "dump the results to given directory")
+      ("zerofs,z",   boost::program_options::bool_switch(&save_zerofs) -> default_value(false),
+                     "save the list of files with zero file size to a file")
+      ("improper,i", boost::program_options::bool_switch(&save_improper) -> default_value(false),
+                     "save the list of files that don't contain correct TTree")
+      ("verbose,v",  boost::program_options::bool_switch(&verbose) -> default_value(false),
+                     "log every file and folder")
+    ;
+    boost::program_options::variables_map vm;
+    boost::program_options::store(
+          boost::program_options::parse_command_line(argc, argv, desc), vm
+    );
+    if(vm.count("help"))
+    {
+      std::cout << desc << '\n';
+      return EXIT_SUCCESS;
+    }
+    boost::program_options::notify(vm); // might throw
+  }
+  catch(boost::program_options::error & e)
+  {
+    std::cerr << "User error: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  catch(std::exception & e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  catch(...)
+  {
+    std::cerr << "Unknown error!\n";
+    return EXIT_FAILURE;
+  }
+  if(! boost::filesystem::is_directory(target_str))
+  {
+    std::cerr << "No such directory: '" << target_str << "'\n";
+    return EXIT_FAILURE;
+  }
+
+  const boost::filesystem::path output_dir_path =
+    boost::filesystem::canonical(output_dir_str);
+  if(! output_dir_path.empty() && ! boost::filesystem::is_directory(output_dir_path))
+  {
+    std::cerr << "No such directory: '" << output_dir_str << "'\n";
+    return EXIT_FAILURE;
+  }
+
+//--- quick printout of the chosen options
+  std::cout << "Directory to analyze: " << target_str << '\n'
+            << "TTree name: "           << tree_str << '\n'
+            << "Verbose printout: "     << std::boolalpha << verbose << '\n';
+  if(! output_dir_path.empty())
+    std::cout << "Output directory: " << output_dir_path.string() << '\n';
+  std::cout << LINE;
+
+//--- start the clock
+  const std::chrono::high_resolution_clock::time_point start_t =
+      std::chrono::high_resolution_clock::now();
+
+//--- create a list of immediate subdirectories of a given directory
+  const boost::filesystem::path target_path(target_str);
+  boost::filesystem::directory_iterator target_it(target_path);
+  std::vector<boost::filesystem::path> immediate_subdirs;
+  for(const boost::filesystem::path & p: target_it)
+    if(boost::filesystem::is_directory(p))
+    {
+      immediate_subdirs.push_back(p);
+      if(verbose) std::cout << "Found sample directory: " << p.string() << '\n';
+    }
+  if(verbose) std::cout << LINE;
+
+//--- loop over the list of immediate subdirectories recursively
+  std::vstring zombie_files, zerofs_files, improper_files;
+  std::map<std::string, unsigned long long> event_counter;
+  unsigned file_counter = 0;
+  for(const boost::filesystem::path & dir_path: immediate_subdirs)
+  {
+    const std::string dir = dir_path.string();
+    unsigned long long nof_total_events = 0;
+    if(verbose) std::cout << ">> Looping over: " << dir << "\n";
+    for(const boost::filesystem::directory_entry & it: recursive_directory_range(dir))
+    {
+      const boost::filesystem::path file = it.path();
+      const std::string file_str = file.string();
+//--- if we're dealing with a valid root file
+      if(boost::filesystem::is_regular_file(file) &&
+         boost::filesystem::extension(file) == ".root")
+      {
+        if(verbose) std::cout << "Checking: " << file_str << '\n';
+        ++file_counter;
+//--- read the file size first
+        const boost::uintmax_t file_size = boost::filesystem::file_size(file);
+        if(! file_size)
+        {
+          zerofs_files.push_back(file_str);
+//--- don't even bother checking whether the file is zombie or not
+          continue;
+        }
+        const long int nof_events = check_broken(file_str, tree_str);
+        if     (nof_events > 0)              nof_total_events += nof_events;
+        else if(nof_events == ZOMBIE_FILE_C)   zombie_files.push_back(file_str);
+        else if(nof_events == IMPROPER_FILE_C) improper_files.push_back(file_str);
+      } // is root file
+    } // recursive loop
+    const std::string sample_name = dir_path.filename().string();
+    event_counter[sample_name] = nof_total_events;
+  } // immediate subdirectory loop
+  if(verbose) std::cout << LINE;
+
+//--- print the results; save them to a file?
+  if(zombie_files.size())
+  {
+    std::cout << "List of broken files:\n";
+    for(const std::string & zombie: zombie_files)
+      std::cout << zombie << '\n';
+    std::cout << LINE;
+  }
+  if(zerofs_files.size())
+  {
+    std::cout << "List of files with zero size:\n";
+    for(const std::string & zerofs: zerofs_files)
+      std::cout << zerofs << '\n';
+    std::cout << LINE;
+  }
+  if(improper_files.size())
+  {
+    std::cout << "List of files which didn't have '" << tree_str << "' "
+              << "as immediate key in the root file:\n";
+    for(const std::string & improper: improper_files)
+      std::cout << improper << '\n';
+    std::cout << LINE;
+  }
+  std::cout << "Total event counts:\n";
+  for(const auto & kv: event_counter)
+    std::cout << kv.first << ": " << kv.second << '\n';
+  std::cout << "Final statistics:\n";
+  std::cout << "\tTotal number of files: " << file_counter << '\n';
+  std::cout << "\tNumber of broken files: " << zombie_files.size() << '\n';
+  std::cout << "\tNumber of files w/ 0 file size: " << zerofs_files.size() << '\n';
+  std::cout << "\tNumber of files that hadn't '" << tree_str << "' "
+            << "as their TTree: " << improper_files.size() << '\n';
+
+//--- optional: dump the results to files
+  if(! output_dir_str.empty())
+  {
+    if(zombie_files.size())
+    {
+      const std::string zombie_path = (output_dir_path / ZOMBIE_FILE_P).string();
+      std::ofstream zombie_outfile(zombie_path);
+      for(const std::string & zombie: zombie_files)
+        zombie_outfile << zombie << '\n';
+      std::cout << "Wrote file: '" << zombie_path << "'\n";
+    }
+    if(save_improper && improper_files.size())
+    {
+      const std::string improper_path = (output_dir_path / IMPROPER_FILE_P).string();
+      std::ofstream improper_outfile(improper_path);
+      for(const std::string & improper: improper_files)
+        improper_outfile << improper << '\n';
+      std::cout << "Wrote file: '" << improper_path << "'\n";
+    }
+    if(save_zerofs && zerofs_files.size())
+    {
+      const std::string zerofs_path = (output_dir_path / ZEROFS_FILE_P).string();
+      std::ofstream zerofs_outfile(zerofs_path);
+      for(const std::string & zerofs: zerofs_files)
+        zerofs_outfile << zerofs << '\n';
+      std::cout << "Wrote file: '" << zerofs_path << "'\n";
+    }
+  }
+
+//--- stop the clock
+  const std::chrono::high_resolution_clock::time_point end_t =
+      std::chrono::high_resolution_clock::now();
+  const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+    (end_t - start_t).count() / 1000.;
+  std::cout << "Total execution time: " << std::setprecision(3) << std::fixed
+            << duration << "s\n";
+
+  return EXIT_SUCCESS;
+}
