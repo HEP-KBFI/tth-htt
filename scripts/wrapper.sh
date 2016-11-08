e2dc1b8b799708aae0bb6e8b25de5888c92dcd83
Margus Pxrt <margus@wave.ee>
2016-11-08 02:22:39 +0200

    made wrapper clearer

--- scripts/wrapper.sh
+++ scripts/wrapper.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+# Resubmits sbatch job if /cvmfs/cms.cern.ch/cmsset_default.sh  missing (no access to network drive)
+
 #SBATCH -o /home/lauri/tmp/slurm-%j.out        # STDOUT
 #
 
 args=$1
-wrapper="/home/software/wrapper"
+wrapper="/opt/software/wrapper"
 basedir="/scratch"
 
 job_user=${SLURM_JOB_USER}
@@ -13,38 +16,38 @@ job_part=${SBATCH_PARTITION}
 
 job_dir=${basedir}/${job_user}/${job_id}
 
-mkenv(){
+prepare_env(){
   mkdir -p ${job_dir}
   cd ${job_dir}
   pwd
 }
 
-mkclean(){
+clean(){
   rm -rvf ${job_dir}
   echo $(date)
 }
 
-mklog(){
+add_log(){
   lstatus=$1
   echo "### ${lstatus}:$(date):${job_id}:${job_user}:${job_host}:${job_part} ###"
 }
 
 #scontrol update NodeName=$HOSTNAME State=drain Reason="Testing"
 if [[ -f /cvmfs/cms.cern.ch/cmsset_default.sh ]]; then
-  mklog START
-  mkenv
+  add_log START
+  prepare_env
   . $args
-  mklog STOP
+  add_log STOP
 else
   echo $LSUB
-  mklog DRAIN-NODE
+  add_log DRAIN-NODE
   sudo scontrol update nodename=$HOSTNAME state=drain reason=Testing
-  mklog TRY-REPOST
+  add_log TRY-REPOST
   if [[ ${LSUB} -lt 2 ]]; then
     LSUB=$((LSUB+1))
     LSUB=${LSUB} sbatch ${wrapper} ${args}
-    mklog START-REPOST
+    add_log START-REPOST
     exit
   fi
-  mklog STOP-REPORT-${LSUB}
+  add_log STOP-REPORT-${LSUB}
 fi
