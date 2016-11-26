58ef64a3eb0255d97b89fbae11dcd3f09e3acafd
Margus Pxrt <margus@wave.ee>
2016-11-26 05:33:45 +0200

    fix

--- specs/execute_command_on_cluster_node_spec.py
+++ specs/execute_command_on_cluster_node_spec.py
@@ -26,7 +26,7 @@ def run_test():
     )
 
     m.waitForJobs()
-    time.sleep(2)
+    time.sleep(2) # TODO fix this (at wait for jobs does not work, because job id is registered)
 
     # Check the result
 
