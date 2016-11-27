ce4861bcddf9c045f2b9e2c7c6bc0be44352abff
Christian Veelken <christian.veelken@cern.ch>
2016-11-27 16:54:25 +0200

    add genWeight = True for aMC@NLO samples

--- python/tthAnalyzeSamples_2016_FastSim.py
+++ python/tthAnalyzeSamples_2016_FastSim.py
@@ -4,7 +4,7 @@ samples_2016_fastsim = OD()
 
 samples_2016_fastsim["/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/matze-fast_ttjets_dl_MiniAOD_6b57d231e28e4ebd8065fc7621fa1f5b-v1/USER"] = OD([
   ("type",                  "mc"),
-  ("sample_category",       "TT"),
+  ("sample_category",       "TT_dilept"),
   ("process_name_specific", "TT_dilept_fastsim_validation"),
   ("nof_files",             17),
   ("nof_events",            1000000),
@@ -24,12 +24,12 @@ samples_2016_fastsim["/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/m
 ])
 samples_2016_fastsim["/TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/matze-fast_ttjets_sl_MiniAOD_6b57d231e28e4ebd8065fc7621fa1f5b-v1/USER"] = OD([
   ("type",                  "mc"),
-  ("sample_category",       "TT"),
+  ("sample_category",       "TT_semilept"),
   ("process_name_specific", "TT_semilept_fastsim_validation"),
   ("nof_files",             18),
   ("nof_events",            1000000),
   ("use_it",                True),
-  ("xsection",              245.000000),
+  ("xsection",              364.310880),
   ("triggers",              [ "1e", "2e", "1mu", "2mu", "1e1mu" ]),
   ("reHLT",                 False),
   ("local_paths",
