e455327bc35b42bde9a051407ddd0d1ae4d73089
Margus Pxrt <margus@wave.ee>
2016-12-01 18:56:49 +0200

    save

---
+++ scripts/hadd_in_cluster.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+import os
+import sys
+from tthAnalysis.HiggsToTauTau.jobTools import run_cmd
+from tthAnalysis.HiggsToTauTau.sbatchManager import sbatchManager
+
+def main():
+    output_histogram_file = sys.argv(1)
+    input_histograms_list_file = sys.argv(2)
+
+    if (!(output_histogram_file && input_histograms_list_file)):
+        print("Usage: hadd_in_cluster.py /full/path/ouput_histogram.root /full/path/input_histograms_list.txt")
+        return False
+
+    input_histograms = []
+
+    with file(input_histograms_list_file) as f:
+        input_histograms = f.read().strip().split("\n")
+
+    print("hadd_in_cluster.py will read %s and add them into %s." % (input_histograms, output_histogram))
+
+    m = sbatchManager()
+    m.setWorkingDir('/tmp/') # Why SBatch manager even needs to know this?
+
+    m.hadd_on_cluster_node(
+        input_histograms=input_histograms,
+        output_histogram=output_histogram_file,
+        output_dir=output_histogram_file.split("/")[0:-1].join("/") # Output to same directory as histogram itself goes (less hassel)
+    )
+
+    m.waitForJobs()
+
+
+
+main()
---
+++ specification/specs/executable_hadd_in_cluster_spec.py
@@ -0,0 +1,35 @@
+import subprocess
+import time
+from tthAnalysis.HiggsToTauTau.jobTools import run_cmd
+from tthAnalysis.HiggsToTauTau.sbatchManager import sbatchManager
+
+from config import config
+
+def executable_hadd_in_cluster_spec():
+
+    # Prepare
+
+    run_cmd("rm -rf /home/%(user)s/tmp/hadd_in_cluster_spec/*" % config)
+    fixtures_dir = '/home/%(user)s/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/specs/fixtures/' % config
+    run_cmd("""echo "%(fixtures_dir)s/histogram_1.root\n\%(fixtures_dir)s/histogram_2.root\n" > /home/%(user)s/tmp/hadd_in_cluster_spec/input_histograms_list.txt""" % { 'fixtures_dir': fixtures_dir })
+
+
+    # Run task
+
+    run_cmd('python /home/%(user)s/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/scripts/hadd_in_cluster.py /home/%(user)s/tmp/hadd_in_cluster_spec/output_histogram.root /home/%(user)s/tmp/hadd_in_cluster_spec/input_histograms_list.txt' % config)
+
+
+    # Check the result
+
+    root_result_file = '/home/%(user)s/tmp/hadd_in_cluster_spec/output_histogram.root' % config
+    result_successful = os.path.isfile(root_result_file)
+
+
+    # Output result
+
+    if result_successful:
+        print('Executable for HADD in cluster is WORKING')
+    else:
+        print('Executable for HADD in cluster is NOT WORKING')
+
+    return result_successful
