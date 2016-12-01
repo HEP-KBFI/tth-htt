39ec5cbf17303eee6749b085ff762cb341576449
Margus Pxrt <margus@wave.ee>
2016-12-01 19:24:02 +0200

    fix

--- scripts/hadd_in_cluster.py
+++ scripts/hadd_in_cluster.py
@@ -6,16 +6,18 @@ from tthAnalysis.HiggsToTauTau.jobTools import run_cmd
 from tthAnalysis.HiggsToTauTau.sbatchManager import sbatchManager
 
 def main():
-    output_histogram_file = sys.argv[1]
-    input_histograms_list_file = sys.argv[2]
+    output_histogram = sys.argv[1]
+    input_histograms_list = sys.argv[2]
 
-    if (not (output_histogram_file and input_histograms_list_file)):
+    print('hadd_in_cluster.py %s %s' % (output_histogram, input_histograms_list))
+
+    if (not (output_histogram and input_histograms_list)):
         print("Usage: hadd_in_cluster.py /full/path/ouput_histogram.root /full/path/input_histograms_list.txt")
         return False
 
     input_histograms = []
 
-    with file(input_histograms_list_file) as f:
+    with file(input_histograms_list) as f:
         input_histograms = f.read().strip().split("\n")
 
     print("hadd_in_cluster.py will read %s and add them into %s." % (input_histograms, output_histogram))
@@ -25,8 +27,8 @@ def main():
 
     m.hadd_on_cluster_node(
         input_histograms=input_histograms,
-        output_histogram=output_histogram_file,
-        output_dir=output_histogram_file.split("/")[0:-1].join("/") # Output to same directory as histogram itself goes (less hassel)
+        output_histogram=output_histogram,
+        output_dir=output_histogram.split("/")[0:-1].join("/") # Output to same directory as histogram itself goes (less hassel)
     )
 
     m.waitForJobs()
