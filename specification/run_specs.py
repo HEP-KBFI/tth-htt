f4c026c7a5698eb1d2d25659535b1f38e5b7325d
Margus Pxrt <margus@wave.ee>
2016-12-01 18:58:59 +0200

    fix

--- specification/run_specs.py
+++ specification/run_specs.py
@@ -5,7 +5,8 @@ import models
 spec_runner = models.SpecRunner()
 spec_runner.run_specs([
     specs.call_histogram_aggregation_on_cluster_node_spec,
-    specs.execute_command_on_cluster_node_spec
+    specs.execute_command_on_cluster_node_spec,
+    specs.executable_hadd_in_cluster_spec
 ])
 
 spec_runner.print_summary()
