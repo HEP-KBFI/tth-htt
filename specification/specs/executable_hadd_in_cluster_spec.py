a40531049859362a6a45f21ae07476763b64f623
Margus Pxrt <margus@wave.ee>
2016-12-01 19:11:51 +0200

    save

--- specification/specs/executable_hadd_in_cluster_spec.py
+++ specification/specs/executable_hadd_in_cluster_spec.py
@@ -10,19 +10,20 @@ def executable_hadd_in_cluster_spec():
 
     # Prepare
 
-    run_cmd("rm -rf /home/%(user)s/tmp/hadd_in_cluster_spec/*" % config)
+    run_cmd("rm -rf /home/%(user)s/tmp/executable_hadd_in_cluster_spec/*" % config)
+    run_cmd("mkdir -p /home/%(user)s/tmp/executable_hadd_in_cluster_spec/*" % config)
     fixtures_dir = '/home/%(user)s/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/specs/fixtures/' % config
-    run_cmd("""echo "%(fixtures_dir)s/histogram_1.root\n\%(fixtures_dir)s/histogram_2.root\n" > /home/%(user)s/tmp/hadd_in_cluster_spec/input_histograms_list.txt""" % { 'fixtures_dir': fixtures_dir, 'user': config['user'] })
+    run_cmd("""echo "%(fixtures_dir)s/histogram_1.root\n\%(fixtures_dir)s/histogram_2.root\n" > /home/%(user)s/tmp/executable_hadd_in_cluster_spec/input_histograms_list.txt""" % { 'fixtures_dir': fixtures_dir, 'user': config['user'] })
 
 
     # Run task
 
-    run_cmd('python /home/%(user)s/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/scripts/hadd_in_cluster.py /home/%(user)s/tmp/hadd_in_cluster_spec/output_histogram.root /home/%(user)s/tmp/hadd_in_cluster_spec/input_histograms_list.txt' % config)
+    run_cmd('python /home/%(user)s/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/scripts/hadd_in_cluster.py /home/%(user)s/tmp/executable_hadd_in_cluster_spec/output_histogram.root /home/%(user)s/tmp/executable_hadd_in_cluster_spec/input_histograms_list.txt' % config)
 
 
     # Check the result
 
-    root_result_file = '/home/%(user)s/tmp/hadd_in_cluster_spec/output_histogram.root' % config
+    root_result_file = '/home/%(user)s/tmp/executable_hadd_in_cluster_spec/output_histogram.root' % config
     result_successful = os.path.isfile(root_result_file)
 
 
