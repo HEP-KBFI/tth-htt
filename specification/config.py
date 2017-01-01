dc671622783a0ba6dd316a5024fa10db30c33245
Margus Pxrt <margus@wave.ee>
2017-01-01 20:08:59 +0200

    save

--- specification/config.py
+++ specification/config.py
@@ -14,6 +14,7 @@ user = run_cmd('whoami').strip()
 cmssw_base = run_cmd('echo $CMSSW_BASE').strip()
 temp_dir = '/home/%s/tmp/' % user
 fixtures_dir = '%s/src/tthAnalysis/HiggsToTauTau/specification/fixtures/' % cmssw_base
+sbatch_priority = run_cmd('echo $SBATCH_PRIORITY').strip()
 
 
 # create config
@@ -22,7 +23,8 @@ config = {
     'user': user,
     'cmssw_base': cmssw_base,
     'temp_dir': temp_dir,
-    'fixtures_dir': fixtures_dir
+    'fixtures_dir': fixtures_dir,
+    'sbatch_priority': sbatch_priority
 }
 
 
