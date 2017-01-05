4f77b6e32b2bf0622fba8087b1dd8a64246a32b3
Margus Pxrt <margus@wave.ee>
2017-01-05 23:29:17 +0200

    save

--- scripts/check_that_histograms_are_equal.py
+++ scripts/check_that_histograms_are_equal.py
@@ -29,8 +29,16 @@ def main():
 def get_events_count(root_file):
     f = ROOT.TFile(root_file)
     count = f.Get("Count")
-    entries_count = count.GetEntries()
-    print('Event count for %s is %s' % (root_file, entries_count))
-    return entries_count
+    has_entries_count = hasattr(count, 'GetEntries')
+
+    if has_entries_count:
+        entries_count = count.GetEntries()
+        print('Event count for %s is %s' % (root_file, entries_count))
+        return entries_count
+    else:
+        entries_count = 0.0
+        print('Event count for %s is %s, because files does not have count entries' % (root_file, entries_count))
+        return entries_count
+
 
 main()
