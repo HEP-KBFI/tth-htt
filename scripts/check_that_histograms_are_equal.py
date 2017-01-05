500372cbde443825233e27c2be377665f7096a81
Margus Pxrt <margus@wave.ee>
2017-01-05 23:14:39 +0200

    save

--- scripts/check_that_histograms_are_equal.py
+++ scripts/check_that_histograms_are_equal.py
@@ -7,12 +7,30 @@ import ROOT
 import sys
 
 def main():
-
     output_histogram = sys.argv[1]
     input_histograms = sys.argv[2:len(sys.argv)]
 
-    print('check_that_histograms_are_equal.py %s %s' % (output_histogram, input_histograms))
-    print('check_that_histograms_are_equal.py will always exit with success. TODO')
-    sys.exit(0)
+    output_histogram_events_total_count = get_events_count(output_histogram)
+    input_histograms_events_counts = []
+
+    for input_histogram in input_histograms:
+        input_histograms_events_count = get_events_count(input_histogram)
+        input_histograms_events_counts.append(input_histograms_events_count)
+
+    input_histograms_events_total_count = sum(input_histograms_events_counts)
+
+    if abs(output_histogram_events_total_count - input_histograms_events_total_count) < 2.0:
+        print('Output histogram event count is same as input histograms event counts sum (%s and %s)' % (output_histogram_events_total_count, input_histograms_events_total_count))
+        sys.exit(0)
+    else:
+        print('ERROR: count(output_histogram.events) != count(input_histograms.events) (%s and %s)' % (output_histogram_events_total_count, input_histograms_events_total_count))
+        sys.exit(1)
+
+def get_events_count(root_file):
+    f = ROOT.TFile(root_file)
+    count = f.Get("Count")
+    entries_count = count.GetEntries()
+    print('Event count for %s is %s' % (root_file, entries_count))
+    return entries_count
 
 main()
