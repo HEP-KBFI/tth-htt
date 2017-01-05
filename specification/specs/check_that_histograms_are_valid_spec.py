f8633f73501f1deb64cdea7a1c4883568574c7e9
Margus Pxrt <margus@wave.ee>
2017-01-05 22:00:04 +0200

    save

---
+++ specification/specs/check_that_histograms_are_valid_spec.py
@@ -0,0 +1,27 @@
+import subprocess
+import time
+import os
+from tthAnalysis.HiggsToTauTau.jobTools import run_cmd
+from tthAnalysis.HiggsToTauTau.sbatchManager import sbatchManager
+
+from config import config
+
+def check_that_histograms_are_valid_spec():
+
+    # Prepare
+
+    valid_histograms = [
+        "%(fixtures_dir)s/histogram_1.root" % config,
+        "%(fixtures_dir)s/histogram_2.root" % config
+    ]
+
+    # Run task
+
+    command = 'python %(cmssw_base)s/src/tthAnalysis/HiggsToTauTau/scripts/check_that_histograms_are_valid.py' % config
+    command_arguments = command + " ".join(valid_histograms)
+    command_with_arguments = command + " " + command_arguments
+
+    result = run_cmd(command_with_arguments)
+
+    print(result)
+    return True
