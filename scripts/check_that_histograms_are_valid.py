e650893904f098458d2440f910a26a86fd8e9f45
Margus Pxrt <margus@wave.ee>
2017-01-05 22:19:06 +0200

    save

--- scripts/check_that_histograms_are_valid.py
+++ scripts/check_that_histograms_are_valid.py
@@ -12,6 +12,15 @@ def main():
     print('check_that_histograms_are_valid.py %s' % input_histograms)
 
     for input_histogram in input_histograms:
+
+        if not os.path.isfile(input_histogram):
+            print('ERROR: root input file is missing: %s' % input_histogram)
+            sys.exit(1)
+
+        if os.path.getsize(path) < 5:
+            print('ERROR: root input file is too small: %s' % input_histogram)
+            sys.exit(1)
+
         root_tfile = ROOT.TFile(input_histogram, "read")
 
         if root_tfile.IsZombie(): # THIS IS NOT WORKING :(
@@ -19,7 +28,6 @@ def main():
             sys.exit(1)
 
         print('All input files are ok.')
-
         sys.exit(0)
 
 main()
