7ec1930918f8a5b070fa1015fbca0af8bad12633
Andres <andres.tiko@cern.ch>
2017-01-18 15:32:59 +0200

    Added CF pseudodata creation for muons

--- test/create_pseudodata_datacard_chargeflip.py
+++ test/create_pseudodata_datacard_chargeflip.py
@@ -10,24 +10,28 @@ samples = ["data_obs",
 	    "Singletop", 
 	    "Diboson", 
 	    "TTbar"]
-categories = ["BB_LL", "BB_ML", "BB_MM", "BB_HL", "BB_HM", "BB_HH", "EE_LL", "EE_ML", "EE_MM", "EE_HL", "EE_HM", "EE_HH", "BE_LL", "BE_ML", "EB_ML", "BE_MM", "BE_HL", "EB_HL", "BE_HM", "EB_HM", "BE_HH"]
+categories = {}
+categories["ele"] = ["BB_LL", "BB_ML", "BB_MM", "BB_HL", "BB_HM", "BB_HH", "EE_LL", "EE_ML", "EE_MM", "EE_HL", "EE_HM", "EE_HH", "BE_LL", "BE_ML", "EB_ML", "BE_MM", "BE_HL", "EB_HL", "BE_HM", "EB_HM", "BE_HH"]
+categories["mu"] = ["total"]
 prefix = "ttH_charge_flip"
 charges = ["OS", "SS"]
-systematics = ["CMS_ttHl_electronERUp",
+systematics = {}
+systematics["ele"] = ["CMS_ttHl_electronERUp",
        "CMS_ttHl_electronERDown",
        "CMS_ttHl_electronESEndcapUp",
        "CMS_ttHl_electronESEndcapDown",
        "CMS_ttHl_electronESBarrelUp",
        "CMS_ttHl_electronESBarrelDown"]
+systematics["mu"] = []
 
 
-def create_pseudodata(infile, outfile_data, outfile_pseudodata):
+def create_pseudodata(infile, outfile_data, outfile_pseudodata, channel):
     f = TFile(infile)
     fout = TFile(outfile_data, "RECREATE")
     fout_pseudo = TFile(outfile_pseudodata, "RECREATE")
     
     for charge in charges:
-        for cat in categories:
+        for cat in categories[channel]:
             first = True
             dirname = "%s_%s_%s" % (prefix, charge, cat)
             cd = fout.mkdir(dirname)
@@ -50,7 +54,7 @@ def create_pseudodata(infile, outfile_data, outfile_pseudodata):
                     data_histo.Add(histo_nominal)
                 
                 
-                for syst in systematics:
+                for syst in systematics[channel]:
                   if syst.startswith("CMS_ttHl_electronER") and not sample == "DY": continue
                   histo = f.Get("%s/x_%s_%s"  % (dirname, sample, syst))
                   #if histo.Integral() > 0 or not sample == "DY":
@@ -72,11 +76,15 @@ def create_pseudodata(infile, outfile_data, outfile_pseudodata):
 
 if __name__ == "__main__":
   np.random.seed(123)
-  indir = "/home/andres/tth/histograms/histosCF_data_newconf/datacards/"
+  #indir = "/home/andres/tth/histograms/histosCF_data_newconf/datacards/"
   infile = "prepareDatacards_charge_flip_mass_ll.root"
   datafile = "prepareDatacards_data_charge_flip_mass_ll.root"
   pseudodatafile = "prepareDatacards_pseudodata_charge_flip_mass_ll.root"
+  #create_pseudodata(indir + infile, 
+  #  indir + datafile, 
+  #  indir + pseudodatafile, "ele")
+  indir = "/home/andres/tth/histograms/histosCF_mu_1bin/datacards/"
   create_pseudodata(indir + infile, 
     indir + datafile, 
-    indir + pseudodatafile)
+    indir + pseudodatafile, "mu")
 
