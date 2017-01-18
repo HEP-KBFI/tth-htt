b737cbba93f47e63440d77f209eb9c7feb3c1c4d
Andres <andres.tiko@cern.ch>
2017-01-18 15:27:11 +0200

    Added lumiscale to sync ntuples

--- test/sync_ntuples_cfg.py
+++ test/sync_ntuples_cfg.py
@@ -15,7 +15,7 @@ process.fwliteInput = cms.PSet(
 process.fwliteOutput = cms.PSet(
 #    fileName = cms.string('/home/karl/sandbox/sync_ntuples/ttHJetToTT_M125_13TeV_ntuples_sync_v0.root')
 #    fileName = cms.string('/home/karl/sandbox/sync_ntuples/ttJet_13TeV_ntuples_sync_v0.root')
-    fileName = cms.string('/home/andres/tth/sync/sync_ntuple_v1.root')
+    fileName = cms.string('/home/andres/tth/sync/sync_ntuple_v1_5.root')
 )
 
 process.syncNtuple = cms.PSet(
@@ -41,14 +41,16 @@ process.syncNtuple = cms.PSet(
     selEventsFileName_input = cms.string(''),
     selEventsFileName_output = cms.string(''),
 
+    leptonSelection = cms.string('Fakeable'),
+
     hadTauSelection = cms.string('dR03mvaTight'),
     leptonFakeRateWeight = cms.PSet(
-        inputFileName = cms.string("tthAnalysis/HiggsToTauTau/data/FR_lep_ttH_mva_2015_data.root"),
+        inputFileName = cms.string("tthAnalysis/HiggsToTauTau/data/FR_lep_ttH_mva_2016_data.root"),
         histogramName_e = cms.string("FR_mva075_el_data_comb"),
         histogramName_mu = cms.string("FR_mva075_mu_data_comb")
     ),
     hadTauFakeRateWeight = cms.PSet(
-        inputFileName = cms.string("tthAnalysis/HiggsToTauTau/data/FR_tau_2015.root"),
+        inputFileName = cms.string("tthAnalysis/HiggsToTauTau/data/FR_tau_2016.root"),
         lead = cms.PSet(
             absEtaBins = cms.vdouble(-1., 1.479, 9.9),
             graphName = cms.string("jetToTauFakeRate/dR03mvaTight/$etaBin/jetToTauFakeRate_mc_hadTaus_pt"),
@@ -57,5 +59,13 @@ process.syncNtuple = cms.PSet(
             applyFitFunction = cms.bool(True)
         )
     ),    
-    debug = cms.bool(False) # set it to True if you select only few events
+    debug = cms.bool(False), # set it to True if you select only few events
+    lumiScale = cms.double(0.000446),
+    apply_trigger_bits = cms.bool(False),
+    apply_hadTauGenMatching = cms.bool(False),
+    #applyFakeRateWeights = cms.string('disabled')
+    isMC = cms.bool(True)
+    #apply_genWeight = cms.bool(True)
+    
+
 )
