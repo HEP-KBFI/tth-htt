d3f06acaf854958864464162a8aa49e6310f4915
Margus Pxrt <margus@wave.ee>
2017-04-20 04:23:42 +0300

    creating also metadata on the run

---
+++ scripts/create_histogram_metadata.py
@@ -0,0 +1,12 @@
+from tthAnalysis.HiggsToTauTau.commands import *
+from tthAnalysis.HiggsToTauTau.jobTools import run_cmd
+
+def main():
+    histogram = sys.argv[1]
+    metadata_file = histogram.replace('.root', '.metadata.txt')
+    metadata_txt = get_histogram_metadata(input_histogram)
+    run_cmd("echo %s > %s" % (metadata_txt, metadata_file_file))
+
+    print("Created metadata file %s with contents: \n%s" % (metadata_file, metadata_txt))
+
+main()
