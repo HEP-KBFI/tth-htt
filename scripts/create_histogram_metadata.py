95bb08c68527e50515a9a39672917d7640a9f1e7
Margus Pxrt <margus@wave.ee>
2017-04-20 05:35:48 +0300

    fixed naming

--- scripts/create_histogram_metadata.py
+++ scripts/create_histogram_metadata.py
@@ -6,7 +6,7 @@ def main():
     histogram = sys.argv[1]
     metadata_file = histogram + '.metadata'
     metadata_txt = get_histogram_metadata(histogram)
-    run_cmd("echo %s > %s" % (metadata_txt, metadata_file))
+    run_cmd("echo '%s' > '%s'" % (metadata_txt, metadata_file))
 
     print("Created metadata file %s with contents: \n%s" % (metadata_file, metadata_txt))
 
