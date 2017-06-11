1aa25c93896c7b059a6faeb4f6351ab709fcad88
Andres <andres.tiko@cern.ch>
2017-06-11 15:06:03 +0300

    Updates for pseudodata datacard creation

--- test/create_pseudodata_datacard_chargeflip.py
+++ test/create_pseudodata_datacard_chargeflip.py
@@ -22,7 +22,17 @@ systematics["ele"] = ["CMS_ttHl_electronERUp",
        "CMS_ttHl_electronESEndcapDown",
        "CMS_ttHl_electronESBarrelUp",
        "CMS_ttHl_electronESBarrelDown"]
-systematics["mu"] = []
+systematics["mu"] = [#"CMS_ttHl_muonERUp",
+       #"CMS_ttHl_muonERDown",
+       #"CMS_ttHl_muonESEndcap1Up",
+       #"CMS_ttHl_muonESEndcap1Down",
+       #"CMS_ttHl_muonESEndcap2Up",
+       #"CMS_ttHl_muonESEndcap2Down",
+       #"CMS_ttHl_muonESBarrel1Up",
+       #"CMS_ttHl_muonESBarrel1Down",
+       #"CMS_ttHl_muonESBarrel2Up",
+       #"CMS_ttHl_muonESBarrel2Down"
+      ]
 
 
 def create_pseudodata(infile, outfile_data, outfile_pseudodata, channel):
@@ -40,6 +50,12 @@ def create_pseudodata(infile, outfile_data, outfile_pseudodata, channel):
                 cd.cd() 
                 histo_nominal = f.Get("%s/x_%s"  % (dirname, sample))
                 print "%s/x_%s"  % (dirname, sample)
+                for b in range(1, histo_nominal.GetNbinsX()+1):
+                  content = histo_nominal.GetBinContent(b)
+                  if content < 0:
+                    histo_nominal.SetBinContent(b, 0.0)
+                    histo_nominal.SetBinError(b, math.sqrt(histo_nominal.GetBinError(b)**2 + histo_nominal.GetBinContent(b)) )
+                    
                 histo_nominal.Write()
                 
                 if sample == "data_obs": continue
@@ -56,14 +72,23 @@ def create_pseudodata(infile, outfile_data, outfile_pseudodata, channel):
                 
                 for syst in systematics[channel]:
                   if syst.startswith("CMS_ttHl_electronER") and not sample == "DY": continue
+                  if syst.startswith("CMS_ttHl_muonER") and not sample == "DY": continue
                   histo = f.Get("%s/x_%s_%s"  % (dirname, sample, syst))
                   #if histo.Integral() > 0 or not sample == "DY":
                   cd.cd() 
+                  print histo, dirname, sample, syst
+                  for b in range(1, histo.GetNbinsX()+1):
+                    content = histo.GetBinContent(b)
+                    if content < 0:
+                      histo.SetBinContent(b, 0.0)
+                      histo.SetBinError(b, math.sqrt(histo.GetBinError(b)**2 + histo.GetBinContent(b)) )
+                    
                   histo.Write()
                   cd_pseudo.cd() 
                   histo.Write()
             data_histo.SetNameTitle("x_data_obs", "x_data_obs")
             for b in range(1, data_histo.GetNbinsX()+1):
+                print data_histo.GetBinContent(b)
                 data_histo.SetBinContent(b, np.random.poisson(data_histo.GetBinContent(b)))
                 data_histo.Sumw2(ROOT.kFALSE)
                 print data_histo.GetBinContent(b), data_histo.GetBinError(b)
@@ -76,15 +101,15 @@ def create_pseudodata(infile, outfile_data, outfile_pseudodata, channel):
 
 if __name__ == "__main__":
   np.random.seed(123)
-  #indir = "/home/andres/tth/histograms/histosCF_data_newconf/datacards/"
+  indir = "/home/andres/ttHAnalysis/2016/histosCF_summer_June6/datacards/charge_flip/"
   infile = "prepareDatacards_charge_flip_mass_ll.root"
   datafile = "prepareDatacards_data_charge_flip_mass_ll.root"
   pseudodatafile = "prepareDatacards_pseudodata_charge_flip_mass_ll.root"
-  #create_pseudodata(indir + infile, 
-  #  indir + datafile, 
-  #  indir + pseudodatafile, "ele")
-  indir = "/home/andres/tth/histograms/histosCF_mu_1bin/datacards/"
   create_pseudodata(indir + infile, 
     indir + datafile, 
-    indir + pseudodatafile, "mu")
+    indir + pseudodatafile, "ele")
+  indir = "/home/andres/ttHAnalysis/2016/histosCF_mu_summer/datacards/charge_flip/"
+  #create_pseudodata(indir + infile, 
+  #  indir + datafile, 
+  #  indir + pseudodatafile, "mu")
 
