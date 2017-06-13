7196fe00a5e5a43758765794a6cf6ee269ecf01d
Margus Pxrt <margus@wave.ee>
2017-06-13 16:51:15 +0300

    Fixed compile issues. (RootMetadata).

--- src/RootMetadata.cc
+++ src/RootMetadata.cc
@@ -7,14 +7,14 @@ using namespace std;
 
 
 std::string RootMetadata::toString() {
-  return std::string("selectedEntries: ") + std::to_string(this.selectedEntries) + std::string("\n");
+  return std::string("selectedEntries: ") + std::to_string(this->selectedEntries) + std::string("\n");
 }
 
 bool RootMetadata::save(std::string fileName)
 {
   ofstream f;
   f.open(fileName);
-  f << this.toString();
+  f << this->toString();
   f.close();
   return true;
 }
