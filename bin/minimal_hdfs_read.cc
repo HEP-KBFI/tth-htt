e1340e58d0c73a544757be107ab08f22423ea95e
Karl Ehatäht <karlehataht@gmail.com>
2017-09-07 17:53:22 +0300

    Add a minimal example that reads a file via the HDFS protocol

---
+++ bin/minimal_hdfs_read.cc
@@ -0,0 +1,22 @@
+#include "tthAnalysis/HiggsToTauTau/interface/THDFSFile.h"
+
+#include <TTree.h>
+
+#include <iostream>
+
+/* in order to run this piece of code you must set environment variables in your current shell session as follows:
+
+export CLASSPATH=`hadoop classpath --glob`
+for j in $(ls /usr/lib/hadoop/client/*.jar); do export CLASSPATH=$CLASSPATH:$j; done
+
+*/
+
+int main(int argc, char **argv) {
+    
+    THDFSFile * f = new THDFSFile("hdfs:///local/karl/sandbox/tree_1.root", "read");
+    TTree * t     = static_cast<TTree *>(f -> Get("tree"));
+    const unsigned n = t -> GetEntries();
+    std::cout << "Read " << n <<" events\n";
+
+    return 0;
+}
