1be5bd5465849512f03a1470df21bd96d180c485
Karl Ehatäht <karlehataht@gmail.com>
2017-10-14 17:42:57 +0300

    Add missing TTWW bkg plots for 2lss1tau and 2los1tau channels

--- python/analyzeConfig_2lss_1tau.py
+++ python/analyzeConfig_2lss_1tau.py
@@ -111,7 +111,7 @@ class analyzeConfig_2lss_1tau(analyzeConfig):
     self.nonfake_backgrounds = [ "TT", "TTW", "TTZ", "TTWW", "EWK", "Rares", "tH" ]
 
     self.prep_dcard_processesToCopy = [ "data_obs", "TTW", "TTZ", "TTWW", "EWK", "Rares", "tH", "fakes_data", "fakes_mc", "flips_data" ]
-    self.make_plots_backgrounds = [ "TTW", "TTZ", "EWK", "Rares", "fakes_data", "flips_data" ]
+    self.make_plots_backgrounds = [ "TTW", "TTWW", "TTZ", "EWK", "Rares", "fakes_data", "flips_data" ]
 
     self.cfgFile_analyze = os.path.join(self.workingDir, cfgFile_analyze)
     self.inputFiles_hadd_stage1_6 = []
