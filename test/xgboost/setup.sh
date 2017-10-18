cfd8b9b3b177180853ef395aa858f256e10712cf
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2017-10-18 18:03:55 +0300

    nstructions

--- test/xgboost/setup.sh
+++ test/xgboost/setup.sh
@@ -1,8 +1,9 @@
 
 # cmsenv in the release you are going to work (eg CMSSW_9_4_0_pre1)
 # install conda to a home directory -- it will install python2.7 althoguether
+# https://conda.io/docs/user-guide/install/linux.html (with prefix)
 # https://www.anaconda.com/download/ 
-# install pip: 
+# install pip: https://pip.pypa.io/en/stable/installing/ (--user)
 # pip install scikit-learn --user
 # pip install xgboost --user
 # pip install catboost --user
