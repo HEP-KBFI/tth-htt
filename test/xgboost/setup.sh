9ddf81ca306fee27c762170b89eac9bf0105d2af
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2017-10-18 16:26:08 +0300

    install and export

---
+++ test/xgboost/setup.sh
@@ -0,0 +1,14 @@
+
+# cmsenv in the release you are going to work (eg CMSSW_9_4_0_pre1)
+# install conda to a home directory -- it will install python2.7 althoguether
+# https://www.anaconda.com/download/ 
+# install pip: 
+# pip install scikit-learn --user
+# pip install xgboost --user
+# pip install catboost --user
+# pip uninstall numpy (yes, unistall, otherwise will conflict with ROOT)
+
+export PYTHONUSERBASE=/home/acaan/python_local/
+export PATH=/home/acaan/python_local/bin:$PATH
+export PYTHONPATH=/home/acaan/python_local/lib/python2.7/site-packages:$PYTHONPATH
+
