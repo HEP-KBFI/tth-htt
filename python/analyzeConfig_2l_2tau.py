e36fc4168c8d85f2abdd0d11aeb212b2806af024
Christian Veelken <christian.veelken@cern.ch>
2017-10-31 13:48:18 +0200

    bux-fix for fake background estimation

--- python/analyzeConfig_2l_2tau.py
+++ python/analyzeConfig_2l_2tau.py
@@ -182,7 +182,7 @@ class analyzeConfig_2l_2tau(analyzeConfig):
     lines.append("process.analyze_2l_2tau.hadTauSelection = cms.string('%s')" % jobOptions['hadTau_selection'])
     lines.append("process.analyze_2l_2tau.apply_hadTauGenMatching = cms.bool(%s)" % (jobOptions['apply_hadTauGenMatching'] and jobOptions['is_mc']))
     lines.append("process.analyze_2l_2tau.applyFakeRateWeights = cms.string('%s')" % jobOptions['applyFakeRateWeights'])
-    if jobOptions['hadTau_selection'].find("Fakeable") != -1 and jobOptions['applyFakeRateWeights'] in [ "3L", "2tau" ]:
+    if jobOptions['hadTau_selection'].find("Fakeable") != -1 and jobOptions['applyFakeRateWeights'] in [ "4L", "2tau" ]:
       fitFunctionName = None
       if self.era == "2015":
         lines.append("process.analyze_2l_2tau.hadTauFakeRateWeight.inputFileName = cms.string('tthAnalysis/HiggsToTauTau/data/FR_tau_2015.root')")
