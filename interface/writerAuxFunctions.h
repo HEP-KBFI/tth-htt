f452b8a0be19ef47203b30b9a4c6cb52557b6ab0
Karl Ehatäht <karlehataht@gmail.com>
2017-11-05 02:12:08 +0200

    Template-base branch address setting
    Instead of using a function for each primitive type (int, float, their arrays etc) to set the branch addresses for the purpose of filling the branches, we can do a lot better with a single template that is able to deduce the correct string format by looking at the data type itself. Avoid potential compilation errors when the source data type and the desired one (specified by the function name) do not match.

--- interface/writerAuxFunctions.h
+++ interface/writerAuxFunctions.h
@@ -1,10 +1,26 @@
 #ifndef tthAnalysis_HiggsToTauTau_writerAuxFunctions_h
 #define tthAnalysis_HiggsToTauTau_writerAuxFunctions_h
 
+#include "tthAnalysis/HiggsToTauTau/interface/TypeTraits.h" // Traits<>
+
 #include <Rtypes.h> // Int_t, UInt_t, ULong64_t, Float_t
 #include <TTree.h> // TTree
 
+#include <type_traits> // std::enable_if<,>, std::is_scalar<>
 #include <string> // std::string
+
+template<typename T,
+         typename = std::enable_if<std::is_scalar<T>::value>>
+void
+setBranch(TTree * tree,
+          T * address,
+          const std::string & branchName,
+          const std::string & branchName_n = "")
+{
+  tree -> Branch(branchName.data(), address, Form(
+    "%s%s/%s", branchName.data(), branchName_n.empty() ? "" : Form("[%s]", branchName_n.data()), Traits<T>::TYPE_NAME)
+  );
+}
 
 void setBranchI(TTree* tree, const std::string& branchName, Int_t* address);
 void setBranchUI(TTree* tree, const std::string& branchName, UInt_t* address);
