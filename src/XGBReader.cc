7c254e35234cb0d6a5f6c2aa3e92643846453fc8
Karl Ehatäht <karlehataht@gmail.com>
2017-11-15 14:19:22 +0200

    Do not use all available threads when running XGB
    When XGB is initialized, it tries to allocate as many threads as there are cores in the system. Fortunately, this behavior is configurable, but the documation nor the API isn't clear about it. Setting the parameters explicitly on the unpickled data variable didn't work. It turns out that the parallelization is achieved via OMP (as described here: https://github.com/dmlc/xgboost/issues/1345#issuecomment-231898237). Thus, the workaround was to set the number of available threads to sklearn by setting up an environment variable. This variable won't be seen from the shell session which launches the application, b/c the whole process will be executed in a fork of the original shell.

--- src/XGBReader.cc
+++ src/XGBReader.cc
@@ -39,6 +39,7 @@ PyObject* vectorToTuple_String(std::vector<std::basic_string<char>> &data) {
 
 double XGBReader( std::map<std::string, double> mvaInputs_ , char* pklpath )
 {
+  setenv("OMP_NUM_THREADS", "1", 0);
 	double mvaOutput_=-20;
     // here I can start the XGBReader
 	char* pkldir=(char*) "/home/acaan/CMSSW_9_4_0_pre1/src/tth-bdt-training-test/HadTopTagger/";
@@ -64,61 +65,61 @@ double XGBReader( std::map<std::string, double> mvaInputs_ , char* pklpath )
     // https://gist.github.com/rjzak/5681680
 	PyRun_SimpleString(
 	"from time import time,ctime\n"
-	"import sys,os \n"\
-	"import sklearn\n"\
-	"import pandas\n"\
-	"import cPickle as pickle\n"\
-	"import numpy as np \n"\
-	"import subprocess \n"\
-	"from sklearn.externals import joblib \n"\
-	"from itertools import izip \n"\
-	"sys.path.insert(0, '/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-pippkgs_depscipy/3.0-njopjo7/lib/python2.7/site-packages') \n"\
-	"import xgboost as xgb  \n"\
-	"#print('The xgb version is {}.'.format(xgb.__version__)) \n"\
-	"def mul(vec, vec2,pkldir,pklpath): \n"\
-	"	#print 'Today is',ctime(time()), 'All python libraries we need loaded good	HTT'\n"\
-	"	new_dict = dict(izip(vec2,vec)) \n"\
-	"	#print (new_dict) \n"\
-	"	data = pandas.DataFrame() \n"\
-	"	data=data.append(new_dict, ignore_index=True) \n"\
-	"	result=-20 \n"\
-	"	f = None  \n"\
-	"	try: \n"\
-	"		f = open(pkldir+pklpath,'rb') \n"\
-	"	except IOError as e: \n"\
-	"		print('Couldnt open or write to file (%s).' % e) \n"\
-	"	else: \n"\
-	"			#print ('file opened') \n"\
-	"			try: \n"\
-	"				pkldata = pickle.load(f) \n"\
-	"			except pickle.UnpicklingError as e:  \n"\
-    "				# normal, somewhat expected  \n"\
-	"				#print ('pkl loaded') \n"\
-	"				model = pkldata.booster().get_dump() \n"\
-	"			except (AttributeError,  EOFError, ImportError, IndexError) as e:  \n"\
-    "				# secondary errors  \n"\
-	"				print(traceback.format_exc(e))  \n"\
-	"			except Exception as e:  \n"\
-    "				# everything else \n"\
-	"				print(traceback.format_exc(e))  \n"\
-	"			else:  \n"\
-	"				try: \n"\
-	"					proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  ) \n"\
-	"				except : \n"\
-	"					print('Oops!',sys.exc_info()[0],'occured.') \n"\
-	"				else:  \n"\
-	"					#print (proba)  \n"\
-	"					result = proba[:,1][0]\n"\
-	"					#print ('predict BDT to one event',result)  \n"\
-	"			f.close()  \n"\
+  "import sys,os \n"
+  "import sklearn\n"
+  "import pandas\n"
+  "import cPickle as pickle\n"
+  "import numpy as np \n"
+  "import subprocess \n"
+  "from sklearn.externals import joblib \n"
+  "from itertools import izip \n"
+  "sys.path.insert(0, '/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-pippkgs_depscipy/3.0-njopjo7/lib/python2.7/site-packages') \n"
+  "import xgboost as xgb  \n"
+  "#print('The xgb version is {}.'.format(xgb.__version__)) \n"
+  "def mul(vec, vec2,pkldir,pklpath): \n"
+  "	#print 'Today is',ctime(time()), 'All python libraries we need loaded good	HTT'\n"
+  "	new_dict = dict(izip(vec2,vec)) \n"
+  "	#print (new_dict) \n"
+  "	data = pandas.DataFrame() \n"
+  "	data=data.append(new_dict, ignore_index=True) \n"
+  "	result=-20 \n"
+  "	f = None  \n"
+  "	try: \n"
+  "		f = open(pkldir+pklpath,'rb') \n"
+  "	except IOError as e: \n"
+  "		print('Couldnt open or write to file (%s).' % e) \n"
+  "	else: \n"
+  "			#print ('file opened') \n"
+  "			try: \n"
+  "				pkldata = pickle.load(f) \n"
+  "				#print ('pkl loaded') \n"
+  "			except pickle.UnpicklingError as e:  \n"
+  "				# normal, somewhat expected  \n"
+  "				model = pkldata.booster().get_dump() \n"
+  "			except (AttributeError,  EOFError, ImportError, IndexError) as e:  \n"
+  "				# secondary errors  \n"
+  "				print(traceback.format_exc(e))  \n"
+  "			except Exception as e:  \n"
+  "				# everything else \n"
+  "				print(traceback.format_exc(e))  \n"
+  "			else:  \n"
+  "				try: \n"
+  "					proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  ) \n"
+  "					#print ('proba', proba)  \n"
+  "				except : \n"
+  "					print('Oops!',sys.exc_info()[0],'occured.') \n"
+  "				else:  \n"
+  "					result = proba[:,1][0]\n"
+  "					#print ('predict BDT to one event',result)  \n"
+  "			f.close()  \n"
 	"	return result                                \n"
 	);
 	PyObject *func = PyObject_GetAttrString(moduleMain, "mul");
 	PyObject* vectorValues=vectorToTuple_Float(vectorValuesVec);
 	PyObject* vecNames=vectorToTuple_String(vectorNamesVec);
 	PyObject *args = PyTuple_Pack(4, vectorValues, vecNames , PyString_FromString( (char*) pkldir), PyString_FromString( (char*) pklpath) ); //    
-	PyObject *result = PyObject_CallObject(func, args);
-	mvaOutput_=PyFloat_AsDouble(result);
+  PyObject *result = PyObject_CallObject(func, args);
+  mvaOutput_=PyFloat_AsDouble(result);
     return mvaOutput_;
 }
 
