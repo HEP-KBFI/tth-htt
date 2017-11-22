1e80ce4857dfa5d1b7e69b44d2e978f960cd8d81
Karl Ehatäht <karlehataht@gmail.com>
2017-11-22 15:09:01 +0200

    Fix compilation error in 1l_2tau event histogram manager

--- src/EvtHistManager_1l_2tau.cc
+++ src/EvtHistManager_1l_2tau.cc
@@ -16,11 +16,12 @@ void EvtHistManager_1l_2tau::bookHistograms(TFileDirectory& dir)
   histogram_numJets_ = book1D(dir, "numJets", "numJets", 20, -0.5, +19.5);
   histogram_numBJets_loose_ = book1D(dir, "numBJets_loose", "numBJets_loose", 10, -0.5, +9.5);
   histogram_numBJets_medium_ = book1D(dir, "numBJets_medium", "numBJets_medium", 10, -0.5, +9.5);
-  
+
   histogram_numBJets_loose_vs_numJets_ = book2D(dir, "numBJets_loose_vs_numJets", "numBJets_loose_vs_numJets", 8, -0.5, +7.5, 6, -0.5, +5.5);
   histogram_numBJets_medium_vs_numJets_ = book2D(dir, "numBJets_medium_vs_numJets", "numBJets_medium_vs_numJets", 8, -0.5, +7.5, 6, -0.5, +5.5);
 
   histogram_mvaOutput_1l_2tau_ttbar_ = book1D(dir, "mvaOutput_1l_2tau_ttbar", "mvaOutput_1l_2tau_ttbar", 20, -1., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly", "mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly", 20, -1., +1.);
   histogram_mvaOutput_1l_2tau_ttV_ = book1D(dir, "mvaOutput_1l_2tau_ttV", "mvaOutput_1l_2tau_ttV", 20, -1., +1.);
   histogram_mvaDiscr_1l_2tau_ = book1D(dir, "mvaDiscr_1l_2tau", "mvaDiscr_1l_2tau", 10, 0.5, 10.5);
 
@@ -29,9 +30,7 @@ void EvtHistManager_1l_2tau::bookHistograms(TFileDirectory& dir)
   histogram_EventCounter_ = book1D(dir, "EventCounter", "EventCounter", 1, -0.5, +0.5);
 }
 
-void EvtHistManager_1l_2tau::fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium,
-					    double mvaOutput_1l_2tau_ttbar, double mvaOutput_1l_2tau_ttV, double mvaDiscr_1l_2tau,
-					    double mTauTauVis, double evtWeight)
+void EvtHistManager_1l_2tau::fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium, double mvaOutput_1l_2tau_ttbar, double mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly, double mvaOutput_1l_2tau_ttV, double mvaDiscr_1l_2tau, double mTauTauVis, double evtWeight)
 {
   double evtWeightErr = 0.;
 
@@ -46,6 +45,8 @@ void EvtHistManager_1l_2tau::fillHistograms(int numElectrons, int numMuons, int 
   fillWithOverFlow2d(histogram_numBJets_medium_vs_numJets_, numJets, numBJets_medium, evtWeight, evtWeightErr);
 
   fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_, mvaOutput_1l_2tau_ttbar, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly_, mvaOutput_1l_2tau_ttbar_HadTopTaggerVarMVAonly, evtWeight, evtWeightErr);
+
   fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttV_, mvaOutput_1l_2tau_ttV, evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_mvaDiscr_1l_2tau_, mvaDiscr_1l_2tau, evtWeight, evtWeightErr);
 
