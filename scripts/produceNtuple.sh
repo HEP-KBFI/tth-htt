79c314680987f99c167c8275dd28540ba19a30e8
Karl Ehatäht <karlehataht@gmail.com>
2017-11-30 18:41:34 +0200

    Generate the counter TH1Fs in the pre-processing stage of Ntuple production
    Changed propagated from https://github.com/HEP-KBFI/tth-nanoAOD-tools/commit/7bc7b1bf7536ca1109fa7ca6c68818d5367ee95c

--- scripts/produceNtuple.sh
+++ scripts/produceNtuple.sh
@@ -49,12 +49,13 @@ for F in $FILES; do
   F_i=$(basename "${F%.*}_i.${F##*.}")
   F_ii=$(basename "${F%.*}_ii.${F##*.}")
   echo "Adding new branches: $F -> $F_i"
-  nano_postproc.py -s _i -I tthAnalysis.NanoAODTools.postprocessing.tthModules                                                                    \
-                             genHiggsDecayMode,lepJetVar,genLepton,btagSF,puWeight,jecUncert_cpp,jetmetUncertainties,tauIDLog,eventCountHistogram \
+  nano_postproc.py -s _i -I tthAnalysis.NanoAODTools.postprocessing.tthModules                                                \
+                             genHiggsDecayMode,lepJetVar,genLepton,btagSF,puWeight,jecUncert_cpp,jetmetUncertainties,tauIDLog \
                    . $F;
   test_exit_code $?
   echo "Removing useless branches: $F_i -> $F_ii"
-  nano_postproc.py -s i -b $CMSSW_BASE/src/tthAnalysis/NanoAODTools/data/keep_or_drop.txt \
+  nano_postproc.py -s i -I tthAnalysis.NanoAODTools.postprocessing.tthModules countHistogramAll \
+                   -b $CMSSW_BASE/src/tthAnalysis/NanoAODTools/data/keep_or_drop.txt            \
                    . $F_i;
   test_exit_code $?
 done
