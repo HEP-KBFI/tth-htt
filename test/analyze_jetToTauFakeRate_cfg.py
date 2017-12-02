298f4af175387b1ed89749decc8f25c48f992991
Christian Veelken <christian.veelken@cern.ch>
2017-12-02 16:04:57 +0200

    updated config files for jet->tau and jet->lepton fake rate measurement

--- test/analyze_jetToTauFakeRate_cfg.py
+++ test/analyze_jetToTauFakeRate_cfg.py
@@ -5,8 +5,7 @@ import os
 process = cms.PSet()
   
 process.fwliteInput = cms.PSet(
-    ##fileNames = cms.vstring('/afs/cern.ch/user/v/veelken/scratch0/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/VHbbAnalysis/Heppy/test/latest_Loop/tree.root'),
-    fileNames = cms.vstring('/afs/cern.ch/user/k/kaehatah/public/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/VHBB_HEPPY_V12_ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_Py8_mWCutfix__fall15MAv2-pu25ns15v1_76r2as_v12-v1/160330_172426/0000/tree_1.root'),
+    fileNames = cms.vstring('/hdfs/cms/store/user/atiko/VHBBHeppyV25tthtautau/MC/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/VHBB_HEPPY_V25tthtautau_ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_Py8_mWCutfix__RunIISummer16MAv2-PUMoriond17_80r2as_2016_TrancheIV_v6_ext1-v1/170207_122849/0000/tree_1.root'),
     maxEvents = cms.int32(-1),
     outputEvery = cms.uint32(100000)
 )
@@ -20,13 +19,13 @@ process.analyze_jetToTauFakeRate = cms.PSet(
 
     process = cms.string('ttH'),
 
-    era = cms.string('2015'),
+    era = cms.string('2016'),
 
-    triggers_1e = cms.vstring("HLT_BIT_HLT_Ele23_WPLoose_Gsf_v"),
+    triggers_1e = cms.vstring('HLT_BIT_HLT_Ele25_WPTight_Gsf_v', 'HLT_BIT_HLT_Ele27_eta2p1_WPLoose_Gsf_v'),
     use_triggers_1e = cms.bool(True),
-    triggers_1mu = cms.vstring("HLT_BIT_HLT_IsoMu20_v", "HLT_BIT_HLT_IsoTkMu20_v"),
+    triggers_1mu = cms.vstring('HLT_BIT_HLT_IsoMu22_v', 'HLT_BIT_HLT_IsoTkMu22_v'),
     use_triggers_1mu = cms.bool(True),
-    triggers_1e1mu = cms.vstring("HLT_BIT_HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v", "HLT_BIT_HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v"),
+    triggers_1e1mu = cms.vstring('HLT_BIT_HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v', 'HLT_BIT_HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v'),
     use_triggers_1e1mu = cms.bool(True),
 
     apply_offline_e_trigger_cuts_1e = cms.bool(True),
@@ -39,13 +38,14 @@ process.analyze_jetToTauFakeRate = cms.PSet(
     jet_maxPt = cms.double(1.e+6),
     jet_minAbsEta = cms.double(-1.),
     jet_maxAbsEta = cms.double(2.3),
-    
-    hadTauSelections = cms.vstring(
-        'dR05isoLoose',
-        'dR05isoMedium',
-        'dR05isoTight',
-        'dR03mvaVLoose',
-        'dR03mvaLoose',
+
+    hadTauSelection_denominator = cms.string('dR03mvaLoose'),
+    hadTauSelections_numerator = cms.vstring(
+        #'dR05isoLoose',
+        #'dR05isoMedium',
+        #'dR05isoTight',
+        #'dR03mvaVLoose',
+        #'dR03mvaLoose',
         'dR03mvaMedium',
         'dR03mvaTight',
         'dR03mvaVTight',
@@ -59,9 +59,22 @@ process.analyze_jetToTauFakeRate = cms.PSet(
     isMC = cms.bool(False),
     central_or_shift = cms.string('central'),
     lumiScale = cms.double(1.),
-    apply_trigger_bits = cms.bool(True),
+    apply_genWeight = cms.bool(True),
+    apply_trigger_bits = cms.bool(False),
 
     fillGenEvtHistograms = cms.bool(False),
+
+    branchName_electrons = cms.string('selLeptons'),
+    branchName_muons = cms.string('selLeptons'),
+    branchName_hadTaus = cms.string('TauGood'),
+    branchName_jets = cms.string('Jet'),
+    branchName_met = cms.string('met'),
+
+    branchName_genLeptons1 = cms.string('GenLep'),
+    branchName_genLeptons2 = cms.string('GenLepFromTau'),
+    branchName_genHadTaus = cms.string('GenHadTaus'),
+    branchName_genJets = cms.string('GenJet'),
+    redoGenMatching = cms.bool(True),
     
     selEventsFileName_input = cms.string(''),
     selEventsFileName_output = cms.string('')
