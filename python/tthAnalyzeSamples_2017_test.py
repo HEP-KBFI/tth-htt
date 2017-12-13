419121121e036390734aab8a466b12d272c41ae4
Karl Ehatäht <karlehataht@gmail.com>
2017-12-13 02:45:30 +0200

    Add 2017 python dictionaries for testing
    python/tthAnalyzeSamples_2017_test.py generated by running scripts/create_dictionary.py

--- python/tthAnalyzeSamples_2017_test.py
+++ python/tthAnalyzeSamples_2017_test.py
@@ -1,20 +1,49 @@
 from collections import OrderedDict as OD
 
-samples_2017 = OD()
+# file generated with the following command:
+# ./scripts/create_dictionary.py -v -m python/metaDict_2017_test.py -p /hdfs/local/karl/nanoAOD -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017
 
-# generated from file:
-# /hdfs/local/karl/store/mc/RunIISummer17MiniAOD/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/92X_upgrade2017_realistic_v10_ext1-v1/110000/187F7EDA-0986-E711-ABB3-02163E014C21.root
+samples_2017 = OD()
+samples_2017["/ttHToNonbb_M125_TuneCUETP8M2_13TeV-powheg-pythia8/RunIISummer17MiniAOD-NZSFlatPU28to62_92X_upgrade2017_realistic_v10-v1/MINIAODSIM"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "ttHToNonbb_M125"),
+  ("nof_files",                       1),
+  ("nof_events",                      -1),
+  ("nof_tree_events",                 8096),
+  ("nof_db_events",                   3999999),
+  ("use_HIP_mitigation_bTag",         False),
+  ("use_HIP_mitigation_mediumMuonId", False),
+  ("use_it",                          True),
+  ("xsection",                        0.2151),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("reHLT",                           True),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/local/karl/nanoAOD/92X_upgrade2017_realistic_v10_ext1-v1/ttHToNonbb_M125"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+])
 
 samples_2017["/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/MINIAODSIM"] = OD([
   ("type",                            "mc"),
   ("sample_category",                 "TT"),
   ("process_name_specific",           "TT"),
   ("nof_files",                       1),
-  ("nof_events",                      10000),
+  ("nof_events",                      -1),
+  ("nof_tree_events",                 10000),
+  ("nof_db_events",                   33844772),
   ("use_HIP_mitigation_bTag",         False),
   ("use_HIP_mitigation_mediumMuonId", False),
   ("use_it",                          True),
-  ("triggers",                        [ "1e", "1mu", "2e", "1e1mu", "2mu", "3e", "2e1mu", "1e2mu", "3mu", "1e1tau", "1mu1tau", "2tau" ]),
+  ("xsection",                        182.18),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
   ("reHLT",                           True),
   ("local_paths",
     [
@@ -27,27 +56,3 @@ samples_2017["/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer17MiniAOD-92X_u
   ),
 ])
 
-# generated from file:
-# /hdfs/local/karl/store/mc/RunIISummer17MiniAOD/ttHToNonbb_M125_TuneCUETP8M2_13TeV-powheg-pythia8/MINIAODSIM/NZSFlatPU28to62_92X_upgrade2017_realistic_v10-v1/150000/563AD7C5-25AF-E711-B84F-FA163EBF5DEF.root
-
-samples_2017["/ttHToNonbb_M125_TuneCUETP8M2_13TeV-powheg-pythia8/RunIISummer17MiniAOD-NZSFlatPU28to62_92X_upgrade2017_realistic_v10-v1/MINIAODSIM"] = OD([
-  ("type",                            "mc"),
-  ("sample_category",                 "signal"),
-  ("process_name_specific",           "ttHToNonbb_M125"),
-  ("nof_files",                       1),
-  ("nof_events",                      8096),
-  ("use_HIP_mitigation_bTag",         False),
-  ("use_HIP_mitigation_mediumMuonId", False),
-  ("use_it",                          True),
-  ("triggers",                        [ "1e", "1mu", "2e", "1e1mu", "2mu", "3e", "2e1mu", "1e2mu", "3mu", "1e1tau", "1mu1tau", "2tau" ]),
-  ("reHLT",                           True),
-  ("local_paths",
-    [
-      OD([
-        ("path",      "/hdfs/local/karl/nanoAOD/92X_upgrade2017_realistic_v10_ext1-v1/ttHToNonbb_M125"),
-        ("selection", "*"),
-        ("blacklist", []),
-      ]),
-    ]
-  ),
-])
