HEP-KBFI/tth-htt

commit 12fcdd0221994564be6f0a1ffbafbc8a1c78042a

Remove XGBReader
This is not needed since XGBInterface does exactly the same job. There is absolutely no reason to maintain two classes with the same functionality.

Also discovered a possible bug in HadTopTagger code: instead of mvaFileNameNoKinFit only mvaFileNameWithKinFit were read.

Fixed some syntax issues in python/xgb/applicationLoad.py.

src/HadTopTagger.cc
@@ -32,8 +32,8 @@ HadTopTagger::HadTopTagger(
   : kinFit_(0),
     mva_(0),
     mvaOutput_(-1.),
-    mva_hadTopTagger_xgb_(0),
-    mva_hadTopTagger_xgb_2_(0),
+    mva_hadTopTagger_xgb_withKinFit(0),
+    mva_hadTopTagger_xgb_noKinFit_(0),
     mva_hadTopTagger_tmva_(0)
 
 {
@@ -47,7 +47,7 @@ HadTopTagger::HadTopTagger(
   mvaInputsWithKinFitSort.push_back("pT_b_o_kinFit_pT_b");
   mvaInputsWithKinFitSort.push_back("pT_Wj2");
   mvaInputVariables_=mvaInputsWithKinFitSort;
-  mva_hadTopTagger_xgb_ = new XGBInterface(mvaFileNameWithKinFit,mvaInputsWithKinFitSort);
+  mva_hadTopTagger_xgb_withKinFit = new XGBInterface(mvaFileNameWithKinFit,mvaInputsWithKinFitSort);
   // order IS important - I will fix that
   // ['CSV_b', 'qg_Wj2', 'pT_bWj1Wj2', 'm_Wj1Wj2', 'nllKinFit', 'pT_b_o_kinFit_pT_b', 'pT_Wj2']
   //char* pklpath=(char*) mvaFileNameWithKinFitRead;
@@ -63,7 +63,7 @@ HadTopTagger::HadTopTagger(
   mvaFileNameNoKinFitSort.push_back("pT_bWj1Wj2");
   mvaFileNameNoKinFitSort.push_back("m_Wj1Wj2");
   mvaFileNameNoKinFitSort.push_back("pT_Wj2");
-  mva_hadTopTagger_xgb_2_ = new XGBInterface(mvaFileNameWithKinFit,mvaFileNameNoKinFitSort);
+  mva_hadTopTagger_xgb_noKinFit_ = new XGBInterface(mvaFileNameNoKinFit,mvaFileNameNoKinFitSort);
 
 }
 
@@ -71,8 +71,8 @@ HadTopTagger::~HadTopTagger()
 {
   delete kinFit_;
   delete mva_;
-  delete mva_hadTopTagger_xgb_;
-  delete mva_hadTopTagger_xgb_2_;
+  delete mva_hadTopTagger_xgb_withKinFit;
+  delete mva_hadTopTagger_xgb_noKinFit_;
   delete mva_hadTopTagger_tmva_;
 }
 
@@ -89,12 +89,7 @@ bool HadTopTagger::operator()(const RecoJet& recBJet, const RecoJet& recWJet1, c
   mvaInputsWithKinFit["nllKinFit"]              = kinFit_->nll();
   mvaInputsWithKinFit["pT_b_o_kinFit_pT_b"]     = recBJet.pt()/kinFit_->fittedBJet().pt();
   mvaInputsWithKinFit["pT_Wj2"]                 = recWJet2.pt();
-  // functuion way to read pkl - please leave this template here by now
-  //std::string mvaFileName_hadTopTaggerWithKinFitR = "all_HadTopTagger_sklearnV0o17o1_HypOpt_XGB_ntrees_1000_deph_3_lr_0o01_CSV_sort_withKinFit.pkl";
-  //const char* mvaFileNameWithKinFitRead= mvaFileName_hadTopTaggerWithKinFitR.c_str(); // to remove
-  //char* pklpathWithKinFit=(char*) mvaFileNameWithKinFitRead;
-  //double mvaOutputNoKinFit=XGBReader( mvaInputsWithKinFit , mvaInputsWithKinFitSort , pklpathWithKinFit );
-  double HTT_WithKin_xgb= (*mva_hadTopTagger_xgb_)(mvaInputsWithKinFit);
+  double HTT_WithKin_xgb= (*mva_hadTopTagger_xgb_withKinFit)(mvaInputsWithKinFit);
   result[0]=HTT_WithKin_xgb;
   //result[1]=mvaOutputNoKinFit;
 
@@ -110,7 +105,7 @@ bool HadTopTagger::operator()(const RecoJet& recBJet, const RecoJet& recWJet1, c
   mvaInputsNoKinFit["m_Wj1Wj2"]               = p4_Wj1Wj2.mass();
   mvaInputsNoKinFit["pT_Wj2"]                 = recWJet2.pt();
 
-  double HTT_NoKin_xgb=(*mva_hadTopTagger_xgb_2_)(mvaInputsNoKinFit);
+  double HTT_NoKin_xgb=(*mva_hadTopTagger_xgb_noKinFit_)(mvaInputsNoKinFit);
   result[2]=HTT_NoKin_xgb;
   return 1;
 }

bin/analyze_1l_2tau.cc
@@ -75,7 +75,6 @@
 #include "tthAnalysis/HiggsToTauTau/interface/hadTopTaggerAuxFunctions.h" // isGenMatchedJetTriplet
 #include "tthAnalysis/HiggsToTauTau/interface/TTreeWrapper.h" // TTreeWrapper
 
-#include "tthAnalysis/HiggsToTauTau/interface/XGBReader.h" // XGBReader
 #include "tthAnalysis/HiggsToTauTau/interface/GenParticle.h" // GenParticle
 #include "tthAnalysis/HiggsToTauTau/interface/GenParticleReader.h" // GenParticleReader
 #include "TLorentzVector.h"
@@ -1661,7 +1660,7 @@ int main(int argc, char* argv[])
     mvaInputs["dr_lep_tau_lead"]             = deltaR(selLepton->p4(), selHadTau_lead->p4());
     mvaInputs["costS_tau"]             = cosThetaS_hadTau;
     double mvaOutput_1l_2tau_ttbar=mva_1l_2tau_ttbar(mvaInputs);
-    //XGBReader(mvaInputs , mvaInputsSort , (char*) pklpath.c_str() );
+
     ////////////////////////////////////////////////////////////////////////
     std::map<std::string, double> mvaInputsHTTWithKinFit;
     mvaInputsHTTWithKinFit["avg_dr_jet"]                  = comp_avg_dr_jet(selJets);

interface/HadTopTagger.h
@@ -52,8 +52,8 @@ class HadTopTagger
   TMVAInterface* mva_;
   double mvaOutput_;
   //XGBInterface* xgb_;
-  XGBInterface* mva_hadTopTagger_xgb_;
-  XGBInterface* mva_hadTopTagger_xgb_2_;
+  XGBInterface* mva_hadTopTagger_xgb_withKinFit;
+  XGBInterface* mva_hadTopTagger_xgb_noKinFit_;
   TMVAInterface* mva_hadTopTagger_tmva_;
 
 

interface/XGBReader.h
@@ -1,4 +1,0 @@
-#include <vector> // std::vector<>
-#include <map>
-
-double XGBReader( std::map<std::string, double> map , std::vector<std::string> sort , char* pklpath );

python/xgb/applicationLoad.py
@@ -27,7 +27,7 @@ def load(pklfile):
   try:
     f = open(pklfile,'rb')
   except IOError as e:
-    print('Couldnt open or write to file (%s).' % e)
+    print('Couldnt open or read from file (%s).' % e)
   else:
       try:
         pkldata = pickle.load(f)
@@ -41,11 +41,14 @@ def load(pklfile):
   return pkldata
 
 def evaluate(vec, vec2, pkldata):
-	new_dict = collections.OrderedDict(itertools.izip(vec2,vec))
-	data = pandas.DataFrame(columns = list(new_dict.keys()))
-	data=data.append(new_dict, ignore_index = True)
-	result=-20
-	try: proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  )
-	except : print('Oops!',sys.exc_info()[0],'occured.')
-	else: result = proba[:,1][0]
-	return result
+  new_dict = collections.OrderedDict(itertools.izip(vec2,vec))
+  data = pandas.DataFrame(columns = list(new_dict.keys()))
+  data = data.append(new_dict, ignore_index = True)
+  result = -20
+  try:
+    proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  )
+  except:
+    print('Oops!',sys.exc_info()[0],'occured.')
+  else:
+    result = proba[:,1][0]
+  return result

src/XGBReader.cc
@@ -1,123 +1,0 @@
-#include <Python.h>
-#include <iostream> // std::cerr, std::fixed
-#include <iomanip> // std::setprecision(), std::setw()
-#include <string> // std::string
-#include <vector> // std::vector<>
-#include <map>
-#include "tthAnalysis/HiggsToTauTau/interface/mvaAuxFunctions.h" // check_mvaInputs
-
-// https://gist.github.com/rjzak/5681680
-PyObject* vectorToTuple_Float(const std::vector<float> &data) {
-  PyObject* tuple = PyTuple_New( data.size() );
-  if (!tuple) throw std::logic_error("Unable to allocate memory for Python tuple");
-  for (unsigned int i = 0; i < data.size(); i++) {
-    PyObject *num = PyFloat_FromDouble( (double) data[i]);
-    if (!num) {
-      Py_DECREF(tuple);
-      throw std::logic_error("Unable to allocate memory for Python tuple");
-    }
-    PyTuple_SET_ITEM(tuple, i, num);
-  }
-
-  return tuple;
-}
-
-PyObject* vectorToTuple_String(std::vector<std::basic_string<char>> &data) {
-  PyObject* tuple = PyTuple_New( data.size() );
-  if (!tuple) throw std::logic_error("Unable to allocate memory for Python tuple");
-  for (unsigned int i = 0; i < data.size(); i++) {
-    PyObject *num = PyString_FromString( (char*) data[i].c_str());
-    if (!num) {
-      Py_DECREF(tuple);
-      throw std::logic_error("Unable to allocate memory for Python tuple");
-    }
-    PyTuple_SET_ITEM(tuple, i, num);
-  }
-
-  return tuple;
-}
-
-double XGBReader( std::map<std::string, double> mvaInputs , std::vector<std::string>  sort , char* pklpath )
-{
-  setenv("OMP_NUM_THREADS", "1", 0);
-  double mvaOutput_=-20;
-    // here I can start the XGBReader
-  char* pkldir=(char*) "/home/acaan/VHbbNtuples_8_0_x/CMSSW_8_0_21/src/tthAnalysis/HiggsToTauTau/data/"; //
-  std::vector<float> vectorValuesVec;
-  std::vector<std::basic_string<char>> vectorNamesVec;
-  // prepare dictionary to pass to pyObject functions
-
-  //for ( std::vector<std::string>::const_iterator sorted = sort.begin(); sorted != sort.end(); ++sorted ) {
-  for ( unsigned int ii = 0 ; ii <  sort.size() ; ii++ ) {
-    double value = mvaInputs.find(sort[ii])->second;
-    vectorValuesVec.push_back(value);
-    std::basic_string<char> name =  mvaInputs.find(sort[ii])->first;
-    vectorNamesVec.push_back(name);
-    //vectorNamesVec.push_back((std::basic_string<char>) mvaInputs.find(sorted)->first);
-    //std::cout << " " << name << " : " << value << " "<< sort.size() <<std::endl;
-  }
-
-  //check_mvaInputs(mvaInputs);
-  // https://stackoverflow.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value
-  //std::cout << "Do python, HTT, size: "<<mvaInputs.size() << std::endl;
-  Py_SetProgramName((char*) "application");
-  PyObject *moduleMainString = PyString_FromString("__main__");
-  PyObject *moduleMain = PyImport_Import(moduleMainString);
-  // https://ubuntuforums.org/archive/index.php/t-324544.html
-  // https://stackoverflow.com/questions/4060221/how-to-reliably-open-a-file-in-the-same-directory-as-a-python-script
-  // https://gist.github.com/rjzak/5681680
-  // "print('The scikit-learn version is {}.'.format(sklearn.__file__))\n"
-  //"print('The pandas version is {}.'.format(pandas.__file__))\n" /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-scikit-learn/0.17.1-ikhhed/lib/python2.7/site-packages
-  PyRun_SimpleString(
-  "from time import time,ctime\n"
-  "import sys,os \n"
-  "sys.path.insert(0, '/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-scikit-learn/0.17.1-ikhhed/lib/python2.7/site-packages')\n"
-  "import sklearn\n"
-  "sys.path.insert(0, '/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-pandas/0.17.1-ikhhed/lib/python2.7/site-packages/pandas-0.17.1-py2.7-linux-x86_64.egg')\n"
-  "import pandas\n"
-  "import cPickle as pickle\n"
-  "import numpy as np \n"
-  "import subprocess \n"
-  "from itertools import izip \n"
-  "sys.path.insert(0, '/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/py2-pippkgs_depscipy/3.0-njopjo7/lib/python2.7/site-packages') \n"
-  "import xgboost as xgb  \n"
-  "from collections import OrderedDict \n"
-  "def mul(vec, vec2,pkldir,pklpath): \n"
-  "  #print 'Today is',ctime(time()), 'All python libraries we need loaded good  HTT'\n"
-  "  new_dict = OrderedDict(izip(vec2,vec)) \n"
-  "  #print (new_dict) \n"
-  "  data = pandas.DataFrame(columns=list(new_dict.keys())) \n"
-  "  data=data.append(new_dict, ignore_index=True) \n"
-  "  result=-20 \n"
-  "  f = None  \n"
-  "  try: \n"
-  "    f = open(pkldir+pklpath,'rb') \n"
-  "  except IOError as e: \n"
-  "    print('Couldnt open or write to file (%s).' % e) \n"
-  "  else: \n"
-  "      try: \n"
-  "        pkldata = pickle.load(f) \n"
-  "      except pickle.UnpicklingError as e: # normal, somewhat expected  \n"
-  "        model = pkldata.booster().get_dump() \n"
-  "      except (AttributeError,  EOFError, ImportError, IndexError) as e:  \n"
-  "        print(traceback.format_exc(e))  \n"
-  "      except Exception as e: print(traceback.format_exc(e))  \n"
-  "      else:  \n"
-  "        try: \n"
-  "          proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  ) \n"
-  "        except : \n"
-  "          print('Oops!',sys.exc_info()[0],'occured.') \n"
-  "        else:  \n"
-  "          result = proba[:,1][0]\n"
-  "      f.close()  \n"
-  "  return result                                \n"
-  );
-  PyObject *func = PyObject_GetAttrString(moduleMain, "mul");
-  PyObject* vectorValues=vectorToTuple_Float(vectorValuesVec);
-  PyObject* vecNames=vectorToTuple_String(vectorNamesVec);
-  PyObject *args = PyTuple_Pack(4, vectorValues, vecNames , PyString_FromString( (char*) pkldir), PyString_FromString( (char*) pklpath) ); //
-  PyObject *result = PyObject_CallObject(func, args);
-  mvaOutput_=PyFloat_AsDouble(result);
-  //delete *result;
-  return mvaOutput_;
-}

commit 6c2bbb5a2873b40086bfab247e920df39ab9d171

Fixed compilation error introduced in 12fcdd0221994564be6f0a1ffbafbc8a1c78042a

src/HadTopTagger.cc
@@ -1,6 +1,5 @@
 
 #include "tthAnalysis/HiggsToTauTau/interface/HadTopTagger.h" // HadTopTagger
-#include "tthAnalysis/HiggsToTauTau/interface/XGBReader.h" // XGBReader
 #include "FWCore/ParameterSet/interface/ParameterSet.h" // edm::ParameterSet
 #include "FWCore/PythonParameterSet/interface/MakeParameterSets.h" // edm::readPSetsFrom()
 #include "FWCore/Utilities/interface/Exception.h" // cms::Exception