7c955f0e3f1d41bee2085f497de79258bf7a86f6
Christian Veelken <christian.veelken@cern.ch>
2018-01-29 13:16:37 +0200

    removed config files for obsolete control region

--- python/tthAnalyzeSamples_Zctrl_2015.py
+++
@@ -1,7 +0,0 @@
-from collections import OrderedDict as OD
-
-from tthAnalyzeSamples_2015 import samples_2015
-
-samples_2015["/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM"]["sample_category"] = "DYJets"
-samples_2015["/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM"]["sample_category"] = "DYJets"
-
