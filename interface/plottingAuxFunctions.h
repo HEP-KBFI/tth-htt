c43c06b6644f1b47f22a5c38103e1554df454661
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 20:18:49 +0200

    Refactor plottingAuxFunctions
    - remove unnecessary inclusions
    - use forward declarations for the classes the pointer of which is used in the definition of the classes/structs
    - include C++ headers instead of C headers
    - use member initialization lists as much as possible
    - contemporize for-loops
    - free memory after regex matching is done
    - const-correctness
    - use nullptr instead of 0 (zero) when initializing a pointer
    - use boost::replace_all_copy() instead of TString member function ReplaceAll() to reduce the number of object castings while retaining the same results
    - code style

--- interface/plottingAuxFunctions.h
+++ interface/plottingAuxFunctions.h
@@ -1,24 +1,24 @@
 #ifndef tthAnalysis_HiggsToTauTau_plottingAuxFunctions_h
 #define tthAnalysis_HiggsToTauTau_plottingAuxFunctions_h
 
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include <FWCore/ParameterSet/interface/ParameterSet.h> // edm::ParameterSet, edm::VParameterSet
 
-#include <TH1.h>
-
-#include <string>
-#include <vector>
+// forward declarations
+class TH1;
+class TDirectory;
 
 struct histogramEntryType
 {
-  histogramEntryType(const std::string& process, TH1* histogram);
+  histogramEntryType(const std::string & process,
+                     TH1 * histogram);
   ~histogramEntryType() {}
   std::string process_;
-  TH1* histogram_;
+  TH1 * histogram_;
 };
 
 struct plotEntryType
 {
-  plotEntryType(const edm::ParameterSet& cfg);
+  plotEntryType(const edm::ParameterSet & cfg);
   ~plotEntryType() {}
   std::string histogramName_;
   std::string outputFileName_;
@@ -30,43 +30,64 @@ struct plotEntryType
   double yMax_;
   std::string yAxisTitle_;
   double yAxisOffset_;
-  typedef std::pair<double, double> pdouble; 
-  std::vector<pdouble> keepBlinded_;
+  std::vector<std::pair<double, double>> keepBlinded_;
   double legendTextSize_;
   double legendPosX_;
   double legendPosY_;
   double legendSizeX_;
   double legendSizeY_; 
 };
-  
+
 //-----------------------------------------------------------------------------
-void parseValue_and_Uncertainty(const std::string& valueErr_string, double& value, double& err);
+void
+parseValue_and_Uncertainty(const std::string & valueErr_string,
+                           double & value,
+                           double & err);
 
-TH1* getHistogram_wrapper(const TDirectory* dir, const std::string& process, const std::string& histogramName, const std::string& central_or_shift, bool enableException);
+TH1 *
+getHistogram_wrapper(const TDirectory * dir,
+                     const std::string & process,
+                     const std::string & histogramName,
+                     const std::string & central_or_shift,
+                     bool enableException);
 
-void printHistogram(const TH1* histogram);
+void
+printHistogram(const TH1 * histogram);
 
 struct HistogramManager
 {
-  HistogramManager(const std::vector<std::string>& processesBackground, const std::string& processSignal, const std::vector<std::string>& categories, const edm::ParameterSet& cfg);
-  ~HistogramManager();
-  
-  void setDirectory(TDirectory* dir);
-  void setCategory(const std::string& category);
-  void setHistogram(const std::string& histogramName);
-  
-  TH1* getHistogramPrefit(const std::string& process, bool enableException);
-  TH1* getHistogramPostfit(const std::string& process, bool enableException);
-  TH1* getHistogramUncertainty();
+  HistogramManager(const std::vector<std::string> & processesBackground,
+                   const std::string & processSignal,
+                   const std::vector<std::string> & categories,
+                   const edm::ParameterSet & cfg);
 
-  void getSF(const std::string& process, double& sf, double& sfErr);    
+  ~HistogramManager() {}
+
+  void setDirectory(TDirectory * dir);
+  void setCategory(const std::string & category);
+  void setHistogram(const std::string & histogramName);
+
+  TH1 *
+  getHistogramPrefit(const std::string & process,
+                     bool enableException);
+  TH1 *
+  getHistogramPostfit(const std::string & process,
+                      bool enableException);
+  TH1 *
+  getHistogramUncertainty();
+
+  void
+  getSF(const std::string & process,
+        double & sf,
+        double & sfErr) const;
   
   void update();
 
   typedef std::vector<std::string> vstring;
   vstring processes_;
+
   struct normEntryType
-  {     
+  {
     std::string process_;
     std::string category_;
     double sf_;
@@ -74,36 +95,42 @@ struct HistogramManager
   };
   typedef std::map<std::string, normEntryType> stringToNormEntryTypeMap;
   std::map<std::string, stringToNormEntryTypeMap> normalizationValues_and_Uncertainties_; // keys = process, category
-  
   vstring sysShifts_;
+
   struct shapeEntryType
-  {     
+  {
     std::string sysShift_;
     double value_;
     double err_;
   };
   std::map<std::string, shapeEntryType> shapeValues_and_Uncertainties_; // key = sysShift
-  
-  TDirectory* currentDir_;
-  
+
+  TDirectory * currentDir_;
   std::string currentCategory_;
-  
   std::string currentHistogramName_;
-  
+
   bool isUpToDate_;
-  
-  typedef std::map<std::string, TH1*> map_string_to_TH1;
+
+  typedef std::map<std::string, TH1 *> map_string_to_TH1;
   map_string_to_TH1 histograms_prefit_;  // key = process
   map_string_to_TH1 histograms_postfit_; // key = process
   std::map<std::string, map_string_to_TH1> histograms_sysShifts_; // key = process, sysShift
-  TH1* histogram_uncertainty_;
+  TH1 * histogram_uncertainty_;
 };
 //-----------------------------------------------------------------------------
 
-TH1* divideHistogramByBinWidth(TH1* histogram);
+TH1 *
+divideHistogramByBinWidth(TH1* histogram);
 
-double compYmaxForClearance(TH1* histogram, double legendPosX, double legendPosY, double labelPosY, bool useLogScale, double numOrdersOfMagnitude);
+double
+compYmaxForClearance(TH1 * histogram,
+                     double legendPosX,
+                     double legendPosY,
+                     double labelPosY,
+                     bool useLogScale,
+                     double numOrdersOfMagnitude);
 
-std::vector<plotEntryType*> readDistributions(const edm::VParameterSet& cfgDistributions);
+std::vector<plotEntryType *>
+readDistributions(const edm::VParameterSet & cfgDistributions);
 
 #endif
