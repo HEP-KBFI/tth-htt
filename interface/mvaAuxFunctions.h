5e9ebf7c9253aeca259d445a0f1624174e82904c
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 18:28:17 +0200

    Refactor mvaAuxFunctions
    - remove unnecessary header inclusions
    - contemporize the for-loops
    - code styling

--- interface/mvaAuxFunctions.h
+++ interface/mvaAuxFunctions.h
@@ -3,14 +3,18 @@
 
 #include "tthAnalysis/HiggsToTauTau/interface/EventInfo.h"
 
-#include <string> // std::string
-#include <vector> // std::vector<>
-#include <map> // std::map<>
+void
+check_mvaInputs(std::map<std::string, double> & mvaInputs,
+                UInt_t run = 0,
+                UInt_t lumi = 0,
+                ULong64_t event = 0);
 
-void check_mvaInputs(std::map<std::string, double>& mvaInputs, RUN_TYPE run = 0, LUMI_TYPE lumi = 0, EVT_TYPE event = 0);
+void
+check_mvaInputs(std::map<std::string, double> & mvaInputs,
+                const EventInfo & info);
 
-void check_mvaInputs(std::map<std::string, double>& mvaInputs, const EventInfo& info);
-
-std::vector<std::string> get_mvaInputVariables(const std::vector<std::string>& mvaInputVariables_ttV, const std::vector<std::string>& mvaInputVariables_ttbar);
+std::vector<std::string>
+get_mvaInputVariables(const std::vector<std::string> & mvaInputVariables_ttV,
+                      const std::vector<std::string> & mvaInputVariables_ttbar);
 
 #endif // tthAnalysis_HiggsToTauTau_mvaAuxFunctions_h
--- src/mvaAuxFunctions.cc
+++ src/mvaAuxFunctions.cc
@@ -1,38 +1,46 @@
 #include "tthAnalysis/HiggsToTauTau/interface/mvaAuxFunctions.h"
 
-#include <TMath.h> // IsNaN
+#include <TMath.h> // IsNaN()
 
-#include <iostream> // std::cerr, std::fixed
-#include <iomanip> // std::setprecision(), std::setw()
 #include <set> // std::set<>
 
-void check_mvaInputs(std::map<std::string, double>& mvaInputs, RUN_TYPE run, LUMI_TYPE lumi, EVT_TYPE event)
+void
+check_mvaInputs(std::map<std::string, double> & mvaInputs,
+                UInt_t run,
+                UInt_t lumi,
+                ULong64_t event)
 {
   int index = 1;
-  for ( std::map<std::string, double>::const_iterator mvaInput = mvaInputs.begin();
-	mvaInput != mvaInputs.end(); ++mvaInput ) {
-    if ( TMath::IsNaN(mvaInput->second) ) {
+  for(const auto & mvaInput: mvaInputs)
+  {
+    if(TMath::IsNaN(mvaInput.second))
+    {
       std::cout << "Warning";
-      if ( !(run == 0 && lumi == 0 && event == 0) ) {
-	std::cout << " in run = " << run << ", lumi = " << lumi << ", event = " << event;
+      if(! (run == 0 && lumi == 0 && event == 0))
+      {
+        std::cout << " in run = " << run << ", lumi = " << lumi << ", event = " << event;
       }
-      std::cout << ":" << std::endl; 
-      std::cout << " mvaInput #" << index << " ('" << mvaInput->first << "') = " << mvaInput->second << " --> setting mvaInput value to zero !!" << std::endl; 
-      mvaInputs[mvaInput->first] = 0.;
+      std::cout << ":\n mvaInput #" << index << " ('" << mvaInput.first << "') = "
+                << mvaInput.second << " --> setting mvaInput value to zero !!\n";
+      mvaInputs[mvaInput.first] = 0.;
       ++index;
     }
   }
 }
 
-void check_mvaInputs(std::map<std::string, double>& mvaInputs, const EventInfo& info)
+void
+check_mvaInputs(std::map<std::string, double> & mvaInputs,
+                const EventInfo & info)
 {
   return check_mvaInputs(mvaInputs, info.run, info.lumi, info.event);
 }
 
-std::vector<std::string> get_mvaInputVariables(const std::vector<std::string>& mvaInputVariables_ttV, const std::vector<std::string>& mvaInputVariables_ttbar)
+std::vector<std::string>
+get_mvaInputVariables(const std::vector<std::string> & mvaInputVariables_ttV,
+                      const std::vector<std::string> & mvaInputVariables_ttbar)
 {
   std::set<std::string> mvaInputVariables_set;
-  mvaInputVariables_set.insert(mvaInputVariables_ttV.begin(), mvaInputVariables_ttV.end());
+  mvaInputVariables_set.insert(mvaInputVariables_ttV.begin(),   mvaInputVariables_ttV.end());
   mvaInputVariables_set.insert(mvaInputVariables_ttbar.begin(), mvaInputVariables_ttbar.end());
   std::vector<std::string> mvaInputVariables(mvaInputVariables_set.begin(), mvaInputVariables_set.end());
   return mvaInputVariables;
