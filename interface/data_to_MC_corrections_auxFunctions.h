e2ea853577370e6ebe8f1399750db6f00f01ac05
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 19:37:39 +0200

    Common functions for channel-specific data2MC correction classes
    In future commits these functions are used across the channel-specific data-to-MC correction interface classes.

---
+++ interface/data_to_MC_corrections_auxFunctions.h
@@ -0,0 +1,48 @@
+#ifndef DATA_TO_MC_CORRECTIONS_AUXFUNCTIONS_H
+#define DATA_TO_MC_CORRECTIONS_AUXFUNCTIONS_H
+
+#include <string> // std::string
+#include <cmath> // std::min(), std::max()
+#include <vector> // std::vector<>
+#include <map> // std::map<>
+
+// forward declarations
+class lutWrapperBase;
+
+namespace aux
+{
+  std::string
+  getEtaBinLabel(double etaMin,
+                 double etaMax);
+
+  std::string
+  getHadTauIdxLabel(int idx);
+
+  std::string
+  getHadTauSelectionLabel(const std::string & hadTauSelection);
+
+  std::string
+  getHadTauDecayModeLabel(int hadTauDecayMode);
+
+  void
+  clearCollection(std::vector<lutWrapperBase *> & collection);
+
+  void
+  clearCollection(std::map<int, std::vector<lutWrapperBase *>> & collection);
+
+  double
+  get_from_lut(const std::map<int, std::vector<lutWrapperBase *>> & corrections,
+               double hadTau_pt,
+               double hadTau_eta,
+               int hadTau_decayMode,
+               bool isDEBUG);
+
+  double constexpr
+  compSF(double eff_data,
+         double eff_mc)
+  {
+    return std::min(eff_data / std::max(1.e-6, eff_mc), 1.e+1);
+  }
+}
+
+#endif // DATA_TO_MC_CORRECTIONS_AUXFUNCTIONS_H
---
+++ src/data_to_MC_corrections_auxFunctions.cc
@@ -0,0 +1,119 @@
+#include "tthAnalysis/HiggsToTauTau/interface/data_to_MC_corrections_auxFunctions.h"
+
+#include "tthAnalysis/HiggsToTauTau/interface/lutAuxFunctions.h" // lutWrapperBase, vLutWrapperBase
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
+
+#include <TString.h> // Form()
+
+#include <boost/algorithm/string/replace.hpp> // boost::replace_all_copy()
+
+#include <cassert> // assert()
+
+namespace aux
+{
+  std::string
+  getEtaBinLabel(double etaMin,
+                 double etaMax)
+  {
+    std::string etaBinLabel_tstring;
+    if(etaMin != -1. && etaMax != -1.)
+    {
+      etaBinLabel_tstring = Form("%1.4gto%1.4g", etaMin, etaMax);
+    }
+    else if(etaMin != -1.)
+    {
+      etaBinLabel_tstring = Form("Gt%1.4g", etaMin);
+    }
+    else if(etaMax != -1.)
+    {
+      etaBinLabel_tstring = Form("Lt%1.4g", etaMax);
+    }
+    else
+    {
+      assert(0);
+    }
+    const std::string etaBinLabel = boost::replace_all_copy(etaBinLabel_tstring, ".", "p");
+    return etaBinLabel;
+  }
+
+  std::string
+  getHadTauIdxLabel(int idx)
+  {
+    std::string hadTauIdxLabel;
+    switch(idx)
+    {
+      case 0:  hadTauIdxLabel = "_lead";    break;
+      case 1:  hadTauIdxLabel = "_sublead"; break;
+      case 2:  hadTauIdxLabel = "_third";   break;
+      default: assert(0);
+    }
+    return hadTauIdxLabel;
+  }
+
+  std::string
+  getHadTauSelectionLabel(const std::string & hadTauSelection)
+  {
+    std::string hadTauSelectionLabel;
+    if     (hadTauSelection == "dR03mvaVVLoose") hadTauSelectionLabel = "VLooseIso"; // CV: custom WP for which no trigger efficiency turn-on curve has been measured
+    else if(hadTauSelection == "dR03mvaVLoose" ) hadTauSelectionLabel = "VLooseIso";
+    else if(hadTauSelection == "dR03mvaLoose"  ) hadTauSelectionLabel = "LooseIso";
+    else if(hadTauSelection == "dR03mvaMedium" ) hadTauSelectionLabel = "MediumIso";
+    else if(hadTauSelection == "dR03mvaTight"  ) hadTauSelectionLabel = "TightIso";
+    else if(hadTauSelection == "dR03mvaVTight" ) hadTauSelectionLabel = "VTightIso";
+    else if(hadTauSelection == "dR03mvaVVTight") hadTauSelectionLabel = "VVTightIso";
+    else throw cmsException(__func__, __LINE__)
+           << "Invalid Configuration parameter 'hadTauSelection' = " << hadTauSelection;
+    return hadTauSelectionLabel;
+  }
+
+  std::string
+  getHadTauDecayModeLabel(int hadTauDecayMode)
+  {
+    std::string hadTauDecayModeLabel;
+    if     (hadTauDecayMode ==  0                        ) hadTauDecayModeLabel = "dm0";  // 1-prong without pi0s
+    else if(hadTauDecayMode ==  1 || hadTauDecayMode == 2) hadTauDecayModeLabel = "dm1";  // 1-prong with pi0s
+    else if(hadTauDecayMode == 10                        ) hadTauDecayModeLabel = "dm10"; // 3-prong
+    else throw cmsException(__func__, __LINE__)
+           << "Invalid Configuration parameter 'hadTauDecayMode' = " << hadTauDecayMode;
+    return hadTauDecayModeLabel;
+  }
+
+  void
+  clearCollection(std::vector<lutWrapperBase *> & collection)
+  {
+    for(lutWrapperBase * it: collection)
+    {
+      delete it;
+    }
+    collection.clear();
+  }
+
+  void
+  clearCollection(std::map<int, std::vector<lutWrapperBase *>> & collection)
+  {
+    for(auto & kv: collection)
+    {
+      for(lutWrapperBase * it: kv.second)
+      {
+        delete it;
+      }
+      kv.second.clear();
+    }
+  }
+
+  double
+  get_from_lut(const std::map<int, std::vector<lutWrapperBase *>> & corrections,
+               double hadTau_pt,
+               double hadTau_eta,
+               int hadTau_decayMode,
+               bool isDEBUG)
+  {
+    if(! corrections.count(hadTau_decayMode))
+    {
+      throw cmsException(__func__, __LINE__)
+              << "Invalid parameter 'hadTauDecayMode' = " << hadTau_decayMode;
+    }
+    const double sf = get_from_lut(corrections.at(hadTau_decayMode), hadTau_pt, hadTau_eta, isDEBUG);
+    return sf;
+  }
+}
