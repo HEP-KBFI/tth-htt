589a65582d7a6a49d13df4390db4add5318f71ee
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 19:04:26 +0200

    Refactor leptonTypes
    - use switch instead of the if-statement (supposedly compiler is better at optimizing switch blocks than if conditionals)
    - include C++ headers instead of the C headers
    - code styling

--- src/leptonTypes.cc
+++ src/leptonTypes.cc
@@ -1,14 +1,16 @@
 #include "tthAnalysis/HiggsToTauTau/interface/leptonTypes.h"
 
-#include <stdlib.h> // abs
-#include <assert.h> // assert
+#include <cmath> // std::abs()
+#include <cassert> // assert()
 
-int getLeptonType(int pdgId)
+int
+getLeptonType(int pdgId)
 {
-  int leptonType = -1;
-  int abs_pdgId = abs(pdgId);
-  if      ( abs_pdgId == 11 ) leptonType = kElectron;
-  else if ( abs_pdgId == 13 ) leptonType = kMuon;
-  else assert(0);
-  return leptonType;
+  const int abs_pdgId = std::abs(pdgId);
+  switch(abs_pdgId)
+  {
+    case 11: return kElectron;
+    case 13: return kMuon;
+    default: assert(0);
+  }
 }
