78411706aa6bb9451830c87596ee44b7df62063b
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 20:42:53 +0200

    Refactor generalAuxFunctions
    - remove unnecessary header inclusions
    - const-correctness
    - contemporize for-loops
    - code style

--- interface/generalAuxFunctions.h
+++ interface/generalAuxFunctions.h
@@ -3,15 +3,27 @@
 
 #include "FWCore/Utilities/interface/InputTag.h"
 
-#include <string>
-#include <vector>
+#include <vector> // std::vector<>
 
-std::string format_vstring(const std::vector<std::string>&);
-std::string format_vdouble(const std::vector<double>&);
-std::string format_vfloat(const std::vector<float>&);
-std::string format_vint(const std::vector<int>&);
-std::string format_vunsigned(const std::vector<unsigned>&);
-std::string format_vbool(const std::vector<bool>&);
-std::string format_vInputTag(const std::vector<edm::InputTag>&);
+std::string
+format_vstring(const std::vector<std::string>&);
+
+std::string
+format_vdouble(const std::vector<double>&);
+
+std::string
+format_vfloat(const std::vector<float>&);
+
+std::string
+format_vint(const std::vector<int>&);
+
+std::string
+format_vunsigned(const std::vector<unsigned>&);
+
+std::string
+format_vbool(const std::vector<bool>&);
+
+std::string
+format_vInputTag(const std::vector<edm::InputTag> &);
 
 #endif
--- src/generalAuxFunctions.cc
+++ src/generalAuxFunctions.cc
@@ -1,61 +1,71 @@
 #include "tthAnalysis/HiggsToTauTau/interface/generalAuxFunctions.h"
 
-#include <sstream>
+#include <sstream> // std::ostringstream
 
 template <typename T>
-std::string format_vT(const std::vector<T>& vT)
+std::string
+format_vT(const std::vector<T> & vT)
 {
   std::ostringstream os;
-  
+  const unsigned numEntries = vT.size();
+
   os << "{ ";
-
-  unsigned numEntries = vT.size();
-  for ( unsigned iEntry = 0; iEntry < numEntries; ++iEntry ) {
+  for(unsigned iEntry = 0; iEntry < numEntries; ++iEntry)
+  {
     os << vT[iEntry];
-    if ( iEntry < (numEntries - 1) ) os << ", ";
+    if(iEntry < numEntries - 1)
+    {
+      os << ", ";
+    }
   }
-
   os << " }";
-  
+
   return os.str();
 }
 
-std::string format_vstring(const std::vector<std::string>& vs)
+std::string
+format_vstring(const std::vector<std::string> & vs)
 {
   return format_vT(vs);
 }
 
-std::string format_vdouble(const std::vector<double>& vd)
+std::string
+format_vdouble(const std::vector<double> & vd)
 {
   return format_vT(vd);
 }
 
-std::string format_vfloat(const std::vector<float>& vf)
+std::string
+format_vfloat(const std::vector<float> & vf)
 {
   return format_vT(vf);
 }
 
-std::string format_vint(const std::vector<int>& vi)
+std::string
+format_vint(const std::vector<int> & vi)
 {
   return format_vT(vi);
 }
 
-std::string format_vunsigned(const std::vector<unsigned>& vu)
+std::string
+format_vunsigned(const std::vector<unsigned> & vu)
 {
   return format_vT(vu);
 }
 
-std::string format_vbool(const std::vector<bool>& vb)
+std::string
+format_vbool(const std::vector<bool> & vb)
 {
   return format_vT(vb);
 }
 
-std::string format_vInputTag(const std::vector<edm::InputTag>& vit)
+std::string
+format_vInputTag(const std::vector<edm::InputTag> & vit)
 {
   std::vector<std::string> vit_string;
-  for ( std::vector<edm::InputTag>::const_iterator vit_i = vit.begin();
-	vit_i != vit.end(); ++vit_i ) {
-    vit_string.push_back(vit_i->label());
+  for(const edm::InputTag & vit_i: vit)
+  {
+    vit_string.push_back(vit_i.label());
   }
   return format_vT(vit_string);
 }
