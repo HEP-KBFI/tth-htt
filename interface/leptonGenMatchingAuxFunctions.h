17b6225c7a9f69cb139b95f1e980175a89cda1c7
Karl Ehatäht <karlehataht@gmail.com>
2018-02-01 21:07:26 +0200

    Refactor leptonGenMatchingAuxFunctions
    - remove unnecessary header inclusions
    - use forward declaration
    - use member initialization lists as much as possible
    - contemporize for-loops
    - const-correctness
    - use nullptr instead of 0
    - remove << operator between string literals (they are concatenated anyways)
    - code style

--- interface/leptonGenMatchingAuxFunctions.h
+++ interface/leptonGenMatchingAuxFunctions.h
@@ -1,10 +1,12 @@
 #ifndef tthAnalysis_HiggsToTauTau_leptonGenMatchingAuxFunctions_h
 #define tthAnalysis_HiggsToTauTau_leptonGenMatchingAuxFunctions_h
 
-#include "tthAnalysis/HiggsToTauTau/interface/RecoLepton.h" // RecoLepton
+#include <string> // std::string
+#include <vector> // std::vector<>
+#include <iosfwd> // std::ostream
 
-#include <string>
-#include <vector>
+// forward declarations
+class RecoLepton;
 
 enum { kGen_LeptonUndefined1, kGen_LeptonAll1, kGen_1l0j, kGen_0l1j };
 enum { kGen_LeptonUndefined2, kGen_LeptonAll2, kGen_2l0j, kGen_1l1j, kGen_0l2j };
@@ -12,12 +14,10 @@ enum { kGen_LeptonUndefined3, kGen_LeptonAll3, kGen_3l0j, kGen_2l1j, kGen_1l2j, 
 
 struct leptonGenMatchEntry
 {
-  leptonGenMatchEntry(const std::string& name, int idx, int numGenMatchedLeptons, int numGenMatchedJets)
-    : name_(name),
-      idx_(idx),
-      numGenMatchedLeptons_(numGenMatchedLeptons),
-      numGenMatchedJets_(numGenMatchedJets)
-  {}
+  leptonGenMatchEntry(const std::string & name,
+                      int idx,
+                      int numGenMatchedLeptons,
+                      int numGenMatchedJets);
   ~leptonGenMatchEntry() {}
   std::string name_;
   int idx_;
@@ -29,13 +29,26 @@ std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_1lepton(bool appl
 std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_2lepton(bool apply_leptonGenMatching);
 std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_3lepton(bool apply_leptonGenMatching);
 
-std::string getLeptonGenMatch_string(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions, int leptonGenMatch_int);
-int getLeptonGenMatch_int(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions, const std::string& leptonGenMatch_string);
+std::string
+getLeptonGenMatch_string(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                         int leptonGenMatch_int);
 
-const leptonGenMatchEntry& getLeptonGenMatch(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions,
-					     const RecoLepton* lepton_lead, const RecoLepton* lepton_sublead = 0, const RecoLepton* lepton_third = 0);
+int
+getLeptonGenMatch_int(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                      const std::string & leptonGenMatch_string);
 
-std::ostream& operator<<(std::ostream& stream, const leptonGenMatchEntry& leptonGenMatch_definition);
-std::ostream& operator<<(std::ostream& stream, const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions);
+const leptonGenMatchEntry &
+getLeptonGenMatch(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                  const RecoLepton * lepton_lead,
+                  const RecoLepton * lepton_sublead = nullptr,
+                  const RecoLepton * lepton_third = nullptr);
+
+std::ostream &
+operator<<(std::ostream & stream,
+           const leptonGenMatchEntry & leptonGenMatch_definition);
+
+std::ostream &
+operator<<(std::ostream & stream,
+           const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions);
 
 #endif // tthAnalysis_HiggsToTauTau_leptonGenMatchingAuxFunctions_h
--- src/leptonGenMatchingAuxFunctions.cc
+++ src/leptonGenMatchingAuxFunctions.cc
@@ -1,132 +1,205 @@
 #include "tthAnalysis/HiggsToTauTau/interface/leptonGenMatchingAuxFunctions.h"
 
-#include "FWCore/Utilities/interface/Exception.h" // cms::Exception
+#include "tthAnalysis/HiggsToTauTau/interface/RecoLepton.h" // RecoLepton
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 
-#include <cmath>
+#include <iostream> // std::ostream
 
-std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_1lepton(bool apply_leptonGenMatching)
+leptonGenMatchEntry::leptonGenMatchEntry(const std::string & name,
+                                         int idx,
+                                         int numGenMatchedLeptons,
+                                         int numGenMatchedJets)
+  : name_(name)
+  , idx_(idx)
+  , numGenMatchedLeptons_(numGenMatchedLeptons)
+  , numGenMatchedJets_(numGenMatchedJets)
+{}
+
+std::vector<leptonGenMatchEntry>
+getLeptonGenMatch_definitions_1lepton(bool apply_leptonGenMatching)
 {
   std::vector<leptonGenMatchEntry> leptonGenMatch_definitions;
-  if ( apply_leptonGenMatching ) {
+  if(apply_leptonGenMatching)
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("1l0j", kGen_1l0j, 1, 0));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("0l1j", kGen_0l1j, 0, 1));
-  } else {
+  }
+  else
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("all", kGen_LeptonAll1, -1, -1));
   }
   return leptonGenMatch_definitions;
 }
 
-std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_2lepton(bool apply_leptonGenMatching)
+std::vector<leptonGenMatchEntry>
+getLeptonGenMatch_definitions_2lepton(bool apply_leptonGenMatching)
 {
   std::vector<leptonGenMatchEntry> leptonGenMatch_definitions;
-  if ( apply_leptonGenMatching ) {
+  if(apply_leptonGenMatching)
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("2l0j", kGen_2l0j, 2, 0));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("1l1j", kGen_1l1j, 1, 1));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("0l2j", kGen_0l2j, 0, 2));
-  } else {
+  }
+  else
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("all", kGen_LeptonAll2, -1, -1));
   }
   return leptonGenMatch_definitions;
 }
 
-std::vector<leptonGenMatchEntry> getLeptonGenMatch_definitions_3lepton(bool apply_leptonGenMatching)
+std::vector<leptonGenMatchEntry>
+getLeptonGenMatch_definitions_3lepton(bool apply_leptonGenMatching)
 {
   std::vector<leptonGenMatchEntry> leptonGenMatch_definitions;
-  if ( apply_leptonGenMatching ) {
+  if(apply_leptonGenMatching)
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("3l0j", kGen_3l0j, 3, 0));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("2l1j", kGen_2l1j, 2, 1));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("1l2j", kGen_1l2j, 1, 2));
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("0l3j", kGen_0l3j, 0, 3));
-  } else {
+  }
+  else
+  {
     leptonGenMatch_definitions.push_back(leptonGenMatchEntry("all", kGen_LeptonAll3, -1, -1));
   }
   return leptonGenMatch_definitions;
 }
 
-std::string getLeptonGenMatch_string(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions, int leptonGenMatch_int)
+std::string
+getLeptonGenMatch_string(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                         int leptonGenMatch_int)
 {
-  const leptonGenMatchEntry* leptonGenMatch = 0;
-  for ( std::vector<leptonGenMatchEntry>::const_iterator leptonGenMatch_definition = leptonGenMatch_definitions.begin();
-	leptonGenMatch_definition != leptonGenMatch_definitions.end(); ++leptonGenMatch_definition ) {
-    if ( leptonGenMatch_definition->idx_ == leptonGenMatch_int ) leptonGenMatch = &(*leptonGenMatch_definition);
+  const leptonGenMatchEntry * leptonGenMatch = nullptr;
+
+  for(const leptonGenMatchEntry & leptonGenMatch_definition: leptonGenMatch_definitions)
+  {
+    if(leptonGenMatch_definition.idx_ == leptonGenMatch_int)
+    {
+      leptonGenMatch = &leptonGenMatch_definition;
+    }
   }
-  if ( !leptonGenMatch ) throw cms::Exception("getLeptonGenMatch_string") 
-    << "Invalid parameter 'leptonGenMatch_int' = " << leptonGenMatch_int << " !!\n";
+  if(! leptonGenMatch)
+  {
+    throw cmsException(__func__)
+      << "Invalid parameter 'leptonGenMatch_int' = " << leptonGenMatch_int;
+  }
   return leptonGenMatch->name_;
 }
 
-int getLeptonGenMatch_int(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions, const std::string& leptonGenMatch_string)
+int
+getLeptonGenMatch_int(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                      const std::string & leptonGenMatch_string)
 {
-  const leptonGenMatchEntry* leptonGenMatch = 0;
-  for ( std::vector<leptonGenMatchEntry>::const_iterator leptonGenMatch_definition = leptonGenMatch_definitions.begin();
-	leptonGenMatch_definition!= leptonGenMatch_definitions.end(); ++leptonGenMatch_definition ) {
-    if ( leptonGenMatch_definition->name_ == leptonGenMatch_string ) leptonGenMatch = &(*leptonGenMatch_definition);
+  const leptonGenMatchEntry * leptonGenMatch = nullptr;
+  for(const leptonGenMatchEntry & leptonGenMatch_definition: leptonGenMatch_definitions)
+  {
+    if(leptonGenMatch_definition.name_ == leptonGenMatch_string)
+    {
+      leptonGenMatch = &leptonGenMatch_definition;
+    }
   }
-  if ( !leptonGenMatch ) throw cms::Exception("getHadTauGenMatch_int") 
-    << "Invalid parameter 'leptonGenMatch_string' = " << leptonGenMatch_string << " !!\n";
+  if(! leptonGenMatch)
+  {
+    throw cmsException(__func__)
+      << "Invalid parameter 'leptonGenMatch_string' = " << leptonGenMatch_string;
+  }
   return leptonGenMatch->idx_;
 }
 
 namespace
 {
-  void resetLeptonGenMatches(int& numGenMatchedLeptons, int& numGenMatchedJets)
+  void
+  resetLeptonGenMatches(int & numGenMatchedLeptons,
+                        int & numGenMatchedJets)
   {
     numGenMatchedLeptons = 0;
     numGenMatchedJets = 0;
   }
-  
-  void countLeptonGenMatches(const RecoLepton* lepton, int& numGenMatchedLeptons, int& numGenMatchedJets)
+
+  void
+  countLeptonGenMatches(const RecoLepton * lepton,
+                        int & numGenMatchedLeptons,
+                        int & numGenMatchedJets)
   {
-    if   ( lepton->genLepton() ) ++numGenMatchedLeptons;
-    else                         ++numGenMatchedJets;
+    if(lepton->genLepton())
+    {
+      ++numGenMatchedLeptons;
+    }
+    else
+    {
+      ++numGenMatchedJets;
+    }
   }
 
-  bool matches(int nSel, int nMatches)
+  bool
+  matches(int nSel,
+          int nMatches)
   {
-    if ( nSel == -1 || nSel == nMatches ) return true;
-    else return false;
-  } 
-  
-  const leptonGenMatchEntry& getLeptonGenMatch(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions,
-					       int numGenMatchedLeptons, int numGenMatchedJets)
+    return nSel == -1 || nSel == nMatches;
+  }
+
+  const leptonGenMatchEntry &
+  getLeptonGenMatch(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                    int numGenMatchedLeptons,
+                    int numGenMatchedJets)
   {
-    const leptonGenMatchEntry* leptonGenMatch = 0;
-    for ( std::vector<leptonGenMatchEntry>::const_iterator leptonGenMatch_definition = leptonGenMatch_definitions.begin();
-  	  leptonGenMatch_definition != leptonGenMatch_definitions.end(); ++leptonGenMatch_definition ) {
-      if ( matches(leptonGenMatch_definition->numGenMatchedLeptons_, numGenMatchedLeptons) &&
-	   matches(leptonGenMatch_definition->numGenMatchedJets_, numGenMatchedJets)       ) leptonGenMatch = &(*leptonGenMatch_definition);
+    const leptonGenMatchEntry * leptonGenMatch = nullptr;
+    for(const leptonGenMatchEntry & leptonGenMatch_definition: leptonGenMatch_definitions)
+    {
+      if(matches(leptonGenMatch_definition.numGenMatchedLeptons_, numGenMatchedLeptons) &&
+         matches(leptonGenMatch_definition.numGenMatchedJets_,    numGenMatchedJets)     )
+      {
+        leptonGenMatch = &leptonGenMatch_definition;
+      }
     }
-    if ( !leptonGenMatch ) throw cms::Exception("getLeptonGenMatch") 
-      << "Failed to compute 'leptonGenMatch' for numGenMatched:"
-      << " leptons = " << numGenMatchedLeptons << ","
-      << " jets = " << numGenMatchedJets << " !!\n";
+    if(! leptonGenMatch)
+      throw cmsException(__func__)
+        << "Failed to compute 'leptonGenMatch' for numGenMatched:"
+           " leptons = " << numGenMatchedLeptons << ","
+           " jets = "    << numGenMatchedJets;
     return *leptonGenMatch;
   }
 }
 
-const leptonGenMatchEntry& getLeptonGenMatch(const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions,
-					     const RecoLepton* lepton_lead, const RecoLepton* lepton_sublead, const RecoLepton* lepton_third)
+const leptonGenMatchEntry &
+getLeptonGenMatch(const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions,
+                  const RecoLepton * lepton_lead,
+                  const RecoLepton * lepton_sublead,
+                  const RecoLepton * lepton_third)
 {
   int numGenMatchedLeptons, numGenMatchedJets;
   resetLeptonGenMatches(numGenMatchedLeptons, numGenMatchedJets);
   assert(lepton_lead);
+
   countLeptonGenMatches(lepton_lead, numGenMatchedLeptons, numGenMatchedJets);
-  if ( lepton_sublead ) countLeptonGenMatches(lepton_sublead, numGenMatchedLeptons, numGenMatchedJets);
-  if ( lepton_third   ) countLeptonGenMatches(lepton_third, numGenMatchedLeptons, numGenMatchedJets);
+  if(lepton_sublead)
+  {
+    countLeptonGenMatches(lepton_sublead, numGenMatchedLeptons, numGenMatchedJets);
+  }
+  if(lepton_third)
+  {
+    countLeptonGenMatches(lepton_third, numGenMatchedLeptons, numGenMatchedJets);
+  }
   return getLeptonGenMatch(leptonGenMatch_definitions, numGenMatchedLeptons, numGenMatchedJets);
 }
 
-std::ostream& operator<<(std::ostream& stream, const leptonGenMatchEntry& leptonGenMatch_definition)\
+std::ostream &
+operator<<(std::ostream & stream,
+           const leptonGenMatchEntry & leptonGenMatch_definition)\
 {
-  stream << " leptonGenMatch #" << leptonGenMatch_definition.idx_ << ": " << leptonGenMatch_definition.name_ << std::endl;
+  stream << " leptonGenMatch #" << leptonGenMatch_definition.idx_
+         << ": "                << leptonGenMatch_definition.name_;
   return stream;
 }
 
-std::ostream& operator<<(std::ostream& stream, const std::vector<leptonGenMatchEntry>& leptonGenMatch_definitions)
+std::ostream &
+operator<<(std::ostream & stream,
+           const std::vector<leptonGenMatchEntry> & leptonGenMatch_definitions)
 {
-  for ( std::vector<leptonGenMatchEntry>::const_iterator leptonGenMatch_definition = leptonGenMatch_definitions.begin();
-	leptonGenMatch_definition != leptonGenMatch_definitions.end(); ++leptonGenMatch_definition ) {
-    stream << (*leptonGenMatch_definition);
+  for(const leptonGenMatchEntry & leptonGenMatch_definition: leptonGenMatch_definitions)
+  {
+    stream << leptonGenMatch_definition << '\n';
   }
   return stream;
 }
