0f49af73d616618bbbd3b674c425fa8bd7c8df5e
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 16:36:03 +0200

    Refactor EvtHistManager_ttWctrl
    - remove unncessary header inclusions
    - const-correctness
    - initialize era_ to -1 in the member initializer list
    - code style

--- interface/EvtHistManager_ttWctrl.h
+++ interface/EvtHistManager_ttWctrl.h
@@ -6,7 +6,7 @@
  * Book and fill histograms for event-level quantities in ttH, H->tautau analysis
  * in control region enriched in ttW background
  *
- * \author Christian Veelken, Tallin
+ * \author Christian Veelken, Tallinn
  *
  */
 
@@ -16,39 +16,51 @@ class EvtHistManager_ttWctrl
   : public HistManagerBase
 {
  public:
-  EvtHistManager_ttWctrl(edm::ParameterSet const& cfg);
+  EvtHistManager_ttWctrl(const edm::ParameterSet & cfg);
   ~EvtHistManager_ttWctrl() {}
 
   /// book and fill histograms
-  void bookHistograms(TFileDirectory& dir);
-  void fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium, 
-		      double mvaOutput_2lss_ttV, double mvaOutput_2lss_ttbar, double mvaDiscr_2lss, double mvaOutput_2lss_1tau_ttV, double mvaOutput_2lss_1tau_ttbar, 
-		      int sumLeptonCharge, double sumLeptonPt, double evtWeight);
+  void
+  bookHistograms(TFileDirectory & dir) override;
+
+  void
+  fillHistograms(int numElectrons,
+                 int numMuons,
+                 int numHadTaus,
+                 int numJets,
+                 int numBJets_loose,
+                 int numBJets_medium,
+                 double mvaOutput_2lss_ttV,
+                 double mvaOutput_2lss_ttbar,
+                 double mvaDiscr_2lss,
+                 double mvaOutput_2lss_1tau_ttV,
+                 double mvaOutput_2lss_1tau_ttbar,
+                 int sumLeptonCharge,
+                 double sumLeptonPt,
+                 double evtWeight);
 
  private:
   int era_;
 
-  TH1* histogram_numElectrons_;
-  TH1* histogram_numMuons_;
-  TH1* histogram_numLeptons_;
-  TH1* histogram_numHadTaus_;
-  TH1* histogram_numJets_;
-  TH1* histogram_numBJets_loose_;
-  TH1* histogram_numBJets_medium_;
+  TH1 * histogram_numElectrons_;
+  TH1 * histogram_numMuons_;
+  TH1 * histogram_numLeptons_;
+  TH1 * histogram_numHadTaus_;
+  TH1 * histogram_numJets_;
+  TH1 * histogram_numBJets_loose_;
+  TH1 * histogram_numBJets_medium_;
 
-  TH1* histogram_mvaOutput_2lss_ttV_;
-  TH1* histogram_mvaOutput_2lss_ttbar_;
-  TH1* histogram_mvaDiscr_2lss_;
+  TH1 * histogram_mvaOutput_2lss_ttV_;
+  TH1 * histogram_mvaOutput_2lss_ttbar_;
+  TH1 * histogram_mvaDiscr_2lss_;
 
-  TH1* histogram_mvaOutput_2lss_1tau_ttV_;
-  TH1* histogram_mvaOutput_2lss_1tau_ttbar_;
+  TH1 * histogram_mvaOutput_2lss_1tau_ttV_;
+  TH1 * histogram_mvaOutput_2lss_1tau_ttbar_;
 
-  TH1* histogram_sumLeptonCharge_;
-  TH1* histogram_sumLeptonPt_;
+  TH1 * histogram_sumLeptonCharge_;
+  TH1 * histogram_sumLeptonPt_;
 
-  TH1* histogram_EventCounter_;
-
-  std::vector<TH1*> histograms_;
+  TH1 * histogram_EventCounter_;
 };
 
 #endif
