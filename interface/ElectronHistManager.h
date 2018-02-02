069ea7f6f0b7026fda00d492a013ee8d84cd5b47
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 15:52:15 +0200

    Refactor ElectronHistManager
    - put the initialization of idx_ member variable to constructor's member initializer list
    - const-correctness
    - code style

--- interface/ElectronHistManager.h
+++ interface/ElectronHistManager.h
@@ -5,53 +5,58 @@
  *
  * Book and fill histograms for electrons in ttH, H->tautau analysis
  *
- * \author Christian Veelken, Tallin
+ * \author Christian Veelken, Tallinn
  *
  */
 
 #include "tthAnalysis/HiggsToTauTau/interface/HistManagerBase.h" // HistManagerBase
-#include "tthAnalysis/HiggsToTauTau/interface/RecoElectron.h"
+#include "tthAnalysis/HiggsToTauTau/interface/RecoElectron.h" // RecoElectron
 
 class ElectronHistManager
   : public HistManagerBase
 {
- public:
-  ElectronHistManager(edm::ParameterSet const& cfg);
+public:
+  ElectronHistManager(const edm::ParameterSet & cfg);
   ~ElectronHistManager() {}
 
   /// book and fill histograms
-  void bookHistograms(TFileDirectory& dir);
-  void fillHistograms(const RecoElectron& electron, double evtWeight);
-  void fillHistograms(const std::vector<const RecoElectron*>& electrons, double evtWeight);
+  void
+  bookHistograms(TFileDirectory & dir) override;
 
-  private:
-  TH1* histogram_pt_;
-  TH1* histogram_eta_;
-  TH1* histogram_phi_;
-  TH1* histogram_charge_;
+  void
+  fillHistograms(const RecoElectron & electron,
+                 double evtWeight);
 
-  TH1* histogram_dxy_;
-  TH1* histogram_dz_;     
-  TH1* histogram_relIso_;  
-  TH1* histogram_sip3d_;
-  TH1* histogram_mvaRawTTH_; 
-  TH1* histogram_jetPtRatio_;  
-  TH1* histogram_jetBtagCSV_; 
-  TH1* histogram_tightCharge_;      
-  TH1* histogram_mvaRawPOG_; 
-  TH1* histogram_mvaRawPOG_HZZ_; 
-  TH1* histogram_sigmaEtaEta_; 
-  TH1* histogram_HoE_;   
-  TH1* histogram_deltaEta_;  
-  TH1* histogram_deltaPhi_; 
-  TH1* histogram_OoEminusOoP_;
-  TH1* histogram_nLostHits_;  
-  TH1* histogram_passesConversionVeto_;
+  void
+  fillHistograms(const std::vector<const RecoElectron *> & electrons,
+                 double evtWeight);
 
-  TH1* histogram_abs_genPdgId_;
-  TH1* histogram_gen_times_recCharge_;
+private:
+  TH1 * histogram_pt_;
+  TH1 * histogram_eta_;
+  TH1 * histogram_phi_;
+  TH1 * histogram_charge_;
 
-  std::vector<TH1*> histograms_;
+  TH1 * histogram_dxy_;
+  TH1 * histogram_dz_;
+  TH1 * histogram_relIso_;
+  TH1 * histogram_sip3d_;
+  TH1 * histogram_mvaRawTTH_;
+  TH1 * histogram_jetPtRatio_;
+  TH1 * histogram_jetBtagCSV_;
+  TH1 * histogram_tightCharge_;
+  TH1 * histogram_mvaRawPOG_;
+  TH1 * histogram_mvaRawPOG_HZZ_;
+  TH1 * histogram_sigmaEtaEta_;
+  TH1 * histogram_HoE_;
+  TH1 * histogram_deltaEta_;
+  TH1 * histogram_deltaPhi_;
+  TH1 * histogram_OoEminusOoP_;
+  TH1 * histogram_nLostHits_;
+  TH1 * histogram_passesConversionVeto_;
+
+  TH1 * histogram_abs_genPdgId_;
+  TH1 * histogram_gen_times_recCharge_;
 
   int idx_; // flag to select leading or subleading electron (set idx to -1 to make plots for all electrons)
 };
--- src/ElectronHistManager.cc
+++ src/ElectronHistManager.cc
@@ -2,70 +2,74 @@
 
 #include "tthAnalysis/HiggsToTauTau/interface/histogramAuxFunctions.h"
 
-#include <TMath.h>
+#include <TMath.h> // TMath::Pi()
 
-ElectronHistManager::ElectronHistManager(const edm::ParameterSet& cfg)
+ElectronHistManager::ElectronHistManager(const edm::ParameterSet & cfg)
   : HistManagerBase(cfg)
-{
-  idx_ = cfg.getParameter<int>("idx");
-}
+  , idx_(cfg.getParameter<int>("idx"))
+{}
 
-void ElectronHistManager::bookHistograms(TFileDirectory& dir)
+void
+ElectronHistManager::bookHistograms(TFileDirectory & dir)
 {
-  histogram_pt_ = book1D(dir, "pt", "pt", 40, 0., 200.);
-  histogram_eta_ = book1D(dir, "eta", "eta", 50, -2.5, +2.5);
-  histogram_phi_ = book1D(dir, "phi", "phi", 36, -TMath::Pi(), +TMath::Pi());
+  histogram_pt_     = book1D(dir, "pt",     "pt",    40,  0., 200.);
+  histogram_eta_    = book1D(dir, "eta",    "eta",   50, -2.5, +2.5);
+  histogram_phi_    = book1D(dir, "phi",    "phi",   36, -TMath::Pi(), +TMath::Pi());
   histogram_charge_ = book1D(dir, "charge", "charge", 3, -1.5, +1.5);
 
-  histogram_dxy_ = book1D(dir, "dxy", "dxy", 40, -0.05, +0.05);
-  histogram_dz_ = book1D(dir, "dz", "dz", 40, -0.2, +0.2);
-  histogram_relIso_ = book1D(dir, "relIso", "relIso", 40, 0., 0.40);
-  histogram_sip3d_ = book1D(dir, "sip3d", "sip3d", 40, 0., 8.);
-  histogram_mvaRawTTH_ = book1D(dir, "mvaRawTTH", "mvaRawTTH", 40, -1., +1.);
-  histogram_jetPtRatio_ = book1D(dir, "jetPtRatio", "jetPtRatio", 24, 0., 1.2);
-  histogram_jetBtagCSV_ = book1D(dir, "jetBtagCSV", "jetBtagCSV", 40, 0., 1.);
-  histogram_tightCharge_ = book1D(dir, "tightCharge", "tightCharge", 3, -0.5, +2.5);      
-  histogram_mvaRawPOG_ = book1D(dir, "mvaRawPOG_GP", "mvaRawPOG_GP", 40, -1., +1.);
-  histogram_mvaRawPOG_HZZ_ = book1D(dir, "mvaRawPOG_HZZ", "mvaRawPOG_HZZ", 40, -1., +1.);
-  histogram_sigmaEtaEta_ = book1D(dir, "sigmaEtaEta", "sigmaEtaEta", 40, 0., 0.04); 
-  histogram_HoE_ = book1D(dir, "HoE", "HoE", 40, 0., 0.20);   
-  histogram_deltaEta_ = book1D(dir, "deltaEta", "deltaEta", 40, 0., 0.02);  
-  histogram_deltaPhi_ = book1D(dir, "deltaPhi", "deltaPhi", 40, 0., 0.10); 
-  histogram_OoEminusOoP_ = book1D(dir, "OoEminusOoP", "OoEminusOoP", 40, -0.05, +0.01);
-  histogram_nLostHits_ = book1D(dir, "nLostHits", "nLostHits", 2, -0.5, +1.5);  
-  histogram_passesConversionVeto_ = book1D(dir, "passesConversionVeto", "passesConversionVeto", 3, -0.5, +2.5);
+  histogram_dxy_                  = book1D(dir, "dxy",                  "dxy",                  40, -0.05, +0.05);
+  histogram_dz_                   = book1D(dir, "dz",                   "dz",                   40, -0.2,  +0.2);
+  histogram_relIso_               = book1D(dir, "relIso",               "relIso",               40,  0.,    0.40);
+  histogram_sip3d_                = book1D(dir, "sip3d",                "sip3d",                40,  0.,    8.);
+  histogram_mvaRawTTH_            = book1D(dir, "mvaRawTTH",            "mvaRawTTH",            40, -1.,   +1.);
+  histogram_jetPtRatio_           = book1D(dir, "jetPtRatio",           "jetPtRatio",           24,  0.,    1.2);
+  histogram_jetBtagCSV_           = book1D(dir, "jetBtagCSV",           "jetBtagCSV",           40,  0.,    1.);
+  histogram_tightCharge_          = book1D(dir, "tightCharge",          "tightCharge",           3, -0.5,  +2.5);
+  histogram_mvaRawPOG_            = book1D(dir, "mvaRawPOG_GP",         "mvaRawPOG_GP",         40, -1.,   +1.);
+  histogram_mvaRawPOG_HZZ_        = book1D(dir, "mvaRawPOG_HZZ",        "mvaRawPOG_HZZ",        40, -1.,   +1.);
+  histogram_sigmaEtaEta_          = book1D(dir, "sigmaEtaEta",          "sigmaEtaEta",          40,  0.,    0.04);
+  histogram_HoE_                  = book1D(dir, "HoE",                  "HoE",                  40,  0.,    0.20);
+  histogram_deltaEta_             = book1D(dir, "deltaEta",             "deltaEta",             40,  0.,    0.02);
+  histogram_deltaPhi_             = book1D(dir, "deltaPhi",             "deltaPhi",             40,  0.,    0.10);
+  histogram_OoEminusOoP_          = book1D(dir, "OoEminusOoP",          "OoEminusOoP",          40, -0.05, +0.01);
+  histogram_nLostHits_            = book1D(dir, "nLostHits",            "nLostHits",             2, -0.5,  +1.5);
+  histogram_passesConversionVeto_ = book1D(dir, "passesConversionVeto", "passesConversionVeto",  3, -0.5,  +2.5);
 
-  histogram_abs_genPdgId_ = book1D(dir, "abs_genPdgId", "abs_genPdgId", 22, -0.5, +21.5);
-  histogram_gen_times_recCharge_ = book1D(dir, "gen_times_recCharge", "gen_times_recCharge", 3, -1.5, +1.5);
+  histogram_abs_genPdgId_        = book1D(dir, "abs_genPdgId",        "abs_genPdgId",       22, -0.5, +21.5);
+  histogram_gen_times_recCharge_ = book1D(dir, "gen_times_recCharge", "gen_times_recCharge", 3, -1.5,  +1.5);
 }
 
-void ElectronHistManager::fillHistograms(const RecoElectron& electron, double evtWeight)
-{ 
-  double evtWeightErr = 0.;
- 
-  fillWithOverFlow(histogram_pt_, electron.pt(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_eta_, electron.eta(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_phi_, electron.phi(), evtWeight, evtWeightErr);
+void
+ElectronHistManager::fillHistograms(const RecoElectron & electron,
+                                    double evtWeight)
+{
+  const double evtWeightErr = 0.;
+
+  fillWithOverFlow(histogram_pt_,     electron.pt(),     evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_eta_,    electron.eta(),    evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_phi_,    electron.phi(),    evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_charge_, electron.charge(), evtWeight, evtWeightErr);
-    
-  fillWithOverFlow(histogram_dxy_, electron.dxy(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_dz_, electron.dz(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_relIso_, electron.relIso(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_sip3d_, electron.sip3d(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaRawTTH_, electron.mvaRawTTH(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_jetPtRatio_, electron.jetPtRatio(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_jetBtagCSV_, electron.jetBtagCSV(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_tightCharge_, electron.tightCharge(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaRawPOG_, electron.mvaRawPOG_GP(), evtWeight, evtWeightErr);
+
+  fillWithOverFlow(histogram_dxy_,           electron.dxy(),           evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_dz_,            electron.dz(),            evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_relIso_,        electron.relIso(),        evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_sip3d_,         electron.sip3d(),         evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaRawTTH_,     electron.mvaRawTTH(),     evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_jetPtRatio_,    electron.jetPtRatio(),    evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_jetBtagCSV_,    electron.jetBtagCSV(),    evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_tightCharge_,   electron.tightCharge(),   evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaRawPOG_,     electron.mvaRawPOG_GP(),  evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_mvaRawPOG_HZZ_, electron.mvaRawPOG_HZZ(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_sigmaEtaEta_, electron.sigmaEtaEta(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_HoE_, electron.HoE(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_deltaEta_, electron.deltaEta(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_deltaPhi_, electron.deltaPhi(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_OoEminusOoP_, electron.OoEminusOoP(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_nLostHits_, electron.nLostHits(), evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_passesConversionVeto_, static_cast<int>(electron.passesConversionVeto()), evtWeight, evtWeightErr);
-  
+  fillWithOverFlow(histogram_sigmaEtaEta_,   electron.sigmaEtaEta(),   evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_HoE_,           electron.HoE(),           evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_deltaEta_,      electron.deltaEta(),      evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_deltaPhi_,      electron.deltaPhi(),      evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_OoEminusOoP_,   electron.OoEminusOoP(),   evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_nLostHits_,     electron.nLostHits(),     evtWeight, evtWeightErr);
+
+  const int passesConversionVeto = static_cast<int>(electron.passesConversionVeto());
+  fillWithOverFlow(histogram_passesConversionVeto_, passesConversionVeto, evtWeight, evtWeightErr);
+
   int abs_genPdgId = 0;
   if      ( electron.genLepton() ) abs_genPdgId = std::abs(electron.genLepton()->pdgId()); // generator level match to electron or muon
   else if ( electron.genHadTau() ) abs_genPdgId = 15; // generator level match to hadronic tau decay 
@@ -77,13 +81,19 @@ void ElectronHistManager::fillHistograms(const RecoElectron& electron, double ev
   }
 }
 
-void ElectronHistManager::fillHistograms(const std::vector<const RecoElectron*>& electron_ptrs, double evtWeight)
-{    
-  size_t numElectrons = electron_ptrs.size();
-  for ( size_t idxElectron = 0; idxElectron < numElectrons; ++idxElectron ) {
-    const RecoElectron* electron = electron_ptrs[idxElectron];
-    if ( idx_ >= 0 && (int)idxElectron != idx_ ) continue;
-        fillHistograms(*electron, evtWeight);
+void
+ElectronHistManager::fillHistograms(const std::vector<const RecoElectron *> & electron_ptrs,
+                                    double evtWeight)
+{
+  const int numElectrons = electron_ptrs.size();
+  for(int idxElectron = 0; idxElectron < numElectrons; ++idxElectron)
+  {
+    const RecoElectron * electron = electron_ptrs[idxElectron];
+    if(idx_ >= 0 && idxElectron != idx_)
+    {
+      continue;
+    }
+    fillHistograms(*electron, evtWeight);
   }
 }
 
