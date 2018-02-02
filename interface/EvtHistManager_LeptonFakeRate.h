c924a578654fb8202b121bd5c26c7797e85953b2
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 16:46:45 +0200

    Refactor EvtHistManager_LeptonFakeRate
    - remove unnecessary header inclusions
    - const-correctness
    - code style

--- interface/EvtHistManager_LeptonFakeRate.h
+++ interface/EvtHistManager_LeptonFakeRate.h
@@ -6,45 +6,34 @@
  * Book and fill histograms for event-level quantities in the control region 
  * used to measure the Lepton fake-rate in the ttH, H->tautau analysis
  *
- * \author Ram Krishna Dewanjee, Tallin
+ * \author Ram Krishna Dewanjee, Tallinn
  *
  */
 
 #include "tthAnalysis/HiggsToTauTau/interface/HistManagerBase.h" // HistManagerBase
-#include "tthAnalysis/HiggsToTauTau/interface/RecoElectron.h"
-#include "tthAnalysis/HiggsToTauTau/interface/RecoMuon.h"
-#include "tthAnalysis/HiggsToTauTau/interface/RecoLepton.h"
-#include "tthAnalysis/HiggsToTauTau/interface/jetToTauFakeRateAuxFunctions.h" // getEtaBin, getPtBin
 
 class EvtHistManager_LeptonFakeRate
   : public HistManagerBase
 {
- public:
-  EvtHistManager_LeptonFakeRate(edm::ParameterSet const& cfg);
+public:
+  EvtHistManager_LeptonFakeRate(const edm::ParameterSet & cfg);
   ~EvtHistManager_LeptonFakeRate() {}
 
   /// book and fill histograms
-  void bookHistograms(TFileDirectory& dir);
-  // void fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium, double mLL, double mT_e, double mT_mu, double evtWeight);
-  void fillHistograms(double met, double mT_L, double mT_fix_L, double evtWeight);
+  void
+  bookHistograms(TFileDirectory & dir) override;
 
- private:
-  // TH1* histogram_numElectrons_;
-  // TH1* histogram_numMuons_;
-  // TH1* histogram_numHadTaus_;
-  // TH1* histogram_numJets_;
-  // TH1* histogram_numBJets_loose_;
-  // TH1* histogram_numBJets_medium_;
+  void
+  fillHistograms(double met,
+                 double mT_L,
+                 double mT_fix_L,
+                 double evtWeight);
 
-  // TH1* histogram_mLL_;
-  // TH1* histogram_mT_e_;
-  // TH1* histogram_mT_mu_;
-
-  TH1* histogram_mT_L_;
-  TH1* histogram_mT_fix_L_;
-  TH1* histogram_MET_;
-  TH1* histogram_EventCounter_;
-  // std::vector<TH1*> histograms_;
+private:
+  TH1 * histogram_mT_L_;
+  TH1 * histogram_mT_fix_L_;
+  TH1 * histogram_MET_;
+  TH1 * histogram_EventCounter_;
 };
 
 #endif
--- src/EvtHistManager_LeptonFakeRate.cc
+++ src/EvtHistManager_LeptonFakeRate.cc
@@ -1,37 +1,30 @@
 #include "tthAnalysis/HiggsToTauTau/interface/EvtHistManager_LeptonFakeRate.h"
 
-#include "tthAnalysis/HiggsToTauTau/interface/histogramAuxFunctions.h"
+#include "tthAnalysis/HiggsToTauTau/interface/histogramAuxFunctions.h" // fillWithOverFlow()
 
-#include <TMath.h>
-
-EvtHistManager_LeptonFakeRate::EvtHistManager_LeptonFakeRate(const edm::ParameterSet& cfg)
+EvtHistManager_LeptonFakeRate::EvtHistManager_LeptonFakeRate(const edm::ParameterSet & cfg)
   : HistManagerBase(cfg)
 {}
 
-void EvtHistManager_LeptonFakeRate::bookHistograms(TFileDirectory& dir)
+void
+EvtHistManager_LeptonFakeRate::bookHistograms(TFileDirectory & dir)
 {
-  // histogram_numElectrons_ = book1D(dir, "numElectrons", "numElectrons", 5, -0.5, +4.5);
-  // histogram_numMuons_ = book1D(dir, "numMuons", "numMuons", 5, -0.5, +4.5);
-  // histogram_numHadTaus_ = book1D(dir, "numHadTaus", "numHadTaus", 5, -0.5, +4.5);
-  // histogram_numJets_ = book1D(dir, "numJets", "numJets", 20, -0.5, +19.5);
-  // histogram_numBJets_loose_ = book1D(dir, "numBJets_loose", "numBJets_loose", 10, -0.5, +9.5);
-  // histogram_numBJets_medium_ = book1D(dir, "numBJets_medium", "numBJets_medium", 10, -0.5, +9.5);
-  // histogram_mLL_ = book1D(dir, "mLL", "mLL", 30, 60., 120.);
-  // histogram_mT_e_ = book1D(dir, "mT_e", "mT_e", 40, 0., 200.);
-  // histogram_mT_mu_ = book1D(dir, "mT_mu", "mT_mu", 40, 0., 200.);
-  histogram_mT_fix_L_ = book1D(dir, "mT_fix_L", "mT_fix_L", 40, 0., 200.);
-  histogram_mT_L_ = book1D(dir, "mT_L", "mT_L", 40, 0., 200.);
-  histogram_MET_ = book1D(dir, "MET", "MET", 40, 0., 200.);
-  histogram_EventCounter_ = book1D(dir, "EventCounter", "EventCounter", 1, -0.5, +0.5);
+  histogram_mT_fix_L_     = book1D(dir, "mT_fix_L",    "mT_fix_L",      40,  0.,  200.);
+  histogram_mT_L_         = book1D(dir, "mT_L",         "mT_L",         40,  0.,  200.);
+  histogram_MET_          = book1D(dir, "MET",          "MET",          40,  0.,  200.);
+  histogram_EventCounter_ = book1D(dir, "EventCounter", "EventCounter",  1, -0.5,  +0.5);
 }
 
-void EvtHistManager_LeptonFakeRate::fillHistograms(double met, double mT_L, double mT_fix_L, double evtWeight)
+void
+EvtHistManager_LeptonFakeRate::fillHistograms(double met,
+                                              double mT_L,
+                                              double mT_fix_L,
+                                              double evtWeight)
 {
-  double evtWeightErr = 0.;
-  fillWithOverFlow(histogram_MET_, met, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mT_fix_L_, mT_fix_L, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mT_L_, mT_L, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_EventCounter_, 0., evtWeight, evtWeightErr);
+  const double evtWeightErr = 0.;
+
+  fillWithOverFlow(histogram_MET_,          met,      evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mT_fix_L_,     mT_fix_L, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mT_L_,         mT_L,     evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_EventCounter_, 0.,       evtWeight, evtWeightErr);
 }
-
-
