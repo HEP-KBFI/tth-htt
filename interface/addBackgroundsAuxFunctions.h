082bfde5504ebdd7bb8895d58faaf0e37cad8851
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 21:26:05 +0200

    Cosmetics in addBackgroundAuxFunctions

--- interface/addBackgroundsAuxFunctions.h
+++ interface/addBackgroundsAuxFunctions.h
@@ -1,12 +1,16 @@
 #ifndef tthAnalysis_HiggsToTauTau_addBackgroundsAuxFunctions_h
 #define tthAnalysis_HiggsToTauTau_addBackgroundsAuxFunctions_h
 
-#include <TDirectory.h>
+#include <vector> // std::vector<>
+#include <string> // std::string
 
-#include <vector>
-#include <string>
+// forward declarations
+class TDirectory;
 
-std::vector<const TDirectory*> getSubdirectories(const TDirectory*);
-std::vector<std::string> getSubdirectoryNames(const TDirectory*);
+std::vector<const TDirectory *>
+getSubdirectories(const TDirectory *);
+
+std::vector<std::string>
+getSubdirectoryNames(const TDirectory *);
 
 #endif // tthAnalysis_HiggsToTauTau_addBackgroundsAuxFunctions_h
