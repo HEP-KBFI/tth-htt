f03c7a58875c698c48718907c0a4fc7ae28033f8
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 21:28:41 +0200

    Refactor addBackgroundAuxFunctions
    Forgot that I had an implementation file as well in commit https://github.com/HEP-KBFI/tth-htt/commit/082bfde5504ebdd7bb8895d58faaf0e37cad8851
    
    The changes:
    - forward declare TDirectory
    - contemporize for-loops
    - use nullptr instead of 0 when initializing an empty pointer
    - const-correctness
    - code style

--- src/addBackgroundsAuxFunctions.cc
+++ src/addBackgroundsAuxFunctions.cc
@@ -1,32 +1,37 @@
 #include "tthAnalysis/HiggsToTauTau/interface/addBackgroundsAuxFunctions.h"
 
-#include <TList.h>
-#include <TKey.h>
-#include <TObject.h>
+#include <TList.h> // TList
+#include <TKey.h> // TKey
+#include <TDirectory.h> // TDirectory
 
-#include <vector>
-
-std::vector<const TDirectory*> getSubdirectories(const TDirectory* dir)
+std::vector<const TDirectory *>
+getSubdirectories(const TDirectory * dir)
 {
-  std::vector<const TDirectory*> subdirectories;
-  TList* list = dir->GetListOfKeys();
+  std::vector<const TDirectory *> subdirectories;
+  TList * list = dir->GetListOfKeys();
   TIter next(list);
-  TKey* key = 0;
-  while ( (key = dynamic_cast<TKey*>(next())) ) {
-    TObject* object = key->ReadObj();
-    TDirectory* subdirectory = dynamic_cast<TDirectory*>(object);
-    if ( subdirectory ) subdirectories.push_back(subdirectory);
+  TKey * key = nullptr;
+
+  while((key = dynamic_cast<TKey*>(next())))
+  {
+    const TObject * object = key->ReadObj();
+    const TDirectory * subdirectory = dynamic_cast<const TDirectory *>(object);
+    if(subdirectory)
+    {
+      subdirectories.push_back(subdirectory);
+    }
   }
   return subdirectories;
 }
 
-std::vector<std::string> getSubdirectoryNames(const TDirectory* dir)
+std::vector<std::string>
+getSubdirectoryNames(const TDirectory * dir)
 {
-  std::vector<const TDirectory*> subdirectories = getSubdirectories(dir);
+  const std::vector<const TDirectory *> subdirectories = getSubdirectories(dir);
   std::vector<std::string> subdirectoryNames;
-  for ( std::vector<const TDirectory*>::const_iterator subdirectory = subdirectories.begin();
-	subdirectory != subdirectories.end(); ++subdirectory ) {
-    subdirectoryNames.push_back((*subdirectory)->GetName());
+  for(const TDirectory * subdirectory: subdirectories)
+  {
+    subdirectoryNames.push_back(subdirectory->GetName());
   }
   return subdirectoryNames;
 }
