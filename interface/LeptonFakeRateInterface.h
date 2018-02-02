52e399dbac89a5fa3c21e8c97ce030d3a4c51430
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 20:06:24 +0200

    Refactor LeptonFakeRateInterface
    - remove unnecessary header inclusions
    - use nullptr instead of 0 when initializing an empty pointer
    - const-correctness
    - contemporize for-loops
    - code style

--- interface/LeptonFakeRateInterface.h
+++ interface/LeptonFakeRateInterface.h
@@ -1,34 +1,45 @@
 #ifndef tthAnalysis_HiggsToTauTau_LeptonFakeRateInterface_h
 #define tthAnalysis_HiggsToTauTau_LeptonFakeRateInterface_h
 
-#include "FWCore/ParameterSet/interface/ParameterSet.h" // edm::ParameterSet
+#include <FWCore/ParameterSet/interface/ParameterSet.h> // edm::ParameterSet
 
-#include "tthAnalysis/HiggsToTauTau/interface/lutAuxFunctions.h" // lutWrapperBase
+#include <map> // std::map<,>
 
-#include <TFile.h> // TFile
+// forward declarations
+class lutWrapperBase;
+class TFile;
 
-#include <map>
-
-enum { kFRl_central, 
-       kFRe_shape_ptUp, kFRe_shape_ptDown, kFRe_shape_etaUp, kFRe_shape_etaDown, kFRe_shape_eta_barrelUp, kFRe_shape_eta_barrelDown,  
-       kFRm_shape_ptUp, kFRm_shape_ptDown, kFRm_shape_etaUp, kFRm_shape_etaDown };
+enum {
+  kFRl_central,
+  kFRe_shape_ptUp, kFRe_shape_ptDown,
+  kFRe_shape_etaUp, kFRe_shape_etaDown,
+  kFRe_shape_eta_barrelUp, kFRe_shape_eta_barrelDown,
+  kFRm_shape_ptUp, kFRm_shape_ptDown,
+  kFRm_shape_etaUp, kFRm_shape_etaDown
+};
 
 class LeptonFakeRateInterface
 {
- public:
-  LeptonFakeRateInterface(const edm::ParameterSet& cfg, int central_or_shift = kFRl_central);
+public:
+  LeptonFakeRateInterface(const edm::ParameterSet & cfg,
+                          int central_or_shift = kFRl_central);
   ~LeptonFakeRateInterface();
 
-  double getWeight_e(double electronPt, double electronAbsEta) const;
-  double getWeight_mu(double muonPt, double muonAbsEta) const;
-  
- private:
+  double
+  getWeight_e(double electronPt,
+              double electronAbsEta) const;
+
+  double
+  getWeight_mu(double muonPt,
+               double muonAbsEta) const;
+
+private:
   int central_or_shift_;
 
-  std::map<std::string, TFile*> inputFiles_;
-  
-  lutWrapperBase* lutFakeRate_e_;
-  lutWrapperBase* lutFakeRate_mu_;
+  std::map<std::string, TFile *> inputFiles_;
+
+  lutWrapperBase * lutFakeRate_e_;
+  lutWrapperBase * lutFakeRate_mu_;
 };
 
 #endif
--- src/LeptonFakeRateInterface.cc
+++ src/LeptonFakeRateInterface.cc
@@ -1,77 +1,99 @@
 #include "tthAnalysis/HiggsToTauTau/interface/LeptonFakeRateInterface.h"
 
-#include "FWCore/Utilities/interface/Exception.h" // cms::Exception
+#include "tthAnalysis/HiggsToTauTau/interface/lutAuxFunctions.h" // lutWrapperTH2
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 
-#include "tthAnalysis/HiggsToTauTau/interface/lutAuxFunctions.h" // openFile
+#include <TFile.h> // TFile
 
-LeptonFakeRateInterface::LeptonFakeRateInterface(const edm::ParameterSet& cfg, int central_or_shift)
+LeptonFakeRateInterface::LeptonFakeRateInterface(const edm::ParameterSet & cfg,
+                                                 int central_or_shift)
   : central_or_shift_(central_or_shift)
-  , lutFakeRate_e_(0)
-  , lutFakeRate_mu_(0)
+  , lutFakeRate_e_(nullptr)
+  , lutFakeRate_mu_(nullptr)
 {
-  std::string inputFileName = cfg.getParameter<std::string>("inputFileName");
-  std::string histogramName_e = cfg.getParameter<std::string>("histogramName_e");
-  std::string histogramName_mu = cfg.getParameter<std::string>("histogramName_mu");
-  lutFakeRate_e_ = new lutWrapperTH2(
-    inputFiles_, inputFileName, histogramName_e,
-    lut::kXptYabsEta, -1., 100.);
-  lutFakeRate_mu_ = new lutWrapperTH2(
-    inputFiles_, inputFileName, histogramName_mu,
-    lut::kXptYabsEta, -1., 100.);
+  const std::string inputFileName    = cfg.getParameter<std::string>("inputFileName");
+  const std::string histogramName_e  = cfg.getParameter<std::string>("histogramName_e");
+  const std::string histogramName_mu = cfg.getParameter<std::string>("histogramName_mu");
+  lutFakeRate_e_  = new lutWrapperTH2(inputFiles_, inputFileName, histogramName_e,  lut::kXptYabsEta, -1., 100.);
+  lutFakeRate_mu_ = new lutWrapperTH2(inputFiles_, inputFileName, histogramName_mu, lut::kXptYabsEta, -1., 100.);
 }
 
 LeptonFakeRateInterface::~LeptonFakeRateInterface()
 {
-  for ( std::map<std::string, TFile*>::iterator it = inputFiles_.begin();
-        it != inputFiles_.end(); ++it ) {
-    delete it->second;
+  for(auto & kv: inputFiles_)
+  {
+    delete kv.second;
   }
 }
 
-double LeptonFakeRateInterface::getWeight_e(double electronPt, double electronAbsEta) const
+double
+LeptonFakeRateInterface::getWeight_e(double electronPt,
+                                     double electronAbsEta) const
 {
   double weight = lutFakeRate_e_->getSF(electronPt, electronAbsEta);
-  if ( central_or_shift_ != kFRl_central ) {
-    if ( central_or_shift_ == kFRe_shape_ptUp ) {
-      if ( electronPt > 30. ) weight *= 1.4;
-      else weight *= 0.6;
-    } else if ( central_or_shift_ == kFRe_shape_ptDown ) {
-      if ( electronPt > 30. ) weight *= 0.6;
-      else weight *= 1.4;
-    } else if ( central_or_shift_ == kFRe_shape_etaUp ) {
-      if ( electronAbsEta < 1.479 ) weight *= 1.4;
-      else weight *= 0.6;
-    } else if ( central_or_shift_ == kFRe_shape_etaDown ) {
-      if ( electronAbsEta < 1.479 ) weight *= 0.6;
-      else weight *= 1.4;
-    } else if ( central_or_shift_ == kFRe_shape_eta_barrelUp ) {
-      if ( electronAbsEta < 0.8 ) weight *= 1.4;
-      else if ( electronAbsEta < 1.479 ) weight *= 0.6;
-    } else if ( central_or_shift_ == kFRe_shape_eta_barrelDown ) {
-      if ( electronAbsEta < 0.8 ) weight *= 0.6;
-      else if ( electronAbsEta < 1.479 ) weight *= 1.4;
-    } 
+  if(central_or_shift_ != kFRl_central)
+  {
+    if(central_or_shift_ == kFRe_shape_ptUp)
+    {
+      if(electronPt > 30.) weight *= 1.4;
+      else                 weight *= 0.6;
+    }
+    else if( central_or_shift_ == kFRe_shape_ptDown)
+    {
+      if(electronPt > 30.) weight *= 0.6;
+      else                 weight *= 1.4;
+    }
+    else if(central_or_shift_ == kFRe_shape_etaUp)
+    {
+      if(electronAbsEta < 1.479) weight *= 1.4;
+      else                       weight *= 0.6;
+    }
+    else if(central_or_shift_ == kFRe_shape_etaDown)
+    {
+      if(electronAbsEta < 1.479) weight *= 0.6;
+      else                       weight *= 1.4;
+    }
+    else if(central_or_shift_ == kFRe_shape_eta_barrelUp)
+    {
+      if     (electronAbsEta < 0.8)   weight *= 1.4;
+      else if(electronAbsEta < 1.479) weight *= 0.6;
+    }
+    else if(central_or_shift_ == kFRe_shape_eta_barrelDown)
+    {
+      if     (electronAbsEta < 0.8)   weight *= 0.6;
+      else if(electronAbsEta < 1.479) weight *= 1.4;
+    }
   }
   return weight;
 }
 
-double LeptonFakeRateInterface::getWeight_mu(double muonPt, double muonAbsEta) const
+double
+LeptonFakeRateInterface::getWeight_mu(double muonPt,
+                                      double muonAbsEta) const
 {
   double weight = lutFakeRate_mu_->getSF(muonPt, muonAbsEta);
-  if ( central_or_shift_ != kFRl_central ) {
-    if ( central_or_shift_ == kFRm_shape_ptUp ) {
-      if ( muonPt > 30. ) weight *= 1.4;
-      else weight *= 0.6;
-    } else if ( central_or_shift_ == kFRm_shape_ptDown ) {
-      if ( muonPt > 30. ) weight *= 0.6;
-      else weight *= 1.4;
-    } else if ( central_or_shift_ == kFRm_shape_etaUp ) {
-      if ( muonAbsEta < 1.479 ) weight *= 1.4;
-      else weight *= 0.6;
-    } else if ( central_or_shift_ == kFRm_shape_etaDown ) {
-      if ( muonAbsEta < 1.479 ) weight *= 0.6;
-      else weight *= 1.4;
-    } 
+  if(central_or_shift_ != kFRl_central)
+  {
+    if(central_or_shift_ == kFRm_shape_ptUp)
+    {
+      if(muonPt > 30.) weight *= 1.4;
+      else             weight *= 0.6;
+    }
+    else if(central_or_shift_ == kFRm_shape_ptDown)
+    {
+      if(muonPt > 30.) weight *= 0.6;
+      else             weight *= 1.4;
+    }
+    else if(central_or_shift_ == kFRm_shape_etaUp)
+    {
+      if(muonAbsEta < 1.479) weight *= 1.4;
+      else                   weight *= 0.6;
+    }
+    else if(central_or_shift_ == kFRm_shape_etaDown)
+    {
+      if(muonAbsEta < 1.479) weight *= 0.6;
+      else                   weight *= 1.4;
+    }
   }
   return weight;
 }
