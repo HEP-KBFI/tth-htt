54108ad61ddba1c03aa4d48f6da357674770f42a
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 13:53:39 +0200

    Refactor branchEntryType
    - use C++ headers instead of C headers
    - use override keyword for functions that override the pure virtual functions
    - const-correctness
    - when writing a single character to a stream, use char instead of string literal
    - code style

--- interface/branchEntryType.h
+++ interface/branchEntryType.h
@@ -1,127 +1,297 @@
 #ifndef tthAnalysis_HiggsToTauTau_branchEntryType_h
 #define tthAnalysis_HiggsToTauTau_branchEntryType_h
 
-#include "FWCore/Utilities/interface/Exception.h"
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 
-#include <Rtypes.h> // Float_t, Double_t, Int_t, UInt_t, ULong64_t, Char_t
-#include <TTree.h>
-#include <TTreeFormula.h>
-#include <TString.h>
-#include <TMath.h>
+#include <TTree.h> // TTree
+#include <TTreeFormula.h> // TTreeFormula
+#include <TString.h> // Form()
+#include <TMath.h> // TMath::Nint()
 
-#include <string>
-#include <vector>
-#include <iostream>
-#include <iomanip>
-#include <assert.h> // assert
+#include <cassert> // assert()
 
 struct branchEntryBaseType
 {
-  branchEntryBaseType(const std::string& inputBranchName, const std::string& inputBranchType, const std::string& outputBranchName, const std::string& outputBranchType, int idx = -1)
-    : inputBranchName_(inputBranchName),
-      inputBranchType_string_(inputBranchType),
-      idxColumn_(idx),
-      outputBranchName_(outputBranchName),
-      outputBranchType_string_(outputBranchType)
-  {
-    assert(inputBranchName_ != "");
-    if      ( inputBranchType == "F" ) inputBranchType_ = kF;
-    else if ( inputBranchType == "D" ) inputBranchType_ = kD;
-    else if ( inputBranchType == "I" ) inputBranchType_ = kI;
-    else if ( inputBranchType == "i" ) inputBranchType_ = kUI;
-    else if ( inputBranchType == "l" ) inputBranchType_ = kUL;
-    else if ( inputBranchType == "b" ) inputBranchType_ = kUC;
-    else if ( inputBranchType == "S" ) inputBranchType_ = kS;
-    else if ( inputBranchType == "s" ) inputBranchType_ = kUS;
-    else if ( inputBranchType == "O" ) inputBranchType_ = kB;
-    else throw cms::Exception("branchEntryBaseType") 
-      << "Invalid branch type = '" << inputBranchType << "' for branch = '" << inputBranchName << "' !!\n";
-    assert(outputBranchName_ != "");
-    if ( outputBranchType == "F" ) {
-      outputBranchType_ = kF;
-      outputBranchFormat_ = kScientific;
-      outputBranchPrecision_ = 6;
-    } else if ( outputBranchType == "D" ) {
-      outputBranchType_ = kD;
-      outputBranchFormat_ = kScientific;
-      outputBranchPrecision_ = 6;
-    } else if ( outputBranchType == "I" ) {
-      outputBranchType_ = kI;
-    } else if ( outputBranchType == "i" ) {
-      outputBranchType_ = kUI;
-    } else if ( outputBranchType == "l" ) {
-      outputBranchType_ = kUL;
-    } else if ( outputBranchType == "b" ) {
-      outputBranchType_ = kUC;
-    } else if ( outputBranchType == "S" ) {
-      outputBranchType_ = kS;
-    } else if ( outputBranchType == "s" ) {
-      outputBranchType_ = kUS;
-    } else if ( outputBranchType == "O" ) {
-      outputBranchType_ = kB;
-    } else throw cms::Exception("branchEntryBaseType")
-        << "Invalid branch type = '" << outputBranchType << "' for branch = '" << outputBranchName << "' !!\n";
-  }
+  enum { kF, kD, kI, kUI, kUL, kUC, kS, kUS, kB };
+  enum { kFixed, kScientific };
+
+  branchEntryBaseType(const std::string & inputBranchName,
+                      const std::string & inputBranchType,
+                      const std::string & outputBranchName,
+                      const std::string & outputBranchType,
+                      int idx = -1);
   virtual ~branchEntryBaseType() {}
-  virtual void setInputTree(TTree*) = 0;
-  virtual void setOutputTree(TTree*) = 0;
+
+  virtual void setInputTree(TTree *) = 0;
+  virtual void setOutputTree(TTree *) = 0;
+
   virtual void update() {}
   virtual void copyBranch() = 0;
-  virtual Float_t getValue_float(int idxElement = 0) const = 0;
+
+  virtual Float_t  getValue_float(int idxElement = 0) const = 0;
   virtual Double_t getValue_double(int idxElement = 0) const = 0;
-  virtual Int_t getValue_int(int idxElement = 0) const = 0;
+  virtual Int_t    getValue_int(int idxElement = 0) const = 0;
+
+  int idxColumn_;
+
   std::string inputBranchName_;
   std::string inputBranchType_string_;
-  enum { kF, kD, kI, kUI, kUL, kUC, kS, kUS, kB };
   int inputBranchType_;
-  int idxColumn_;
+
   std::string outputBranchName_;
   std::string outputBranchType_string_;
   int outputBranchType_;
-  enum { kFixed, kScientific };
+
   int outputBranchFormat_;
   int outputBranchPrecision_;
 };
 
 template <typename T1, typename T2>
-struct branchEntryType : branchEntryBaseType
+struct branchEntryType
+  : public branchEntryBaseType
 {
-  branchEntryType(const std::string& inputBranchName, const std::string& inputBranchType, const std::string& outputBranchName, const std::string& outputBranchType, int idx = -1)
+  branchEntryType(const std::string & inputBranchName,
+                  const std::string & inputBranchType,
+                  const std::string & outputBranchName,
+                  const std::string & outputBranchType,
+                  int idx = -1)
     : branchEntryBaseType(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx)
   {}
+
   ~branchEntryType() {}
-  void setInputTree(TTree* inputTree)
+
+  void
+  setInputTree(TTree * inputTree) override
   {
     inputTree->SetBranchAddress(inputBranchName_.data(), &inputValue_);
   }
-  void setOutputTree(TTree* outputTree)
+
+  void
+  setOutputTree(TTree * outputTree) override
   {
-    outputTree->Branch(outputBranchName_.data(), &outputValue_, Form("%s/%s", outputBranchName_.data(), outputBranchType_string_.data()));
+    outputTree->Branch(
+      outputBranchName_.data(), &outputValue_, Form("%s/%s", outputBranchName_.data(), outputBranchType_string_.data())
+    );
   }
-  void copyBranch()
+
+  void
+  copyBranch() override
   {
-    if ( inputBranchType_ == kF && outputBranchType_ == kI ) {
-      outputValue_ = TMath::Nint((Float_t)inputValue_);
-    } else if ( inputBranchType_ == kD && outputBranchType_ == kI ) {
-      outputValue_ = TMath::Nint((Double_t)inputValue_);
-    } else {
+    if(inputBranchType_ == kF && outputBranchType_ == kI)
+    {
+      outputValue_ = TMath::Nint(static_cast<Float_t>(inputValue_));
+    }
+    else if(inputBranchType_ == kD && outputBranchType_ == kI)
+    {
+      outputValue_ = TMath::Nint(static_cast<Double_t>(inputValue_));
+    }
+    else
+    {
       outputValue_ = inputValue_;
     }
   }
-  Float_t getValue_float(int idxElement = 0) const
+
+  Float_t
+  getValue_float(int idxElement = 0) const override
   {
     return outputValue_;
   }
-  Double_t getValue_double(int idxElement = 0) const
+
+  Double_t
+  getValue_double(int idxElement = 0) const override
   {
     return outputValue_;
   }
-  Int_t getValue_int(int idxElement = 0) const
+
+  Int_t
+  getValue_int(int idxElement = 0) const override
+  {
+    // Note that if T2 is Bool_t, then the function below will produce
+    // a compilation warning that's not suppressable via gcc diagnostic
+    // Also, RTTI is toothless here since it's not possible to partially
+    // enable/disable member functions at well
+    // And thirdly, the partial specialization of a template class is
+    // extremely complicated
+    // So we have to live with the warning messages for now and keep
+    // ourselves from not adding -Werror to the build flags
+    return TMath::Nint(outputValue_);
+  }
+
+  T1 inputValue_;
+  T2 outputValue_;
+};
+
+
+template <typename T>
+struct branchEntryFormulaType
+  : public branchEntryBaseType
+{
+  branchEntryFormulaType(const std::string & inputExpression,
+                         const std::string & outputBranchName,
+                         const std::string & outputBranchType,
+                         int idx = -1)
+    : branchEntryBaseType(inputExpression, "F", outputBranchName, outputBranchType, idx)
+    , inputExpression_string_(inputExpression)
+    , inputExpression_(nullptr)
+  {}
+
+  ~branchEntryFormulaType() 
+  {
+    delete inputExpression_;
+  }
+
+  void
+  setInputTree(TTree * inputTree) override
+  {
+    delete inputExpression_;
+    inputExpression_ = new TTreeFormula(
+      Form("branchEntryTypeFormula_%s", outputBranchName_.data()), inputExpression_string_.data(), inputTree
+    );
+  }
+
+  void
+  setOutputTree(TTree * outputTree) override
+  {
+    outputTree->Branch(
+      outputBranchName_.data(), &outputValue_, Form("%s/%s", outputBranchName_.data(), outputBranchType_string_.data())
+    );
+  }
+
+  void
+  update() override
+  {
+    inputExpression_->UpdateFormulaLeaves();
+  }
+
+  void
+  copyBranch() override
+  {
+    const Double_t inputValue = inputExpression_->EvalInstance();
+    outputValue_ = outputBranchType_ == kI ? TMath::Nint(inputValue) : inputValue;
+  }
+
+  Float_t
+  getValue_float(int idxElement = 0) const override
+  {
+    return outputValue_;
+  }
+
+  Double_t
+  getValue_double(int idxElement = 0) const override
+  {
+    return outputValue_;
+  }
+
+  Int_t
+  getValue_int(int idxElement = 0) const override
   {
     return TMath::Nint(outputValue_);
   }
-  T1 inputValue_;
-  T2 outputValue_;
+
+  std::string inputExpression_string_;
+  TTreeFormula * inputExpression_;
+  T outputValue_;
+};
+
+template <typename T1,
+          typename T2>
+struct branchEntryVType
+  : public branchEntryBaseType
+{
+  branchEntryVType(const branchEntryBaseType * branch_nElements,
+                   int max_nElements,
+                   const std::string & inputBranchName,
+                   const std::string & inputBranchType,
+                   const std::string & outputBranchName,
+                   const std::string & outputBranchType,
+                   int idx = -1)
+    : branchEntryBaseType(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx)
+    , branch_nElements_(branch_nElements)
+    , max_nElements_(max_nElements)
+  {
+    assert(branch_nElements_);
+    assert(max_nElements_ >= 1);
+    inputValues_  = new T1[max_nElements_];
+    outputValues_ = new T2[max_nElements_];
+  }
+
+  ~branchEntryVType() 
+  {
+    delete[] inputValues_;
+    delete[] outputValues_;
+  }
+
+  void
+  setInputTree(TTree * inputTree) override
+  {
+    inputTree->SetBranchAddress(inputBranchName_.data(), inputValues_);
+  }
+
+  void
+  setOutputTree(TTree * outputTree) override
+  {
+    outputTree->Branch(
+      outputBranchName_.data(), outputValues_,
+      Form("%s[%s]/%s", outputBranchName_.data(), branch_nElements_->outputBranchName_.data(), outputBranchType_string_.data())
+    );
+  }
+
+  void
+  copyBranch() override
+  {
+    assert(branch_nElements_);
+    // CV: branches of "simple" type need to be copied before branches of "vector" type,
+    //     to ensure that the branches containing the number of elements in the vectors are initialized before the vectors get copied
+    const_cast<branchEntryBaseType *>(branch_nElements_)->copyBranch();
+
+    const int numElements = branch_nElements_->getValue_int();
+    assert(numElements <= max_nElements_);
+
+    for(int idxElement = 0; idxElement < numElements; ++idxElement)
+    {
+      if(inputBranchType_ == kF && outputBranchType_ == kI)
+      {
+        outputValues_[idxElement] = TMath::Nint(static_cast<Float_t>(inputValues_[idxElement]));
+      }
+      else if(inputBranchType_ == kD && outputBranchType_ == kI)
+      {
+        outputValues_[idxElement] = TMath::Nint(static_cast<Double_t>(inputValues_[idxElement]));
+      }
+      else
+      {
+        outputValues_[idxElement] = inputValues_[idxElement];
+      }
+    }
+  }
+
+  Float_t
+  getValue_float(int idxElement = 0) const override
+  {
+    int numElements = branch_nElements_->getValue_int();
+    assert(idxElement < numElements);
+    return outputValues_[idxElement];
+  }
+
+  Double_t
+  getValue_double(int idxElement = 0) const override
+  {
+    int numElements = branch_nElements_->getValue_int();
+    assert(idxElement < numElements);
+    return outputValues_[idxElement];
+  }
+
+  Int_t
+  getValue_int(int idxElement = 0) const override
+  {
+    int numElements = branch_nElements_->getValue_int();
+    assert(idxElement < numElements);
+    return TMath::Nint(outputValues_[idxElement]);
+  }
+
+  const branchEntryBaseType * branch_nElements_;
+  int max_nElements_;
+  T1 * inputValues_;
+  T2 * outputValues_;
 };
 
 // no Long_t or Char_t b/c apparently these types depend on the machine
@@ -139,132 +309,17 @@ typedef branchEntryType<Short_t,     Short_t> branchEntryTypeSS;
 typedef branchEntryType<UShort_t,   UShort_t> branchEntryTypeUSUS;
 typedef branchEntryType<Bool_t,       Bool_t> branchEntryTypeBB;
 
-template <typename T>
-struct branchEntryFormulaType : branchEntryBaseType
-{
-  branchEntryFormulaType(const std::string& inputExpression, const std::string& outputBranchName, const std::string& outputBranchType, int idx = -1)
-    : branchEntryBaseType(inputExpression, "F", outputBranchName, outputBranchType, idx),
-      inputExpression_string_(inputExpression),
-      inputExpression_(0)
-  {}
-  ~branchEntryFormulaType() 
-  {
-    delete inputExpression_;
-  }
-  void setInputTree(TTree* inputTree)
-  {
-    delete inputExpression_;
-    inputExpression_ = new TTreeFormula(Form("branchEntryTypeFormula_%s", outputBranchName_.data()), inputExpression_string_.data(), inputTree);
-  }
-  void setOutputTree(TTree* outputTree)
-  {
-    outputTree->Branch(outputBranchName_.data(), &outputValue_, Form("%s/%s", outputBranchName_.data(), outputBranchType_string_.data()));
-  }
-  void update()
-  {
-    inputExpression_->UpdateFormulaLeaves();
-  }
-  void copyBranch()
-  {
-    Double_t inputValue = inputExpression_->EvalInstance();
-    if ( outputBranchType_ == kI ) {
-      outputValue_ = TMath::Nint(inputValue);
-    } else {
-      outputValue_ = inputValue;
-    }
-  }
-  Float_t getValue_float(int idxElement = 0) const
-  {
-    return outputValue_;
-  }
-  Double_t getValue_double(int idxElement = 0) const
-  {
-    return outputValue_;
-  }
-  Int_t getValue_int(int idxElement = 0) const
-  {
-    return TMath::Nint(outputValue_);
-  }
-  std::string inputExpression_string_;
-  TTreeFormula* inputExpression_;
-  T outputValue_;
-};
 typedef branchEntryFormulaType<Float_t>  branchEntryFormulaTypeF;
 typedef branchEntryFormulaType<Double_t> branchEntryFormulaTypeD;
 
-template <typename T1, typename T2>
-struct branchEntryVType : branchEntryBaseType
-{
-  branchEntryVType(const branchEntryBaseType* branch_nElements, int max_nElements,
-		   const std::string& inputBranchName, const std::string& inputBranchType, const std::string& outputBranchName, const std::string& outputBranchType, int idx = -1)
-    : branchEntryBaseType(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx)
-    , branch_nElements_(branch_nElements)
-    , max_nElements_(max_nElements)
-  {
-    assert(branch_nElements_);
-    assert(max_nElements_ >= 1);
-    inputValues_ = new T1[max_nElements_];
-    outputValues_ = new T2[max_nElements_];
-  }
-  ~branchEntryVType() 
-  {
-    delete[] inputValues_;    
-    delete[] outputValues_;
-  }
-  void setInputTree(TTree* inputTree)
-  {
-    inputTree->SetBranchAddress(inputBranchName_.data(), inputValues_);
-  }
-  void setOutputTree(TTree* outputTree)
-  {
-    outputTree->Branch(outputBranchName_.data(), outputValues_, 
-      Form("%s[%s]/%s", outputBranchName_.data(), branch_nElements_->outputBranchName_.data(), outputBranchType_string_.data()));
-  }
-  void copyBranch()
-  {
-    assert(branch_nElements_);
-    // CV: branches of "simple" type need to be copied before branches of "vector" type,
-    //     to ensure that the branches containing the number of elements in the vectors are initialized before the vectors get copied
-    (const_cast<branchEntryBaseType*>(branch_nElements_))->copyBranch();
-    int numElements = branch_nElements_->getValue_int();
-    assert(numElements <= max_nElements_);
-    for ( int idxElement = 0; idxElement < numElements; ++idxElement ) {
-      if ( inputBranchType_ == kF && outputBranchType_ == kI ) {
-	outputValues_[idxElement] = TMath::Nint((Float_t)inputValues_[idxElement]);
-      } else if ( inputBranchType_ == kD && outputBranchType_ == kI ) {
-	outputValues_[idxElement] = TMath::Nint((Double_t)inputValues_[idxElement]);
-      } else {
-	outputValues_[idxElement] = inputValues_[idxElement];
-      }
-    }
-  }
-  Float_t getValue_float(int idxElement = 0) const
-  {
-    int numElements = branch_nElements_->getValue_int();
-    assert(idxElement < numElements);
-    return outputValues_[idxElement];
-  }
-  Double_t getValue_double(int idxElement = 0) const
-  {
-    int numElements = branch_nElements_->getValue_int();
-    assert(idxElement < numElements);
-    return outputValues_[idxElement];
-  }
-  Int_t getValue_int(int idxElement = 0) const
-  {
-    int numElements = branch_nElements_->getValue_int();
-    assert(idxElement < numElements);
-    return TMath::Nint(outputValues_[idxElement]);
-  }
-  const branchEntryBaseType* branch_nElements_;
-  int max_nElements_;
-  T1* inputValues_;
-  T2* outputValues_;
-};
 typedef branchEntryVType<Float_t,   Float_t> branchEntryTypeVFVF;
 typedef branchEntryVType<Double_t, Double_t> branchEntryTypeVDVD;
 typedef branchEntryVType<Int_t,       Int_t> branchEntryTypeVIVI;
 
-branchEntryBaseType* addBranch(std::vector<branchEntryBaseType*>&, const std::string&, const std::string&, int = -1);
+branchEntryBaseType *
+addBranch(std::vector<branchEntryBaseType *> &,
+          const std::string &,
+          const std::string &,
+          int = -1);
 
 #endif
--- src/branchEntryType.cc
+++ src/branchEntryType.cc
@@ -1,37 +1,125 @@
 #include "tthAnalysis/HiggsToTauTau/interface/branchEntryType.h"
 
-branchEntryBaseType* addBranch(std::vector<branchEntryBaseType*>& branches, const std::string& outputBranchName, const std::string& inputBranchName_and_Type, int idx)
+branchEntryBaseType::branchEntryBaseType(const std::string & inputBranchName,
+                                         const std::string & inputBranchType,
+                                         const std::string & outputBranchName,
+                                         const std::string & outputBranchType,
+                                         int idx)
+  : idxColumn_(idx)
+  , inputBranchName_(inputBranchName)
+  , inputBranchType_string_(inputBranchType)
+  , outputBranchName_(outputBranchName)
+  , outputBranchType_string_(outputBranchType)
 {
-  size_t idx1 = inputBranchName_and_Type.find_last_of("/");
+  assert(! inputBranchName_.empty());
+
+  if     (inputBranchType == "F") inputBranchType_ = kF;
+  else if(inputBranchType == "D") inputBranchType_ = kD;
+  else if(inputBranchType == "I") inputBranchType_ = kI;
+  else if(inputBranchType == "i") inputBranchType_ = kUI;
+  else if(inputBranchType == "l") inputBranchType_ = kUL;
+  else if(inputBranchType == "b") inputBranchType_ = kUC;
+  else if(inputBranchType == "S") inputBranchType_ = kS;
+  else if(inputBranchType == "s") inputBranchType_ = kUS;
+  else if(inputBranchType == "O") inputBranchType_ = kB;
+  else throw cmsException(this)
+    << "Invalid input branch type = '" << inputBranchType
+    << "' for branch = '" << inputBranchName << '\'';
+
+  assert(! outputBranchName_.empty());
+  if(outputBranchType == "F")
+  {
+    outputBranchType_      = kF;
+    outputBranchFormat_    = kScientific;
+    outputBranchPrecision_ = 6;
+  }
+  else if(outputBranchType == "D")
+  {
+    outputBranchType_      = kD;
+    outputBranchFormat_    = kScientific;
+    outputBranchPrecision_ = 6;
+  }
+  else if(outputBranchType == "I")
+  {
+    outputBranchType_ = kI;
+  }
+  else if(outputBranchType == "i")
+  {
+    outputBranchType_ = kUI;
+  }
+  else if(outputBranchType == "l")
+  {
+    outputBranchType_ = kUL;
+  }
+  else if(outputBranchType == "b")
+  {
+    outputBranchType_ = kUC;
+  }
+  else if( outputBranchType == "S")
+  {
+    outputBranchType_ = kS;
+  }
+  else if(outputBranchType == "s")
+  {
+    outputBranchType_ = kUS;
+  }
+  else if(outputBranchType == "O")
+  {
+    outputBranchType_ = kB;
+  }
+  else
+  {
+    throw cmsException(this)
+      << "Invalid output branch type = '" << outputBranchType
+      << "' for branch = '" << outputBranchName << '\'';
+  }
+}
+
+branchEntryBaseType *
+addBranch(std::vector<branchEntryBaseType *> & branches,
+          const std::string & outputBranchName,
+          const std::string & inputBranchName_and_Type,
+          int idx)
+{
+  const std::size_t idx1 = inputBranchName_and_Type.find_last_of("/");
   std::string inputBranchName = "";
   std::string inputBranchType = "";
   std::string outputBranchType = "";
-  if ( idx1 != std::string::npos ) {
-    size_t idx2 = inputBranchName_and_Type.find_last_of("->");
-    if ( idx2 > idx1 && idx2 != std::string::npos ) {
-      inputBranchName = std::string(inputBranchName_and_Type, 0, idx1);
-      inputBranchType = std::string(inputBranchName_and_Type, idx1 + 1, (idx2 - 2) - idx1);
+
+  if(idx1 != std::string::npos)
+  {
+    const std::size_t idx2 = inputBranchName_and_Type.find_last_of("->");
+
+    if(idx2 > idx1 && idx2 != std::string::npos)
+    {
+      inputBranchName  = std::string(inputBranchName_and_Type, 0, idx1);
+      inputBranchType  = std::string(inputBranchName_and_Type, idx1 + 1, (idx2 - 2) - idx1);
       outputBranchType = std::string(inputBranchName_and_Type, idx2 + 1);
-    } else {
-      inputBranchName = std::string(inputBranchName_and_Type, 0, idx1);
-      inputBranchType = std::string(inputBranchName_and_Type, idx1 + 1);
+    }
+    else
+    {
+      inputBranchName  = std::string(inputBranchName_and_Type, 0, idx1);
+      inputBranchType  = std::string(inputBranchName_and_Type, idx1 + 1);
       outputBranchType = inputBranchType;
     }
   }
-  branchEntryBaseType* branch = 0;
-  if      ( inputBranchType == "F"       && outputBranchType == "F" ) branch = new branchEntryTypeFD(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "F"       && outputBranchType == "D" ) branch = new branchEntryTypeFD(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "F"       && outputBranchType == "I" ) branch = new branchEntryTypeFI(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "D"       && outputBranchType == "F" ) branch = new branchEntryTypeFD(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "D"       && outputBranchType == "D" ) branch = new branchEntryTypeFD(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "D"       && outputBranchType == "I" ) branch = new branchEntryTypeFI(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "I"       && outputBranchType == "I" ) branch = new branchEntryTypeII(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "i"       && outputBranchType == "i" ) branch = new branchEntryTypeUIUI(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "l"       && outputBranchType == "l" ) branch = new branchEntryTypeULUL(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "Formula" && outputBranchType == "F" ) branch = new branchEntryFormulaTypeF(inputBranchName, outputBranchName, outputBranchType, idx);
-  else if ( inputBranchType == "Formula" && outputBranchType == "D" ) branch = new branchEntryFormulaTypeD(inputBranchName, outputBranchName, outputBranchType, idx);
-  else throw cms::Exception("addBranch") 
-    << "Invalid branch declaration = '" << inputBranchName_and_Type << "' for branch = '" << outputBranchName << "' !!\n";
+
+  branchEntryBaseType * branch = nullptr;
+  if     (inputBranchType == "F"       && outputBranchType == "F") branch = new branchEntryTypeFD  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "F"       && outputBranchType == "D") branch = new branchEntryTypeFD  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "F"       && outputBranchType == "I") branch = new branchEntryTypeFI  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "D"       && outputBranchType == "F") branch = new branchEntryTypeFD  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "D"       && outputBranchType == "D") branch = new branchEntryTypeFD  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "D"       && outputBranchType == "I") branch = new branchEntryTypeFI  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "I"       && outputBranchType == "I") branch = new branchEntryTypeII  (inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "i"       && outputBranchType == "i") branch = new branchEntryTypeUIUI(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "l"       && outputBranchType == "l") branch = new branchEntryTypeULUL(inputBranchName, inputBranchType, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "Formula" && outputBranchType == "F") branch = new branchEntryFormulaTypeF(inputBranchName, outputBranchName, outputBranchType, idx);
+  else if(inputBranchType == "Formula" && outputBranchType == "D") branch = new branchEntryFormulaTypeD(inputBranchName, outputBranchName, outputBranchType, idx);
+  else throw cmsException(__func__, __LINE__)
+         << "Invalid branch declaration = '" << inputBranchName_and_Type
+         << "' for branch = '" << outputBranchName << '\'';
+
   branches.push_back(branch);
   return branch;
 }
