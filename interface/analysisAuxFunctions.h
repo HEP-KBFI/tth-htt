2321c5603f5cee901a25602b8f34c52c09f654a2
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 22:05:16 +0200

    Refactor analysisAuxFunctions
    - remove unnecessary header inclusions
    - forward declare: Particle, RecoHadTau, RecoJet, RecoLepton, RecoMuon, RecoElectron
    - const-correctness
    - use std::copy_n() function to copy the first N elements from a vector instead of writing an explicit for-loop for it
    - use switch statement on the selection variable to decide if a given lepton passes loose, fakeable or tight definition
    - define a common printCollection() template function that prints the contents of the vector holding instances of some particle objects, which is better than maintaining a separate function for each type of particle collection (the template function is specialized for the case when the vector holds RecoLepton instances which allows to print out electron- or muon-specific variables instead of the common RecoLepton variables)
    - contemporize for-loops
    - code style

--- interface/analysisAuxFunctions.h
+++ interface/analysisAuxFunctions.h
@@ -1,18 +1,22 @@
 #ifndef tthAnalysis_HiggsToTauTau_analysisAuxFunctions_h
 #define tthAnalysis_HiggsToTauTau_analysisAuxFunctions_h
 
-#include "DataFormats/Math/interface/deltaR.h" // deltaR
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 
-#include "tthAnalysis/HiggsToTauTau/interface/Particle.h" // Particle, Particle::LorentzVector
-#include "tthAnalysis/HiggsToTauTau/interface/RecoLepton.h" // RecoLepton
-#include "tthAnalysis/HiggsToTauTau/interface/RecoElectron.h" // RecoElectron
-#include "tthAnalysis/HiggsToTauTau/interface/RecoMuon.h" // RecoMuon
-#include "tthAnalysis/HiggsToTauTau/interface/RecoHadTau.h" // RecoHadTau
-#include "tthAnalysis/HiggsToTauTau/interface/RecoJet.h" // RecoJet
-#include "tthAnalysis/HiggsToTauTau/interface/RecoElectronCollectionSelectorTight.h" // RecoElectronSelectorTight
-#include "tthAnalysis/HiggsToTauTau/interface/RecoMuonCollectionSelectorTight.h" // RecoMuonSelectorTight
+#include <DataFormats/Math/interface/deltaR.h> // deltaR()
+#include <DataFormats/Math/interface/LorentzVector.h> // math::PtEtaPhiMLorentzVector
 
-#include <vector>
+#include <vector> // std::vector<>
+#include <map> // std::map<,>
+#include <algorithm> // std::copy_n()
+
+// forward declarations
+class Particle;
+class RecoLepton;
+class RecoJet;
+class RecoHadTau;
+class RecoMuon;
+class RecoElectron;
 
 //--- declare constants
 const double z_mass   = 91.1876;
@@ -21,14 +25,30 @@ const double met_coef =  0.00397;
 const double mht_coef =  0.00265;
 
 //--- declare data-taking periods
-enum { kEra_2017 };
+enum
+{
+  kEra_2017
+};
 
-//--- declare systematic uncertainties on data/MC corrections for b-tagging efficiency and mistag rates
-enum { kBtag_central,
-       kBtag_hfUp, kBtag_hfDown, kBtag_hfStats1Up, kBtag_hfStats1Down, kBtag_hfStats2Up, kBtag_hfStats2Down,
-       kBtag_lfUp, kBtag_lfDown, kBtag_lfStats1Up, kBtag_lfStats1Down, kBtag_lfStats2Up, kBtag_lfStats2Down,
-       kBtag_cErr1Up, kBtag_cErr1Down, kBtag_cErr2Up, kBtag_cErr2Down,
-       kBtag_jesUp, kBtag_jesDown };
+//--- declare systematic uncertainties on data/MC corrections for
+//    b-tagging efficiency and mistag rates
+enum
+{
+  kBtag_central,
+
+  kBtag_hfUp,       kBtag_hfDown,
+  kBtag_hfStats1Up, kBtag_hfStats1Down,
+  kBtag_hfStats2Up, kBtag_hfStats2Down,
+
+  kBtag_lfUp,       kBtag_lfDown,
+  kBtag_lfStats1Up, kBtag_lfStats1Down,
+  kBtag_lfStats2Up, kBtag_lfStats2Down,
+
+  kBtag_cErr1Up, kBtag_cErr1Down,
+  kBtag_cErr2Up, kBtag_cErr2Down,
+
+  kBtag_jesUp, kBtag_jesDown
+};
 
 //--- declare selection criteria for leptons and hadronic taus
 enum { kLoose, kFakeable, kTight };
@@ -46,11 +66,7 @@ selectObjects(int objectSelection,
     case kLoose:    return preselObjects;
     case kFakeable: return fakeableObjects;
     case kTight:    return tightObjects;
-    default:
-    {
-      std::cerr << "Invalid selection: " << objectSelection;
-      throw 1;
-    }
+    default:        throw cmsException(__func__) << "Invalid selection: " << objectSelection;
   }
 }
 
@@ -59,10 +75,6 @@ get_selection(const std::string & selectionString);
 
 int
 get_era(const std::string & eraString);
-
-//--- default branch fillers
-void setValue_int(Int_t* array, int n, Int_t value = 0);
-void setValue_float(Float_t* array, int n, Float_t value = 0.);
 
 //--- define the tau MVA ID WPs
 const std::map<std::string, int>
@@ -88,14 +100,18 @@ id_mva_dr05_map = {
  * @param Given pair of leptons
  * @return True, if first lepton has higher pT; false if second lepton has higher pT
  */
-bool isHigherPt(const Particle* particle1, const Particle* particle2);
+bool
+isHigherPt(const Particle * particle1,
+           const Particle * particle2);
 
 /**
  * @brief Auxiliary function used for sorting leptons by decreasing cone pT
  * @param Given pair of leptons
  * @return True, if first lepton has higher cone pT; false if second lepton has higher cone pT
  */
-bool isHigherConePt(const RecoLepton* particle1, const RecoLepton* particle2);
+bool
+isHigherConePt(const RecoLepton * particle1,
+               const RecoLepton * particle2);
 
 /**
  * @brief Auxiliary function for sorting a collection of RecoJet pointers
@@ -104,109 +120,136 @@ bool isHigherConePt(const RecoLepton* particle1, const RecoLepton* particle2);
  * @param jet2 The second jet
  * @return True, if the 1st jet has higher CSV score
  */
-bool isHigherCSV(const RecoJet* jet1, const RecoJet* jet2);
+bool
+isHigherCSV(const RecoJet * jet1,
+            const RecoJet * jet2);
 
 /**
- * @brief Auxiliary function for checking if leptons passing fake-able lepton selection pass tight lepton identification criteria also
+ * @brief Auxiliary function for checking if leptons passing fake-able lepton selection
+ *        pass tight lepton identification criteria also
  */
 template <typename Tfakeable, typename Ttight>
-bool isMatched(const Tfakeable& fakeableLepton, const std::vector<Ttight*>& tightLeptons, double dRmax = 1.e-2)
+bool
+isMatched(const Tfakeable & fakeableLepton,
+          const std::vector<Ttight *> & tightLeptons,
+          double dRmax = 1.e-2)
 {
-  for ( typename std::vector<const Ttight*>::const_iterator tightLepton = tightLeptons.begin();
-        tightLepton != tightLeptons.end(); ++tightLepton ) {
-    double dR = deltaR(fakeableLepton.eta(), fakeableLepton.phi(), (*tightLepton)->eta(), (*tightLepton)->phi());
-    if ( dR < dRmax ) return true; // found match
+  for(const Ttight * tightLepton: tightLeptons)
+  {
+    const double dR = deltaR(
+      fakeableLepton.eta(), fakeableLepton.phi(), tightLepton->eta(), tightLepton->phi()
+    );
+    if(dR < dRmax)
+    {
+      return true; // found match
+    }
   }
   return false; // no match found
 }
 
 /**
- * @brief Return branchName to read weights that need to be applied, per jet, to MC events in order to correct for data/MC differences in b-tagging efficiency and mistag rates
+ * @brief Return branchName to read weights that need to be applied, per jet, to MC events
+ *       in order to correct for data/MC differences in b-tagging efficiency and mistag rates
  */
-std::string getBranchName_bTagWeight(int era, const std::string& central_or_shift);
-std::string getBranchName_bTagWeight(int era, int central_or_shift);
+std::string
+getBranchName_bTagWeight(int era,
+                         const std::string & central_or_shift);
+
+std::string
+getBranchName_bTagWeight(int era,
+                         int central_or_shift);
 
 /**
  * @brief Return branch name to read MEt pt and phi
  */
-std::string getBranchName_MEt(int era,
-                              const std::string & defaul_branchName,
-                              int central_or_shift);
+std::string
+getBranchName_MEt(int era,
+                  const std::string & defaul_branchName,
+                  int central_or_shift);
 
 /**
- * @brief Return first N objects from collection given as function argument. In case the input collection contains fewer than N objects, the whole input collection is returned
+ * @brief Return first N objects from collection given as function argument. In case the input
+ *        collection contains fewer than N objects, the whole input collection is returned
  */
 template <typename T>
-std::vector<T> pickFirstNobjects(const std::vector<T>& objects_input, size_t N)
+std::vector<T>
+pickFirstNobjects(const std::vector<T> & objects_input,
+                  std::size_t N)
 {
+  const std::size_t N_input = std::min(objects_input.size(), N);
   std::vector<T> objects_output;
-  size_t N_input = objects_input.size();
-  for ( size_t idx = 0; idx < std::min(N_input, N); ++idx ) {
-    objects_output.push_back(objects_input[idx]);
-  }
+  std::copy_n(objects_input.begin(), N_input, std::back_inserter(objects_output));
   return objects_output;
 }
 
-int getHadTau_genPdgId(const RecoHadTau* hadTau);
+int
+getHadTau_genPdgId(const RecoHadTau * hadTau);
 
 /**
  * @brief Compute MHT
  */
-math::PtEtaPhiMLorentzVector compMHT(const std::vector<const RecoLepton*>& leptons, const std::vector<const RecoHadTau*>& hadTaus, const std::vector<const RecoJet*>& jets);
+math::PtEtaPhiMLorentzVector
+compMHT(const std::vector<const RecoLepton *> & leptons,
+        const std::vector<const RecoHadTau *> & hadTaus,
+        const std::vector<const RecoJet *> & jets);
 
 /**
  * @brief Compute linear discriminator based on MET and MHT
  */
-double compMEt_LD(const Particle::LorentzVector& met_p4, const Particle::LorentzVector& mht_p4);
+double
+compMEt_LD(const math::PtEtaPhiMLorentzVector & met_p4,
+           const math::PtEtaPhiMLorentzVector & mht_p4);
 
 /**
  * @brief Set flags indicating whether or not lepton passes loose, fakeable and/or tight selection criteria
  */
 template <typename T>
-void set_selection_flags(std::vector<const T*>& leptons, int selection)
+void
+set_selection_flags(std::vector<const T *> & leptons,
+                    int selection)
 {
-  for ( typename std::vector<const T*>::iterator lepton = leptons.begin();
-	lepton != leptons.end(); ++lepton ) {
-    if      ( selection == kLoose    ) (*lepton)->set_isLoose();
-    else if ( selection == kFakeable ) (*lepton)->set_isFakeable();
-    else if ( selection == kTight    ) (*lepton)->set_isTight();
-    else assert(0);
+  for(const T * lepton: leptons)
+  {
+    switch (selection) {
+      case kLoose:    lepton->set_isLoose();    break;
+      case kFakeable: lepton->set_isFakeable(); break;
+      case kTight:    lepton->set_isTight();    break;
+      default:        assert(0);
+    }
   }
 }
 
 /**
  * @brief Build collection of selected leptons by merging collections of selected electrons and selected muons
  */
-std::vector<const RecoLepton*> mergeLeptonCollections(const std::vector<const RecoElectron*>& electrons, const std::vector<const RecoMuon*>& muons);
+std::vector<const RecoLepton *>
+mergeLeptonCollections(const std::vector<const RecoElectron *> & electrons,
+                       const std::vector<const RecoMuon *> & muons);
 
-/**
- * @brief Print values of pT, eta, phi as well as ID and isolation variables for electrons and muons (for debugging purposes)
- */
-void printGenLeptonCollection(const std::string& collection_name, const std::vector<GenLepton>& genLeptons);
-void printLeptonCollection(const std::string& collection_name, const std::vector<const RecoLepton*>& leptons);
-
-/**
- * @brief Print values of pT, eta, phi as well as of tau ID/isolation variables for hadronic taus (for debugging purposes)
- */
-void printGenHadTauCollection(const std::string& collection_name, const std::vector<GenHadTau>& genHadTaus);
-void printHadTauCollection(const std::string& collection_name, const std::vector<const RecoHadTau*>& hadTaus);
-
-/**
- * @brief Print values of pT, eta, phi as well as the b-tagging discriminator values for jets (for debugging purposes)
- */
-void printGenJetCollection(const std::string& collection_name, const std::vector<GenJet>& genJets);
-void printJetCollection(const std::string& collection_name, const std::vector<const RecoJet*>& jets);
-
-/**
- * @brief Compute sign of value given as function parameter
- * @return +1 if val > 0, 0 if val = 0, -1 of val < 0
- */
-template <typename T>
-int sgn(T val)
+template <typename T,
+          typename = typename std::enable_if<! std::is_pointer<T>::value>>
+void
+printCollection(const std::string & collection_name,
+                const std::vector<T> & collection)
 {
-  if      ( val > 0 ) return +1;
-  else if ( val < 0 ) return -1;
-  else                return  0;
+  std::cout << " (#" << collection_name << " = " << collection.size() << ")\n";
+  for(std::size_t idx = 0; idx < collection.size(); ++idx)
+  {
+    std::cout << collection_name << "  #" << idx << ":\n" << collection[idx];
+  }
+}
+
+template <typename T,
+          typename = typename std::enable_if<! std::is_pointer<T>::value>>
+void
+printCollection(const std::string & collection_name,
+                const std::vector<const T *> & collection)
+{
+  std::cout << " (#" << collection_name << " = " << collection.size() << ")\n";
+  for(std::size_t idx = 0; idx < collection.size(); ++idx)
+  {
+    std::cout << collection_name << "  #" << idx << ":\n" << *(collection[idx]);
+  }
 }
 
 /**
--- src/analysisAuxFunctions.cc
+++ src/analysisAuxFunctions.cc
@@ -1,26 +1,31 @@
 #include "tthAnalysis/HiggsToTauTau/interface/analysisAuxFunctions.h"
 
 #include "tthAnalysis/HiggsToTauTau/interface/RecoMEtReader.h" // RecoMEtReader::kMEt_*
+#include "tthAnalysis/HiggsToTauTau/interface/RecoElectron.h" // RecoElectron
+#include "tthAnalysis/HiggsToTauTau/interface/RecoMuon.h" // RecoMuon
+#include "tthAnalysis/HiggsToTauTau/interface/RecoHadTau.h" // RecoHadTau
+#include "tthAnalysis/HiggsToTauTau/interface/RecoJet.h" // RecoJet
+#include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 
-#include <TRandom3.h> // TRandom3
-#include <TMath.h> // TMath::Nint()
-
-#include <map> // std::map
-#include <algorithm> // std::sort
-
-bool isHigherPt(const Particle* particle1, const Particle* particle2)
+bool
+isHigherPt(const Particle * particle1,
+           const Particle * particle2)
 {
-  return (particle1->pt() > particle2->pt());
+  return particle1->pt() > particle2->pt();
 }
 
-bool isHigherConePt(const RecoLepton* particle1, const RecoLepton* particle2)
+bool
+isHigherConePt(const RecoLepton * particle1,
+               const RecoLepton * particle2)
 {
   return (particle1->cone_pt() > particle2->cone_pt());
 }
 
-bool isHigherCSV(const RecoJet* jet1, const RecoJet* jet2)
+bool
+isHigherCSV(const RecoJet * jet1,
+            const RecoJet * jet2)
 {
-  return (jet1 -> BtagCSV()) > (jet2 -> BtagCSV());
+  return jet1->BtagCSV() > jet2->BtagCSV();
 }
 
 int
@@ -29,59 +34,53 @@ get_selection(const std::string & selectionString)
   if(selectionString == "Loose")    return kLoose;
   if(selectionString == "Fakeable") return kFakeable;
   if(selectionString == "Tight")    return kTight;
-  std::cerr << "Invalid Configuration parameter for object selection = " << selectionString << " !!\n";
-  throw 1;
+  throw cmsException(__func__) << "Invalid Configuration parameter for object selection = " << selectionString;
 }
 
 int
 get_era(const std::string & eraString)
 {
-  if(eraString == "2017") return kEra_2017;
-  std::cerr  << "Invalid Configuration parameter 'era' = " << eraString << " !!\n";
-  throw 1;
+  if(eraString == "2017")
+  {
+    return kEra_2017;
+  }
+  throw cmsException(__func__) << "Invalid Configuration parameter 'era' = " << eraString;
 }
 
-void setValue_int(Int_t* array, int n, Int_t value)
-{
-  for ( int idx = 0; idx < n; ++idx ) {
-    array[idx] = value;
-  }
-}
-void setValue_float(Float_t* array, int n, Float_t value)
-{
-  for ( int idx = 0; idx < n; ++idx ) {
-    array[idx] = value;
-  }
-}
 
-std::string getBranchName_bTagWeight(int era, const std::string& central_or_shift)
+std::string
+getBranchName_bTagWeight(int era,
+                         const std::string & central_or_shift)
 {
   int central_or_shift_int = kBtag_central;
-  if      ( central_or_shift == "CMS_ttHl_btag_HFUp"         ) central_or_shift_int = kBtag_hfUp;
-  else if ( central_or_shift == "CMS_ttHl_btag_HFDown"       ) central_or_shift_int = kBtag_hfDown;
-  else if ( central_or_shift == "CMS_ttHl_btag_HFStats1Up"   ) central_or_shift_int = kBtag_hfStats1Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_HFStats1Down" ) central_or_shift_int = kBtag_hfStats1Down;
-  else if ( central_or_shift == "CMS_ttHl_btag_HFStats2Up"   ) central_or_shift_int = kBtag_hfStats2Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_HFStats2Down" ) central_or_shift_int = kBtag_hfStats2Down;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFUp"         ) central_or_shift_int = kBtag_lfUp;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFDown"       ) central_or_shift_int = kBtag_lfDown;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFStats1Up"   ) central_or_shift_int = kBtag_lfStats1Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFStats1Down" ) central_or_shift_int = kBtag_lfStats1Down;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFStats2Up"   ) central_or_shift_int = kBtag_lfStats2Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_LFStats2Down" ) central_or_shift_int = kBtag_lfStats2Down;
-  else if ( central_or_shift == "CMS_ttHl_btag_cErr1Up"      ) central_or_shift_int = kBtag_cErr1Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_cErr1Down"    ) central_or_shift_int = kBtag_cErr1Down;
-  else if ( central_or_shift == "CMS_ttHl_btag_cErr2Up"      ) central_or_shift_int = kBtag_cErr2Up;
-  else if ( central_or_shift == "CMS_ttHl_btag_cErr2Down"    ) central_or_shift_int = kBtag_cErr2Down;
-  else if ( central_or_shift == "CMS_ttHl_JESUp"             ) central_or_shift_int = kBtag_jesUp;
-  else if ( central_or_shift == "CMS_ttHl_JESDown"           ) central_or_shift_int = kBtag_jesDown;
+  if     (central_or_shift == "CMS_ttHl_btag_HFUp"        ) central_or_shift_int = kBtag_hfUp;
+  else if(central_or_shift == "CMS_ttHl_btag_HFDown"      ) central_or_shift_int = kBtag_hfDown;
+  else if(central_or_shift == "CMS_ttHl_btag_HFStats1Up"  ) central_or_shift_int = kBtag_hfStats1Up;
+  else if(central_or_shift == "CMS_ttHl_btag_HFStats1Down") central_or_shift_int = kBtag_hfStats1Down;
+  else if(central_or_shift == "CMS_ttHl_btag_HFStats2Up"  ) central_or_shift_int = kBtag_hfStats2Up;
+  else if(central_or_shift == "CMS_ttHl_btag_HFStats2Down") central_or_shift_int = kBtag_hfStats2Down;
+  else if(central_or_shift == "CMS_ttHl_btag_LFUp"        ) central_or_shift_int = kBtag_lfUp;
+  else if(central_or_shift == "CMS_ttHl_btag_LFDown"      ) central_or_shift_int = kBtag_lfDown;
+  else if(central_or_shift == "CMS_ttHl_btag_LFStats1Up"  ) central_or_shift_int = kBtag_lfStats1Up;
+  else if(central_or_shift == "CMS_ttHl_btag_LFStats1Down") central_or_shift_int = kBtag_lfStats1Down;
+  else if(central_or_shift == "CMS_ttHl_btag_LFStats2Up"  ) central_or_shift_int = kBtag_lfStats2Up;
+  else if(central_or_shift == "CMS_ttHl_btag_LFStats2Down") central_or_shift_int = kBtag_lfStats2Down;
+  else if(central_or_shift == "CMS_ttHl_btag_cErr1Up"     ) central_or_shift_int = kBtag_cErr1Up;
+  else if(central_or_shift == "CMS_ttHl_btag_cErr1Down"   ) central_or_shift_int = kBtag_cErr1Down;
+  else if(central_or_shift == "CMS_ttHl_btag_cErr2Up"     ) central_or_shift_int = kBtag_cErr2Up;
+  else if(central_or_shift == "CMS_ttHl_btag_cErr2Down"   ) central_or_shift_int = kBtag_cErr2Down;
+  else if(central_or_shift == "CMS_ttHl_JESUp"            ) central_or_shift_int = kBtag_jesUp;
+  else if(central_or_shift == "CMS_ttHl_JESDown"          ) central_or_shift_int = kBtag_jesDown;
   return getBranchName_bTagWeight(era, central_or_shift_int);
 }
 
-std::string getBranchName_bTagWeight(int era, int central_or_shift)
+std::string
+getBranchName_bTagWeight(int era,
+                         int central_or_shift)
 {
   std::map<int, std::string> branchNames_bTagWeight;
-  if ( era == kEra_2017 ) {
+  if(era == kEra_2017)
+  {
     branchNames_bTagWeight[kBtag_central]      = "Jet_btagSF_csvv2_shape";
     branchNames_bTagWeight[kBtag_hfUp]         = branchNames_bTagWeight[kBtag_central] + "_up_hf";
     branchNames_bTagWeight[kBtag_hfDown]       = branchNames_bTagWeight[kBtag_central] + "_down_hf";
@@ -101,10 +100,13 @@ std::string getBranchName_bTagWeight(int era, int central_or_shift)
     branchNames_bTagWeight[kBtag_cErr2Down]    = branchNames_bTagWeight[kBtag_central] + "_down_cferr2";
     branchNames_bTagWeight[kBtag_jesUp]        = branchNames_bTagWeight[kBtag_central] + "_up_jes";
     branchNames_bTagWeight[kBtag_jesDown]      = branchNames_bTagWeight[kBtag_central] + "_down_jes";
-  } else assert(0);
-  std::map<int, std::string>::const_iterator branchName_bTagWeight = branchNames_bTagWeight.find(central_or_shift);
-  assert(branchName_bTagWeight != branchNames_bTagWeight.end());
-  return branchName_bTagWeight->second;
+  }
+  else
+  {
+    assert(0);
+  }
+  assert(branchNames_bTagWeight.count(central_or_shift));
+  return branchNames_bTagWeight.at(central_or_shift);
 }
 
 std::string
@@ -131,43 +133,51 @@ getBranchName_MEt(int,
   return branchName;
 }
 
-int getHadTau_genPdgId(const RecoHadTau* hadTau)
+int
+getHadTau_genPdgId(const RecoHadTau * hadTau)
 {
   int hadTau_genPdgId = -1;
-  if      ( hadTau->genHadTau() ) hadTau_genPdgId = 15;
-  else if ( hadTau->genLepton() ) hadTau_genPdgId = std::abs(hadTau->genLepton()->pdgId());
+  if     (hadTau->genHadTau()) hadTau_genPdgId = 15;
+  else if(hadTau->genLepton()) hadTau_genPdgId = std::abs(hadTau->genLepton()->pdgId());
   return hadTau_genPdgId;
 }
 
-Particle::LorentzVector compMHT(const std::vector<const RecoLepton*>& leptons, const std::vector<const RecoHadTau*>& hadTaus, const std::vector<const RecoJet*>& jets)
+Particle::LorentzVector
+compMHT(const std::vector<const RecoLepton *> & leptons,
+        const std::vector<const RecoHadTau *> & hadTaus,
+        const std::vector<const RecoJet *> & jets)
 {
   math::PtEtaPhiMLorentzVector mht_p4(0,0,0,0);
-  for ( std::vector<const RecoLepton*>::const_iterator lepton = leptons.begin();
-	lepton != leptons.end(); ++lepton ) {
-    mht_p4 += (*lepton)->p4();
+  for(const RecoLepton * lepton: leptons)
+  {
+    mht_p4 += lepton->p4();
   }
-  for ( std::vector<const RecoHadTau*>::const_iterator hadTau = hadTaus.begin();
-	hadTau != hadTaus.end(); ++hadTau ) {
-    mht_p4 += (*hadTau)->p4();
+  for(const RecoHadTau * hadTau: hadTaus)
+  {
+    mht_p4 += hadTau->p4();
   }
-  for ( std::vector<const RecoJet*>::const_iterator jet = jets.begin();
-	jet != jets.end(); ++jet ) {
-    mht_p4 += (*jet)->p4();
+  for(const RecoJet * jet: jets)
+  {
+    mht_p4 += jet->p4();
   }
   return mht_p4;
 }
 
-double compMEt_LD(const Particle::LorentzVector& met_p4, const Particle::LorentzVector& mht_p4)
+double
+compMEt_LD(const Particle::LorentzVector & met_p4,
+           const Particle::LorentzVector & mht_p4)
 {
-  double met_LD = met_coef*met_p4.pt() + mht_coef*mht_p4.pt(); 
-  return met_LD;
+  return met_coef * met_p4.pt() + mht_coef * mht_p4.pt();
 }
 
-std::vector<const RecoLepton*> mergeLeptonCollections(const std::vector<const RecoElectron*>& electrons, const std::vector<const RecoMuon*>& muons)
+std::vector<const RecoLepton *>
+mergeLeptonCollections(const std::vector<const RecoElectron *> & electrons,
+                       const std::vector<const RecoMuon *> & muons)
 { 
-  std::vector<const RecoLepton*> leptons;  
-  size_t nLeptons = electrons.size() + muons.size();
-  if ( nLeptons > 0 ) {
+  std::vector<const RecoLepton *> leptons;
+  const std::size_t nLeptons = electrons.size() + muons.size();
+  if(nLeptons > 0)
+  {
     leptons.reserve(nLeptons);
     leptons.insert(leptons.end(), electrons.begin(), electrons.end());
     leptons.insert(leptons.end(), muons.begin(), muons.end());
@@ -176,67 +186,32 @@ std::vector<const RecoLepton*> mergeLeptonCollections(const std::vector<const Re
   return leptons;
 }
 
-void printGenLeptonCollection(const std::string& collection_name, const std::vector<GenLepton>& genLeptons)
+template<>
+void
+printCollection<RecoLepton>(const std::string & collection_name,
+                            const std::vector<const RecoLepton *> & leptons)
 {
-  std::cout << " (#" << collection_name << " = " << genLeptons.size() << ")" << std::endl;
-  for ( size_t idxGenLepton = 0; idxGenLepton < genLeptons.size(); ++idxGenLepton ) {
-    std::cout << collection_name << "  #" << idxGenLepton << ":" << std::endl;
-    std::cout << genLeptons[idxGenLepton];
-  }
-} 
+  std::cout << " (#" << collection_name << " = " << leptons.size() << ")\n";
+  for(std::size_t idxLepton = 0; idxLepton < leptons.size(); ++idxLepton)
+  {
+    std::cout << collection_name << "  #" << idxLepton << ":\n";
+    const RecoLepton * const lepton = leptons[idxLepton];
 
-void printLeptonCollection(const std::string& collection_name, const std::vector<const RecoLepton*>& leptons)
-{
-  std::cout << " (#" << collection_name << " = " << leptons.size() << ")" << std::endl;
-  for ( size_t idxLepton = 0; idxLepton < leptons.size(); ++idxLepton ) {
-    std::cout << collection_name << "  #" << idxLepton << ":" << std::endl;
-    const RecoElectron* electron = dynamic_cast<const RecoElectron*>(leptons[idxLepton]);
-    if ( electron ) {
+    const RecoElectron * const electron = dynamic_cast<const RecoElectron *>(lepton);
+    if(electron)
+    {
       std::cout << (*electron);
       continue;
     }
-    const RecoMuon* muon = dynamic_cast<const RecoMuon*>(leptons[idxLepton]);
-    if ( muon ) {
+
+    const RecoMuon * const muon = dynamic_cast<const RecoMuon *>(lepton);
+    if(muon)
+    {
       std::cout << (*muon);
       continue;
     }
-    std::cout << (*leptons[idxLepton]);
-  }
-} 
 
-void printGenHadTauCollection(const std::string& collection_name, const std::vector<GenHadTau>& genHadTaus)
-{
-  std::cout << " (#" << collection_name << " = " << genHadTaus.size() << ")" << std::endl;
-  for ( size_t idxGenHadTau = 0; idxGenHadTau < genHadTaus.size(); ++idxGenHadTau ) {
-    std::cout << collection_name << "  #" << idxGenHadTau << ":" << std::endl;
-    std::cout << genHadTaus[idxGenHadTau];
-  }
-}
-
-void printHadTauCollection(const std::string& collection_name, const std::vector<const RecoHadTau*>& hadTaus)
-{
-  std::cout << " (#" << collection_name << " = " << hadTaus.size() << ")" << std::endl;
-  for ( size_t idxHadTau = 0; idxHadTau < hadTaus.size(); ++idxHadTau ) {
-    std::cout << collection_name << "  #" << idxHadTau << ":" << std::endl;
-    std::cout << (*hadTaus[idxHadTau]);
-  }
-}
-
-void printGenJetCollection(const std::string& collection_name, const std::vector<GenJet>& genJets)
-{
-  std::cout << " (#" << collection_name << " = " << genJets.size() << ")" << std::endl;
-  for ( size_t idxGenJet = 0; idxGenJet < genJets.size(); ++idxGenJet ) {
-    std::cout << collection_name << "  #" << idxGenJet << ":" << std::endl;
-    std::cout << genJets[idxGenJet];
-  }
-}
-
-void printJetCollection(const std::string& collection_name, const std::vector<const RecoJet*>& jets)
-{
-  std::cout << " (#" << collection_name << " = " << jets.size() << ")" << std::endl;
-  for ( size_t idxJet = 0; idxJet < jets.size(); ++idxJet ) {
-    std::cout << collection_name << "  #" << idxJet << ":" << std::endl;
-    std::cout << (*jets[idxJet]);
+    std::cout << (*lepton);
   }
 }
 
@@ -257,4 +232,3 @@ nCombinationsK(int n,
   }
   return result;
 }
-
