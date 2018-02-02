ccd964794bdcdaafc8868e994742afc79ad86e0c
Karl Ehatäht <karlehataht@gmail.com>
2018-02-02 17:08:39 +0200

    Refactor EvtHistManager_1l_2tau
    - remove unnecessary header inclusions
    - remove unused member variables
    - const-correctness
    - code style

--- interface/EvtHistManager_1l_2tau.h
+++ interface/EvtHistManager_1l_2tau.h
@@ -6,7 +6,7 @@
  * Book and fill histograms for event-level quantities in ttH, H->tautau analysis
  * in 1l_2tau category
  *
- * \author Christian Veelken, Tallin
+ * \author Christian Veelken, Tallinn
  *
  */
 
@@ -15,51 +15,63 @@
 class EvtHistManager_1l_2tau
   : public HistManagerBase
 {
- public:
-  EvtHistManager_1l_2tau(edm::ParameterSet const& cfg);
+public:
+  EvtHistManager_1l_2tau(const edm::ParameterSet & cfg);
   ~EvtHistManager_1l_2tau() {}
 
   /// book and fill histograms
-  void bookHistograms(TFileDirectory& dir);
-  void fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium,
-    double mvaOutput_1l_2tau_ttbar,
-    double mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly,
-    double mvaOutput_1l_2tau_ttbar_HTTNoKinFit_MVAonly,
-    double mvaOutput_1l_2tau_ttbar_Old,
-    double mvaOutput_1l_2tau_ttbar_HTTLepID,
-    double mvaOutput_1l_2tau_ttbar_OldVar,
-    double mvaOutput_1l_2tau_ttbar_OldVarHTT,
-    double mvaOutput_1l_2tau_ttV, double mvaDiscr_1l_2tau, double mTauTauVis, double evtWeight);
+  void
+  bookHistograms(TFileDirectory & dir) override;
 
-  const TH1* getHistogram_EventCounter() const { return histogram_EventCounter_; }
+  void
+  fillHistograms(int numElectrons,
+                 int numMuons,
+                 int numHadTaus,
+                 int numJets,
+                 int numBJets_loose,
+                 int numBJets_medium,
+                 double mvaOutput_1l_2tau_ttbar,
+                 double mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly,
+                 double mvaOutput_1l_2tau_ttbar_HTTNoKinFit_MVAonly,
+                 double mvaOutput_1l_2tau_ttbar_Old,
+                 double mvaOutput_1l_2tau_ttbar_HTTLepID,
+                 double mvaOutput_1l_2tau_ttbar_OldVar,
+                 double mvaOutput_1l_2tau_ttbar_OldVarHTT,
+                 double mvaOutput_1l_2tau_ttV,
+                 double mvaDiscr_1l_2tau,
+                 double mTauTauVis,
+                 double evtWeight);
+
+  const TH1 *
+  getHistogram_EventCounter() const;
 
  private:
-  TH1* histogram_numElectrons_;
-  TH1* histogram_numMuons_;
-  TH1* histogram_numHadTaus_;
-  TH1* histogram_numJets_;
-  TH1* histogram_numBJets_loose_;
-  TH1* histogram_numBJets_medium_;
+  TH1 * histogram_numElectrons_;
+  TH1 * histogram_numMuons_;
+  TH1 * histogram_numHadTaus_;
+  TH1 * histogram_numJets_;
+  TH1 * histogram_numBJets_loose_;
+  TH1 * histogram_numBJets_medium_;
 
-  TH2* histogram_numBJets_loose_vs_numJets_;  // CV: used to check loss in signal efficiency in case events with high jet and b-jet multiplicity are vetoed
-  TH2* histogram_numBJets_medium_vs_numJets_; //     to avoid overlap with ttH, H->bb analysis (alternative: ttH, H->bb analysis adds hadronic tau veto)
+  // CV: used to check loss in signal efficiency in case events with
+  // high jet and b-jet multiplicity are vetoed to avoid overlap with ttH, H->bb analysis
+  // (alternative: ttH, H->bb analysis adds hadronic tau veto)
+  TH2 * histogram_numBJets_loose_vs_numJets_;
+  TH2 * histogram_numBJets_medium_vs_numJets_;
 
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_Old_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_OldVar_;
-  TH1* histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_Old_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_OldVar_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_;
 
-  TH1* histogram_mvaOutput_1l_2tau_ttV_;
-  TH1* histogram_mvaDiscr_1l_2tau_;
+  TH1 * histogram_mvaOutput_1l_2tau_ttV_;
+  TH1 * histogram_mvaDiscr_1l_2tau_;
 
-  TH1* histogram_mTauTauVis_;
-
-  TH1* histogram_EventCounter_;
-
-  std::vector<TH1*> histograms_;
+  TH1 * histogram_mTauTauVis_;
+  TH1 * histogram_EventCounter_;
 };
 
 #endif
--- src/EvtHistManager_1l_2tau.cc
+++ src/EvtHistManager_1l_2tau.cc
@@ -1,75 +1,88 @@
 #include "tthAnalysis/HiggsToTauTau/interface/EvtHistManager_1l_2tau.h"
 
-#include "tthAnalysis/HiggsToTauTau/interface/histogramAuxFunctions.h"
+#include "tthAnalysis/HiggsToTauTau/interface/histogramAuxFunctions.h" // fillWithOverFlow(), fillWithOverFlow2d()
 
-#include <TMath.h>
-
-EvtHistManager_1l_2tau::EvtHistManager_1l_2tau(const edm::ParameterSet& cfg)
+EvtHistManager_1l_2tau::EvtHistManager_1l_2tau(const edm::ParameterSet & cfg)
   : HistManagerBase(cfg)
 {}
 
-void EvtHistManager_1l_2tau::bookHistograms(TFileDirectory& dir)
+const TH1 *
+EvtHistManager_1l_2tau::getHistogram_EventCounter() const
 {
-  histogram_numElectrons_ = book1D(dir, "numElectrons", "numElectrons", 5, -0.5, +4.5);
-  histogram_numMuons_ = book1D(dir, "numMuons", "numMuons", 5, -0.5, +4.5);
-  histogram_numHadTaus_ = book1D(dir, "numHadTaus", "numHadTaus", 5, -0.5, +4.5);
-  histogram_numJets_ = book1D(dir, "numJets", "numJets", 20, -0.5, +19.5);
-  histogram_numBJets_loose_ = book1D(dir, "numBJets_loose", "numBJets_loose", 10, -0.5, +9.5);
-  histogram_numBJets_medium_ = book1D(dir, "numBJets_medium", "numBJets_medium", 10, -0.5, +9.5);
+  return histogram_EventCounter_;
+}
 
-  histogram_numBJets_loose_vs_numJets_ = book2D(dir, "numBJets_loose_vs_numJets", "numBJets_loose_vs_numJets", 8, -0.5, +7.5, 6, -0.5, +5.5);
+void
+EvtHistManager_1l_2tau::bookHistograms(TFileDirectory & dir)
+{
+  histogram_numElectrons_    = book1D(dir, "numElectrons",    "numElectrons",     5, -0.5,  +4.5);
+  histogram_numMuons_        = book1D(dir, "numMuons",        "numMuons",         5, -0.5,  +4.5);
+  histogram_numHadTaus_      = book1D(dir, "numHadTaus",      "numHadTaus",       5, -0.5,  +4.5);
+  histogram_numJets_         = book1D(dir, "numJets",         "numJets",         20, -0.5, +19.5);
+  histogram_numBJets_loose_  = book1D(dir, "numBJets_loose",  "numBJets_loose",  10, -0.5,  +9.5);
+  histogram_numBJets_medium_ = book1D(dir, "numBJets_medium", "numBJets_medium", 10, -0.5,  +9.5);
+
+  histogram_numBJets_loose_vs_numJets_  = book2D(dir, "numBJets_loose_vs_numJets",  "numBJets_loose_vs_numJets",  8, -0.5, +7.5, 6, -0.5, +5.5);
   histogram_numBJets_medium_vs_numJets_ = book2D(dir, "numBJets_medium_vs_numJets", "numBJets_medium_vs_numJets", 8, -0.5, +7.5, 6, -0.5, +5.5);
 
-  histogram_mvaOutput_1l_2tau_ttbar_ = book1D(dir, "mvaOutput_1l_2tau_ttbar", "mvaOutput_1l_2tau_ttbar", 500, 0., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly", "mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly", 500, 0., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_HTTWithKinFit", "mvaOutput_1l_2tau_ttbar_HTTWithKinFit", 500, 0., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_Old_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_Old", "mvaOutput_1l_2tau_ttbar_Old", 500, -1., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_HTTLepID", "mvaOutput_1l_2tau_ttbar_HTTLepID", 500, 0., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_OldVar_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_OldVar", "mvaOutput_1l_2tau_ttbar_OldVar", 500, 0., +1.);
-  histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_ = book1D(dir, "mvaOutput_1l_2tau_ttbar_OldVarHTT", "mvaOutput_1l_2tau_ttbar_OldVarHTT", 500, 0., +1.);
+  const char * prefix = "mvaOutput_1l_2tau_ttbar";
+  histogram_mvaOutput_1l_2tau_ttbar_ = book1D(dir, prefix, prefix, 500, 0., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly_ = book1D(dir, Form("%s_HTTWithKinFit_MVAonly", prefix), Form("%s_HTTWithKinFit_MVAonly", prefix), 500,  0., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_         = book1D(dir, Form("%s_HTTWithKinFit",         prefix), Form("%s_HTTWithKinFit",         prefix), 500,  0., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_Old_                   = book1D(dir, Form("%s_Old",                   prefix), Form("%s_Old",                   prefix), 500, -1., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_              = book1D(dir, Form("%s_HTTLepID",              prefix), Form("%s_HTTLepID",              prefix), 500,  0., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_OldVar_                = book1D(dir, Form("%s_OldVar",                prefix), Form("%s_OldVar",                prefix), 500,  0., +1.);
+  histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_             = book1D(dir, Form("%s_OldVarHTT",             prefix), Form("%s_OldVarHTT",             prefix), 500,  0., +1.);
 
-  histogram_mvaOutput_1l_2tau_ttV_ = book1D(dir, "mvaOutput_1l_2tau_ttV", "mvaOutput_1l_2tau_ttV", 20, -1., +1.);
-  histogram_mvaDiscr_1l_2tau_ = book1D(dir, "mvaDiscr_1l_2tau", "mvaDiscr_1l_2tau", 10, 0.5, 10.5);
+  histogram_mvaOutput_1l_2tau_ttV_ = book1D(dir, "mvaOutput_1l_2tau_ttV", "mvaOutput_1l_2tau_ttV", 20, -1.,  +1.);
+  histogram_mvaDiscr_1l_2tau_      = book1D(dir, "mvaDiscr_1l_2tau",      "mvaDiscr_1l_2tau",      10,  0.5, 10.5);
 
-  histogram_mTauTauVis_ = book1D(dir, "mTauTauVis", "mTauTauVis", 20, 0., 200.);
-
+  histogram_mTauTauVis_   = book1D(dir, "mTauTauVis",   "mTauTauVis",  20,  0., 200.);
   histogram_EventCounter_ = book1D(dir, "EventCounter", "EventCounter", 1, -0.5, +0.5);
 }
 
-void EvtHistManager_1l_2tau::fillHistograms(int numElectrons, int numMuons, int numHadTaus, int numJets, int numBJets_loose, int numBJets_medium,
-  double mvaOutput_1l_2tau_ttbar,
-  double mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly,
-  double mvaOutput_1l_2tau_ttbar_HTTWithKinFit,
-  double mvaOutput_1l_2tau_ttbar_Old,
-  double mvaOutput_1l_2tau_ttbar_HTTLepID,
-  double mvaOutput_1l_2tau_ttbar_OldVar,
-  double mvaOutput_1l_2tau_ttbar_OldVarHTT,
-  double mvaOutput_1l_2tau_ttV, double mvaDiscr_1l_2tau, double mTauTauVis, double evtWeight)
+void
+EvtHistManager_1l_2tau::fillHistograms(int numElectrons,
+                                       int numMuons,
+                                       int numHadTaus,
+                                       int numJets,
+                                       int numBJets_loose,
+                                       int numBJets_medium,
+                                       double mvaOutput_1l_2tau_ttbar,
+                                       double mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly,
+                                       double mvaOutput_1l_2tau_ttbar_HTTWithKinFit,
+                                       double mvaOutput_1l_2tau_ttbar_Old,
+                                       double mvaOutput_1l_2tau_ttbar_HTTLepID,
+                                       double mvaOutput_1l_2tau_ttbar_OldVar,
+                                       double mvaOutput_1l_2tau_ttbar_OldVarHTT,
+                                       double mvaOutput_1l_2tau_ttV,
+                                       double mvaDiscr_1l_2tau,
+                                       double mTauTauVis,
+                                       double evtWeight)
 {
-  double evtWeightErr = 0.;
+  const double evtWeightErr = 0.;
 
-  fillWithOverFlow(histogram_numElectrons_, numElectrons, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_numMuons_, numMuons, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_numHadTaus_, numHadTaus, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_numJets_, numJets, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_numBJets_loose_, numBJets_loose, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numElectrons_,    numElectrons,    evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numMuons_,        numMuons,        evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numHadTaus_,      numHadTaus,      evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numJets_,         numJets,         evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numBJets_loose_,  numBJets_loose,  evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numBJets_medium_, numBJets_medium, evtWeight, evtWeightErr);
 
-  fillWithOverFlow2d(histogram_numBJets_loose_vs_numJets_, numJets, numBJets_loose, evtWeight, evtWeightErr);
+  fillWithOverFlow2d(histogram_numBJets_loose_vs_numJets_,  numJets, numBJets_loose,  evtWeight, evtWeightErr);
   fillWithOverFlow2d(histogram_numBJets_medium_vs_numJets_, numJets, numBJets_medium, evtWeight, evtWeightErr);
 
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_, mvaOutput_1l_2tau_ttbar, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_Old_, mvaOutput_1l_2tau_ttbar_Old, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_,                       mvaOutput_1l_2tau_ttbar,                       evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_Old_,                   mvaOutput_1l_2tau_ttbar_Old,                   evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly_, mvaOutput_1l_2tau_ttbar_HTTWithKinFit_MVAonly, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_, mvaOutput_1l_2tau_ttbar_HTTWithKinFit, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_, mvaOutput_1l_2tau_ttbar_HTTLepID, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_OldVar_, mvaOutput_1l_2tau_ttbar_OldVar, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_, mvaOutput_1l_2tau_ttbar_OldVarHTT, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HTTWithKinFit_,         mvaOutput_1l_2tau_ttbar_HTTWithKinFit,         evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_HTTLepID_,              mvaOutput_1l_2tau_ttbar_HTTLepID,              evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_OldVar_,                mvaOutput_1l_2tau_ttbar_OldVar,                evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttbar_OldVarHTT_,             mvaOutput_1l_2tau_ttbar_OldVarHTT,             evtWeight, evtWeightErr);
 
   fillWithOverFlow(histogram_mvaOutput_1l_2tau_ttV_, mvaOutput_1l_2tau_ttV, evtWeight, evtWeightErr);
-  fillWithOverFlow(histogram_mvaDiscr_1l_2tau_, mvaDiscr_1l_2tau, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mvaDiscr_1l_2tau_,      mvaDiscr_1l_2tau,      evtWeight, evtWeightErr);
 
-  fillWithOverFlow(histogram_mTauTauVis_, mTauTauVis, evtWeight, evtWeightErr);
-
-  fillWithOverFlow(histogram_EventCounter_, 0., evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_mTauTauVis_,   mTauTauVis, evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_EventCounter_, 0.,         evtWeight, evtWeightErr);
 }
