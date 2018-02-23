46a8228027188edf174d3bc9fb30ee801247e077
Karl Ehatäht <karlehataht@gmail.com>
2018-02-23 03:09:01 +0200

    Update all sample dictionaries with new event counts
    Change due to bugfix in https://github.com/HEP-KBFI/tth-htt/commit/75b74809bebfd4f5319e54f99e27f5da995c9691

--- python/samples/tthAnalyzeSamples_2017_nanoAOD.py
+++ python/samples/tthAnalyzeSamples_2017_nanoAOD.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
 # file generated with the following command:
-# ./scripts/create_dictionary.py -v -m python/samples/metaDict_2017.py -p /hdfs/cms/store/user/kaehatah/NanoAOD_2017 -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017
+# /home/karl/CMSSW_9_4_4/bin/slc6_amd64_gcc630/create_dictionary.py -v -m python/samples/metaDict_2017.py -p /hdfs/cms/store/user/kaehatah/NanoAOD_2017 -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017
 
 samples_2017 = OD()
 samples_2017["/ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM"] = OD([
@@ -9,8 +9,8 @@ samples_2017["/ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/Run
   ("sample_category",                 "signal"),
   ("process_name_specific",           "ttHJetToNonbb_M125_amcatnlo"),
   ("nof_files",                       168),
-  ("nof_events",                      8169531),
-  ("nof_tree_events",                 8358504),
+  ("nof_events",                      0),
+  ("nof_tree_events",                 7714849),
   ("nof_db_events",                   7714849),
   ("use_HIP_mitigation_bTag",         False),
   ("use_HIP_mitigation_mediumMuonId", False),
--- python/samples/tthAnalyzeSamples_2017_nanoAOD_leptonFR_test.py
+++ python/samples/tthAnalyzeSamples_2017_nanoAOD_leptonFR_test.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
 # file generated with the following command:
-# ./scripts/create_dictionary.py -v -m python/samples/metaDict_2017_leptonFR_test.py -p /hdfs/cms/store/user/kaehatah/NanoAOD_2017 -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017
+# /home/karl/CMSSW_9_4_4/bin/slc6_amd64_gcc630/create_dictionary.py -v -m python/samples/metaDict_2017_leptonFR_test.py -p /hdfs/cms/store/user/kaehatah/NanoAOD_2017 -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017
 
 samples_2017 = OD()
 samples_2017["/SingleMuon/Run2017E-17Nov2017-v1/MINIAOD"] = OD([
@@ -9,8 +9,8 @@ samples_2017["/SingleMuon/Run2017E-17Nov2017-v1/MINIAOD"] = OD([
   ("sample_category",                 "data_obs"),
   ("process_name_specific",           "SingleMuon_Run2017E_17Nov2017_v1"),
   ("nof_files",                       3113),
-  ("nof_events",                      143253628),
-  ("nof_tree_events",                 143253628),
+  ("nof_events",                      151665771),
+  ("nof_tree_events",                 151665771),
   ("nof_db_events",                   154630534),
   ("use_HIP_mitigation_bTag",         False),
   ("use_HIP_mitigation_mediumMuonId", False),
@@ -22,7 +22,7 @@ samples_2017["/SingleMuon/Run2017E-17Nov2017-v1/MINIAOD"] = OD([
       OD([
         ("path",      "/hdfs/cms/store/user/kaehatah/NanoAOD_2017/SingleMuon/NanoAOD_2017_v5_SingleMuon__Run2017E-17Nov2017-v1/180130_140806"),
         ("selection", "*"),
-        ("blacklist", [276,3104]),
+        ("blacklist", [276]),
       ]),
     ]
   ),
