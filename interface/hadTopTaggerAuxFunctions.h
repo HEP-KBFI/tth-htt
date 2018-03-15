610f47325d8f1fa632c5016543b161eac353127f
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2018-03-16 01:32:00 +0200

    import Karl rewritting

--- interface/hadTopTaggerAuxFunctions.h
+++ interface/hadTopTaggerAuxFunctions.h
@@ -3,7 +3,8 @@
 
 #include "tthAnalysis/HiggsToTauTau/interface/RecoJet.h" // RecoJet
 #include "tthAnalysis/HiggsToTauTau/interface/GenParticle.h" // GenParticle
-
+#include "tthAnalysis/HiggsToTauTau/interface/TMVAInterface.h" // TMVAInterface
+#include "tthAnalysis/HiggsToTauTau/interface/mvaAuxFunctions.h" // check_mvaInputs, get_mvaInputVariables
 #include <vector>
 
 enum { kGenMatchedBJet, kGenMatchedWJet1, kGenMatchedWJet2, kGenMatchedTriplet };
@@ -20,4 +21,17 @@ std::vector<bool> isGenMatchedJetTriplet(const RecoJet& recBJet, const RecoJet& 
 std::vector<float> isGenMatchedJetTripletVar(const std::vector<GenParticle>& genTopQuarks,
 					 const std::vector<GenParticle>& genBJets, const std::vector<GenParticle>& genWBosons, const std::vector<GenParticle>& genWJets, int mode
 				 );
+
+double comp_mvaOutput_Hjj_tagger(const RecoJet* jet1, const RecoJet* jet2, const std::vector<const RecoJet*>& jets,
+ const std::vector<const RecoLepton*>& leptons,
+ std::map<std::string, double>& mvaInputs_Hjj_tagger, TMVAInterface& mva_Hjj_tagger,
+ std::map<std::string, double>& mvaInputs_Hj_tagger, TMVAInterface& mva_Hj_tagger,
+ const EventInfo & eventInfo);
+
+double comp_mvaOutput_Hj_tagger(const RecoJet* jet,
+ const std::vector<const RecoLepton*>& leptons,
+ std::map<std::string, double>& mvaInputs_Hj_tagger,
+ TMVAInterface& mva_Hj_tagger,
+ const EventInfo & eventInfo);
+
 #endif
