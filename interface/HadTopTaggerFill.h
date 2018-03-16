69ef0c6723a20ef3db46715252af2e9c761c42c2
Siddhesh Sawant <siddhesh.gopichand.sawant@cern.ch>
2018-03-16 18:02:54 +0200

    Add operators for boostedTop cases

--- interface/HadTopTaggerFill.h
+++ interface/HadTopTaggerFill.h
@@ -2,6 +2,8 @@
 #define tthAnalysis_HiggsToTauTau_HadTopTaggerFill_h
 
 #include "tthAnalysis/HiggsToTauTau/interface/RecoJet.h" // RecoJet
+#include "tthAnalysis/HiggsToTauTau/interface/RecoJetAK12.h"
+#include "tthAnalysis/HiggsToTauTau/interface/RecoJetHTTv2.h"
 
 // forward declarations
 class HadTopKinFit;
@@ -18,6 +20,7 @@ public:
    * @param mvaInputs Values of MVA input variables (stored in std::map with key = MVA input variable name)
    * @return          MVA output
    */
+	// Case III] Non-boosted top 
   double
   operator()(const RecoJet & recBJet,
              const RecoJet & recWJet1,
@@ -49,6 +52,20 @@ public:
   const HadTopKinFit *
   kinFit() const;
 
+
+  // Case I] Boosted top with merged b and W jets from top
+	double
+	operator()(const RecoSubjetHTTv2 & recBJet,
+						 const RecoSubjetHTTv2 & recWJet1,
+						 const RecoSubjetHTTv2 & recWJet2);
+	
+	// Case II] Boosted W with merged WJet1 and WJet2 and separate b-jet
+	double
+	operator()(const RecoJet & recBJet,
+						 const RecoSubjetAK12 & recWJet1,
+						 const RecoSubjetAK12  & recWJet2);
+
+	
 protected:
   HadTopKinFit * kinFit_;
   std::vector<std::string> mvaInputVariables_;
