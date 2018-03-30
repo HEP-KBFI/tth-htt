HEP-KBFI/tth-htt

commit c741fc5baea0b5b2f04c16a04a9f20ff5e5fb66d

Complete debug logging in all electron and muon selectors

src/RecoMuonCollectionSelectorLoose.cc
@@ -5,9 +5,10 @@
 RecoMuonSelectorLoose::RecoMuonSelectorLoose(int era,
                                              int index,
                                              bool debug,
-                                             bool set_selection_flags)
-  : set_selection_flags_(set_selection_flags)
-  , era_(era)
+                                             bool set_selection_flags) 
+  : era_(era)
+  , debug_(debug)
+  , set_selection_flags_(set_selection_flags)
   , tightMuonSelector_(era_, index, debug, false)
   , min_pt_(5.)
   , max_absEta_(2.4)
@@ -23,24 +24,91 @@ RecoMuonSelectorLoose::RecoMuonSelectorLoose(int era,
 bool
 RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
 {
-  if(muon.pt()             >= min_pt_                &&
-     muon.absEta()         <= max_absEta_            &&
-     std::fabs(muon.dxy()) <= max_dxy_               &&
-     std::fabs(muon.dz())  <= max_dz_                &&
-     muon.relIso()         <= max_relIso_            &&
-     muon.sip3d()          <= max_sip3d_             &&
-     (muon.passesLooseIdPOG() || !apply_looseIdPOG_) &&
-     (muon.passesMediumIdPOG() || !apply_mediumIdPOG_))
+  if(debug_)
   {
-    if(set_selection_flags_)
-    {
-      muon.set_isLoose();
-      if(tightMuonSelector_(muon))
-      {
-        muon.set_isTight();
-      }
-    }
-    return true;
+    std::cout << __func__ << ":\n" << muon;
   }
-  return false;
+
+  if(muon.pt() < min_pt_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS pT >= " << min_pt_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(muon.absEta() > max_absEta_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS pT <= " << max_absEta_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(muon.dxy()) > max_dxy_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dxy) <= " << max_dxy_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(muon.dz()) > max_dz_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dz) <= " << max_dz_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(muon.relIso() > max_relIso_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS relIso <= " << max_relIso_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(muon.sip3d() > max_sip3d_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS sip3d <= " << max_sip3d_ << " loose cut\n";
+    }
+    return false;
+  }
+
+  if(muon.passesLooseIdPOG() || ! apply_looseIdPOG_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS loose POG loose cut\n";
+    }
+    return false;
+  }
+
+  if(muon.passesMediumIdPOG() || ! apply_mediumIdPOG_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS medium POG loose cut\n";
+    }
+    return false;
+  }
+
+  if(set_selection_flags_)
+  {
+    muon.set_isLoose();
+    if(tightMuonSelector_(muon))
+    {
+      muon.set_isTight();
+    }
+  }
+
+  return true;
 }

interface/RecoElectronCollectionSelectorFakeable.h
@@ -27,6 +27,7 @@ public:
 
 protected:
   int era_;
+  bool debug_;
   bool set_selection_flags_;
   bool apply_offline_e_trigger_cuts_;
 

interface/RecoMuonCollectionSelectorFakeable.h
@@ -24,6 +24,7 @@ public:
 
 protected:
   int era_;
+  bool debug_;
   bool set_selection_flags_;
 
   RecoMuonSelectorTight tightMuonSelector_;

interface/RecoMuonCollectionSelectorLoose.h
@@ -21,8 +21,9 @@ public:
   operator()(const RecoMuon & muon) const;
 
 protected:
-  bool set_selection_flags_;
   int era_;
+  bool debug_;
+  bool set_selection_flags_;
   RecoMuonSelectorTight tightMuonSelector_;
 
   Double_t min_pt_;        ///< lower cut threshold on pT

src/RecoElectronCollectionSelectorFakeable.cc
@@ -8,6 +8,7 @@ RecoElectronSelectorFakeable::RecoElectronSelectorFakeable(int era,
                                                            bool debug,
                                                            bool set_selection_flags)
   : era_(era)
+  , debug_(debug)
   , set_selection_flags_(set_selection_flags)
   , apply_offline_e_trigger_cuts_(true)
   , tightElectronSelector_(era_, index, debug, false)
@@ -20,7 +21,7 @@ RecoElectronSelectorFakeable::RecoElectronSelectorFakeable(int era,
   , max_sip3d_(8.)
   , mvaPOGwp_(EGammaPOG::kWPL)
   , binning_absEta_({ 0.8, 1.479 })
-  , min_pt_trig_(-1.) // Lines:237-240 in AN_2017_029_v5
+  , min_pt_trig_(-1.) // LFR sync; used to be 30 GeV Lines:237-240 in AN_2017_029_v5
   , max_sigmaEtaEta_trig_({ 0.011, 0.011, 0.030 })
   , max_HoE_trig_({ 0.10, 0.10, 0.07 })
   , max_deltaEta_trig_({ 0.01, 0.01, 0.008 })
@@ -73,50 +74,190 @@ RecoElectronSelectorFakeable::set_selection_flags(bool selection_flags)
 bool
 RecoElectronSelectorFakeable::operator()(const RecoElectron & electron) const
 {
-  if(electron.cone_pt() >= min_cone_pt_                         &&
-     electron.lepton_pt() >= min_lepton_pt_                     &&
-     electron.absEta() <= max_absEta_                           &&
-     std::fabs(electron.dxy()) <= max_dxy_                      &&
-     std::fabs(electron.dz()) <= max_dz_                        &&
-     electron.relIso() <= max_relIso_                           &&
-     electron.sip3d() <= max_sip3d_                             &&
-     electron.nLostHits() <= max_nLostHits_                     &&
-     (electron.passesConversionVeto() || ! apply_conversionVeto_))
+  if(debug_)
+  {
+    std::cout << __func__ << ":\n" << electron;
+  }
+
+  if(electron.cone_pt() < min_cone_pt_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS cone pT >= " << min_cone_pt_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.lepton_pt() < min_lepton_pt_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS pT >= " << min_lepton_pt_ << " fakeable cut\n";
+    }
+  }
+
+  if(electron.absEta() > max_absEta_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(eta) <= " << max_absEta_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(electron.dxy()) > max_dxy_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dxy) <= " << max_dxy_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(electron.dz()) > max_dz_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dz) <= " << max_dz_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.relIso() > max_relIso_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS relIso <= " << max_relIso_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.sip3d() > max_sip3d_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS sip3d <= " << max_sip3d_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.nLostHits() > max_nLostHits_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS nLostHits <= " << max_nLostHits_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(! electron.passesConversionVeto() && apply_conversionVeto_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS conversion veto fakeable cut\n";
+    }
+    return false;
+  }
+
+  const int idxBin_mvaTTH = electron.mvaRawTTH() <= binning_mvaTTH_[0] ? 0 : 1;
+
+  if(! electron.mvaRawPOG_WP(mvaPOGwp_))
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS EGamma POG MVA " << as_integer(mvaPOGwp_) << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.jetPtRatio() < min_jetPtRatio_[idxBin_mvaTTH])
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS jetPtRatio >= " << min_jetPtRatio_[idxBin_mvaTTH] << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.jetBtagCSV() > max_jetBtagCSV_[idxBin_mvaTTH])
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS jetBtagCSV <= " << max_jetBtagCSV_[idxBin_mvaTTH] << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(electron.cone_pt() > min_pt_trig_ && apply_offline_e_trigger_cuts_)
   {
     const int idxBin_absEta = electron.absEta() <= binning_absEta_[0] ? 0 :
                              (electron.absEta() <= binning_absEta_[1] ? 1 : 2)
     ;
-    const int idxBin_mvaTTH = electron.mvaRawTTH() <= binning_mvaTTH_[0] ? 0 : 1;
-
-    if(electron.mvaRawPOG_WP(mvaPOGwp_)                        &&
-       electron.jetPtRatio() >= min_jetPtRatio_[idxBin_mvaTTH] &&
-       electron.jetBtagCSV() <= max_jetBtagCSV_[idxBin_mvaTTH] &&
-       (
-         (electron.cone_pt() <= min_pt_trig_ || ! apply_offline_e_trigger_cuts_) ||
-          (
-            electron.cone_pt() > min_pt_trig_                                   &&
-            electron.sigmaEtaEta() <= max_sigmaEtaEta_trig_[idxBin_absEta]      &&
-            electron.HoE() <= max_HoE_trig_[idxBin_absEta]                      &&
-            std::fabs(electron.deltaEta()) <= max_deltaEta_trig_[idxBin_absEta] &&
-            std::fabs(electron.deltaPhi()) <= max_deltaPhi_trig_[idxBin_absEta] &&
-            electron.OoEminusOoP() >= min_OoEminusOoP_trig_                     &&
-            electron.OoEminusOoP() <= max_OoEminusOoP_trig_[idxBin_absEta]
-          )
-        )
-      )
+    if(electron.sigmaEtaEta() > max_sigmaEtaEta_trig_[idxBin_absEta])
     {
-      if(set_selection_flags_)
+      if(debug_)
       {
-        electron.set_isFakeable();
-        if(tightElectronSelector_(electron))
-        {
-          electron.set_isTight();
-        }
+        std::cout << "FAILS sigmaEtaEta <= " << max_sigmaEtaEta_trig_[idxBin_absEta] << " fakeable cut\n";
       }
-      return true;
+      return false;
+    }
+
+    if(electron.HoE() > max_HoE_trig_[idxBin_absEta])
+    {
+      if(debug_)
+      {
+        std::cout << "FAILS HoE <= " << max_HoE_trig_[idxBin_absEta] << " fakeable cut\n";
+      }
+      return false;
+    }
+
+    if(std::fabs(electron.deltaEta()) > max_deltaEta_trig_[idxBin_absEta])
+    {
+      if(debug_)
+      {
+        std::cout << "FAILS abs(deltaEta) <= " << max_deltaEta_trig_[idxBin_absEta] << " fakeable cut\n";
+      }
+      return false;
+    }
+
+    if(std::fabs(electron.deltaPhi()) > max_deltaPhi_trig_[idxBin_absEta])
+    {
+      if(debug_)
+      {
+        std::cout << "FAILS abs(deltaPhi) <= " << max_deltaPhi_trig_[idxBin_absEta] << " fakeable cut\n";
+      }
+      return false;
+    }
+
+    if(electron.OoEminusOoP() < min_OoEminusOoP_trig_)
+    {
+      if(debug_)
+      {
+        std::cout << "FAILS OoEminusOoP >= " << min_OoEminusOoP_trig_ << " fakeable cut\n";
+      }
+      return false;
+    }
+
+    if(electron.OoEminusOoP() > max_OoEminusOoP_trig_[idxBin_absEta])
+    {
+      if(debug_)
+      {
+        std::cout << "FAILS OoEminusOoP <= " << max_OoEminusOoP_trig_[idxBin_absEta] << " fakeable cut\n";
+      }
+      return false;
     }
   }
-  return false;
+
+  if(set_selection_flags_)
+  {
+    electron.set_isFakeable();
+    if(tightElectronSelector_(electron))
+    {
+      electron.set_isTight();
+    }
+  }
+
+  return true;
 }
 
 RecoElectronCollectionSelectorFakeable::RecoElectronCollectionSelectorFakeable(int era,

src/RecoElectronCollectionSelectorLoose.cc
@@ -28,7 +28,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
 {
   if(debug_)
   {
-    std::cout << "<RecoElectronSelectorTight::operator()>:\n" << electron;
+    std::cout << __func__ << ":\n" << electron;
   }
 
   if(electron.pt() < min_pt_)

src/RecoElectronCollectionSelectorTight.cc
@@ -83,7 +83,7 @@ RecoElectronSelectorTight::operator()(const RecoElectron & electron) const
 {
   if(debug_)
   {
-    std::cout << "<RecoElectronSelectorTight::operator()>:\n" << electron;
+    std::cout << __func__ << ":\n" << electron;
   }
 
   if(electron.pt() < min_pt_)
@@ -167,7 +167,7 @@ RecoElectronSelectorTight::operator()(const RecoElectron & electron) const
     return false;
   }
 
-  if(electron.mvaRawPOG_WP(mvaPOGwp_) < 1)
+  if(! electron.mvaRawPOG_WP(mvaPOGwp_))
   {
     if(debug_)
     {
@@ -176,14 +176,14 @@ RecoElectronSelectorTight::operator()(const RecoElectron & electron) const
     return false;
   }
 
-  const int idxBin = electron.absEta() <= binning_absEta_[0] ? 0 :
-                    (electron.absEta() <= binning_absEta_[1] ? 1 : 2)
-  ;
-
   // extra cuts for electrons passing pT threshold of single electron trigger,
   // as explained in section 3.3.4 of AN-2015/321
   if(apply_offline_e_trigger_cuts_ && electron.pt() >= min_pt_trig_)
   {
+    const int idxBin = electron.absEta() <= binning_absEta_[0] ? 0 :
+                      (electron.absEta() <= binning_absEta_[1] ? 1 : 2)
+    ;
+
     if(electron.sigmaEtaEta() > max_sigmaEtaEta_trig_[idxBin])
     {
       if(debug_)

src/RecoMuonCollectionSelectorFakeable.cc
@@ -8,6 +8,7 @@ RecoMuonSelectorFakeable::RecoMuonSelectorFakeable(int era,
                                                    bool debug,
                                                    bool set_selection_flags)
   : era_(era)
+  , debug_(debug)
   , set_selection_flags_(set_selection_flags)
   , tightMuonSelector_(era_, index, debug, false)
   , min_cone_pt_(10.)
@@ -37,33 +38,130 @@ RecoMuonSelectorFakeable::RecoMuonSelectorFakeable(int era,
 bool
 RecoMuonSelectorFakeable::operator()(const RecoMuon & muon) const
 {
-  if(muon.cone_pt()        >= min_cone_pt_            &&
-     muon.lepton_pt()      >= min_lepton_pt_          &&
-     muon.absEta()         <= max_absEta_             &&
-     std::fabs(muon.dxy()) <= max_dxy_                &&
-     std::fabs(muon.dz())  <= max_dz_                 &&
-     muon.relIso()         <= max_relIso_             &&
-     muon.sip3d()          <= max_sip3d_              &&
-     (muon.passesLooseIdPOG() || ! apply_looseIdPOG_) &&
-     (muon.passesMediumIdPOG() || ! apply_mediumIdPOG_))
+  if(debug_)
   {
-    const int idxBin = muon.mvaRawTTH() <= binning_mvaTTH_[0] ? 0 : 1;
-    if(muon.jetPtRatio() >= min_jetPtRatio_[idxBin] &&
-       muon.jetBtagCSV() <= max_jetBtagCSV_[idxBin] &&
-       muon.segmentCompatibility() > min_segmentCompatibility_[idxBin])
-    {
-      if(set_selection_flags_)
-      {
-        muon.set_isFakeable();
-        if(tightMuonSelector_(muon))
-        {
-          muon.set_isTight();
-        }
-      } // set_selection_flags
-      return true;
-    } // muon.jetPtRatio, muon.jetBtagCSV
+    std::cout << __func__ << ":\n" << muon;
   }
-  return false;
+
+  if(muon.cone_pt() < min_cone_pt_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS cone pT >= " << min_cone_pt_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(muon.lepton_pt() < min_lepton_pt_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS pT >= " << min_lepton_pt_ << " fakeable cut\n";
+    }
+  }
+
+  if(muon.absEta() > max_absEta_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(eta) <= " << max_absEta_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(muon.dxy()) > max_dxy_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dxy) <= " << max_dxy_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(std::fabs(muon.dz()) > max_dz_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS abs(dz) <= " << max_dz_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(muon.relIso() > max_relIso_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS relIso <= " << max_relIso_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(muon.sip3d() > max_sip3d_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS sip3d <= " << max_sip3d_ << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(! muon.passesLooseIdPOG() && apply_looseIdPOG_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS loose POG fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(! muon.passesMediumIdPOG() && apply_mediumIdPOG_)
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS medium POG fakeable cut\n";
+    }
+    return false;
+  }
+
+  const int idxBin = muon.mvaRawTTH() <= binning_mvaTTH_[0] ? 0 : 1;
+
+  if(muon.jetPtRatio() < min_jetPtRatio_[idxBin])
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS jetPtRatio >= " << min_jetPtRatio_[idxBin] << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(muon.jetBtagCSV() > max_jetBtagCSV_[idxBin])
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS jetBtagCSV <= " << max_jetBtagCSV_[idxBin] << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(muon.segmentCompatibility() <= min_segmentCompatibility_[idxBin])
+  {
+    if(debug_)
+    {
+      std::cout << "FAILS segmentCompatibility > " << min_segmentCompatibility_[idxBin] << " fakeable cut\n";
+    }
+    return false;
+  }
+
+  if(set_selection_flags_)
+  {
+    muon.set_isFakeable();
+    if(tightMuonSelector_(muon))
+    {
+      muon.set_isTight();
+    }
+  }
+
+  return true;
 }
 
 void

commit b7ad88eed0adc5c2a2347f33dbc3ea8d9605aada

Correct POG cut in loose muon selector
Accidental bug introduced in c741fc5baea0b5b2f04c16a04a9f20ff5e5fb66d

src/RecoMuonCollectionSelectorLoose.cc
@@ -83,7 +83,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.passesLooseIdPOG() || ! apply_looseIdPOG_)
+  if(! muon.passesLooseIdPOG() && apply_looseIdPOG_)
   {
     if(debug_)
     {
@@ -92,7 +92,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.passesMediumIdPOG() || ! apply_mediumIdPOG_)
+  if(! muon.passesMediumIdPOG() && apply_mediumIdPOG_)
   {
     if(debug_)
     {