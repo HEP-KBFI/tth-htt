ec7106b9f3759b526124998a668498847bff5588
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2018-04-12 12:16:06 +0300

    constructing only kinematic based

--- interface/HadTopTaggerFill.h
+++ interface/HadTopTaggerFill.h
@@ -15,33 +15,11 @@ public:
   HadTopTaggerFill();
   ~HadTopTaggerFill();
 
-  /**
-   * @brief Calculates MVA output.
-   * @param mvaInputs Values of MVA input variables (stored in std::map with key = MVA input variable name)
-   * @return          MVA output
-   */
-	// Case III] Non-boosted top 
-  double
-  operator()(const RecoJet & recBJet,
-             const RecoJet & recWJet1,
-             const RecoJet & recWJet2);
-
-  std::vector<bool>
-  isTruth(const RecoJet & recBJet,
-          const RecoJet & recWJet1,
-          const RecoJet & recWJet2,
-          const std::vector<GenParticle> & genTopQuarks,
-          const std::vector<GenParticle> & genBJets,
-          const std::vector<GenParticle> & genWBosons);
-
-  std::vector<bool>
-  isTruth3Jet(const RecoJet & recBJet,
-              const RecoJet & recWJet1,
-              const RecoJet & recWJet2,
-              const std::vector<GenParticle> & genTopQuarks,
-              const std::vector<GenParticle> & genBJets,
-              const std::vector<GenParticle> & genWBosons,
-              const std::vector<GenParticle> & genWJets);
+  void
+  operator()(const Particle::LorentzVector & recBJet,
+                               const Particle::LorentzVector & recWJet1,
+                               const Particle::LorentzVector & recWJet2
+                             );
 
   const std::vector<std::string> &
   defineBinnings(MVAInputVarHistManager * mvaInputHistManager);
@@ -52,20 +30,6 @@ public:
   const HadTopKinFit *
   kinFit() const;
 
-
-  // Case I] Boosted top with merged b and W jets from top
-	double
-	operator()(const RecoSubjetHTTv2 & recBJet,
-						 const RecoSubjetHTTv2 & recWJet1,
-						 const RecoSubjetHTTv2 & recWJet2);
-	
-	// Case II] Boosted W with merged WJet1 and WJet2 and separate b-jet
-	double
-	operator()(const RecoJet & recBJet,
-						 const RecoSubjetAK12 & recWJet1,
-						 const RecoSubjetAK12  & recWJet2);
-
-	
 protected:
   HadTopKinFit * kinFit_;
   std::vector<std::string> mvaInputVariables_;
