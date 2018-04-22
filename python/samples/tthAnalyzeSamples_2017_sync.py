b3de24cd63013287e10766999c635defd6fb8ef9
Karl Ehatäht <karlehataht@gmail.com>
2018-04-22 19:23:53 +0300

    Update dictionary entry for the sync Ntuple

--- python/samples/tthAnalyzeSamples_2017_sync.py
+++ python/samples/tthAnalyzeSamples_2017_sync.py
@@ -2,7 +2,7 @@ from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2017 import samples_201
 
 sync_key = "/ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1/MINIAODSIM"
 
-samples_2017[sync_key]['local_paths'][0]['path'] = '/hdfs/local/karl/ttHNtupleProduction/2017/2018Apr20_ele_woPresel_nonNom_sync/ntuples/ttHJetToNonbb_M125_amcatnlo'
+samples_2017[sync_key]['local_paths'][0]['path'] = '/hdfs/local/karl/ttHNtupleProduction/2017/2018Apr21_woPresel_nonNom_sync/ntuples/ttHJetToNonbb_M125_amcatnlo'
 samples_2017[sync_key]['nof_files'] = 1
 
 for sample_key in samples_2017:
