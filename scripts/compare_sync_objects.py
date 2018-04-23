3acc02b6b4e095b77e0a3f98c1b59bcf81cca6ff
Karl Ehatäht <karlehataht@gmail.com>
2018-04-23 16:20:56 +0300

    Refactor variable names in compare_sync_objects.py

--- scripts/compare_sync_objects.py
+++ scripts/compare_sync_objects.py
@@ -46,7 +46,7 @@ class SmartFormatter(argparse.ArgumentDefaultsHelpFormatter):
       return text[2:].splitlines()
     return argparse.ArgumentDefaultsHelpFormatter._split_lines(self, text, width)
 
-def pos_float(value):
+def positive_float_type(value):
   try:
     value_float = float(value)
   except ValueError:
@@ -80,7 +80,7 @@ parser.add_argument('-n', '--max-events',
   help = 'R|Maximum number of events to be considered (default: -1, i.e. all dR-matched objects)',
 )
 parser.add_argument('-d', '--dr',
-  type = pos_float, dest = 'dr', metavar = 'cone size', default = 0.01, required = False,
+  type = positive_float_type, dest = 'dr', metavar = 'cone size', default = 0.01, required = False,
   help = 'R|Maximum cone size used in object dR-matching'
 )
 parser.add_argument('-v', '--verbose',
@@ -89,11 +89,11 @@ parser.add_argument('-v', '--verbose',
 )
 args = parser.parse_args()
 
-fn_ref  = args.input_ref
-fn_test = args.input_test
-tn      = args.tree
-maxn    = args.max_events
-dr_max  = args.dr
+filename_ref  = args.input_ref
+filename_test = args.input_test
+tree_name     = args.tree
+max_events    = args.max_events
+dr_max        = args.dr
 
 logging.basicConfig(
   stream = sys.stdout,
@@ -124,57 +124,57 @@ else:
     raise ValueError("Not all RLE numbers adhere to format 'run:lumi:event'")
   rle_selection = args.rle
 
-for fn in [fn_ref, fn_test]:
-  if not os.path.isfile(fn):
-    raise ValueError('No such file: %s' % fn)
+for filename in [filename_ref, filename_test]:
+  if not os.path.isfile(filename):
+    raise ValueError('No such file: %s' % filename)
 
-f_ref  = ROOT.TFile.Open(fn_ref,  'read')
-if not f_ref:
-  raise ValueError('File %s is not a valid ROOT file' % fn_ref)
-f_test = ROOT.TFile.Open(fn_test, 'read')
-if not f_test:
-  raise ValueError('File %s is not a valid ROOT file' % fn_test)
+file_ref  = ROOT.TFile.Open(filename_ref, 'read')
+if not file_ref:
+  raise ValueError('File %s is not a valid ROOT file' % filename_ref)
+file_test = ROOT.TFile.Open(filename_test, 'read')
+if not file_test:
+  raise ValueError('File %s is not a valid ROOT file' % filename_test)
 
-t_ref = f_ref.Get(tn)
-if not t_ref:
-  raise ValueError('File %s does not contain TTree named %s' % (fn_ref, tn))
-t_test = f_test.Get(tn)
-if not t_test:
-  raise ValueError('File %s does not contain TTree named %s' % (fn_test, tn))
+tree_ref = file_ref.Get(tree_name)
+if not tree_ref:
+  raise ValueError('File %s does not contain TTree named %s' % (filename_ref, tree_name))
+tree_test = file_test.Get(tree_name)
+if not tree_test:
+  raise ValueError('File %s does not contain TTree named %s' % (filename_test, tree_name))
 
-brns_ref    = { br.GetName() for br in t_ref.GetListOfBranches()  }
-brns_test   = { br.GetName() for br in t_test.GetListOfBranches() }
-brns_common = brns_ref & brns_test
+branch_names_ref    = { branch.GetName() for branch in tree_ref.GetListOfBranches()  }
+branch_names_test   = { branch.GetName() for branch in tree_test.GetListOfBranches() }
+branch_names_common = branch_names_ref & branch_names_test
 
-run_bn = 'run'
-ls_bn  = 'ls'
-evt_bn = 'nEvent'
+run_branch_name = 'run'
+ls_branch_name  = 'ls'
+evt_branch_name = 'nEvent'
 
-objs = {
+objects_map = {
   'mu'  : { 'n' : 2 },
   'ele' : { 'n' : 2 },
   'tau' : { 'n' : 2 },
   'jet' : { 'n' : 4 },
 }
 
-common_brns = { 'pt', 'eta', 'phi', 'E', }
+common_branch_names = { 'pt', 'eta', 'phi', 'E', }
 
-lep_brns = common_brns | {
+lepton_branch_names = common_branch_names | {
   'charge', 'dxy', 'dz', 'miniRelIso', 'miniIsoCharged', 'miniIsoNeutral', 'PFRelIso04',
   'jetNDauChargedMVASel', 'jetPtRel', 'jetPtRatio', 'jetCSV', 'sip3D', 'dxyAbs',
   'isfakeablesel', 'ismvasel', 'leptonMVA',
 }
 
-objs['mu']['brns'] = lep_brns | {
+objects_map['mu']['branch_names'] = lepton_branch_names | {
   'segmentCompatibility', 'mediumID', 'dpt_div_pt',
 }
 
-objs['ele']['brns'] = lep_brns | {
+objects_map['ele']['branch_names'] = lepton_branch_names | {
   'ntMVAeleID', 'isChargeConsistent', 'passesConversionVeto', 'nMissingHits',
   'sigmaEtaEta', 'HoE', 'deltaEta', 'deltaPhi', 'OoEminusOoP',
 }
 
-objs['tau']['brns'] = common_brns | {
+objects_map['tau']['branch_names'] = common_branch_names | {
   'charge', 'dxy', 'dz',
   'decayModeFindingOldDMs', 'decayModeFindingNewDMs',
 
@@ -200,73 +200,75 @@ objs['tau']['brns'] = common_brns | {
   'againstElectronMediumMVA6', 'againstElectronTightMVA6',
 }
 
-objs['jet']['brns'] = common_brns | { 'CSV', }
+objects_map['jet']['branch_names'] = common_branch_names | { 'CSV', }
 
 # The branch names of run, lumi and event numbers should definitely be in the sync Ntuple
-evt_brns_list = [ run_bn, ls_bn, evt_bn ]
-evt_brns = set(evt_brns_list)
-if evt_brns - brns_common:
+event_branch_names_list = [ run_branch_name, ls_branch_name, evt_branch_name ]
+event_branch_names      = set(event_branch_names_list)
+if event_branch_names - branch_names_common:
   raise ValueError(
-    'Invalid branch names for run, lumi and event numbers: %s, %s, %s' % (run_bn, ls_bn, evt_bn)
+    'Invalid branch names for run, lumi and event numbers: %s, %s, %s' % \
+    (run_branch_name, ls_branch_name, evt_branch_name)
   )
 
-# Construct explicit object-level branch names
-expl = lambda obj_pfx, obj_brns, nmax_obj: {
-  '%s%d_%s' % (obj_pfx, idx, obj_brn) for idx in range(1, nmax_obj + 1) for obj_brn in obj_brns
-}
-
 # Remove object-specific branch names that are missing from the common list of branch names
-def clean_brns(obj_brns, obj_pfx, nmax_obj):
-  obj_brns_to_remove = set()
-  for obj_brn in obj_brns:
+def filter_branch_names(obj_branch_names, obj_prefix, nmax_obj):
+  obj_branch_names_to_remove = set()
+  for obj_branch_name in obj_branch_names:
     is_present = True
     for obj_idx in range(1, nmax_obj + 1):
-      br = '%s%d_%s' % (obj_pfx, obj_idx, obj_brn)
-      if br not in brns_common:
+      br = '%s%d_%s' % (obj_prefix, obj_idx, obj_branch_name)
+      if br not in branch_names_common:
         is_present = False
     if not is_present:
-      logging.debug("Removing missing attribute '%s' from collection %s" % (obj_brn, obj_pfx))
-      obj_brns_to_remove.add(obj_brn)
-  cleaned_obj_brns = obj_brns - obj_brns_to_remove
-  for obj_brn in cleaned_obj_brns:
-    logging.debug("Attribute '%s' available for collection %s" % (obj_brn, obj_pfx))
-  return list(sorted(list(cleaned_obj_brns)))
+      logging.debug("Removing missing attribute '{}' from collection {}".format(
+        obj_branch_name, obj_prefix,
+      ))
+      obj_branch_names_to_remove.add(obj_branch_name)
+  filtered_obj_branch_names = obj_branch_names - obj_branch_names_to_remove
+  for obj_branch_name in filtered_obj_branch_names:
+    logging.debug("Attribute '%s' available for collection %s" % (obj_branch_name, obj_prefix))
+  return list(sorted(list(filtered_obj_branch_names)))
 
-for obj_pfx, obj_entry in objs.items():
-  obj_entry['brns'] = clean_brns(obj_entry['brns'], obj_pfx, obj_entry['n'])
+for obj_prefix, obj_entry in objects_map.items():
+  obj_entry['branch_names'] = filter_branch_names(
+    obj_entry['branch_names'], obj_prefix, obj_entry['n']
+  )
 
 # Reconstruct the list of common branch names once again
-def expl_brns(obj_brns, obj_pfx, nmax_obj):
+def explicit_branch_names(obj_branch_names, obj_prefix, nmax_obj):
   return {
-    '%s%d_%s' % (obj_pfx, obj_idx, obj_brn) \
-    for obj_idx in range(1, nmax_obj + 1)   \
-    for obj_brn in obj_brns
+    '%s%d_%s' % (obj_prefix, obj_idx, obj_branch_name) \
+    for obj_idx in range(1, nmax_obj + 1)           \
+    for obj_branch_name in obj_branch_names
   }
 
-brns_common_final = evt_brns
-for obj_pfx, obj_entry in objs.items():
-  brns_common_final = brns_common_final | expl_brns(obj_entry['brns'], obj_pfx, obj_entry['n'])
+branch_names_common_final = event_branch_names
+for obj_prefix, obj_entry in objects_map.items():
+  branch_names_common_final = branch_names_common_final | explicit_branch_names(
+    obj_entry['branch_names'], obj_prefix, obj_entry['n']
+  )
 
-def get_brs(t):
+def get_branches(tree):
   return {
-    br.GetName() : get_array(t, br) for br in t.GetListOfBranches() \
-    if br.GetName() in brns_common_final
+    branch.GetName() : get_array(tree, branch) for branch in tree.GetListOfBranches() \
+    if branch.GetName() in branch_names_common_final
   }
 
-brs_ref  = get_brs(t_ref)
-brs_test = get_brs(t_test)
+branches_ref  = get_branches(tree_ref)
+branches_test = get_branches(tree_test)
 
-def get_rles(t, brs):
-  n = t.GetEntries()
+def get_rles(tree, branches):
+  nof_entries = tree.GetEntries()
   rles = collections.OrderedDict()
-  for i in range(n):
-    t.GetEntry(i)
-    rle = ':'.join(map(lambda brn: str(brs[brn][0]), evt_brns_list))
-    rles[rle] = i
+  for event_idx in range(nof_entries):
+    tree.GetEntry(event_idx)
+    rle = ':'.join(map(lambda branch_name: str(branches[branch_name][0]), event_branch_names_list))
+    rles[rle] = event_idx
   return rles
 
-rle_ref  = get_rles(t_ref,  brs_ref)
-rle_test = get_rles(t_test, brs_test)
+rle_ref  = get_rles(tree_ref, branches_ref)
+rle_test = get_rles(tree_test, branches_test)
 
 # Find the list of common RLE numbers
 common_rles = []
@@ -290,33 +292,37 @@ class ParticleBase(object):
   pass
 
 class Particle(ParticleBase):
-  def __init__(self, pfx, brs, brns):
-    for brn in brns:
-      setattr(self, brn, brs['%s_%s' % (pfx, brn)][0])
+  def __init__(self, prefix, branches, branch_names):
+    for branch_name in branch_names:
+      setattr(self, branch_name, branches['%s_%s' % (prefix, branch_name)][0])
 
   def isValid(self):
     return not (self.eta < -10. or self.phi < -10.)
 
 class ParticleWrapper(object):
-  def __init__(self, pfx, brns):
-    self.pfx  = pfx
-    self.brns = brns
+  def __init__(self, prefix, branch_names):
+    self.prefix       = prefix
+    self.branch_names = branch_names
+
     self.ref  = None
     self.test = None
     self.diff = ParticleBase()
+
     self.counter = {
       'ref'    : 0,
       'test'   : 0,
       'common' : 0,
     }
     self.isMatched = False
-    self.maxBrnLen = max(map(lambda brn: len(brn), self.brns)) + 1
+    self.max_branch_name_len = max(map(lambda branch_name: len(branch_name), self.branch_names)) + 1
 
   def update(self):
-    self.ref  = Particle(self.pfx, brs_ref,  self.brns)
-    self.test = Particle(self.pfx, brs_test, self.brns)
-    for brn in self.brns:
-      setattr(self.diff, brn, getattr(self.ref, brn) - getattr(self.test, brn))
+    self.ref  = Particle(self.prefix, branches_ref, self.branch_names)
+    self.test = Particle(self.prefix, branches_test, self.branch_names)
+    for branch_name in self.branch_names:
+      setattr(
+        self.diff, branch_name, getattr(self.ref, branch_name) - getattr(self.test, branch_name)
+      )
     self.isMatched = self.get_matching()
     self.counter['ref']    += int(self.ref.isValid())
     self.counter['test']   += int(self.test.isValid())
@@ -328,71 +334,72 @@ class ParticleWrapper(object):
     dr = math.sqrt(self.diff.eta ** 2 + self.diff.phi ** 2)
     return dr < dr_max
 
-  def print(self, brns_selection = []):
-    missing_brns = set(brns_selection) - set(self.brns)
+  def print(self, branch_names_selection = []):
+    missing_brns = set(branch_names_selection) - set(self.branch_names)
     if missing_brns:
       raise ValueError(
-        'Invalid branch names requested for %s: %s' % (self.pfx, ', '.join(missing_brns))
+        'Invalid branch names requested for %s: %s' % (self.prefix, ', '.join(missing_brns))
       )
-    brns_to_print = brns_selection if brns_selection else self.brns
-    for brn in brns_to_print:
-      ref_val  = getattr(self.ref,  brn)
-      test_val = getattr(self.test, brn)
-      diff_val = getattr(self.diff, brn)
+    branch_names_to_print = branch_names_selection if branch_names_selection else self.branch_names
+    for branch_name in branch_names_to_print:
+      ref_value  = getattr(self.ref,  branch_name)
+      test_value = getattr(self.test, branch_name)
+      diff_value = getattr(self.diff, branch_name)
 
-      ref_fmt  = '{:>13.6f}' if type(ref_val)  == float else '{:^13}'
-      test_fmt = '{:>13.6f}' if type(test_val) == float else '{:^13}'
-      diff_fmt = '{:>13.6f}' if type(diff_val) == float else '{:^13}'
+      ref_format  = '{:>13.6f}' if type(ref_value)  == float else '{:^13}'
+      test_format = '{:>13.6f}' if type(test_value) == float else '{:^13}'
+      diff_format = '{:>13.6f}' if type(diff_value) == float else '{:^13}'
 
-      ref_print  = ref_fmt.format(ref_val)   if ref_val  != -9999 else '{:^13}'.format('-')
-      test_print = test_fmt.format(test_val) if test_val != -9999 else '{:^13}'.format('-')
-      diff_print = diff_fmt.format(diff_val) if ref_val  != -9999 and \
-                                                test_val != -9999 else '{:^13}'.format('-')
+      ref_str  = ref_format.format(ref_value)   if ref_value  != -9999 else '{:^13}'.format('-')
+      test_str = test_format.format(test_value) if test_value != -9999 else '{:^13}'.format('-')
+      diff_str = diff_format.format(diff_value) if ref_value  != -9999 and \
+                                                   test_value != -9999 else '{:^13}'.format('-')
 
       print('  {}  {:{len}}  {}  vs  {}  => {}'.format(
-        self.pfx, brn, ref_print, test_print, diff_print, len = self.maxBrnLen
+        self.prefix, branch_name, ref_str, test_str, diff_str, len = self.max_branch_name_len
       ))
 
   def get_summary(self):
     print(
       '{:<6} {:>7} in ref, {:>7} in test, {:>7} dR-matched'.format(
-      '%s:' % self.pfx, self.counter['ref'], self.counter['test'], self.counter['common']
+      '%s:' % self.prefix, self.counter['ref'], self.counter['test'], self.counter['common']
     ))
 
 class Event(object):
-  def __init__(self, maxn):
-    self.maxn    = maxn
-    self.counter = 0
-    self.obj_list = []
-    for obj_pfx, obj_entry in objs.items():
+  def __init__(self, max_events):
+    self.max_events = max_events
+    self.counter    = 0
+
+    self.objects_list = []
+    for obj_prefix, obj_entry in objects_map.items():
       for obj_idx in range(1, obj_entry['n'] + 1):
-        pfx = '%s%d' % (obj_pfx, obj_idx)
-        setattr(self, pfx, ParticleWrapper(pfx, obj_entry['brns']))
-        self.obj_list.append(pfx)
+        prefix = '%s%d' % (obj_prefix, obj_idx)
+        setattr(self, prefix, ParticleWrapper(prefix, obj_entry['branch_names']))
+        self.objects_list.append(prefix)
 
   def update(self):
-    for obj_pfx in self.obj_list:
-      pw = getattr(self, obj_pfx)
-      pw.update()
+    for obj_prefix in self.objects_list:
+      particle_instance = getattr(self, obj_prefix)
+      particle_instance.update()
     self.counter += 1
 
   def cont(self):
-    return self.maxn < 0 or (0 <= self.counter < self.maxn)
+    return self.max_events < 0 or (0 <= self.counter < self.max_events)
 
   def get_summary(self):
     print('Total number of events considered: %d' % self.counter)
-    for obj_pfx in self.obj_list:
-      pw = getattr(self, obj_pfx)
-      pw.get_summary()
+    for obj_prefix in self.objects_list:
+      particle_instance = getattr(self, obj_prefix)
+      particle_instance.get_summary()
 
-evt = Event(maxn)
+evt = Event(max_events)
 
 for rle in rle_loop:
   if not evt.cont():
     break
 
-  t_ref.GetEntry(rle_ref[rle])
-  t_test.GetEntry(rle_test[rle])
+  tree_ref.GetEntry(rle_ref[rle])
+  tree_test.GetEntry(rle_test[rle])
 
   evt.update()
 
