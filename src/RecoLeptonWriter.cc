7a49110740361a94667ab037ab31256ac8b22970
Karl Ehatäht <karlehataht@gmail.com>
2018-04-26 15:43:51 +0300

    Read deepCSV (not CSVv2) score of the associated jets to the leptons

--- src/RecoLeptonWriter.cc
+++ src/RecoLeptonWriter.cc
@@ -86,7 +86,7 @@ void RecoLeptonWriter::setBranchNames()
   branchName_mvaRawTTH_ = Form("%s_%s", branchName_obj_.data(), "mvaTTH");
   branchName_jetPtRatio_ = Form("%s_%s", branchName_obj_.data(), "jetPtRatio_TTHMVA");
   branchName_jetPtRel_ = Form("%s_%s", branchName_obj_.data(), "jetPtRelv2_TTHMVA");
-  branchName_jetBtagCSV_ = Form("%s_%s", branchName_obj_.data(), "jetBTagCSV_TTHMVA");
+  branchName_jetBtagCSV_ = Form("%s_%s", branchName_obj_.data(), "jetBTagDeepCSV_TTHMVA");
   branchName_jetNDauChargedMVASel_ = Form("%s_%s", branchName_obj_.data(), "jetNDauChargedMVASel");
   branchName_tightCharge_ = Form("%s_%s", branchName_obj_.data(), "tightCharge");
   branchName_charge_ = Form("%s_%s", branchName_obj_.data(), "charge");
