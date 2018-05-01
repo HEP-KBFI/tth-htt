e65f7d8786bad12599ed4e4465c6d89beacdecfc
Karl Ehatäht <karlehataht@gmail.com>
2018-05-01 18:42:00 +0300

    Require at least 2 jets in 2l2tau channel
    As per documentation: https://gitlab.cern.ch/ttH_leptons/doc/blob/master/2017/event_selection.md#43-2-leptons-2-tau

--- test/templates/analyze_2l_2tau_cfg.py
+++ test/templates/analyze_2l_2tau_cfg.py
@@ -78,8 +78,7 @@ process.analyze_2l_2tau = cms.PSet(
         )
     ),
 
-    ##minNumJets = cms.int32(2),
-    minNumJets = cms.int32(1),
+    minNumJets = cms.int32(2),
 
     use_HIP_mitigation_bTag = cms.bool(False),
     use_HIP_mitigation_mediumMuonId = cms.bool(False),
