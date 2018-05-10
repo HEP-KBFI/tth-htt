c09aeb1141e450ef0c1422f15c38420a01dc6e5f
Karl Ehatäht <karlehataht@gmail.com>
2018-05-10 14:14:27 +0300

    Implement sbatch submission for gathering the PU profiles
    TODO: hadd & plot steps

--- python/configs/puHistogramConfig.py
+++ python/configs/puHistogramConfig.py
@@ -1,26 +1,29 @@
 import os, logging, uuid
 
 from tthAnalysis.HiggsToTauTau.jobTools import create_if_not_exists, run_cmd, get_log_version
-from tthAnalysis.HiggsToTauTau.analysisTools import initDict, getKey, create_cfg, generateInputFileList
+from tthAnalysis.HiggsToTauTau.analysisTools import initDict, getKey, createFile, generateInputFileList
 from tthAnalysis.HiggsToTauTau.analysisTools import createMakefile as tools_createMakefile
 from tthAnalysis.HiggsToTauTau.sbatchManagerTools import createScript_sbatch as tools_createScript_sbatch
 
+DKEY_SCRIPTS    = "scripts"
 DKEY_CFGS       = "cfgs"
+DKEY_HISTO_TMP  = "tmp_histograms"
 DKEY_HISTO      = "histograms"
+DKEY_PLOTS      = "plots"
 DKEY_LOGS       = "logs"
 MAKEFILE_TARGET = "sbatch_puProfile"
 
 class puHistogramConfig:
-    """Configuration metadata needed to run Ntuple production.
+    """Configuration metadata needed to run PU profile production.
 
     Args:
         configDir:             The root config dir -- all configuration files are stored in its subdirectories
         outputDir:             The root output dir -- all log and output files are stored in its subdirectories
-        executable_puProfile: Name of the executable that runs the Ntuple production
-        debug:                 if True, checks each input root file (Ntuple) before creating the python configuration files
+        executable:            Name of the executable that runs the PU profile production
+        check_input_files:     if True, checks each input root file (Ntuple) before creating the python configuration files
         running_method:        either `sbatch` (uses SLURM) or `Makefile`
         num_parallel_jobs:     number of jobs that can be run in parallel on local machine
-                               (does not limit number of Ntuple production jobs running in parallel on batch system)
+                               (does not limit number of PU profile production jobs running in parallel on batch system)
 
     """
     def __init__(self, configDir, outputDir, executable,
@@ -66,32 +69,37 @@ class puHistogramConfig:
         ))
 
         self.sbatchFile_puProfile = os.path.join(self.configDir, "sbatch_puProfile.py")
-        self.cfgFiles_puProfile_modified = {}
-        self.logFiles_puProfile          = {}
+        self.cfgFiles_puProfile = {}
+        self.logFiles_puProfile = {}
+        self.jobOptions_sbatch = {}
 
-        self.inputFiles   = {}
-        self.outputFiles  = {}
+        self.inputFiles      = {}
+        self.outputFiles_tmp = {}
+        self.outputFiles     = {}
+
         self.filesToClean = []
-        self.dirs         = {}
+        self.dirs = {}
         for sample_name, sample_info in self.samples.items():
             if not sample_info["use_it"]:
                 continue
             process_name = sample_info["process_name_specific"]
             key_dir = getKey(sample_name)
-            for dir_type in [ DKEY_CFGS, DKEY_HISTO, DKEY_LOGS ]:
+            for dir_type in [ DKEY_CFGS, DKEY_HISTO_TMP, DKEY_HISTO, DKEY_PLOTS, DKEY_LOGS, DKEY_SCRIPTS ]:
                 initDict(self.dirs, [ key_dir, dir_type ])
-                if dir_type in [ DKEY_CFGS, DKEY_LOGS ]:
+                if dir_type in [ DKEY_CFGS, DKEY_LOGS, DKEY_PLOTS, DKEY_SCRIPTS ]:
                     self.dirs[key_dir][dir_type] = os.path.join(self.configDir, dir_type, process_name)
                 else:
                     self.dirs[key_dir][dir_type] = os.path.join(self.outputDir, dir_type, process_name)
-        for dir_type in [ DKEY_CFGS, DKEY_LOGS ]:
+        for dir_type in [ DKEY_CFGS, DKEY_LOGS, DKEY_PLOTS, DKEY_SCRIPTS ]:
             initDict(self.dirs, [ dir_type ])
-            if dir_type in [ DKEY_CFGS, DKEY_HISTO, DKEY_LOGS ]:
+            if dir_type in [ DKEY_CFGS, DKEY_LOGS, DKEY_PLOTS, DKEY_SCRIPTS ]:
                 self.dirs[dir_type] = os.path.join(self.configDir, dir_type)
             else:
                 self.dirs[dir_type] = os.path.join(self.outputDir, dir_type)
 
         self.cvmfs_error_log = {}
+        self.num_jobs = {}
+
 
     def createCfg_puProfile(self, jobOptions):
         """Create python configuration file for the puProfile.sh script
@@ -100,25 +108,81 @@ class puHistogramConfig:
           inputFiles: list of input files (Ntuples)
           outputFile: output file of the job -- a ROOT file containing histogram
         """
+        lines = jobOptions['inputFiles'] + [ '', jobOptions['outputFile'] ]
+        assert(len(lines) >= 3)
+        createFile(jobOptions['cfgFile_path'], lines, nofNewLines = 1)
+
+    def createCfg_hadd(self, jobOptions):
         pass
 
-    def createScript_sbatch(self):
-        """Creates the python script necessary to submit the Ntuple production jobs to the batch system
-        """
-        pass
+    def createScript_sbatch(self, executable, sbatchFile, jobOptions,
+                            key_cfg_file = 'cfgFile_path', key_input_file = 'inputFiles',
+                            key_output_file = 'outputFile', key_log_file = 'logFile',
+                            skipFileSizeCheck = True):
+        num_jobs = tools_createScript_sbatch(
+            sbatch_script_file_name = sbatchFile,
+            executable              = executable,
+            command_line_parameters = { key: value[key_cfg_file]    for key, value in jobOptions.items() },
+            input_file_names        = { key: value[key_input_file]  for key, value in jobOptions.items() },
+            output_file_names       = { key: value[key_output_file] for key, value in jobOptions.items() },
+            script_file_names       = { key: value[key_cfg_file]    for key, value in jobOptions.items() },
+            log_file_names          = { key: value[key_log_file]    for key, value in jobOptions.items() },
+            working_dir             = self.workingDir,
+            max_num_jobs            = self.max_num_jobs,
+            cvmfs_error_log         = self.cvmfs_error_log,
+            pool_id                 = uuid.uuid4(),
+            verbose                 = self.verbose,
+            dry_run                 = self.dry_run,
+            job_template_file       = 'sbatch-node.produce.sh.template',
+            skipFileSizeCheck       = skipFileSizeCheck,
+        )
+        return num_jobs
 
     def addToMakefile_puProfile(self, lines_makefile):
-        """Adds the commands to Makefile that are necessary for running the Ntuple production code
+        """Adds the commands to Makefile that are necessary for running the PU profile production code
         """
-        pass
+        if self.is_sbatch:
+            lines_makefile.extend([
+                "%s:" % MAKEFILE_TARGET,
+                "\t%s %s" % ("python", self.sbatchFile_puProfile),
+                "",
+            ])
+        for key_file, output_file in self.outputFiles_tmp.items():
+            cfg_file = self.cfgFiles_puProfile[key_file]
+            if self.is_makefile:
+                log_file = self.logFiles_puProfile[key_file]
+                lines_makefile.extend([
+                    "%s:" % output_file,
+                    "\t%s %s &> %s" % (self.executable, cfg_file, log_file),
+                    "",
+                ])
+            elif self.is_sbatch:
+                lines_makefile.extend([
+                    "%s: %s" % (output_file, MAKEFILE_TARGET),
+                    "\t%s" % ":",
+                    "",
+                ])
+            self.filesToClean.append(output_file)
+
+    def addToMakefile_hadd(self, lines_makefile):
+        scriptFiles = {}
+        jobOptions = {}
+        for key_file, output_file in self.outputFiles.items():
+            pass
 
     def createMakefile(self, lines_makefile):
-        """Creates Makefile that runs the Ntuple production.
+        """Creates Makefile that runs the PU profile production.
         """
-        pass
+        targets = None
+        if self.is_sbatch:
+            targets = [ MAKEFILE_TARGET ]
+        else:
+            targets = self.outputFiles.values()
+        tools_createMakefile(self.makefile, targets, lines_makefile, self.filesToClean)
+        logging.info("Run it with:\tmake -f %s -j %i " % (self.makefile, self.num_parallel_jobs))
 
     def create(self):
-        """Creates all necessary config files and runs the Ntuple production -- either locally or on the batch system
+        """Creates all necessary config files and runs the PU profile production -- either locally or on the batch system
         """
 
         for key in self.dirs.keys():
@@ -140,6 +204,12 @@ class puHistogramConfig:
 
             inputFileList = generateInputFileList(sample_info, self.max_files_per_job, self.check_input_files)
             key_dir = getKey(sample_name)
+
+            outputFile = os.path.join(
+                self.dirs[key_dir][DKEY_HISTO], "%s.root" % process_name
+            )
+            self.outputFiles[outputFile] = []
+
             for jobId in inputFileList.keys():
 
                 key_file = getKey(sample_name, jobId)
@@ -151,13 +221,42 @@ class puHistogramConfig:
                     )
                     continue
 
-                jobOptions = {
+                self.cfgFiles_puProfile[key_file] = os.path.join(
+                    self.dirs[key_dir][DKEY_CFGS], "puProfile_%s_%i_cfg.txt" % (process_name, jobId)
+                )
+                self.outputFiles_tmp[key_file] = os.path.join(
+                    self.dirs[key_dir][DKEY_HISTO_TMP], "histogram_%i.root" % jobId
+                )
+                self.logFiles_puProfile[key_file] = os.path.join(
+                    self.dirs[key_dir][DKEY_LOGS], "puProfile_%s_%i.log" % (process_name, jobId)
+                )
+                self.jobOptions_sbatch[key_file] = {
+                    'inputFiles'   : self.inputFiles[key_file],
+                    'cfgFile_path' : self.cfgFiles_puProfile[key_file],
+                    'outputFile'   : self.outputFiles_tmp[key_file],
+                    'logFile'      : self.logFiles_puProfile[key_file],
                 }
-                self.createCfg_puProfile(jobOptions)
+                self.createCfg_puProfile(self.jobOptions_sbatch[key_file])
+                self.outputFiles[outputFile].append(self.outputFiles_tmp[key_file])
 
-        return 0
+        if self.is_sbatch:
+          logging.info("Creating script for submitting '%s' jobs to batch system" % self.executable)
+          self.sbatchFile = os.path.join(self.dirs[DKEY_SCRIPTS], "sbatch_puProfile.py")
+          self.num_jobs['puProfile'] = self.createScript_sbatch(self.executable, self.sbatchFile, self.jobOptions_sbatch)
+
+        logging.info("Creating Makefile")
+        lines_makefile = []
+        self.addToMakefile_puProfile(lines_makefile)
+        #self.addToMakefile_hadd(lines_makefile)
+        #self.addToMakefile_plot(lines_makefile)
+        self.createMakefile(lines_makefile)
+        logging.info("Done")
+
+
+        print(self.num_jobs)
+        return self.num_jobs
 
     def run(self):
-        """Runs all Ntuple production jobs -- either locally or on the batch system.
+        """Runs all PU profile production jobs -- either locally or on the batch system.
         """
         pass
