69f114a7d03ec6f79abe7071549a2038fcc08368
Karl Ehatäht <karlehataht@gmail.com>
2018-05-18 00:51:27 +0300

    Still veto events w/ >1 taus passing Medium tau ID WP in 2lss1tau
    The gitlab documentation needs to be updated instead.

--- bin/analyze_2lss_1tau.cc
+++ bin/analyze_2lss_1tau.cc
@@ -1510,8 +1510,8 @@ TMVAInterface mva_Hjj_tagger(mvaFileName_Hjj_tagger, mvaInputVariables_Hjj_tagge
     cutFlowHistManager->fillHistograms(">= 1 sel tau (2)", evtWeight);
 
     // veto events containing more than one tau passing the Medium WP, to avoid overlap with the 2l+2tau category
-    // apply tau veto only in the SR (== 2 selected leptons and a tau are both tight)
-    if ( vetoHadTaus.size() > 1 && tightLeptons.size() == 2 && tightHadTaus.size() == 1 ) {
+    // we want to keep all cuts consistent in SR and fake CR, to obtain a consistent estimate of the fake background
+    if ( !(vetoHadTaus.size() <= 1) ) {
       if ( run_lumi_eventSelector ) {
         std::cout << "event " << eventInfo.str() << " FAILS vetoHadTaus selection." << std::endl;
         printCollection("vetoHadTaus", vetoHadTaus);
