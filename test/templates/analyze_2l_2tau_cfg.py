b8c27edd5e4b0dca844b7087cb330d5a7e8c7ae0
Karl Ehatäht <karlehataht@gmail.com>
2018-05-17 21:10:52 +0300

    Import recommendedMEtFilters to 2l2tau cfg

--- test/templates/analyze_2l_2tau_cfg.py
+++ test/templates/analyze_2l_2tau_cfg.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
-
 import os
+
+from tthAnalysis.HiggsToTauTau.recommendedMEtFilters_cfi import recommendedMEtFilters
 
 process = cms.PSet()
 
@@ -86,6 +87,7 @@ process.analyze_2l_2tau = cms.PSet(
     apply_trigger_bits = cms.bool(False),
     apply_hlt_filter = cms.bool(False),
     apply_met_filters = cms.bool(True),
+    cfgMEtFilter = recommendedMEtFilters,
     apply_hadTauFakeRateSF = cms.bool(False),
 
     fillGenEvtHistograms = cms.bool(False),
