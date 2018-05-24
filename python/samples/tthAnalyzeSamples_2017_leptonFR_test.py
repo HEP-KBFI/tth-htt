18e1187c02f83b80cf7626476d51699d06490384
Karl Ehatäht <karlehataht@gmail.com>
2018-05-24 12:45:33 +0300

    Remove reHLT from smaller sample dictionaries and from the code
    Will soon remove apply_trigger_bits and regenerate the larger dictionaries w/o reHLT entry.

--- python/samples/tthAnalyzeSamples_2017_leptonFR_test.py
+++ python/samples/tthAnalyzeSamples_2017_leptonFR_test.py
@@ -16,7 +16,6 @@ samples_2017["/SingleMuon/Run2017E-17Nov2017-v1/MINIAOD"] = OD([
   ("use_HIP_mitigation_mediumMuonId", False),
   ("use_it",                          True),
   ("triggers",                        ['1mu']),
-  ("reHLT",                           True),
   ("local_paths",
     [
       OD([
