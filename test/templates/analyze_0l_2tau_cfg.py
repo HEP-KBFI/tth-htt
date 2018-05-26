4128285207bf10cf767089e228ad6d4e4f522677
Aruna <Aruna.Nayak@cern.ch>
2018-05-26 12:55:53 +0300

    add HTT to 0l_2tau

--- test/templates/analyze_0l_2tau_cfg.py
+++ test/templates/analyze_0l_2tau_cfg.py
@@ -70,6 +70,13 @@ process.analyze_0l_2tau = cms.PSet(
     branchName_genLeptons = cms.string('GenLep'),
     branchName_genHadTaus = cms.string('GenVisTau'),
     branchName_genJets = cms.string('GenJet'),
+    redoGenMatching = cms.bool(True),
+
+    branchName_genTopQuarks = cms.string('GenTop'),
+    branchName_genBJets = cms.string('GenBQuarkFromTop'),
+    branchName_genWBosons = cms.string('GenVbosons'),
+    branchName_genWJets = cms.string('GenWZQuark'),
+    branchName_genQuarkFromTop = cms.string('GenQuarkFromTop'),
 
     selEventsFileName_input = cms.string(''),
     selEventsFileName_output = cms.string(''),
