0a439b7b97402fa17934adb0c9ef41a92b57acd3
Karl Ehatäht <karlehataht@gmail.com>
2018-05-27 14:25:51 +0300

    Correctly subtract histograms w/ labeled bins
    Same issue as described in 93caee24ed2e30c812f7686c7203cb3f45ec8c6e, but now w/ subtraction. Tested.

--- interface/histogramAuxFunctions.h
+++ interface/histogramAuxFunctions.h
@@ -40,7 +40,10 @@ checkCompatibleBinning(const TH1 * histogram1,
                        const TH1 * histogram2);
 
 bool
-checkIfLabeledHistograms(const std::vector<TH1 *> & histogramsToAdd);
+checkIfLabeledHistogram(const TH1 * histogram);
+
+bool
+checkIfLabeledHistograms(const std::vector<TH1 *> & histograms);
 
 TH1 *
 addHistograms(const std::string & newHistogramName,
