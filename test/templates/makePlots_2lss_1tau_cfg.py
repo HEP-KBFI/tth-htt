ab84aa2c7be0c633872c33616b85ad7dd01cb2c1
Karl Ehatäht <karlehataht@gmail.com>
2018-06-01 02:29:46 +0300

    Add tH to 2lss1tau makePlots cfg

--- test/templates/makePlots_2lss_1tau_cfg.py
+++ test/templates/makePlots_2lss_1tau_cfg.py
@@ -44,6 +44,7 @@ process.makePlots.distributions.extend([
 process.makePlots.nuisanceParameters.normalization = cms.PSet(
     signal = cms.string("1.0 +/- 0.20"),
     TT = cms.string("1.0 +/- 0.20"),
+    tH = cms.string("1.0 +/- 0.20"),
     TTW = cms.string("1.0 +/- 0.20"),
     TTWW = cms.string("1.0 +/- 0.20"),
     TTZ = cms.string("1.0 +/- 0.20"),
