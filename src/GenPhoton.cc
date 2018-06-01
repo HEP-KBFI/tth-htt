aa9a3c22fa53661c002680843cbd5cf2c9487fcb
Karl Ehatäht <karlehataht@gmail.com>
2018-06-01 21:26:15 +0300

    Cast GenPhoton to GenParticle when printing it out

--- src/GenPhoton.cc
+++ src/GenPhoton.cc
@@ -12,6 +12,6 @@ std::ostream &
 operator<<(std::ostream & stream,
            const GenPhoton & genPhoton)
 {
-  stream << static_cast<const GenPhoton &>(genPhoton);
+  stream << static_cast<const GenParticle &>(genPhoton);
   return stream;
 }
