aa239212d97fa730c2ff505c2ddb9f474eba1b71
Karl Ehatäht <karlehataht@gmail.com>
2018-06-19 18:27:04 +0300

    Do not read HLT branches from Ntuples where they're missing
    Compiles, untested.

--- src/hltPath.cc
+++ src/hltPath.cc
@@ -20,7 +20,18 @@ hltPath::hltPath(const std::string & branchName,
 void
 hltPath::setBranchAddress(TTree * tree)
 {
-  tree->SetBranchAddress(branchName_.data(), &value_);
+  if(available_branches_.empty())
+  {
+    this->set_available_branches(tree);
+  }
+  if(std::find(available_branches_.cbegin(), available_branches_.cend(), branchName_) != available_branches_.cend())
+  {
+    tree->SetBranchAddress(branchName_.data(), &value_);
+  }
+  else
+  {
+    std::cout << "Branch '" << branchName_ << "' not available, defaulting to false\n";
+  }
 }
 
 void
@@ -38,7 +49,6 @@ hltPath::getBranchName() const
 Int_t
 hltPath::getValue() const
 {
-  assert(value_ == 0 || value_ == 1);
   return value_;
 }
 
@@ -58,6 +68,19 @@ const std::string &
 hltPath::getLabel() const
 {
   return label_;
+}
+
+void
+hltPath::set_available_branches(TTree * tree)
+{
+  TObjArray * arr = tree->GetListOfBranches();
+  TIter it(arr);
+  TObject * obj = nullptr;
+  while((obj = it.Next()))
+  {
+    available_branches_.push_back(obj->GetName());
+  }
+  delete obj;
 }
 
 std::vector<hltPath *>
@@ -90,7 +113,7 @@ hltPaths_isTriggered(const std::vector<hltPath *> & hltPaths,
     hltPaths.cbegin(), hltPaths.cend(),
     [verbose](hltPath * const & path) -> bool
     {
-      const bool passes = path->getValue() >= 1;
+      const bool passes = path->getValue();
       if(verbose)
       {
         std::cout << "  " << path->getLabel() << ": " << *path;
