44289fe8f8a0ad21ca0d5c40006626d3ad7f44bc
Karl Ehatäht <karlehataht@gmail.com>
2018-07-03 17:13:17 +0300

    Write Bool_t instead of Int_t in hltPathWriter
    Also, polished/modernized some code.

--- interface/hltPathReader.h
+++ interface/hltPathReader.h
@@ -2,29 +2,28 @@
 #define tthAnalysis_HiggsToTauTau_hltPathReader_h
 
 #include "tthAnalysis/HiggsToTauTau/interface/ReaderBase.h" // ReaderBase
-#include "tthAnalysis/HiggsToTauTau/interface/hltPath.h" // hltPath
 
-#include <Rtypes.h> // Int_t
+#include <vector> // std::vector<,>
 
-#include <vector>
-#include <string>
+// forward declaration
+class hltPath;
 
 class hltPathReader
   : public ReaderBase
 {
- public:
-  hltPathReader(const std::vector<hltPath*> & hltPaths);
-  hltPathReader(const std::vector<std::vector<hltPath*>> & hltPaths);
+public:
+  hltPathReader(const std::vector<hltPath *> & hltPaths);
+  hltPathReader(const std::vector<std::vector<hltPath *>> & hltPaths);
   ~hltPathReader() {}
 
   void
   setBranchAddresses(TTree * tree) override;
 
- private:
+private:
   std::vector<std::string>
   get_available_branches(TTree * tree) const;
 
-  std::vector<hltPath*> hltPaths_;
+  std::vector<hltPath *> hltPaths_;
 };
 
 #endif // tthAnalysis_HiggsToTauTau_hltPathReader_h
--- src/hltPathWriter.cc
+++ src/hltPathWriter.cc
@@ -3,15 +3,15 @@
 #include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException()
 #include "tthAnalysis/HiggsToTauTau/interface/BranchAddressInitializer.h" // BranchAddressInitializer, TTree, Form()
 
-hltPathWriter::hltPathWriter(const std::vector<std::string>& hltPath_branchNames)
+hltPathWriter::hltPathWriter(const std::vector<std::string> & hltPath_branchNames)
 {
-  for ( std::vector<std::string>::const_iterator branchName = hltPath_branchNames.begin();
-	branchName != hltPath_branchNames.end(); ++branchName ) {
+  for(const std::string & branchName: hltPath_branchNames)
+  {
     branchEntryType branch;
-    branch.branchName_ = *branchName;
-    branch.value_ = 0;
+    branch.branchName_ = branchName;
+    branch.value_ = false;
     branch.value_isInitialized_ = false;
-    branches_[*branchName] = branch;
+    branches_[branchName] = branch;
   }
 }
 
@@ -19,36 +19,41 @@ void
 hltPathWriter::setBranches(TTree * tree)
 {
   BranchAddressInitializer bai(tree);
-
-  for ( std::map<std::string, branchEntryType>::iterator branch = branches_.begin(); 
-	branch != branches_.end(); ++branch ) {
-    bai.setBranch(branch->second.value_, branch->second.branchName_);
+  for(auto & branch: branches_)
+  {
+    bai.setBranch(branch.second.value_, branch.second.branchName_);
   }
 }
 
 void
-hltPathWriter::write(const std::vector<hltPath*> & hltPaths)
+hltPathWriter::write(const std::vector<hltPath *> & hltPaths)
 {
-  for ( std::map<std::string, branchEntryType>::iterator branch = branches_.begin(); 
-	branch != branches_.end(); ++branch ) {
-    branch->second.value_isInitialized_ = false;
+  for(auto & branch: branches_)
+  {
+    branch.second.value_isInitialized_ = false;
   }
-  for ( std::vector<hltPath*>::const_iterator hltPath = hltPaths.begin();
-	hltPath != hltPaths.end(); ++hltPath ) {
-    std::map<std::string, branchEntryType>::iterator branch = branches_.find((*hltPath)->getBranchName());
-    if ( branch != branches_.end() ) {
-      branch->second.value_ = (*hltPath)->getValue();
+
+  for(const hltPath * const hltPath_: hltPaths)
+  {
+    std::map<std::string, branchEntryType>::iterator branch = branches_.find(hltPath_->getBranchName());
+    if(branch != branches_.end())
+    {
+      branch->second.value_ = hltPath_->getValue();
       branch->second.value_isInitialized_ = true;
-    } else {
+    }
+    else
+    {
       throw cmsException(__func__, __LINE__)
-	<< " HLT path = '" << (*hltPath)->getBranchName() << "' has not been declared !!\n";
+        << " HLT path = '" << hltPath_->getBranchName() << "' has not been declared";
     }
   }
-  for ( std::map<std::string, branchEntryType>::iterator branch = branches_.begin(); 
-	branch != branches_.end(); ++branch ) {
-    if ( !branch->second.value_isInitialized_ ) {
+
+  for(const auto & branch: branches_)
+  {
+    if(! branch.second.value_isInitialized_)
+    {
       throw cmsException(__func__, __LINE__)
-	<< " HLT path = '" << branch->second.branchName_ << "' has not been initialized !!\n";
+        << " HLT path = '" << branch.second.branchName_ << "' has not been initialized";
     }
   }
 }
