b78aaff6a1d5e75617dcfb147fcb6df0de11186e
Karl Ehatäht <karlehataht@gmail.com>
2018-07-19 17:51:39 +0300

    Add meta dicts for 2016 MC samples

---
+++ python/samples/metaDict_2016.py
@@ -0,0 +1,10 @@
+from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_data import meta_dictionary as meta_dictionary_data
+from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_fastsim import meta_dictionary as meta_dictionary_fastsim
+from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_mc import meta_dictionary as meta_dictionary_mc
+from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_mc import sum_events
+
+import itertools, collections
+
+meta_dictionary = collections.OrderedDict(itertools.chain(
+  meta_dictionary_data.items(), meta_dictionary_mc.items(), meta_dictionary_fastsim.items()
+))
