3d4d5f9a8c5a68a94b1928487f4e54636bf76bd8
Karl Ehatäht <karlehataht@gmail.com>
2018-07-20 20:00:40 +0300

    Fill genEvtHistManager_afterCuts w/ inclusive event weight
    (as opposed to the lumiscale weight passed on via py cfg files)

--- bin/analyze_jetToTauFakeRate.cc
+++ bin/analyze_jetToTauFakeRate.cc
@@ -1060,7 +1060,7 @@ int main(int argc, char* argv[])
     }
 
     if ( isMC ) {
-      genEvtHistManager_afterCuts->fillHistograms(genElectrons, genMuons, genHadTaus, genPhotons, genJets, lumiScale);
+      genEvtHistManager_afterCuts->fillHistograms(genElectrons, genMuons, genHadTaus, genPhotons, genJets, evtWeight_inclusive);
       lheInfoHistManager->fillHistograms(*lheInfoReader, evtWeight);
     }
 
