2ca0c7f26be7020e3e8683e5009e48e8080019a4
Karl Ehatäht <karlehataht@gmail.com>
2018-07-24 18:57:12 +0300

    Remove era restrictions from EvtHistManagers
    Task: https://github.com/HEP-KBFI/tth-htt/issues/46

--- src/RecoElectronCollectionSelectorFakeable.cc
+++ src/RecoElectronCollectionSelectorFakeable.cc
@@ -1,6 +1,6 @@
 #include "tthAnalysis/HiggsToTauTau/interface/RecoElectronCollectionSelectorFakeable.h" // RecoElectronSelectorFakeable
 
-#include "tthAnalysis/HiggsToTauTau/interface/analysisAuxFunctions.h" // kEra_2017
+#include "tthAnalysis/HiggsToTauTau/interface/analysisAuxFunctions.h" // kEra_*
 #include "tthAnalysis/HiggsToTauTau/interface/cmsException.h" // cmsException(), assert()
 
 RecoElectronSelectorFakeable::RecoElectronSelectorFakeable(int era,
