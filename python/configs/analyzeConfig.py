402c8850b06a80ae2bc1db2065bb3e4adbdab3e7
Karl Ehatäht <karlehataht@gmail.com>
2018-09-06 12:34:27 -0500

    Remove redundant createCfg_makePlots_mcClosure functions

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -416,6 +416,7 @@ class analyzeConfig(object):
             raise ValueError('Invalid era: %s' % self.era)
         self.isBDTtraining = False
         self.mcClosure_dir = {}
+        self.cfgFile_make_plots_mcClosure = ''
 
     def __del__(self):
         for hostname, times in self.cvmfs_error_log.items():
@@ -760,9 +761,9 @@ class analyzeConfig(object):
 
     def createCfg_addTailFits(self, jobOptions):
         """Create python configuration file for the addBackgrounds_TailFit executable (Tail Fitting of histograms)
-        Args:                                                                                                                                                                                          
-        inputFiles: input file (the ROOT file produced by hadd_stage1)                                                                                                                         
-        outputFile: output file of the job                                                                                                                                                          
+        Args:
+        inputFiles: input file (the ROOT file produced by hadd_stage1)
+        outputFile: output file of the job
         """
         lines = []
         lines.append("process.fwliteInput.fileNames = cms.vstring('%s')" % jobOptions['inputFile'])
@@ -937,6 +938,26 @@ class analyzeConfig(object):
         lines.append(")")
         lines.append("process.makePlots.intLumiData = cms.double(%.1f)" % self.lumi)
         create_cfg(self.cfgFile_make_plots, jobOptions['cfgFile_modified'], lines)
+
+    def createCfg_makePlots_mcClosure(self, jobOptions): #TODO
+      """Fills the template of python configuration file for making control plots
+
+      Args:
+        histogramFile: name of the input ROOT file
+      """
+      lines = []
+      lines.append("process.fwliteInput.fileNames = cms.vstring('%s')" % jobOptions['inputFile'])
+      lines.append("process.makePlots.outputFileName = cms.string('%s')" % jobOptions['outputFile'])
+      lines.append("process.makePlots.processesBackground = cms.vstring(%s)" % self.make_plots_backgrounds)
+      lines.append("process.makePlots.processSignal = cms.string('%s')" % self.make_plots_signal)
+      lines.append("process.makePlots.categories = cms.VPSet(")
+      lines.append("  cms.PSet(")
+      lines.append("    signal = cms.string('%s')," % self.histogramDir_prep_dcard)
+      lines.append("    sideband = cms.string('%s')," % self.histogramDir_prep_dcard.replace("Tight", "Fakeable_mcClosure_wFakeRateWeights"))
+      lines.append("    label = cms.string('%s')" % self.channel)
+      lines.append("  )")
+      lines.append(")")
+      create_cfg(self.cfgFile_make_plots_mcClosure, jobOptions['cfgFile_modified'], lines)
 
     def createScript_sbatch(self, executable, sbatchFile, jobOptions,
                             key_cfg_file = 'cfgFile_modified', key_input_file = 'inputFile',
