62c6ddade524e6aa0e7533580b2ccb0e23ead6df
Karl Ehatäht <karlehataht@gmail.com>
2018-09-06 12:34:27 -0500

    Remove redundant createCfg_makePlots_mcClosure functions

--- python/configs/analyzeConfig_2lss.py
+++ python/configs/analyzeConfig_2lss.py
@@ -213,27 +213,6 @@ class analyzeConfig_2lss(analyzeConfig):
     lines.append("process.addBackgroundLeptonFlips.sysShifts = cms.vstring(%s)" % self.central_or_shifts)
     create_cfg(self.cfgFile_addFlips, jobOptions['cfgFile_modified'], lines)
 
-  def createCfg_makePlots_mcClosure(self, jobOptions): #TODO
-    """Fills the template of python configuration file for making control plots
-
-       Args:
-         histogramFile: name of the input ROOT file
-    """
-    lines = []
-    lines.append("process.fwliteInput.fileNames = cms.vstring('%s')" % jobOptions['inputFile'])
-    lines.append("process.makePlots.outputFileName = cms.string('%s')" % jobOptions['outputFile'])
-    lines.append("process.makePlots.processesBackground = cms.vstring(%s)" % self.make_plots_backgrounds)
-    lines.append("process.makePlots.processSignal = cms.string('%s')" % self.make_plots_signal)
-    lines.append("process.makePlots.categories = cms.VPSet(")
-    lines.append("  cms.PSet(")
-    lines.append("    signal = cms.string('%s')," % jobOptions['histogramDir_signal'])
-    lines.append("    sideband = cms.string('%s')," % jobOptions['histogramDir_sideband'])
-    lines.append("    label = cms.string('%s')" % self.channel)
-    lines.append("  )")
-    lines.append(")")
-    lines.append("process.makePlots.intLumiData = cms.double(%.1f)" % self.lumi)
-    create_cfg(self.cfgFile_make_plots_mcClosure, jobOptions['cfgFile_modified'], lines)
-
   def addToMakefile_hadd_stage1_6(self, lines_makefile):
     """Adds the commands to Makefile that are necessary for building the intermediate histogram file
        that is used as input for data-driven background estimation.
