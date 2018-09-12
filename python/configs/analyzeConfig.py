b98399a9f019795a2a0cdfebdb5180c21f3e4c06
Ram <ram.krishna.dewanjee@cern.ch>
2018-09-12 15:22:36 +0300

    Bug fix in TailFit code

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -776,7 +776,7 @@ class analyzeConfig(object):
         lines.append("   FitRange      = cms.vdouble(%s)," % jobOptions['fitrange_nom'])
         lines.append("   FitParameters = cms.vdouble(%s)," % jobOptions['fitparam_nom'])
         lines.append("   )")
-        if (jobOptions['histogramName'].find("dihiggsMass") or jobOptions['histogramName'].find("mTauTauVis")) != -1:
+        if jobOptions['histogramName'].find("dihiggsMass") != -1 or jobOptions['histogramName'].find("mTauTauVis") != -1:
             lines.append("process.addBackgrounds_TailFit.alternate_fit_funcs = cms.VPSet(")
             lines.append("   cms.PSet(")
             lines.append("       FitfuncName   = cms.string('%s')," % "LegendrePolynomial3")
@@ -784,6 +784,7 @@ class analyzeConfig(object):
             lines.append("       FitParameters = cms.vdouble(%s)," % jobOptions['fitparam_alt0'])
             lines.append("       ),")
             lines.append(" )")
+
         else:
             lines.append("process.addBackgrounds_TailFit.alternate_fit_funcs = cms.VPSet(")
             lines.append("   cms.PSet(")
