7f0b24914fe716ff10dffc1545b2d8b04fe9ff87
Karl Ehatäht <karlehataht@gmail.com>
2018-09-13 11:47:21 +0300

    Add sys normalization for HH signal when doing prefit plots

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -949,6 +949,7 @@ class analyzeConfig(object):
                   massPoint = jobOptions['massPoint'],
                   plotOption = plotOption,
                 ))
+        lines.append("process.makePlots.nuisanceParameters.normalization.signal_hh_{massPoint} = cms.string('1.0 +/- 0.20')".format(massPoint = jobOptions['massPoint']))
         create_cfg(self.cfgFile_make_plots, jobOptions['cfgFile_modified'], lines)
 
     def createCfg_makePlots_mcClosure(self, jobOptions): #TODO
