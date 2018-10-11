966e69d00fb121594318359552d57b215ce1c266
Christian Veelken <christian.veelken@cern.ch>
2018-10-11 11:40:07 +0300

    added systematic uncertainty for DYMC (Z pT vs mass) reweighting

--- python/configs/analyzeConfig_1l_2tau.py
+++ python/configs/analyzeConfig_1l_2tau.py
@@ -342,6 +342,8 @@ class analyzeConfig_1l_2tau(analyzeConfig):
                   continue
                 if central_or_shift in systematics.LHE().ttZ and sample_category != "TTZ":
                   continue
+                if central_or_shift in systematics.DYMCReweighting and not ('DYMCReweighting' in sample_info.keys() and sample_info["DYMCReweighting"]):
+                  continue
 
                 logging.info(" ... for '%s' and systematic uncertainty option '%s'" % (lepton_and_hadTau_selection_and_frWeight, central_or_shift))
 
