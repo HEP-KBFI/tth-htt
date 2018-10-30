5fc7b132026a7b290df6904a40869e1b0ec245be
ktht <karlehataht@gmail.com>
2018-10-30 20:25:18 +0200

    Use extended ttV samples in the BDT training

--- python/samples/tthAnalyzeSamples_2017_BDT.py
+++ python/samples/tthAnalyzeSamples_2017_BDT.py
@@ -6,7 +6,9 @@ for sample_name, sample_info in samples_2017.items():
   if sample_info["process_name_specific"] in [
         "ttHToNonbb_M125_powheg",
         "TTZJets_LO",
+        "TTZJets_LO_ext1",
         "TTWJets_LO",
+        "TTWJets_LO_ext1",
         "TTTo2L2Nu",
         "TTTo2L2Nu_PSweights",
         "TTToSemiLeptonic",
