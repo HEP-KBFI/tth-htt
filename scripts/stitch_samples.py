6102a793effa0336621b1b39d1519b533fb54d60
ktht <karlehataht@gmail.com>
2018-11-01 12:01:56 +0200

    Compute stitching weights for W/Z+j samples w/o the inclusive sample
    In 0l+2tau channel we want to train using binned DY samples and use the
    inclusive samples to evaluate the analysis.

--- scripts/stitch_samples.py
+++ scripts/stitch_samples.py
@@ -485,6 +485,249 @@ def comp_weights_2(f, samples_to_stitch, split_var_1, split_var_2, apply_sf = Tr
 
           histogram.Write()
 
+def comp_weights_2_wo_inclusive(f, samples_to_stitch, split_var_1, split_var_2):
+  inclusive_samples   = samples_to_stitch['inclusive']['samples']
+  inclusive_binning_1 = samples_to_stitch['inclusive'][split_var_1]
+  inclusive_binning_2 = samples_to_stitch['inclusive'][split_var_2]
+
+  split_dict_1 = samples_to_stitch[split_var_1]
+  split_dict_2 = samples_to_stitch[split_var_2]
+  split_binning_1 = [ sample['value'] for sample in split_dict_1 ]
+  split_binning_2 = [ sample['value'] for sample in split_dict_2 ]
+  complete_binning_1 = list(sorted(list(
+    map(float, set(inclusive_binning_1) | set(list(itertools.chain.from_iterable(split_binning_1))))
+  )))
+  complete_binning_2 = list(sorted(list(
+    map(float, set(inclusive_binning_2) | set(list(itertools.chain.from_iterable(split_binning_2))))
+  )))
+
+  inclusive_xs = -1
+  for sample_key, sample_entry in samples.items():
+    if sample_key == 'sum_events': continue
+    if sample_entry['process_name_specific'] == inclusive_samples[0]:
+      inclusive_xs = sample_entry['xsection']
+  assert(inclusive_xs > 0)
+
+  # sum the inclusive nof events
+  inclusive_nof_events = {}
+  for sample_key, sample_entry in samples.items():
+    if sample_key == 'sum_events': continue
+    if sample_entry['process_name_specific'] in inclusive_samples:
+      if not inclusive_nof_events:
+        inclusive_nof_events = copy.deepcopy(sample_entry['nof_events'])
+      else:
+        assert(set(sample_entry['nof_events'].keys()) == set(inclusive_nof_events))
+        for nof_key, nof_arr in sample_entry['nof_events'].items():
+          assert(len(nof_arr) == len(inclusive_nof_events[nof_key]))
+          for idx, nof in enumerate(nof_arr):
+            assert(nof > 0)
+            inclusive_nof_events[nof_key][idx] += nof
+
+  # sum the binned nof events
+  for binned_sample in split_dict_1:
+    nof_events = {}
+    xs = -1
+    for sample_key, sample_entry in samples.items():
+      if sample_key == 'sum_events': continue
+      if sample_entry['process_name_specific'] in binned_sample['samples']:
+        if not nof_events:
+          nof_events = copy.deepcopy(sample_entry['nof_events'])
+          assert(set(inclusive_nof_events.keys()) == set(nof_events.keys()))
+        else:
+          assert(set(sample_entry['nof_events'].keys()) == set(nof_events.keys()))
+          for nof_key, nof_arr in sample_entry['nof_events'].items():
+            assert(len(nof_arr) == len(nof_events[nof_key]))
+            for idx, nof in enumerate(nof_arr):
+              assert(nof > 0)
+              nof_events[nof_key][idx] += nof
+        if xs < 0:
+          xs = sample_entry['xsection']
+    assert(xs > 0)
+    binned_sample['xsection'] = xs
+    binned_sample['nof_events'] = nof_events
+
+    lumis = {}
+    for nof_key, nof_arr in binned_sample['nof_events'].items():
+      lumis[nof_key] = list(map(lambda nof: nof / binned_sample['xsection'], nof_arr))
+    binned_sample['lumis'] = lumis
+
+  for binned_sample in split_dict_2:
+    nof_events = {}
+    xs = -1
+    for sample_key, sample_entry in samples.items():
+      if sample_key == 'sum_events': continue
+      if sample_entry['process_name_specific'] in binned_sample['samples']:
+        if not nof_events:
+          nof_events = copy.deepcopy(sample_entry['nof_events'])
+          assert(set(inclusive_nof_events.keys()) == set(nof_events.keys()))
+        else:
+          assert(set(sample_entry['nof_events'].keys()) == set(nof_events.keys()))
+          for nof_key, nof_arr in sample_entry['nof_events'].items():
+            assert(len(nof_arr) == len(nof_events[nof_key]))
+            for idx, nof in enumerate(nof_arr):
+              assert(nof > 0)
+              nof_events[nof_key][idx] += nof
+        if xs < 0:
+          xs = sample_entry['xsection']
+    assert(xs > 0)
+    binned_sample['xsection'] = xs
+    binned_sample['nof_events'] = nof_events
+
+    lumis = {}
+    for nof_key, nof_arr in binned_sample['nof_events'].items():
+      lumis[nof_key] = list(map(lambda nof: nof / binned_sample['xsection'], nof_arr))
+    binned_sample['lumis'] = lumis
+
+  # compute integrated luminosities for the inclusive sample
+  inclusive_lumis = {}
+  for nof_key, nof_arr in inclusive_nof_events.items():
+    inclusive_lumis[nof_key] = list(map(lambda nof: nof / inclusive_xs, nof_arr))
+
+  # decide on the bin indices
+  idxs_split_sample_1 = []
+  idxs_split_sample_2 = []
+  for binned_sample in split_dict_1:
+    binned_idx = complete_binning_1.index(binned_sample['value'][0]) + 1
+    binned_sample['idx'] = binned_idx
+    idxs_split_sample_1.append(binned_idx)
+  for binned_sample in split_dict_2:
+    binned_idx = complete_binning_2.index(binned_sample['value'][0]) + 1
+    binned_sample['idx'] = binned_idx
+    idxs_split_sample_2.append(binned_idx)
+
+  for binned_sample_1 in split_dict_1:
+    for sample_name in binned_sample_1['samples']:
+      if sample_name not in [ key.GetName() for key in f.GetListOfKeys() ]:
+        histogram_dir_root = f.mkdir(sample_name)
+      else:
+        histogram_dir_root = f.Get(sample_name)
+      subdir_name = '%s_v_%s_wo_inclusive' % (split_var_1, split_var_2)
+      if subdir_name not in [ key.GetName() for key in histogram_dir_root.GetListOfKeys() ]:
+        histogram_dir = histogram_dir_root.mkdir(subdir_name)
+      else:
+        histogram_dir = histogram_dir_root.Get(subdir_name)
+      histogram_dir.cd()
+
+      for nof_key, lumi_arr in inclusive_lumis.items():
+        for idx, lumi_incl in enumerate(lumi_arr):
+
+          histogram_name = '%s_%d' % (nof_key, idx)
+          binning_1 = array.array('f', complete_binning_1)
+          binning_2 = array.array('f', complete_binning_2)
+
+          histogram = ROOT.TH2D(
+            histogram_name, histogram_name, len(binning_1) - 1, binning_1, len(binning_2) - 1, binning_2
+          )
+          histogram.SetDirectory(histogram_dir)
+          histogram.SetXTitle(split_var_1)
+          histogram.SetYTitle(split_var_2)
+
+          lumi_split_1 = -1
+          for split_idx_1 in range(1, len(binning_1)):
+            if split_idx_1 == binned_sample_1['idx']:
+              lumi_split_1 = binned_sample_1['lumis'][nof_key][idx]
+              for split_idx_2 in range(1, len(binning_2)):
+                lumi_split_2 = 0.
+                for binned_sample_2 in split_dict_2:
+                  if split_idx_2 == binned_sample_2['idx']:
+                    lumi_split_2 = binned_sample_2['lumis'][nof_key][idx]
+                    break
+                if binning_1[split_idx_1] > inclusive_binning_1[1] or \
+                   binning_1[split_idx_1] < inclusive_binning_1[0] or \
+                   binning_2[split_idx_2] > inclusive_binning_2[1] or \
+                   binning_2[split_idx_2] < inclusive_binning_2[0]:
+                  if lumi_split_2 == 0.:
+                    weight = 1.
+                  else:
+                    weight = lumi_split_1 / (lumi_split_1 + lumi_split_2)
+                else:
+                  weight = lumi_split_1 / (lumi_split_1 + lumi_split_2)
+                assert(weight >= 0.)
+                histogram.SetBinContent(split_idx_1, split_idx_2, weight)
+            else:
+              for split_idx_2 in range(1, len(binning_2)):
+                histogram.SetBinContent(split_idx_1, split_idx_2, 0.)
+
+            histogram.GetXaxis().SetBinLabel(
+              split_idx_1, '%.0f <= %s < %.0f' % (
+                complete_binning_1[split_idx_1 - 1], split_var_1, complete_binning_1[split_idx_1]
+              )
+            )
+          for split_idx_2 in range(1, len(binning_2)):
+            histogram.GetYaxis().SetBinLabel(
+              split_idx_2, '%.0f <= %s < %.0f' % (
+                complete_binning_2[split_idx_2 - 1], split_var_2, complete_binning_2[split_idx_2]
+              )
+            )
+
+          histogram.Write()
+
+  for binned_sample_2 in split_dict_2:
+    for sample_name in binned_sample_2['samples']:
+      if sample_name not in [ key.GetName() for key in f.GetListOfKeys() ]:
+        histogram_dir_root = f.mkdir(sample_name)
+      else:
+        histogram_dir_root = f.Get(sample_name)
+      subdir_name = '%s_v_%s_wo_inclusive' % (split_var_1, split_var_2)
+      if subdir_name not in [ key.GetName() for key in histogram_dir_root.GetListOfKeys() ]:
+        histogram_dir = histogram_dir_root.mkdir(subdir_name)
+      else:
+        histogram_dir = histogram_dir_root.Get(subdir_name)
+      histogram_dir.cd()
+
+      for nof_key, lumi_arr in inclusive_lumis.items():
+        for idx, lumi_incl in enumerate(lumi_arr):
+
+          histogram_name = '%s_%d' % (nof_key, idx)
+          binning_1 = array.array('f', complete_binning_1)
+          binning_2 = array.array('f', complete_binning_2)
+
+          histogram = ROOT.TH2D(
+            histogram_name, histogram_name, len(binning_1) - 1, binning_1, len(binning_2) - 1, binning_2
+          )
+          histogram.SetDirectory(histogram_dir)
+          histogram.SetXTitle(split_var_1)
+          histogram.SetYTitle(split_var_2)
+
+          lumi_split_2 = -1
+          for split_idx_2 in range(1, len(binning_2)):
+            if split_idx_2 == binned_sample_2['idx']:
+              lumi_split_2 = binned_sample_2['lumis'][nof_key][idx]
+              for split_idx_1 in range(1, len(binning_1)):
+                lumi_split_1 = 0.
+                for binned_sample_1 in split_dict_1:
+                  if split_idx_1 == binned_sample_1['idx']:
+                    lumi_split_1 = binned_sample_1['lumis'][nof_key][idx]
+                    break
+                if binning_1[split_idx_1] > inclusive_binning_1[1] or \
+                   binning_1[split_idx_1] < inclusive_binning_1[0] or \
+                   binning_2[split_idx_2] > inclusive_binning_2[1] or \
+                   binning_2[split_idx_2] < inclusive_binning_2[0]:
+                  if lumi_split_1 == 0.:
+                    weight = 1.
+                  else:
+                    weight = lumi_split_2 / (lumi_split_1 + lumi_split_2)
+                else:
+                  weight = lumi_split_2 / (lumi_split_1 + lumi_split_2)
+                histogram.SetBinContent(split_idx_1, split_idx_2, weight)
+            else:
+              for split_idx_1 in range(1, len(binning_1)):
+                histogram.SetBinContent(split_idx_1, split_idx_2, 0.)
+
+            histogram.GetYaxis().SetBinLabel(
+              split_idx_2, '%.0f <= %s < %.0f' % (
+                complete_binning_2[split_idx_2 - 1], split_var_2, complete_binning_2[split_idx_2]
+              )
+            )
+          for split_idx_1 in range(1, len(binning_1)):
+            histogram.GetXaxis().SetBinLabel(
+              split_idx_1, '%.0f <= %s < %.0f' % (
+                complete_binning_1[split_idx_1 - 1], split_var_1, complete_binning_1[split_idx_1]
+              )
+            )
+
+          histogram.Write()
+
 apply_sf = True
 fp = ROOT.TFile.Open('data/stitched_weights_2017.root', 'recreate')
 for sample_to_stitch in samples_to_stitch:
@@ -495,6 +738,7 @@ for sample_to_stitch in samples_to_stitch:
     for binning_var in binning_vars:
       comp_weights_1(fp, sample_to_stitch, binning_var, apply_sf)
     comp_weights_2(fp, sample_to_stitch, binning_vars[0], binning_vars[1], apply_sf)
+    comp_weights_2_wo_inclusive(fp, sample_to_stitch, binning_vars[0], binning_vars[1])
   else:
     raise ValueError('More than 2 variables used for the binning: %s' % ', '.join(binning_vars))
 fp.Close()
