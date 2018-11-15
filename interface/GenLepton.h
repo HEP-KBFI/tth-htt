bac10a702c94f2a441c190e659af95db3839ad34
Christian Veelken <christian.veelken@cern.ch>
2018-11-15 17:35:31 +0200

    switched from Double_t to Int_t for charge

--- interface/GenLepton.h
+++ interface/GenLepton.h
@@ -18,7 +18,7 @@ public:
             Double_t phi,
             Double_t mass,
             Int_t pdgId,
-            Double_t charge);
+            Int_t charge);
   GenLepton(const math::PtEtaPhiMLorentzVector & p4,
             Int_t pdgId);
 
