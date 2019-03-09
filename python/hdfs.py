981291f0a2067fd6d7bb69c8a10e2f777bf94792
ktht <karlehataht@gmail.com>
2019-03-09 22:07:05 +0200

    Implement the remaining functions in hdfs module

--- python/hdfs.py
+++ python/hdfs.py
@@ -1,9 +1,13 @@
 import ctypes
 import logging
 import re
-import os.path
+import os
 import datetime
 import dateutil.tz
+import stat
+import pwd
+import grp
+import shutil
 
 #NOTE works only when the following is done:
 # /usr/lib/hadoop/libexec/hadoop-config.sh
@@ -11,15 +15,8 @@ import dateutil.tz
 # for j in $(ls /usr/lib/hadoop/client/*.jar); do export CLASSPATH=$CLASSPATH:$j; done
 #
 #TODO
-# - add methods for creating and removing directories and files
-# - clean up the class
 # - set necessary environment variables at runtime
 # - implement unit tests
-#
-#EXAMPLES
-# 1)
-# from tthAnalysis.HiggsToTauTau.hdfs import hdfs
-# hdfs.get_path_info(os.path.join('/hdfs', 'local', getpass.getuser()))
 
 try:
   from urllib.parse import urlparse
@@ -33,21 +30,25 @@ class _hdfs:
   class hdfsException(Exception):
     pass
 
+  class NoSuchPathException(Exception):
+    pass
+
   @staticmethod
-  def fuse_path(path):
-    if not path.startswith("hdfs://"):
-      raise _hdfs.hdfsException("Invalid path: %s" % path)
-    return re.sub('^hdfs://', '/hdfs', path)
+  def defuse(path):
+    return re.sub('^/hdfs', '', path)
 
   @staticmethod
   def normalize_path(path):
-    if not path.startswith('/hdfs') and not path.startswith("hdfs://"):
+    if not path.startswith("hdfs://"):
       raise _hdfs.hdfsException("Invalid path: %s" % path)
 
-    path_sub = re.sub('^/hdfs', 'hdfs://', path) if path.startswith('/hdfs') else path
-    path_url = urlparse(path_sub)
-    path_final = path_url.scheme + '://' + path_url.path
-    return path_final
+    path_url = urlparse(path)
+    path_normalized = path_url.scheme + '://' + path_url.path
+    return path_normalized
+
+  @staticmethod
+  def get_dt(unix_tm):
+    return datetime.datetime.fromtimestamp(unix_tm, _hdfs._tz)
 
   class hdfsFileInfo(ctypes.Structure):
     _fields_ = [
@@ -66,10 +67,10 @@ class _hdfs:
   class info:
     def __init__(self, hdfsFileInfoObject):
       self.kind        = chr(hdfsFileInfoObject.contents.mKind)
-      self.name        = _hdfs.normalize_path(hdfsFileInfoObject.contents.mName)
-      self.lastMod     = datetime.datetime.fromtimestamp(hdfsFileInfoObject.contents.mLastMod, _hdfs._tz)
+      self.url         = _hdfs.normalize_path(hdfsFileInfoObject.contents.mName)
+      self.name        = re.sub('^hdfs://', '/hdfs', self.url)
+      self.lastMod     = _hdfs.get_dt(hdfsFileInfoObject.contents.mLastMod)
       self.size        = hdfsFileInfoObject.contents.mSize
-      self.replication = hdfsFileInfoObject.contents.mReplication
       self.owner       = hdfsFileInfoObject.contents.mOwner
       self.group       = hdfsFileInfoObject.contents.mGroup
       self.permissions = hdfsFileInfoObject.contents.mPermissions
@@ -79,13 +80,11 @@ class _hdfs:
         self.lastAccess = datetime.datetime.fromtimestamp(self.lastAccess, _hdfs._tz)
       else:
         self.lastAccess = None
-      self.nameFuse = _hdfs.fuse_path(self.name)
-      self.depth    = len(self.nameFuse.split(os.path.sep)) - 1
 
-    def is_file(self):
+    def isfile(self):
       return self.kind == 'F'
 
-    def is_dir(self):
+    def isdir(self):
       return self.kind == 'D'
 
   def __init__(self, nn = "default", port = 0):
@@ -100,6 +99,15 @@ class _hdfs:
     self.lib = ctypes.cdll.LoadLibrary(self.lib_path)
     self.lib.hdfsListDirectory.restype = ctypes.POINTER(_hdfs.hdfsFileInfo)
     self.lib.hdfsGetPathInfo.restype   = ctypes.POINTER(_hdfs.hdfsFileInfo)
+    self.lib.hdfsGetDefaultBlockSize   = ctypes.c_int64
+    self.lib.hdfsGetCapacity           = ctypes.c_int64
+    self.lib.hdfsGetUsed               = ctypes.c_int64
+    self.lib.hdfsDelete                = ctypes.c_int32
+    self.lib.hdfsCreateDirectory       = ctypes.c_int32
+    self.lib.hdfsChown                 = ctypes.c_int32
+    self.lib.hdfsChmod                 = ctypes.c_int32
+    self.lib.hdfsMove                  = ctypes.c_int32
+    self.lib.hdfsCopy                  = ctypes.c_int32
     self.hdfsFileInfo_size             = ctypes.sizeof(_hdfs.hdfsFileInfo)
 
     logging.debug("Building HDFS interface")
@@ -119,70 +127,206 @@ class _hdfs:
       raise _hdfs.hdfsException(
         "Could not connect to the HDFS interface (nn = '%s', port = %d)" % (self.nn, self.port)
       )
+
+    logging.debug("Interfacing to the local file system")
     self.lfs = self.lib.hdfsBuilderConnect(self.lbld)
     if not self.lfs:
       raise _hdfs.hdfsException("Could not create interface to local file system")
 
-  def __del__(self):
-    self.lib.hdfsDisconnect(self.fs)
-    self.lib.hdfsDisconnect(self.lfs)
-
-  '''Get path object from a path that
-  '''
   def get_path_info(self, path):
-    normalized_path = path#_hdfs.normalize_path(path)
+    if not path.startswith('/hdfs'):
+      raise _hdfs.hdfsException("Invalid path: %s" % path)
+    normalized_path = re.sub('^/hdfs', '', path)
     path_info = self.lib.hdfsGetPathInfo(self.fs, normalized_path)
     if not path_info:
-      raise _hdfs.hdfsException("No such path: %s" % normalized_path)
+      raise _hdfs.NoSuchPathException(path)
 
     path_obj = _hdfs.info(path_info)
     self.lib.hdfsFreeFileInfo(path_info, 1)
     return path_obj
 
-  def list_directory(self, path_obj):
-    nof_entries = ctypes.c_int()
-    dir_ptr = self.lib.hdfsListDirectory(self.fs, path_obj.name, ctypes.byref(nof_entries))
-    if not dir_ptr:
-      raise _hdfs.hdfsException("No such path: %s" % path_obj.name)
+  def list_dir(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path) if type(path) == str else path
+      nof_entries = ctypes.c_int()
+      dir_ptr = self.lib.hdfsListDirectory(self.fs, path_obj.url, ctypes.byref(nof_entries))
+      if not dir_ptr:
+        raise _hdfs.NoSuchPathException(path)
 
-    entries = []
-    for idx in range(nof_entries.value):
-      void_p  = ctypes.cast(dir_ptr, ctypes.c_voidp).value + idx * self.hdfsFileInfo_size
-      cur_ptr = ctypes.cast(void_p, ctypes.POINTER(_hdfs.hdfsFileInfo))
-      entries.append(_hdfs.info(cur_ptr))
-    self.lib.hdfsFreeFileInfo(dir_ptr, nof_entries)
+      entries = []
+      for idx in range(nof_entries.value):
+        void_p  = ctypes.cast(dir_ptr, ctypes.c_voidp).value + idx * self.hdfsFileInfo_size
+        cur_ptr = ctypes.cast(void_p, ctypes.POINTER(_hdfs.hdfsFileInfo))
+        path_obj = _hdfs.info(cur_ptr)
+        entries.append(path_obj.name)
+      self.lib.hdfsFreeFileInfo(dir_ptr, nof_entries)
 
-    return entries
+      return entries
+    else:
+      return os.listdir(path)
+
+  def get_default_block_size(self):
+    return self.lib.hdfsGetDefaultBlockSize(self.fs)
+
+  def get_capacity(self):
+    return self.lib.hdfsGetCapacity(self.fs)
+
+  def get_used(self):
+    return self.lib.hdfsGetUsed(self.fs)
 
   def exists(self, path):
-    return self.lib.hdfsExists(self.fs, path) == 0
+    is_local = path.startswith('/hdfs')
+    if is_local:
+      return self.lib.hdfsExists(self.fs, _hdfs.defuse(path)) == 0
+    else:
+      return os.path.exists(path)
 
-  def exists_local(self, path):
-    return self.lib.hdfsExists(self.lfs, path) == 0
+  def isfile(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.isfile()
+    else:
+      return os.path.isfile(path)
 
-  def is_file(self, path):
-    pass
+  def isdir(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.isdir()
+    else:
+      return os.path.isdir(path)
 
-  def is_dir(self, path):
-    pass
+  def getsize(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.size
+    else:
+      return os.path.getsize(path)
+
+  def getatime(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.lastAccess
+    else:
+      return _hdfs.get_dt(os.path.getatime(path))
+
+  def getmtime(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.lastMod
+    else:
+      return _hdfs.get_dt(os.path.getmtime(path))
+
+  def getowner(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.owner
+    else:
+      return pwd.getpwuid(os.stat(path)[stat.ST_UID]).pw_name
+
+  def getgroup(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.group
+    else:
+      return grp.getgrgid(os.stat(path)[stat.ST_UID]).gr_name
+
+  def getpermissions(self, path):
+    if path.startswith('/hdfs'):
+      path_obj = self.get_path_info(path)
+      return path_obj.permissions
+    else:
+      return int(oct(os.stat(path)[stat.ST_MODE])[-3:])
 
   def copy(self, src, dst):
-    pass
+    is_local_src = not src.startswith('/hdfs')
+    is_local_dst = not dst.startswith('/hdfs')
+    src_defused = re.sub('^/hdfs', '', src)
+    dst_defused = re.sub('^/hdfs', '', dst)
+    if is_local_src and is_local_dst:
+      try:
+        shutil.copy(src_defused, dst_defused)
+      except OSError:
+        return -1
+      return 0
+    elif is_local_src and not is_local_dst:
+      return self.lib.hdfsCopy(self.lfs, src_defused, self.fs, dst_defused)
+    elif not is_local_src and is_local_dst:
+      return self.lib.hdfsCopy(self.fs, src_defused, self.lfs, dst_defused)
+    elif not is_local_src and not is_local_dst:
+      return self.lib.hdfsCopy(self.fs, src_defused, self.fs, dst_defused)
+    else:
+      raise RuntimeError("Impossible error")
 
   def move(self, src, dst):
-    pass
+    is_local_src = not src.startswith('/hdfs')
+    is_local_dst = not dst.startswith('/hdfs')
+    src_defused = re.sub('^/hdfs', '', src)
+    dst_defused = re.sub('^/hdfs', '', dst)
+    if is_local_src and is_local_dst:
+      try:
+        shutil.move(src_defused, dst_defused)
+      except OSError:
+        return -1
+      return 0
+    elif is_local_src and not is_local_dst:
+      return self.lib.hdfsMove(self.lfs, src_defused, self.fs, dst_defused)
+    elif not is_local_src and is_local_dst:
+      return self.lib.hdfsMove(self.fs, src_defused, self.lfs, dst_defused)
+    elif not is_local_src and not is_local_dst:
+      return self.lib.hdfsRename(self.fs, src_defused, dst_defused)
+    else:
+      raise RuntimeError("Impossible error")
 
   def remove(self, path):
-    pass
+    if path.startswith('/hdfs'):
+      path_defused = re.sub('^/hdfs', '', path)
+      return self.lib.hdfsDelete(self.fs, path_defused)
+    else:
+      try:
+        shutil.rmtree(path)
+      except OSError:
+        return -1
+      return 0
 
-  def create_directory(self, path):
-    pass
+  def mkdirs(self, path):
+    if path.startswith('/hdfs'):
+      path_defused = re.sub('^/hdfs', '', path)
+      return self.lib.hdfsCreateDirectory(self.fs, path_defused)
+    else:
+      try:
+        os.makedirs(path)
+      except OSError:
+        return -1
+      return 0
 
-  def chown(self, path):
-    pass
+  def chown(self, path, owner = "", group = ""):
+    if not owner and not group:
+      raise _hdfs.hdfsException("Missing owner and group name")
+    if path.startswith('/hdfs'):
+      path_defused = re.sub('^/hdfs', '', path)
+      return self.lib.hdfsChown(self.fs, path_defused, owner, group)
+    else:
+      uid = pwd.getpwnam(owner).pw_uid
+      gid = grp.getgrnam(group).gr_gid
+      try:
+        os.chown(path, uid, gid)
+      except OSError:
+        return -1
+      return 0
 
-  def chmod(self, path):
-    pass
+  def chmod(self, path, permissions):
+    if not (0 <= int(permissions) <= 777):
+      raise _hdfs.hdfsException("Invalid permission code: %s" % str(permissions))
+    permissions_oct = int(str(permissions), 8)
+    if path.startswith('/hdfs'):
+      path_defused = re.sub('^/hdfs', '', path)
+      return self.lib.hdfsChmod(self.fs, path_defused, permissions_oct)
+    else:
+      try:
+        os.chmod(path, permissions_oct)
+      except OSError:
+        return -1
+      return 0
 
 def hdfsWrapper():
   if _hdfs._instance is None:
--- test/hdfs/testHDFS.py
+++ test/hdfs/testHDFS.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-from tthAnalysis.HiggsToTauTau.hdfs import hdfs
-
 import os.path
 import getpass
+import logging
+import sys
 
-dir_str = os.path.join('/local', getpass.getuser())
+logging.basicConfig(
+  stream = sys.stdout,
+  level  = logging.DEBUG,
+  format = '%(asctime)s - %(levelname)s: %(message)s',
+)
+
+from tthAnalysis.HiggsToTauTau.hdfs import hdfs
+
+dir_str = os.path.join('/hdfs/local/%s' % getpass.getuser())
 dir_obj = hdfs.get_path_info(dir_str)
 print(dir_obj.__dict__)
+
+dirs = hdfs.list_directory(dir_str)
+for d in dirs:
+  print(d.name)
+
+print(hdfs.exists(dir_str))
+
