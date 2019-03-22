cc133a1b51752aea4254a138dcb2d0b9664ff117
ktht <karlehataht@gmail.com>
2019-03-22 13:56:39 +0200

    Update pre-processed sync Ntuple for 2016 era

--- python/samples/tthAnalyzeSamples_2016_nanoAOD_sync.py
+++ python/samples/tthAnalyzeSamples_2016_nanoAOD_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2019-03-20 21:16:14 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2016_sync.py -p /hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Mar20 -N samples_2016 -E 2016 -o python/samples -g tthAnalyzeSamples_2016_nanoAOD_sync.py -M
+# file generated at 2019-03-22 13:55:24 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2016_sync.py -p /hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Mar22 -N samples_2016 -E 2016 -o python/samples -g tthAnalyzeSamples_2016_nanoAOD_sync.py -M
 
 samples_2016 = OD()
 samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2/MINIAODSIM"] = OD([
@@ -12,9 +12,9 @@ samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/Run
   ("nof_db_files",                    35),
   ("nof_events",                      {
   }),
-  ("nof_tree_events",                 10),
+  ("nof_tree_events",                 77700),
   ("nof_db_events",                   3981250),
-  ("fsize_local",                     2570262), # 2.57MB, avg file size 2.57MB
+  ("fsize_local",                     258781860), # 258.78MB, avg file size 258.78MB
   ("fsize_db",                        208884155497), # 208.88GB, avg file size 5.97GB
   ("use_it",                          True),
   ("xsection",                        0.2118),
@@ -24,7 +24,7 @@ samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/Run
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Mar20/ttHToNonbb_M125_powheg"),
+        ("path",      "/hdfs/local/karl/sync_ntuples/2016/nanoAODproduction/2019Mar22/ttHToNonbb_M125_powheg"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
