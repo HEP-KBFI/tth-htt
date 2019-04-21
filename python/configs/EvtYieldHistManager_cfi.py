2a56639015cff811f2c92805e0a04c6d4899f3a3
ktht <karlehataht@gmail.com>
2019-04-21 15:09:59 +0300

    Define run ranges and integrated luminosities for 2018 era

--- python/configs/EvtYieldHistManager_cfi.py
+++ python/configs/EvtYieldHistManager_cfi.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+# use full run range (instead of golden) and max recorded integrated luminosity per acquisition era
+# sources:
+# https://github.com/HEP-KBFI/tth-nanoAOD/blob/39eabfec8c21613c7133cff5ce197add27c433a6/test/datasets/txt/datasets_data_2016_17Jul18.txt
+# https://github.com/HEP-KBFI/tth-nanoAOD/blob/39eabfec8c21613c7133cff5ce197add27c433a6/test/datasets/txt/datasets_data_2017_31Mar18.txt
+# https://github.com/HEP-KBFI/tth-nanoAOD/blob/39eabfec8c21613c7133cff5ce197add27c433a6/test/datasets/txt/datasets_data_2018_17Sep18.txt
+
 EvtYieldHistManager_2016 = cms.PSet(
     Run2017B = cms.PSet(
         runRange = cms.string("273150-275376"),
@@ -38,7 +44,7 @@ EvtYieldHistManager_2017 = cms.PSet(
     ),
     Run2017C = cms.PSet(
         runRange = cms.string("299368-302029"),
-        luminosity = cms.double(9.631)
+        luminosity = cms.double(9.633)
     ),
     Run2017D = cms.PSet(
         runRange = cms.string("302030-302663"),
@@ -51,5 +57,24 @@ EvtYieldHistManager_2017 = cms.PSet(
     Run2017F = cms.PSet(
         runRange = cms.string("305040-306462"),
         luminosity = cms.double(13.539)
-    )
+    ),
+)
+
+EvtYieldHistManager_2018 = cms.PSet(
+    Run2017A = cms.PSet(
+        runRange = cms.string("315257-316995"),
+        luminosity = cms.double(14.027)
+    ),
+    Run2017B = cms.PSet(
+        runRange = cms.string("317080-319310"),
+        luminosity = cms.double(7.061)
+    ),
+    Run2017C = cms.PSet(
+        runRange = cms.string("319337-320065"),
+        luminosity = cms.double(6.895)
+    ),
+    Run2017D = cms.PSet(
+        runRange = cms.string("320497-325175"),
+        luminosity = cms.double(31.743)
+    ),
 )
