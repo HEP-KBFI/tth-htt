8d7f027be88ddaabcb50babc70de6011194770ba
ktht <karlehataht@gmail.com>
2019-04-30 21:10:25 +0300

    Do not add sync Ntuple from 1l+1tau channel to makefile target

--- python/configs/analyzeConfig_1l_1tau.py
+++ python/configs/analyzeConfig_1l_1tau.py
@@ -731,7 +731,6 @@ class analyzeConfig_1l_1tau(analyzeConfig):
         self.addToMakefile_syncNtuple(lines_makefile)
         outputFile_sync_path = os.path.join(self.outputDir, DKEY_SYNC, '%s.root' % self.channel)
         self.outputFile_sync['sync'] = outputFile_sync_path
-        self.targets.append(outputFile_sync_path)
         self.addToMakefile_hadd_sync(lines_makefile)
       else:
         raise ValueError("Internal logic error")
