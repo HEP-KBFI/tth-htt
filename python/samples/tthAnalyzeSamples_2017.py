42b84f62317a6be07ba621011ff299aa77b3119f
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-06-19 13:11:09 +0300

    adding one more dumb process

--- python/samples/tthAnalyzeSamples_2017.py
+++ python/samples/tthAnalyzeSamples_2017.py
@@ -459,6 +459,51 @@ samples_2017["/ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/Run
   ]),
 ])
 
+samples_2017["/ttHJet_ctcvcp/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal_ctcvcp"),
+  ("process_name_specific",           "ttH_ctcvcp"),
+  ("nof_files",                       1),
+  ("nof_db_files",                    208),
+  ("nof_events",                      {
+    'Count'                                                      : [        65452, ],
+    'CountWeighted'                                              : [        22455,        22443,        22473, ],
+    'CountWeightedLHEWeightScale'                                : [        22906,        23311,        23703,        22808,        22455,        22179,        21395,        20690,        20108, ],
+    'CountWeightedL1PrefireNom'                                  : [        21660,        21648,        21670, ],
+    'CountWeightedL1Prefire'                                     : [        21660,        21467,        21848, ],
+    'CountWeightedLHEWeightScaleL1PrefireNom'                    : [        22052,        22464,        22862,        21981,        21659,        21409,        20633,        19969,        19421, ],
+  }),
+  ("nof_tree_events",                 65452),
+  ("nof_db_events",                   9779592),
+  ("fsize_local",                     211999808), # 212.00MB, avg file size 212.00MB
+  ("fsize_db",                        625507074411), # 625.51GB, avg file size 3.01GB
+  ("use_it",                          True),
+  ("xsection",                        0.2118),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("LHE_set",                         "LHA IDs 91400 - 91432 -> PDF4LHC15_nnlo_30_pdfas PDF set, expecting 33 weights (counted 33 weights)"),
+  ("nof_reweighting",                 0),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/local/karl/ttHNtupleProduction/2017/2019Jun10_woPresel_nonNom_sync/ntuples/ttHJetToNonbb_M125_amcatnlo"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_from_superset",           [
+    # not computed
+  ]),
+  ("missing_hlt_paths",               [
+
+  ]),
+  ("hlt_paths",               [
+    # not computed
+  ]),
+])
+
 samples_2017["/ttZJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"] = OD([
   ("type",                            "mc"),
   ("sample_category",                 "TTZ"),
