de75f2a97b5323889506bcfddf9d36ef12118a76
ktht <karlehataht@gmail.com>
2019-06-23 11:08:32 +0300

    Const-correctness in selector classes of loose electrons and muons

--- interface/RecoElectronCollectionSelectorLoose.h
+++ interface/RecoElectronCollectionSelectorLoose.h
@@ -23,22 +23,18 @@ public:
 protected:
   bool set_selection_flags_;
   bool debug_;                        ///< enable printout for debugging purposes
-  int era_;
+  const int era_;
 
-  Double_t min_pt_;                   ///< lower cut threshold on pT
-  Double_t max_absEta_;               ///< upper cut threshold on absolute value of eta
-  Double_t max_dxy_;                  ///< upper cut threshold on d_{xy}, distance in the transverse plane w.r.t PV
-  Double_t max_dz_;                   ///< upper cut threshold on d_{z}, distance on the z axis w.r.t PV
-  Double_t max_relIso_;               ///< upper cut threshold on relative isolation
-  Double_t max_sip3d_;                ///< upper cut threshold on significance of IP
-//--- define cuts that dependent on eta
-//    format: central region (|eta| < 0.8) / transition region (0.8 < |eta| < 1.479) / forward region (|eta| > 1.479)
-  typedef std::vector<Double_t> vDouble_t;
-  vDouble_t binning_absEta_;          ///< eta values separating central, transition and forward region (0.8, 1.479)
+  const Double_t min_pt_;                   ///< lower cut threshold on pT
+  const Double_t max_absEta_;               ///< upper cut threshold on absolute value of eta
+  const Double_t max_dxy_;                  ///< upper cut threshold on d_{xy}, distance in the transverse plane w.r.t PV
+  const Double_t max_dz_;                   ///< upper cut threshold on d_{z}, distance on the z axis w.r.t PV
+  const Double_t max_relIso_;               ///< upper cut threshold on relative isolation
+  const Double_t max_sip3d_;                ///< upper cut threshold on significance of IP
 //-------------------------------------------------------------------------------
-  bool apply_tightCharge_;            ///< apply (True) or do not apply (False) tight charge cut
-  bool apply_conversionVeto_;         ///< apply (True) or do not apply (False) conversion veto
-  Int_t max_nLostHits_;               ///< upper cut threshold on lost hits in the innermost layer of the tracker (electrons with lost_hits equal to cut threshold pass)
+  const bool apply_tightCharge_;            ///< apply (True) or do not apply (False) tight charge cut
+  const bool apply_conversionVeto_;         ///< apply (True) or do not apply (False) conversion veto
+  const Int_t max_nLostHits_;               ///< upper cut threshold on lost hits in the innermost layer of the tracker (electrons with lost_hits equal to cut threshold pass)
 //-------------------------------------------------------------------------------
 };
 
--- interface/RecoMuonCollectionSelectorLoose.h
+++ interface/RecoMuonCollectionSelectorLoose.h
@@ -21,19 +21,19 @@ public:
   operator()(const RecoMuon & muon) const;
 
 protected:
-  int era_;
+  const int era_;
   bool debug_;
   bool set_selection_flags_;
 
-  Double_t min_pt_;        ///< lower cut threshold on pT
-  Double_t max_absEta_;    ///< upper cut threshold on absolute value of eta
-  Double_t max_dxy_;       ///< upper cut threshold on d_{xy}, distance in the transverse plane w.r.t PV
-  Double_t max_dz_;        ///< upper cut threshold on d_{z}, distance on the z axis w.r.t PV
-  Double_t max_relIso_;    ///< upper cut threshold on relative isolation
-  Double_t max_sip3d_;     ///< upper cut threshold on significance of IP
-  bool apply_looseIdPOG_;  ///< apply (True) or do not apply (False) loose PFMuon id selection
-  bool apply_mediumIdPOG_; ///< apply (True) or do not apply (False) medium PFMuon id selection
-  bool apply_tightCharge_; ///< apply (True) or do not apply (False) tight charge cut
+  const Double_t min_pt_;        ///< lower cut threshold on pT
+  const Double_t max_absEta_;    ///< upper cut threshold on absolute value of eta
+  const Double_t max_dxy_;       ///< upper cut threshold on d_{xy}, distance in the transverse plane w.r.t PV
+  const Double_t max_dz_;        ///< upper cut threshold on d_{z}, distance on the z axis w.r.t PV
+  const Double_t max_relIso_;    ///< upper cut threshold on relative isolation
+  const Double_t max_sip3d_;     ///< upper cut threshold on significance of IP
+  const bool apply_looseIdPOG_;  ///< apply (True) or do not apply (False) loose PFMuon id selection
+  const bool apply_mediumIdPOG_; ///< apply (True) or do not apply (False) medium PFMuon id selection
+  const bool apply_tightCharge_; ///< apply (True) or do not apply (False) tight charge cut
 };
 
 typedef ParticleCollectionSelector<RecoMuon, RecoMuonSelectorLoose> RecoMuonCollectionSelectorLoose;
