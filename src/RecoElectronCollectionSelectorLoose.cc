HEP-KBFI/tth-htt

commit df1d27ce6b6484eb63f1f05f42c6b769e757d1cf

modifying the cuts by >=

src/RecoElectronCollectionSelectorLoose.cc
@@ -29,7 +29,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     std::cout << get_human_line(this, __func__) << ":\n" << electron;
   }
 
-  if(electron.pt() < min_pt_)
+  if(electron.pt() <= min_pt_)
   {
     if(debug_)
     {
@@ -37,7 +37,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.absEta() > max_absEta_)
+  if(electron.absEta() >= max_absEta_)
   {
     if(debug_)
     {
@@ -45,7 +45,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(std::fabs(electron.dxy()) > max_dxy_)
+  if(std::fabs(electron.dxy()) >= max_dxy_)
   {
     if(debug_)
     {
@@ -53,7 +53,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(std::fabs(electron.dz()) > max_dz_)
+  if(std::fabs(electron.dz()) >= max_dz_)
   {
     if(debug_)
     {
@@ -61,7 +61,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.relIso() > max_relIso_)
+  if(electron.relIso() >= max_relIso_)
   {
     if(debug_)
     {
@@ -69,7 +69,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.sip3d() > max_sip3d_)
+  if(electron.sip3d() >= max_sip3d_)
   {
     if(debug_)
     {

src/RecoJetCollectionSelector.cc
@@ -65,7 +65,7 @@ RecoJetSelector::operator()(const RecoJet & jet) const
     std::cout << get_human_line(this, __func__) << ":\n jet: " << jet << '\n';
   }
 
-  if(jet.pt() < min_pt_)
+  if(jet.pt() <= min_pt_)
   {
     if(debug_)
     {
@@ -73,7 +73,7 @@ RecoJetSelector::operator()(const RecoJet & jet) const
     }
     return false;
   }
-  if(max_absEta_ > 0. && jet.absEta() > max_absEta_)
+  if(max_absEta_ > 0. && jet.absEta() >= max_absEta_)
   {
     if(debug_)
     {

src/RecoMuonCollectionSelectorLoose.cc
@@ -28,7 +28,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     std::cout << get_human_line(this, __func__) << ":\n" << muon;
   }
 
-  if(muon.pt() < min_pt_)
+  if(muon.pt() <= min_pt_)
   {
     if(debug_)
     {
@@ -37,7 +37,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.absEta() > max_absEta_)
+  if(muon.absEta() >= max_absEta_)
   {
     if(debug_)
     {
@@ -46,7 +46,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(std::fabs(muon.dxy()) > max_dxy_)
+  if(std::fabs(muon.dxy()) >= max_dxy_)
   {
     if(debug_)
     {
@@ -55,7 +55,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(std::fabs(muon.dz()) > max_dz_)
+  if(std::fabs(muon.dz()) >= max_dz_)
   {
     if(debug_)
     {
@@ -64,7 +64,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.relIso() > max_relIso_)
+  if(muon.relIso() >= max_relIso_)
   {
     if(debug_)
     {
@@ -73,7 +73,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.sip3d() > max_sip3d_)
+  if(muon.sip3d() >= max_sip3d_)
   {
     if(debug_)
     {

commit 56734e6a7817d8dd6b5761d03464e2970644d788

Undo df1d27ce6b6484eb63f1f05f42c6b769e757d1cf
Introduced regression to ttH synchronization.

src/RecoElectronCollectionSelectorLoose.cc
@@ -29,7 +29,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     std::cout << get_human_line(this, __func__) << ":\n" << electron;
   }
 
-  if(electron.pt() <= min_pt_)
+  if(electron.pt() < min_pt_)
   {
     if(debug_)
     {
@@ -37,7 +37,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.absEta() >= max_absEta_)
+  if(electron.absEta() > max_absEta_)
   {
     if(debug_)
     {
@@ -45,7 +45,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(std::fabs(electron.dxy()) >= max_dxy_)
+  if(std::fabs(electron.dxy()) > max_dxy_)
   {
     if(debug_)
     {
@@ -53,7 +53,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(std::fabs(electron.dz()) >= max_dz_)
+  if(std::fabs(electron.dz()) > max_dz_)
   {
     if(debug_)
     {
@@ -61,7 +61,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.relIso() >= max_relIso_)
+  if(electron.relIso() > max_relIso_)
   {
     if(debug_)
     {
@@ -69,7 +69,7 @@ RecoElectronSelectorLoose::operator()(const RecoElectron & electron) const
     }
     return false;
   }
-  if(electron.sip3d() >= max_sip3d_)
+  if(electron.sip3d() > max_sip3d_)
   {
     if(debug_)
     {

src/RecoJetCollectionSelector.cc
@@ -65,7 +65,7 @@ RecoJetSelector::operator()(const RecoJet & jet) const
     std::cout << get_human_line(this, __func__) << ":\n jet: " << jet << '\n';
   }
 
-  if(jet.pt() <= min_pt_)
+  if(jet.pt() < min_pt_)
   {
     if(debug_)
     {
@@ -73,7 +73,7 @@ RecoJetSelector::operator()(const RecoJet & jet) const
     }
     return false;
   }
-  if(max_absEta_ > 0. && jet.absEta() >= max_absEta_)
+  if(max_absEta_ > 0. && jet.absEta() > max_absEta_)
   {
     if(debug_)
     {

src/RecoMuonCollectionSelectorLoose.cc
@@ -28,7 +28,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     std::cout << get_human_line(this, __func__) << ":\n" << muon;
   }
 
-  if(muon.pt() <= min_pt_)
+  if(muon.pt() < min_pt_)
   {
     if(debug_)
     {
@@ -37,7 +37,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.absEta() >= max_absEta_)
+  if(muon.absEta() > max_absEta_)
   {
     if(debug_)
     {
@@ -46,7 +46,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(std::fabs(muon.dxy()) >= max_dxy_)
+  if(std::fabs(muon.dxy()) > max_dxy_)
   {
     if(debug_)
     {
@@ -55,7 +55,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(std::fabs(muon.dz()) >= max_dz_)
+  if(std::fabs(muon.dz()) > max_dz_)
   {
     if(debug_)
     {
@@ -64,7 +64,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.relIso() >= max_relIso_)
+  if(muon.relIso() > max_relIso_)
   {
     if(debug_)
     {
@@ -73,7 +73,7 @@ RecoMuonSelectorLoose::operator()(const RecoMuon & muon) const
     return false;
   }
 
-  if(muon.sip3d() >= max_sip3d_)
+  if(muon.sip3d() > max_sip3d_)
   {
     if(debug_)
     {