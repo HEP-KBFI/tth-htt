8641c93d6531ca945799905234f805ab065ef654
ktht <karlehataht@gmail.com>
2019-08-30 16:00:25 +0300

    Use correct PSet name in the template cfg file of mu charge flip job

--- test/templates/analyze_charge_flip_mu_cfg.py
+++ test/templates/analyze_charge_flip_mu_cfg.py
@@ -16,7 +16,7 @@ process.fwliteOutput = cms.PSet(
     fileName = cms.string('')
 )
 
-process.analyze_charge_flip = cms.PSet(
+process.analyze_charge_flip_mu = cms.PSet(
     treeName = cms.string('Events'),
 
     process = cms.string(''),
