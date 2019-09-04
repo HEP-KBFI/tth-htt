1959320d262b6989031cb6f5d7affc185411d785
ktht <karlehataht@gmail.com>
2019-09-04 13:35:30 +0300

    Remove obsolete code from xgb application loader
    It doesn't affect the behavior described in https://github.com/HEP-KBFI/tth-htt/issues/73 because the alternative import statements for XGBoost were implemented in 80X days of CMSSW when XGBoost was not part of a CMSSW release.

--- python/xgb/applicationLoad.py
+++ python/xgb/applicationLoad.py
@@ -1,24 +1,10 @@
-import imp
-import os
+import sys
 import pandas
 import cPickle as pickle
 import itertools
 import collections
-
-try:
-  imp.find_module('xgboost')
-  found_xgboost = True
-except:
-  found_xgboost = False
-
-if found_xgboost:
-  import xgboost
-else:
-  xgb_path = os.path.join('/cvmfs/cms.cern.ch', os.environ['SCRAM_ARCH'] ,'external/py2-pippkgs_depscipy/3.0-njopjo7/lib/python2.7/site-packages')
-  if not os.path.isdir(xgb_path):
-    raise ValueError('Cannot import xgboost')
-  sys.path.insert(0, xgb_path)
-  import xgboost as xgb
+import traceback
+import xgboost
 
 def load(pklfile):
   f = None
@@ -35,19 +21,20 @@ def load(pklfile):
           model = pkldata.booster().get_dump() # this only tests load was ok
         except (AttributeError,  EOFError, ImportError, IndexError) as e:
           print(traceback.format_exc(e))
-        except Exception as e: print(traceback.format_exc(e))
+        except Exception as e:
+          print(traceback.format_exc(e))
       f.close()
   return pkldata
 
-def evaluate(vec, vec2, pkldata):
-  new_dict = collections.OrderedDict(itertools.izip(vec2,vec))
+def evaluate(vec_values, vec_names, pkldata):
+  new_dict = collections.OrderedDict(itertools.izip(vec_names, vec_values))
   data = pandas.DataFrame(columns = list(new_dict.keys()))
   data = data.append(new_dict, ignore_index = True)
   result = -20
   try:
     proba = pkldata.predict_proba(data[data.columns.values.tolist()].values  )
   except:
-    print('Oops!',sys.exc_info()[0],'occured.')
+    print('Caught error:', sys.exc_info()[0])
   else:
     result = proba[:,1][0]
   return result
