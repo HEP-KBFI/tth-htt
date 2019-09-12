98a160b44440fdbe6d5fd28d34412d70a9f2d75a
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-09-13 01:16:42 +0300

    fix typo

--- python/configs/analyzeConfig_2lss_1tau.py
+++ python/configs/analyzeConfig_2lss_1tau.py
@@ -699,7 +699,7 @@ class analyzeConfig_2lss_1tau(analyzeConfig):
             sample_categories.extend(self.signalProcs)
             processes_input = []
             for process_input_base in processes_input_base:
-              processes_input.append("%s_fake" % processes_input_base)
+              processes_input.append("%s_fake" % process_input_base)
             self.jobOptions_addBackgrounds_sum[key_addBackgrounds_job_fakes] = {
               'inputFile' : self.outputFile_hadd_stage1_5[key_hadd_stage1_5_job],
               'cfgFile_modified' : os.path.join(self.dirs[key_addBackgrounds_dir][DKEY_CFGS], "addBackgrounds_%s_%s_lep%s_sum%s_cfg.py" % addBackgrounds_job_fakes_tuple),
@@ -721,7 +721,7 @@ class analyzeConfig_2lss_1tau(analyzeConfig):
             sample_categories.extend(self.signalProcs)
             processes_input = []
             for process_input_base in processes_input_base:
-              processes_input.append("%s_flip" % processes_input_base)
+              processes_input.append("%s_flip" % process_input_base)
             self.jobOptions_addBackgrounds_sum[key_addBackgrounds_job_flips] = {
               'inputFile' : self.outputFile_hadd_stage1_5[key_hadd_stage1_5_job],
               'cfgFile_modified' : os.path.join(self.dirs[key_addBackgrounds_dir][DKEY_CFGS], "addBackgrounds_%s_%s_lep%s_sum%s_cfg.py" % addBackgrounds_job_flips_tuple),
@@ -743,7 +743,7 @@ class analyzeConfig_2lss_1tau(analyzeConfig):
             sample_categories.extend(self.signalProcs)
             processes_input = []
             for process_input_base in processes_input_base:
-              processes_input.append("%s_Convs" % processes_input_base)
+              processes_input.append("%s_Convs" % process_input_base)
             self.jobOptions_addBackgrounds_sum[key_addBackgrounds_job_Convs] = {
               'inputFile' : self.outputFile_hadd_stage1_5[key_hadd_stage1_5_job],
               'cfgFile_modified' : os.path.join(self.dirs[key_addBackgrounds_dir][DKEY_CFGS], "addBackgrounds_%s_%s_lep%s_sum%s_cfg.py" % addBackgrounds_job_Convs_tuple),
