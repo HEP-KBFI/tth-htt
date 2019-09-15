6d0496f41c8a150eaa76c5b475f9c88a9c9db364
ktht <karlehataht@gmail.com>
2019-09-15 23:04:50 +0300

    Always write gen jets in produceNtuple

--- bin/produceNtuple.cc
+++ bin/produceNtuple.cc
@@ -859,6 +859,7 @@ main(int argc,
 
       genLeptonWriter->write(convert_to_GenParticle(genLeptons));
       genHadTauWriter->write(convert_to_GenParticle(genHadTaus));
+      genJetWriter   ->write(convert_to_GenParticle(genJets));
 
       if(genMatchingByIndex)
       {
@@ -892,9 +893,7 @@ main(int argc,
       else
       {
         const std::vector<GenPhoton> genPhotons = genPhotonReader->read();
-
         genPhotonWriter->write(convert_to_GenParticle(genPhotons));
-        genJetWriter   ->write(convert_to_GenParticle(genJets));
 
         std::vector<GenLepton> genElectrons;
         std::vector<GenLepton> genMuons;
