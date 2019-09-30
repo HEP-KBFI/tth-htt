9fd6e79ebbf7e6e1def94fa14215da7a71073c0a
ktht <karlehataht@gmail.com>
2019-09-30 22:16:45 +0300

    Patch DeepTau in the sync Ntuples

--- python/samples/tthAnalyzeSamples_2018_nanoAOD_sync.py
+++ python/samples/tthAnalyzeSamples_2018_nanoAOD_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2019-06-05 23:13:35 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2018_sync.py -p /hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Jun01 -N samples_2018 -E 2018 -o python/samples -g tthAnalyzeSamples_2018_nanoAOD_sync.py -M
+# file generated at 2019-09-30 22:03:29 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2018_sync.py -p /hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Sep30 -N samples_2018 -E 2018 -o python/samples -g tthAnalyzeSamples_2018_nanoAOD_sync.py -M
 
 samples_2018 = OD()
 samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM"] = OD([
@@ -14,7 +14,7 @@ samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD
   }),
   ("nof_tree_events",                 63000),
   ("nof_db_events",                   7525991),
-  ("fsize_local",                     182619789), # 182.62MB, avg file size 182.62MB
+  ("fsize_local",                     182545427), # 182.55MB, avg file size 182.55MB
   ("fsize_db",                        469867184231), # 469.87GB, avg file size 2.10GB
   ("use_it",                          True),
   ("xsection",                        0.2118),
@@ -26,23 +26,26 @@ samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Jun01/ttHToNonbb_M125_powheg"),
+        ("path",      "/hdfs/local/karl/sync_ntuples/2018/nanoAODproduction/2019Sep30/ttHToNonbb_M125_powheg"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
     ]
   ),
-  ("missing_from_superset",           [
+  ("missing_completely",           [
     # not computed
   ]),
-  ("missing_hlt_paths",               [
+  ("missing_from_superset",        [
+    # not computed
+  ]),
+  ("missing_hlt_paths",            [
     "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",
     "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",
     "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",
     "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",
     "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
   ]),
-  ("hlt_paths",               [
+  ("hlt_paths",                    [
     # not computed
   ]),
 ])
