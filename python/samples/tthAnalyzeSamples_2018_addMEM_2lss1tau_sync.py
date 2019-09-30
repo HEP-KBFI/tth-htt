59144a234ba9f8dc20c560a927fc746223954285
ktht <karlehataht@gmail.com>
2019-10-01 01:07:06 +0300

    Update MEM scores in the sync Ntuples

--- python/samples/tthAnalyzeSamples_2018_addMEM_2lss1tau_sync.py
+++ python/samples/tthAnalyzeSamples_2018_addMEM_2lss1tau_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2019-09-29 22:32:03 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2018_sync.py -p /hdfs/local/karl/addMEM/2018/2019Sep29_sync_nonNom_full/final_ntuples/2lss_1tau -N samples_2018 -E 2018 -o python/samples -g tthAnalyzeSamples_2018_addMEM_2lss1tau_sync.py -M
+# file generated at 2019-10-01 01:02:13 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2018_sync.py -p /hdfs/local/karl/addMEM/2018/2019Sep30_sync_nonNom_full/final_ntuples/2lss_1tau -N samples_2018 -E 2018 -o python/samples -g tthAnalyzeSamples_2018_addMEM_2lss1tau_sync.py -M
 
 samples_2018 = OD()
 samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM"] = OD([
@@ -15,9 +15,9 @@ samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD
     'CountWeighted'                                              : [        61658,        61657,        61653, ],
     'CountWeightedLHEWeightScale'                                : [        89702,        87072,        85058,        63176,        61658,        60528,        51747,        50385,        49346, ],
   }),
-  ("nof_tree_events",                 12502),
+  ("nof_tree_events",                 13715),
   ("nof_db_events",                   7525991),
-  ("fsize_local",                     147940161), # 147.94MB, avg file size 147.94MB
+  ("fsize_local",                     160260290), # 160.26MB, avg file size 160.26MB
   ("fsize_db",                        469867184231), # 469.87GB, avg file size 2.10GB
   ("use_it",                          True),
   ("xsection",                        0.2118),
@@ -29,7 +29,7 @@ samples_2018["/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/addMEM/2018/2019Sep29_sync_nonNom_full/final_ntuples/2lss_1tau/ttHToNonbb_M125_powheg"),
+        ("path",      "/hdfs/local/karl/addMEM/2018/2019Sep30_sync_nonNom_full/final_ntuples/2lss_1tau/ttHToNonbb_M125_powheg"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
