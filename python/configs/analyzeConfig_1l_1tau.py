236fac451a5cde8fc22201525b461e60780d481d
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-09-30 22:00:03 +0300

    fix typo

--- python/configs/analyzeConfig_1l_1tau.py
+++ python/configs/analyzeConfig_1l_1tau.py
@@ -565,8 +565,8 @@ class analyzeConfig_1l_1tau(analyzeConfig):
             sample_categories.extend(self.nonfake_backgrounds)
             sample_categories.extend(self.signalProcs)
             processes_input = []
-            for process_input_base in processes_input_base :
-              processes_input.append("%s_fake" % processes_input_base)
+            for process_input in processes_input_base :
+              processes_input.append("%s_fake" % process_input)
             self.jobOptions_addBackgrounds_sum[key_addBackgrounds_job_fakes] = {
               'inputFile' : self.outputFile_hadd_stage1_5[key_hadd_stage1_5_job],
               'cfgFile_modified' : os.path.join(self.dirs[key_addBackgrounds_dir][DKEY_CFGS], "addBackgrounds_%s_%s_%s_%s_cfg.py" % addBackgrounds_job_fakes_tuple),
