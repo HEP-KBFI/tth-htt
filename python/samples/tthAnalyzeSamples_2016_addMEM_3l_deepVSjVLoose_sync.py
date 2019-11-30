ee553014d12ad579d0e774d390cd9e508ca8a23f
ktht <karlehataht@gmail.com>
2019-11-30 09:02:29 +0200

    Update MEM scores for the sync

--- python/samples/tthAnalyzeSamples_2016_addMEM_3l_deepVSjVLoose_sync.py
+++ python/samples/tthAnalyzeSamples_2016_addMEM_3l_deepVSjVLoose_sync.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict as OD
 
-# file generated at 2019-10-21 06:02:30 with the following command:
-# create_dictionary.py -m python/samples/metaDict_2016_sync.py -p /hdfs/local/karl/addMEM/2016/2019Oct19_sync_nonNom/final_ntuples/3l -N samples_2016 -E 2016 -o python/samples -g tthAnalyzeSamples_2016_addMEM_3l_deepVSjVLoose_sync.py -M
+# file generated at 2019-11-30 09:00:01 with the following command:
+# create_dictionary.py -m python/samples/metaDict_2016_sync.py -p /hdfs/local/karl/addMEM/2016/2019Nov29_sync_nonNom/final_ntuples/3l -N samples_2016 -E 2016 -o python/samples -g tthAnalyzeSamples_2016_addMEM_3l_deepVSjVLoose_sync.py -M
 
 samples_2016 = OD()
 samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v2/MINIAODSIM"] = OD([
@@ -20,7 +20,7 @@ samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/Run
   }),
   ("nof_tree_events",                 16264),
   ("nof_db_events",                   3981250),
-  ("fsize_local",                     179515172), # 179.52MB, avg file size 179.52MB
+  ("fsize_local",                     179739979), # 179.74MB, avg file size 179.74MB
   ("fsize_db",                        208884155497), # 208.88GB, avg file size 5.97GB
   ("use_it",                          True),
   ("xsection",                        0.2118),
@@ -32,7 +32,7 @@ samples_2016["/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/Run
   ("local_paths",
     [
       OD([
-        ("path",      "/hdfs/local/karl/addMEM/2016/2019Oct19_sync_nonNom/final_ntuples/3l/ttHToNonbb_M125_powheg"),
+        ("path",      "/hdfs/local/karl/addMEM/2016/2019Nov29_sync_nonNom/final_ntuples/3l/ttHToNonbb_M125_powheg"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
