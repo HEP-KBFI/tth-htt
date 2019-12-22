046c65d54544d2f1f13d9454b02f2509363c760f
ktht <karlehataht@gmail.com>
2019-12-22 18:32:39 +0200

    Use the correct MVA name in 2los+1tau plots

--- test/templates/makePlots_2los_1tau_cfg.py
+++ test/templates/makePlots_2los_1tau_cfg.py
@@ -4,12 +4,7 @@ from tthAnalysis.HiggsToTauTau.configs.makePlots_cfi import process
 
 process.makePlots.distributions.extend([
     cms.PSet(
-        histogramName = cms.string("sel/evt/$PROCESS/numJets"),
-        xAxisTitle = cms.string("jet Multiplicity"),
-        yAxisTitle = cms.string("N")
-    ),
-    cms.PSet(
-        histogramName = cms.string("sel/evt/$PROCESS/mvaDiscr_2lss"),
+        histogramName = cms.string("sel/evt/$PROCESS/mvaOutput_legacy"),
         xAxisTitle = cms.string("MVA"),
         yAxisTitle = cms.string("dN/dMVA")
     ),
