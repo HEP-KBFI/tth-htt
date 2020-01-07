10da5047d8b8c6151192e795d1257f68da4aa527
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2020-01-07 13:36:51 +0200

    sync in CR now finished

--- bin/analyze_3l.cc
+++ bin/analyze_3l.cc
@@ -1368,7 +1368,10 @@ HadTopTagger* hadTopTagger = new HadTopTagger();
     const bool tH_like  = (selBJets_medium.size() >= 1 && ((selJets.size() - selBJets_loose.size()) + selJetsForward.size()) >= 1);
     const bool ttH_like = (selBJets_loose.size() >= 2 || selBJets_medium.size() >= 1) && selJets.size() >= 2;
     const bool passEvents = ttH_like || tH_like;
-    if((! passEvents && ! isControlRegion) || (isControlRegion && selJets.size() < 1))
+    if(
+      (! passEvents && ! isControlRegion) ||
+      (isControlRegion && (selJets.size() < 1 || ((int)selJets.size() == 1 && selBJets_medium.size() == 1)))
+    )
     {
       if(run_lumi_eventSelector)
       {
@@ -1583,7 +1586,6 @@ HadTopTagger* hadTopTagger = new HadTopTagger();
     const bool isSameFlavor_OS_FO = isSFOS(fakeableLeptons);
     bool is_tH_like_and_not_ttH_like = tH_like && !ttH_like;
     double met_LD_cut = 0.;
-    //if (tH_like && !do_sync) met_LD_cut = -1.;
     if      ( selJets.size() >= 4 ) met_LD_cut = -1.; // MET LD cut not applied
     else if ( isSameFlavor_OS_FO     ) met_LD_cut = 45.;
     else                            met_LD_cut = 30.;
@@ -1593,7 +1595,7 @@ HadTopTagger* hadTopTagger = new HadTopTagger();
         std::cout << " (met_LD = " << met_LD << ", met_LD_cut = " << met_LD_cut << ")" << std::endl;
       }
 
-      if ( !tH_like ) continue; // MET LD cut not applied if tH_like
+      if ( !tH_like || isControlRegion) continue; // MET LD cut not applied if tH_like
       else is_tH_like_and_not_ttH_like = true;
 
     }
@@ -2013,7 +2015,7 @@ HadTopTagger* hadTopTagger = new HadTopTagger();
   }
   ///////////////////////////////
   // SVA variables
-  if ( selJets.size() > 1)
+  if ( selJets.size() > 1 && ! is_tH_like_and_not_ttH_like)
   {
     if (selJets.size() < 4) category_SVA += "lj";
     else category_SVA += "hj";
