8c98ebc9d074f374889c9e7eb5ea0804400dea46
ktht <karlehataht@gmail.com>
2020-01-18 22:53:06 +0200

    Remove redundant disabling of WG sample

--- python/samples/reclassifySamples.py
+++ python/samples/reclassifySamples.py
@@ -52,8 +52,6 @@ def reclassifySamples(samples_era_base, samples_era_hh_multilepton = None, sampl
         sample_info["sample_category"] = "TTZ"
       elif sample_name.startswith("/ST_"):
         sample_info["sample_category"] = "Rares"
-      elif sample_name.startswith('/WGToLNuG_Tune'):
-        sample_info["use_it"] = False
 
       if sample_name.startswith(('/TGJets', '/TTGJets', '/WGTo', '/ZGTo')):
         sample_info["sample_category"] = "XGamma"
