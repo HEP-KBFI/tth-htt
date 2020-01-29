95870574e7ab0a7614a7c36e007c6a90a2f00332
ktht <karlehataht@gmail.com>
2020-01-29 03:31:55 +0200

    Automatize muon charge flip rate

--- test/tthAnalyzeRun_chargeFlip_mu.py
+++ test/tthAnalyzeRun_chargeFlip_mu.py
@@ -13,6 +13,8 @@ import re
 
 # E.g.: ./test/tthAnalyzeRun_chargeFlip_mu.py -v 2017Dec13 -e 2017
 
+cmssw_base_dir_combine = os.path.expanduser('~/CMSSW_10_2_13') # immediate parent dir to src folder
+
 sys_choices      = [ 'full' ] + systematics.an_chargeFlip_mu_opts
 systematics.full = systematics.an_chargeFlip_mu
 
@@ -94,27 +96,28 @@ if __name__ == '__main__':
   analysis = analyzeConfig_charge_flip_mu(
     configDir = os.path.join("/home",       getpass.getuser(), "ttHAnalysis", era, version),
     outputDir = os.path.join("/hdfs/local", getpass.getuser(), "ttHAnalysis", era, version),
-    executable_analyze    = "analyze_charge_flip_mu",
-    samples               = samples,
-    lepton_selections     = [ "Tight" ],
-    jet_cleaning_by_index = jet_cleaning_by_index,
-    gen_matching_by_index = gen_matching_by_index,
-    central_or_shifts     = central_or_shifts,
-    max_files_per_job     = files_per_job,
-    era                   = era,
-    use_lumi              = True,
-    lumi                  = lumi,
-    check_output_files    = check_output_files,
-    running_method        = running_method,
-    num_parallel_jobs     = num_parallel_jobs,
-    histograms_to_fit     = {
+    cmssw_base_dir_combine = cmssw_base_dir_combine,
+    executable_analyze     = "analyze_charge_flip_mu",
+    samples                = samples,
+    lepton_selections      = [ "Tight" ],
+    jet_cleaning_by_index  = jet_cleaning_by_index,
+    gen_matching_by_index  = gen_matching_by_index,
+    central_or_shifts      = central_or_shifts,
+    max_files_per_job      = files_per_job,
+    era                    = era,
+    use_lumi               = True,
+    lumi                   = lumi,
+    check_output_files     = check_output_files,
+    running_method         = running_method,
+    num_parallel_jobs      = num_parallel_jobs,
+    histograms_to_fit      = {
       "mass_ll" : {},
     },
-    select_rle_output     = False,
-    dry_run               = dry_run,
-    isDebug               = debug,
-    use_home              = use_home,
-    submission_cmd        = sys.argv,
+    select_rle_output      = False,
+    dry_run                = dry_run,
+    isDebug                = debug,
+    use_home               = use_home,
+    submission_cmd         = sys.argv,
   )
 
   job_statistics = analysis.create()
