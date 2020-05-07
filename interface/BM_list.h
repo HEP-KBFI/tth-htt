aa9082d6deea7916bd5accd7205fe7924b8de0f4
ktht <karlehataht@gmail.com>
2020-05-07 21:40:52 +0300

    Fix a compilation error
    Copied from Xandra's area.

---
+++ interface/BM_list.h
@@ -0,0 +1,1 @@
+std::vector<std::string> BMS = { "SM", "BM1", "BM2", "BM3", "BM4", "BM5", "BM6", "BM7", "BM8", "BM9", "BM10", "BM11", "BM12" }; // "SM", "BM1", "BM2", "BM3", "BM4", "BM5", "BM6", "BM7", "BM8", "BM9", "BM10", "BM11", "BM12"
