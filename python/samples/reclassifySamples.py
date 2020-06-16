0d50662bf7244d81dd2c3fc6e7b84192b2afde6a
ktht <karlehataht@gmail.com>
2020-06-17 00:05:55 +0300

    Enable ttW NLO EWK samples in ttH analysis
    https://github.com/HEP-KBFI/tth-htt/issues/148

--- python/samples/reclassifySamples.py
+++ python/samples/reclassifySamples.py
@@ -58,6 +58,9 @@ def reclassifySamples(samples_era_base, samples_era_hh_multilepton = None, sampl
       if sample_name.startswith(('/TGJets', '/TTGJets', '/WGTo', '/ZGTo')):
         sample_info["sample_category"] = "XGamma"
 
+      if sample_info["process_name_specific"] == "TTWJetsToLNu_EWK_5f_NLO":
+        sample_info["use_it"] = True
+
     if sample_info["process_name_specific"].startswith('signal') and 'hh' in sample_info["process_name_specific"]:
       if is_nonresonant(sample_info["sample_category"]) and not sample_info["process_name_specific"].endswith('2b2v_sl'):
         # HH->bbWW single-leptonic samples are disabled because we decided to process these samples too late in ttH analysis
