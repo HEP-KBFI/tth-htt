406e0ba7da661dac3f96e18588b916f4250cf228
ktht <karlehataht@gmail.com>
2020-07-04 22:38:37 +0300

    Script for stitching NLO DY samples
    https://github.com/HEP-KBFI/hh-bbww/issues/9

---
+++ python/samples/stitch_nlo.py
@@ -0,0 +1,18 @@
+samples_to_stitch_2016 = {
+  'inclusive' : [ 'DYJetsToLL_M-50_amcatnloFXFX_ext2' ],
+  'exclusive' : [
+    [ 'DYToLL_0J_ext1', 'DYToLL_0J_backup', ],
+    [ 'DYToLL_1J_ext1', 'DYToLL_1J_backup', ],
+    [ 'DYToLL_2J',      'DYToLL_2J_ext1'    ],
+  ]
+}
+
+samples_to_stitch_2017 = {
+  'inclusive' : [ 'DYJetsToLL_M-50_amcatnloFXFX', 'DYJetsToLL_M-50_amcatnloFXFX_ext1' ],
+  'exclusive' : [ [ 'DYToLL_0J'], [ 'DYToLL_1J' ], [ 'DYToLL_2J' ] ],
+}
+
+samples_to_stitch_2018 = {
+  'inclusive' : [ 'DYJetsToLL_M-50_amcatnloFXFX', 'DYJetsToLL_M-50_amcatnloFXFX_ext2' ],
+  'exclusive' : [ [ 'DYToLL_0J'], [ 'DYToLL_1J' ], [ 'DYToLL_2J' ] ],
+}
