9c80c4650f14aa9cc442ef9fb7d0cdfb2e6dbc2a
ktht <karlehataht@gmail.com>
2020-07-19 15:14:37 +0300

    Update normalization SF for btagging in 2018
    https://github.com/HEP-KBFI/tth-htt/issues/146 https://github.com/HEP-KBFI/tth-htt/issues/147

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -553,7 +553,10 @@ class analyzeConfig(object):
             os.environ['CMSSW_BASE'], "src/tthAnalysis/HiggsToTauTau/data/btagSFRatio_{}.root".format(self.era)
           )
         else:
-          self.btagSFRatioFile = "/hdfs/local/karl/ttHBtagsfProjection/{era}/2020Jul09_all/btagSF_{era}_fullSys.root".format(era = self.era)
+          self.btagSFRatioFile = "/hdfs/local/karl/ttHBtagsfProjection/{era}/{version}_all/btagSF_{era}_fullSys.root".format(
+            era = self.era,
+            version = '2020Jul09' if self.era != '2018' else '2020Jul18',
+          )
 
         self.leptonFakeRateWeight_histogramName_e = None
         self.leptonFakeRateWeight_histogramName_mu = None
