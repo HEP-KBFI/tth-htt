076bd2012021060dde50ea328b0f320da163799d
Christian Veelken <christian.veelken@cern.ch>
2020-10-16 10:57:25 +0300

    added 4-lepton mass plot

--- test/templates/makePlots_ZZctrl_cfg.py
+++ test/templates/makePlots_ZZctrl_cfg.py
@@ -2,3 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from tthAnalysis.HiggsToTauTau.configs.makePlots_cfi import process
 
+process.makePlots.distributions.extend([
+    cms.PSet(
+        histogramName = cms.string("sel/evt/$PROCESS/m4lep"),
+        xAxisTitle = cms.string("m_{4#ell} [GeV]"),
+        yAxisTitle = cms.string("dN/dm_{4#ell}")
+    ),
+])
