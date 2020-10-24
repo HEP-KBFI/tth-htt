c4631ca9e1ed134a7935f52a7772f610dd02f285
ktht <karlehataht@gmail.com>
2020-10-24 22:17:29 +0300

    Propagate reference LHE weights to the analysis
    https://github.com/HEP-KBFI/tth-htt/issues/158

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -998,7 +998,7 @@ class analyzeConfig(object):
 
         if 'hasLHE' not in jobOptions:
             jobOptions['hasLHE'] = sample_info['has_LHE']
-        if 'ref_genWeight' not in jobOptions and is_mc and False: #TODO remove False
+        if 'ref_genWeight' not in jobOptions and is_mc:
             self.load_refGenWeights()
             if process_name not in self.ref_genWeights:
                 raise RuntimeError("Unable to find reference gen weight for process %s from file %s" % (process_name, self.ref_genWeightFile))
