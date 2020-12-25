25b2b53ec16f78249ce2b89ac239432c82a65332
ktht <karlehataht@gmail.com>
2020-12-25 18:45:40 +0200

    Do not modify jet & MET due to JER splitting or the HEM issue
    https://github.com/HEP-KBFI/tth-htt/issues/163

--- bin/analyze_4l.cc
+++ bin/analyze_4l.cc
@@ -1281,8 +1281,7 @@ int main(int argc, char* argv[])
     cutFlowHistManager->fillHistograms(">= 2 loose b-jets || 1 medium b-jet", evtWeightRecorder.get(central_or_shift_main));
 
 //--- compute MHT and linear MET discriminant (met_LD)
-    const RecoMEt met_uncorr = metReader->read();
-    const RecoMEt met = recompute_met(met_uncorr, jets, met_option, isDEBUG);
+    const RecoMEt met = metReader->read();
     const Particle::LorentzVector mht_p4 = compMHT(fakeableLeptonsFull, looseHadTaus, selJets);
     const double met_LD = compMEt_LD(met.p4(), mht_p4);
 
