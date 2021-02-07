d1d6a230f128d66a5ec6a90f20a5f043d9fb36de
ktht <karlehataht@gmail.com>
2021-02-07 19:02:32 +0200

    Remove "intrinsic" JES uncertainty on b-tagging SF from python cfg
    https://github.com/HEP-KBFI/hh-bbww/issues/31

--- python/analysisSettings.py
+++ python/analysisSettings.py
@@ -591,9 +591,7 @@ class systematics(object):
     cErr1 = [ "CMS_ttHl_btag_cErr1Up", "CMS_ttHl_btag_cErr1Down" ]
     cErr2 = [ "CMS_ttHl_btag_cErr2Up", "CMS_ttHl_btag_cErr2Down" ]
 
-    JESbtag = [ "CMS_ttHl_btag_JESUp", "CMS_ttHl_btag_JESDown" ]
-
-    full = HF + HFStats1 + HFStats2 + LF + LFStats1 + LFStats2 + cErr1 + cErr2 + JESbtag
+    full = HF + HFStats1 + HFStats2 + LF + LFStats1 + LFStats2 + cErr1 + cErr2
 
   class FakeRate_e_shape(object):
     pt         = [ "CMS_ttHl_FRe_shape_ptUp",         "CMS_ttHl_FRe_shape_ptDown"         ]
