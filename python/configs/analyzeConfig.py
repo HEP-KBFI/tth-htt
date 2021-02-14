0c1f0d09c8b8da53ef47663ff50fbc587a7396c3
ktht <karlehataht@gmail.com>
2021-02-14 14:00:12 +0200

    Distinguish b/w different sets of HLT paths when measuring lepton FR

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -1292,7 +1292,10 @@ class analyzeConfig(object):
             trigger_string     = '%s.triggers_%s'     % (process_string, trigger)
             trigger_use_string = '%s.use_triggers_%s' % (process_string, trigger)
             if isLeptonFR:
-                available_triggers = list(self.triggerTable.triggers_leptonFR[trigger] - blacklist)
+                if self.enable_LeptonFakeRate_bbwSL:
+                  available_triggers = list(self.triggerTable.triggers_leptonFR_bbWWSL[trigger] - blacklist)
+                else:
+                  available_triggers = list(self.triggerTable.triggers_leptonFR[trigger] - blacklist)
             else:
                 available_triggers = list(set(trigger_stat['name'] for trigger_stat in self.triggerTable.triggers_analysis[trigger]) - blacklist)
             use_trigger = bool(trigger in sample_info['triggers'])
