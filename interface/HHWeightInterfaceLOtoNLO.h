2fff7460ca1888f08cd7e0853e54976ac2ef4e65
Christian Veelken <christian.veelken@cern.ch>
2021-02-25 18:54:40 +0200

    added new version of HH LO->NLO reweighting from Petr Mandrik ( https://github.com/pmandrik/VSEVA/blob/f7224649297f900a4ae25cf721d65cae8bd7b408/HHWWgg/reweight/reweight_HH.C )

--- interface/HHWeightInterfaceLOtoNLO.h
+++ interface/HHWeightInterfaceLOtoNLO.h
@@ -9,9 +9,11 @@
  *
  */
 
-#include "tthAnalysis/HiggsToTauTau/interface/LocalFileInPath.h" // LocalFileInPath
+#include "tthAnalysis/HiggsToTauTau/interface/LocalFileInPath.h"      // LocalFileInPath
+#include "tthAnalysis/HiggsToTauTau/interface/analysisAuxFunctions.h" // Era::k2016, Era::k2017, Era::k2018
 
 #include <TH1.h> // TH1
+#include <TH2.h> // TH2
 
 #include <vector> // std::vector
 #include <string> // std::string
@@ -20,7 +22,7 @@
 class HHWeightInterfaceLOtoNLO
 {
  public:
-  HHWeightInterfaceLOtoNLO(double max_reWeight = 10., bool isDEBUG = false);
+  HHWeightInterfaceLOtoNLO(Era era, bool apply_coupling_fix_CMS = false, double max_reWeight = 10., bool isDEBUG = false);
   ~HHWeightInterfaceLOtoNLO();
 
   /**
@@ -32,6 +34,12 @@ class HHWeightInterfaceLOtoNLO
               double cosThetaStar,
               bool isDEBUG = false) const;
 
+  double
+  getReWeight_V2(const std::string & bmName,
+                 double mHH,
+                 double cosThetaStar,
+                 bool isDEBUG = false) const;
+
  private:
   LocalFileInPath xsecFileName_lo_;
   LocalFileInPath xsecFileName_nlo_;
@@ -39,6 +47,16 @@ class HHWeightInterfaceLOtoNLO
   std::map<std::string, const TH1 *> dXsec_lo_;  // key = bmName
   std::map<std::string, const TH1 *> dXsec_nlo_; // key = bmName
   std::map<std::string, const TH1 *> reWeights_; // key = bmName
+
+  LocalFileInPath xsecFileName_V2_lo_;
+  LocalFileInPath xsecFileName_V2_nlo_;
+
+  std::map<std::string, const TH2 *> dXsec_V2_lo_;  // key = bmName
+  std::map<std::string, const TH2 *> dXsec_V2_nlo_; // key = bmName
+  std::map<std::string, const TH2 *> reWeights_V2_; // key = bmName
+
+  Era era_;
+  bool apply_coupling_fix_CMS_;
 
   double max_reWeight_;
   
