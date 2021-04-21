0767c6b02e7d06224f3c35d65c6ca243096b4dd3
ktht <karlehataht@gmail.com>
2021-04-21 19:01:12 +0300

    Plot the sum of X+gamma and X+jets after passing the gen photon filter
    https://github.com/HEP-KBFI/hh-multilepton/issues/36

--- scripts/plot_genPhotonFilter.py
+++ scripts/plot_genPhotonFilter.py
@@ -12,16 +12,16 @@ import matplotlib.backends.backend_pdf as pdf
 
 plt.rc('font', size = 14)
 
-PROCESSES = collections.OrderedDict([
-  ('WGamma' ,  { 'option' : 'selected', 'label' : r'W+$\gamma$'        }),
-  ('WJets',    { 'option' : 'rejected', 'label' : 'W+jets'             }),
-  ('ZGamma' ,  { 'option' : 'selected', 'label' : r'Z+$\gamma$'        }),
-  ('ZJets',    { 'option' : 'rejected', 'label' : 'Z+jets'             }),
-  ('TGamma' ,  { 'option' : 'selected', 'label' : r't+$\gamma$'        }),
-  ('TJets',    { 'option' : 'rejected', 'label' : 't+jets'             }),
-  ('TTGamma' , { 'option' : 'selected', 'label' : r'$t\bar{t}+\gamma$' }),
-  ('TTJets',   { 'option' : 'rejected', 'label' : r'$t\bar{t}$+jets'   }),
-])
+PROCESSES = [
+  { 'WGamma'  : { 'option' : 'selected', 'label' : r'W+$\gamma$'        },
+    'WJets'   : { 'option' : 'rejected', 'label' : 'W+jets'             } },
+  { 'ZGamma'  : { 'option' : 'selected', 'label' : r'Z+$\gamma$'        },
+    'ZJets'   : { 'option' : 'rejected', 'label' : 'Z+jets'             } },
+  { 'TGamma'  : { 'option' : 'selected', 'label' : r't+$\gamma$'        },
+    'TJets'   : { 'option' : 'rejected', 'label' : 't+jets'             } },
+  { 'TTGamma' : { 'option' : 'selected', 'label' : r'$t\bar{t}+\gamma$' },
+    'TTJets'  : { 'option' : 'rejected', 'label' : r'$t\bar{t}$+jets'   } },
+]
 SELECTIONS = [ 'selected', 'rejected' ]
 LEPTONS = [ 'lepton', 'electron', 'muon' ]
 SUM_KEY = 'sum'
@@ -63,35 +63,75 @@ assert(os.path.isfile(fn))
 assert(outfn.endswith('.pdf'))
 f = ROOT.TFile.Open(fn, 'read')
 
-histograms = collections.OrderedDict()
-for process in PROCESSES:
-  assert(process not in histograms)
-  histograms[process] = read_process(f, process)
+histograms = []
+for process_pair in PROCESSES:
+  results = {}
+  for process in process_pair:
+    assert(process not in results)
+    results[process] = read_process(f, process)
+  histograms.append(results)
 f.Close()
 
 with pdf.PdfPages(outfn) as output:
-  for process in PROCESSES:
+  for pair_idx, process_pair in enumerate(PROCESSES):
     for lepton_type in LEPTONS:
+      for process in process_pair:
+        plt.figure(figsize = (10, 8), dpi = 150)
+
+        plot_xmin = min(histograms[pair_idx][process][lepton_type][SUM_KEY]['xval'])
+        plot_xmax = max(histograms[pair_idx][process][lepton_type][SUM_KEY]['xval'])
+        option = PROCESSES[pair_idx][process]['option']
+        label = PROCESSES[pair_idx][process]['label']
+        plt.step(
+          histograms[pair_idx][process][lepton_type][option]['xval'],
+          histograms[pair_idx][process][lepton_type][option]['yval'],
+          lw = 2, linestyle = '-', label = '{}, after {} by veto'.format(label, option.replace('selected', 'accepted')),
+        )
+        plt.step(
+          histograms[pair_idx][process][lepton_type][SUM_KEY]['xval'],
+          histograms[pair_idx][process][lepton_type][SUM_KEY]['yval'],
+          lw = 2, linestyle = '--', label = r'{}, inclusive'.format(label),
+        )
+        plt.xlabel(r'Subleading {} $p_T$ [GeV]'.format(lepton_type))
+        plt.ylabel('Weighted number of events')
+        plt.grid(True)
+        plt.xlim(plot_xmin, plot_xmax)
+        plt.legend(loc = 'upper right')
+        output.savefig(bbox_inches = 'tight')
+        plt.close()
+
+      assert(len(process_pair) > 0)
       plt.figure(figsize = (10, 8), dpi = 150)
 
-      plot_xmin = min(histograms[process][lepton_type][SUM_KEY]['xval'])
-      plot_xmax = max(histograms[process][lepton_type][SUM_KEY]['xval'])
-      option = PROCESSES[process]['option']
-      label = PROCESSES[process]['label']
-      plt.step(
-        histograms[process][lepton_type][option]['xval'],
-        histograms[process][lepton_type][option]['yval'],
-        lw = 2, linestyle = '-', label = '{}, after {} by veto'.format(label, option.replace('selected', 'accepted')),
-      )
-      plt.step(
-        histograms[process][lepton_type][SUM_KEY]['xval'],
-        histograms[process][lepton_type][SUM_KEY]['yval'],
-        lw = 2, linestyle = '--', label = r'{}, inclusive'.format(label),
-      )
+      binning = histograms[pair_idx][process_pair.keys()[0]][lepton_type][SUM_KEY]['xval']
+      assert(all(binning == histograms[pair_idx][process][lepton_type][SUM_KEY]['xval'] for process in process_pair))
+      yvals_sum = [
+        sum(
+          histograms[pair_idx][process][lepton_type][PROCESSES[pair_idx][process]['option']]['yval'][bin_idx] \
+          for process in process_pair
+        ) for bin_idx in range(len(binning))
+      ]
+
+      plot_xmin = min(binning)
+      plot_xmax = max(binning)
+      plot_ymin = min(yvals_sum)
+      plot_ymin *= 1.05 if plot_ymin < 0. else 0
+      plot_ymax = max(yvals_sum) * 1.05
+      for process in process_pair:
+        option = PROCESSES[pair_idx][process]['option']
+        label = PROCESSES[pair_idx][process]['label']
+        plt.step(
+          binning,
+          histograms[pair_idx][process][lepton_type][option]['yval'],
+          lw = 2, linestyle = '-', label = '{}, after {} by veto'.format(label, option.replace('selected', 'accepted')),
+        )
+      plt.step(binning, yvals_sum, lw = 2, linestyle = '-', label = 'Sum of the above')
+
       plt.xlabel(r'Subleading {} $p_T$ [GeV]'.format(lepton_type))
       plt.ylabel('Weighted number of events')
       plt.grid(True)
       plt.xlim(plot_xmin, plot_xmax)
+      plt.ylim(plot_ymin, plot_ymax)
       plt.legend(loc = 'upper right')
       output.savefig(bbox_inches = 'tight')
       plt.close()
