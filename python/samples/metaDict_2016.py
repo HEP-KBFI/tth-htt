11118d62684c70b112d24169d13c9087854e9aba
ktht <karlehataht@gmail.com>
2021-09-22 23:26:52 +0300

    Process extra 0.4/fb of 2016 data

--- python/samples/metaDict_2016.py
+++ python/samples/metaDict_2016.py
@@ -1,4 +1,5 @@
 from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_data import meta_dictionary as meta_dictionary_data
+from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_data_extra import meta_dictionary as meta_dictionary_data_extra
 from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_mc import meta_dictionary as meta_dictionary_mc
 from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_private import meta_dictionary as meta_dictionary_private
 from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_vh import meta_dictionary as meta_dictionary_vh
@@ -7,5 +8,5 @@ from tthAnalysis.HiggsToTauTau.samples.metaDict_2016_mc import sum_events
 import itertools, collections
 
 meta_dictionary = collections.OrderedDict(itertools.chain(
-  meta_dictionary_data.items(), meta_dictionary_mc.items(), meta_dictionary_private.items(), meta_dictionary_vh.items()
+  meta_dictionary_data.items(), meta_dictionary_data_extra.items(), meta_dictionary_mc.items(), meta_dictionary_private.items(), meta_dictionary_vh.items()
 ))
---
+++ python/samples/metaDict_2016_data_extra.py
@@ -0,0 +1,135 @@
+from collections import OrderedDict as OD
+
+# file created manually
+
+meta_dictionary = OD()
+
+meta_dictionary["/SingleElectron/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_SingleElectron__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "SingleElectron_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         97259854),
+  ("nof_db_files",          674),
+  ("fsize_db",              2468399083535),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/SingleElectron/Run2016G-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_SingleElectron__Run2016G-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "SingleElectron_Run2016G_17Jul2018_v1_extra"),
+  ("nof_db_events",         153363109),
+  ("nof_db_files",          1188),
+  ("fsize_db",              4194700440526),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/SingleElectron/Run2016H-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_SingleElectron__Run2016H-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "SingleElectron_Run2016H_17Jul2018_v1_extra"),
+  ("nof_db_events",         129021893),
+  ("nof_db_files",          969),
+  ("fsize_db",              3621865647527),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/SingleMuon/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_SingleMuon__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "SingleMuon_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         67441308),
+  ("nof_db_files",          369),
+  ("fsize_db",              1467179337613),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/DoubleEG/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_DoubleEG__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "DoubleEG_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         47677856),
+  ("nof_db_files",          427),
+  ("fsize_db",              1264015619729),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/DoubleEG/Run2016G-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_DoubleEG__Run2016G-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "DoubleEG_Run2016G_17Jul2018_v1_extra"),
+  ("nof_db_events",         78797031),
+  ("nof_db_files",          715),
+  ("fsize_db",              2348342983856),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/DoubleEG/Run2016H-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_DoubleEG__Run2016H-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "DoubleEG_Run2016H_17Jul2018_v1_extra"),
+  ("nof_db_events",         85388734),
+  ("nof_db_files",          736),
+  ("fsize_db",              2597544653398),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/DoubleMuon/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_DoubleMuon__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "DoubleMuon_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         27934629),
+  ("nof_db_files",          203),
+  ("fsize_db",              685464884246),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/MuonEG/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_MuonEG__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "MuonEG_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         15405678),
+  ("nof_db_files",          112),
+  ("fsize_db",              425478869475),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
+
+meta_dictionary["/Tau/Run2016C-17Jul2018-v1/MINIAOD/EXTRA"] =  OD([
+  ("crab_string",           "2016v3_2021Sep22_Tau__Run2016C-17Jul2018-v1"),
+  ("sample_category",       "data_obs"),
+  ("process_name_specific", "Tau_Run2016C_17Jul2018_v1_extra"),
+  ("nof_db_events",         36931473),
+  ("nof_db_files",          240),
+  ("fsize_db",              914247099541),
+  ("xsection",              None),
+  ("use_it",                True),
+  ("genWeight",             False),
+  ("comment",               "100.0%"),
+])
