c8d4abfc25b14a150daa0f84fc55b949fac3fb7e
saswatinandan <saswati.nandan@cern.ch>
2022-01-08 11:33:17 +0200

    adding ue systematics

--- python/analysisSettings.py
+++ python/analysisSettings.py
@@ -150,7 +150,7 @@ class systematics(object):
     width = [
       "widthx0p7", "widthx0p85", "widthx1p15", "widthx1p3",
     ]
-    full = hdamp + cr + mtop + width # removed UE because its effect is very small
+    full = hdamp + cr + mtop + ue #width # removed UE because its effect is very small
 
   class L1PreFiring_(object):
     up   = "CMS_ttHl_l1PreFireUp"
