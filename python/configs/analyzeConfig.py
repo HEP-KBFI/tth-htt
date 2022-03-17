c306ca306c529aa7f507067f50630a56469ee6b3
saswatinandan <saswati.nandan@cern.ch>
2022-03-17 16:57:29 +0200

    some minor change as in the addSysTT code name is changed by mistake

--- python/configs/analyzeConfig.py
+++ python/configs/analyzeConfig.py
@@ -1812,7 +1812,7 @@ class analyzeConfig(object):
         central_or_shifts_modified = self.central_or_shifts
         if len(self.central_or_shifts) > 1 and self.ttbar_syst_enabled:
           central_or_shift_remove = systematics.ttbar
-          central_or_shifts_added = [ "colorUp", 'colorDown', 'hdampmodUp', 'hdampmodDown', 'uemodUp', 'uemodDown', 'mtopUp', 'mtopDown' ]
+          central_or_shifts_added = [ "colorUp", 'colorDown', 'hdampmodUp', 'hdampmodDown', 'uemodUp', 'uemodDown', 'mtopmodUp', 'mtopmodDown' ]
           central_or_shifts_modified = [ central_or_shift for central_or_shift in self.central_or_shifts if central_or_shift not in central_or_shift_remove ]
           central_or_shifts_modified += central_or_shifts_added
         ##if 'label' in jobOptions.keys() and jobOptions['label']:
